5b3725e3f57b60c132d7736af2bde4a6755f598c
Patrick Hogg <phogg@novamoon.net>
2019-11-18 23:04:27 -0500

    2015 Day 2 Part 2

--- 2015/Day 2/solution.go
+++ 2015/Day 2/solution.go
@@ -35,4 +35,16 @@ func part1(input string) {
 }
 
 func part2(input string) {
+	total := 0
+	for _, b := range strings.Split(input, "\n") {
+		dims := strings.Split(b, "x")
+		x, _ := strconv.Atoi(dims[0])
+		y, _ := strconv.Atoi(dims[1])
+		z, _ := strconv.Atoi(dims[2])
+		a := 2*x + 2*y
+		b := 2*y + 2*z
+		c := 2*z + 2*x
+		total += min(a, min(b, c)) + x*y*z
+	}
+	println("The answer to part two is " + strconv.Itoa(total))
 }
