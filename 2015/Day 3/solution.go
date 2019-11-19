bbcfb29c5363b95588ac71f16d5fc7933bf45a6d
Patrick Hogg <phogg@novamoon.net>
2019-11-18 23:15:57 -0500

    2015 Day 3 Part 2

--- 2015/Day 3/solution.go
+++ 2015/Day 3/solution.go
@@ -11,21 +11,25 @@ func main() {
 	part2(input)
 }
 
+func move(x int, y int, c rune) (int, int) {
+	if '<' == c {
+		x--
+	} else if '>' == c {
+		x++
+	} else if '^' == c {
+		y++
+	} else if 'v' == c {
+		y--
+	}
+	return x, y
+}
+
 func part1(input string) {
 	m := make(map[string]int)
 	m["0,0"] = 1
-	x := 0
-	y := 0
+	x, y := 0, 0
 	for _, c := range input {
-		if '<' == c {
-			x--
-		} else if '>' == c {
-			x++
-		} else if '^' == c {
-			y++
-		} else if 'v' == c {
-			y--
-		}
+		x, y = move(x, y, c)
 		pos := strconv.Itoa(x) + "," + strconv.Itoa(y)
 		m[pos]++
 	}
@@ -33,4 +37,20 @@ func part1(input string) {
 }
 
 func part2(input string) {
+	m := make(map[string]int)
+	m["0,0"] = 2
+	x1, y1 := 0, 0
+	x2, y2 := 0, 0
+	for idx, c := range input {
+		if 0 == idx%2 {
+			x2, y2 = move(x2, y2, c)
+			pos := strconv.Itoa(x2) + "," + strconv.Itoa(y2)
+			m[pos]++
+		} else {
+			x1, y1 = move(x1, y1, c)
+			pos := strconv.Itoa(x1) + "," + strconv.Itoa(y1)
+			m[pos]++
+		}
+	}
+	println("The answer to part one is " + strconv.Itoa(len(m)))
 }
