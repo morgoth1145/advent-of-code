adcdd93b2dc52aa2f9ffd541d805b04f397f429e
Patrick Hogg <phogg@novamoon.net>
2019-11-18 23:48:37 -0500

    2015 Day 5 Part 2

--- 2015/Day 5/solution.go
+++ 2015/Day 5/solution.go
@@ -51,5 +51,38 @@ func part1(input string) {
 	println("The answer to part one is " + strconv.Itoa(niceStrings))
 }
 
+func test4(s string) bool {
+	m := make(map[string]int)
+	for idx := 0; idx < len(s)-1; idx++ {
+		sub := s[idx : idx+2]
+		otherIdx, present := m[sub]
+		if present && otherIdx+2 <= idx {
+			return true
+		}
+		if !present {
+			m[sub] = idx
+		}
+	}
+	return false
+}
+
+func test5(s string) bool {
+	a, b := rune(s[2]), rune(s[3])
+	for _, c := range s {
+		if c == a {
+			return true
+		}
+		a, b = b, c
+	}
+	return false
+}
+
 func part2(input string) {
+	niceStrings := 0
+	for _, s := range strings.Split(input, "\n") {
+		if test4(s) && test5(s) {
+			niceStrings++
+		}
+	}
+	println("The answer to part one is " + strconv.Itoa(niceStrings))
 }
