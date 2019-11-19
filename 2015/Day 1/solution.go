dd49343d47296d4a644474a37b6e40b44b1ce943
Patrick Hogg <phogg@novamoon.net>
2019-11-18 20:26:41 -0500

    2015 Day 1 Part 2

--- 2015/Day 1/solution.go
+++ 2015/Day 1/solution.go
@@ -9,8 +9,25 @@ import (
 func main() {
 	input := aochelpers.GetInput(2015, 1)
 	part1(input)
+	part2(input)
 }
 
 func part1(input string) {
 	println("The answer to part one is " + strconv.Itoa(strings.Count(input, "(")-strings.Count(input, ")")))
+}
+
+func part2(input string) {
+	floor := 0
+	for idx, c := range input {
+		if '(' == c {
+			floor++
+		} else if ')' == c {
+			floor--
+			if floor < 0 {
+				println("The answer to part two is " + strconv.Itoa(idx+1))
+				return
+			}
+		}
+	}
+	println("Error, no answer found!")
 }
