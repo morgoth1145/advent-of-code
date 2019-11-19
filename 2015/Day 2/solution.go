9ce171c38a20449c258b52dfae9154eb76e7e382
Patrick Hogg <phogg@novamoon.net>
2019-11-18 23:01:47 -0500

    2015 Day 2 Part 1

---
+++ 2015/Day 2/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"advent-of-code/aochelpers"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := aochelpers.GetInput(2015, 2)
+	part1(input)
+	part2(input)
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func part1(input string) {
+	total := 0
+	for _, b := range strings.Split(input, "\n") {
+		dims := strings.Split(b, "x")
+		x, _ := strconv.Atoi(dims[0])
+		y, _ := strconv.Atoi(dims[1])
+		z, _ := strconv.Atoi(dims[2])
+		a := x * y
+		b := y * z
+		c := z * x
+		total += 2*a + 2*b + 2*c + min(a, min(b, c))
+	}
+	println("The answer to part one is " + strconv.Itoa(total))
+}
+
+func part2(input string) {
+}
