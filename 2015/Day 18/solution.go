4b03212e4107050349726b77c8717d2cdf89074f
Patrick Hogg <phogg@novamoon.net>
2019-11-23 11:15:13 -0500

    2015 Day 18 Part 2
    You broke Conway's Game of Life!

--- 2015/Day 18/solution.go
+++ 2015/Day 18/solution.go
@@ -105,5 +105,26 @@ func part1(input string) {
 	println("The answer to part one is " + strconv.Itoa(count))
 }
 
+func turnOnCorners(grid [100][100]bool) [100][100]bool {
+	grid[0][0] = true
+	grid[99][0] = true
+	grid[0][99] = true
+	grid[99][99] = true
+	return grid
+}
+
 func part2(input string) {
+	grid := turnOnCorners(parseGrid(input))
+	for idx := 0; idx < 100; idx++ {
+		grid = turnOnCorners(iterate(grid))
+	}
+	count := 0
+	for x := 0; x < 100; x++ {
+		for y := 0; y < 100; y++ {
+			if grid[x][y] {
+				count++
+			}
+		}
+	}
+	println("The answer to part two is " + strconv.Itoa(count))
 }
