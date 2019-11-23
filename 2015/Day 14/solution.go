7950851ffebf251b336ea6c96fc68b7c2b616f0a
Patrick Hogg <phogg@novamoon.net>
2019-11-23 09:26:47 -0500

    2015 Day 14 Part 1

---
+++ 2015/Day 14/solution.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"advent-of-code/aochelpers"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := aochelpers.GetInput(2015, 14)
+	part1(input)
+	part2(input)
+}
+
+type reindeerStats struct {
+	speed      int
+	flightTime int
+	restTime   int
+}
+
+func parse(input string) []reindeerStats {
+	reindeer := []reindeerStats{}
+	for _, line := range strings.Split(input, "\n") {
+		parts := strings.Split(line, " ")
+		stats := reindeerStats{}
+		stats.speed, _ = strconv.Atoi(parts[3])
+		stats.flightTime, _ = strconv.Atoi(parts[6])
+		stats.restTime, _ = strconv.Atoi(parts[13])
+		reindeer = append(reindeer, stats)
+	}
+	return reindeer
+}
+
+func calcDistance(stats reindeerStats, time int) int {
+	cycleTime := stats.flightTime + stats.restTime
+	fullCycles := time / cycleTime
+	time = time % cycleTime
+	lastCycleFlightTime := stats.flightTime
+	if lastCycleFlightTime > time {
+		lastCycleFlightTime = time
+	}
+	return stats.speed * (fullCycles*stats.flightTime + lastCycleFlightTime)
+}
+
+func part1(input string) {
+	reindeer := parse(input)
+	bestDistance := 0
+	for _, stats := range reindeer {
+		distance := calcDistance(stats, 2503)
+		if distance > bestDistance {
+			bestDistance = distance
+		}
+	}
+	println("The answer to part one is " + strconv.Itoa(bestDistance))
+}
+
+func part2(input string) {
+}
