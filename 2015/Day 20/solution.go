c293e21ddbcf5086ed5c28cd00298340af1fb71b
Patrick Hogg <phogg@novamoon.net>
2019-11-28 21:13:28 -0500

    2015 Day 20 Part 2

--- 2015/Day 20/solution.go
+++ 2015/Day 20/solution.go
@@ -38,4 +38,30 @@ func part1(input string) {
 }
 
 func part2(input string) {
+	value, _ := strconv.Atoi(input)
+
+	powerOfTwoExceedingValue := 1
+	for ; (2*powerOfTwoExceedingValue-1)*10 < value; powerOfTwoExceedingValue *= 2 {
+
+	}
+
+	scratch := []int{}
+	for i := 0; i < powerOfTwoExceedingValue; i++ {
+		scratch = append(scratch, 1)
+	}
+
+	best := powerOfTwoExceedingValue
+
+	for i := 1; i < best; i++ {
+		for housesVisited := 1; housesVisited <= 50; housesVisited++ {
+			idx := i * housesVisited
+			if idx < best {
+				scratch[idx] += i * 11
+				if scratch[idx] >= value {
+					best = idx
+				}
+			}
+		}
+	}
+	println("The answer to part two is " + strconv.Itoa(best))
 }
