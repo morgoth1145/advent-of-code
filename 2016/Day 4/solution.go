e8c40b77eea0a9a42a6bf99fe20abce84a1b4b4b
Patrick Hogg <phogg@novamoon.net>
2019-11-29 19:48:20 -0500

    2016 Day 4 Part 1

---
+++ 2016/Day 4/solution.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"advent-of-code/aochelpers"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := aochelpers.GetInput(2016, 4)
+	part1(input)
+	part2(input)
+}
+
+type room struct {
+	name     string
+	sectorID int
+	checksum string
+}
+
+func parse(input string) []room {
+	out := []room{}
+	for _, line := range strings.Split(input, "\n") {
+		parts := strings.Split(line, "-")
+		numberChecksumPart := parts[len(parts)-1]
+		name := strings.Join(parts[:len(parts)-1], "-")
+		checksumIdx := strings.Index(numberChecksumPart, "[")
+		number, _ := strconv.Atoi(numberChecksumPart[:checksumIdx])
+		checksum := numberChecksumPart[checksumIdx+1 : len(numberChecksumPart)-1]
+		out = append(out, room{name: name, sectorID: number, checksum: checksum})
+	}
+	return out
+}
+
+func getRoomChecksum(name string) string {
+	counts := map[rune]int{}
+	for _, c := range name {
+		if '-' == c {
+			continue
+		}
+		counts[c]++
+	}
+	out := ""
+	for i := 0; i < 5; i++ {
+		maxCount := 0
+		bestC := 'a'
+		for c, num := range counts {
+			if num > maxCount {
+				maxCount = num
+				bestC = c
+			} else if num == maxCount && c < bestC {
+				bestC = c
+			}
+		}
+		out += string(bestC)
+		delete(counts, bestC)
+	}
+	return out
+}
+
+func part1(input string) {
+	total := 0
+	for _, r := range parse(input) {
+		if r.checksum == getRoomChecksum(r.name) {
+			total += r.sectorID
+		}
+	}
+	println("The answer to part one is " + strconv.Itoa(total))
+}
+
+func part2(input string) {
+}
