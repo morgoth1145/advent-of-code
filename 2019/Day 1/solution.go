8d8537059421a96a4b523476d3a5660e4e62b786
Patrick Hogg <phogg@novamoon.net>
2019-12-01 00:01:32 -0500

    2019 Day 1 Part 1

---
+++ 2019/Day 1/solution.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"advent-of-code/aochelpers"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := aochelpers.GetInput(2019, 1)
+	part1(input)
+	part2(input)
+}
+
+func part1(input string) {
+	totalFuel := 0
+	for _, line := range strings.Split(input, "\n") {
+		mass, _ := strconv.Atoi(line)
+		fuel := mass/3 - 2
+		totalFuel += fuel
+	}
+	println("The answer to part one is " + strconv.Itoa(totalFuel))
+}
+
+func part2(input string) {
+}
