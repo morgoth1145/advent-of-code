af53cefc5dcc71f34528017d03db42ee2ac1f566
Patrick Hogg <phogg@novamoon.net>
2019-12-01 00:05:50 -0500

    2019 Day 1 Part 2

--- 2019/Day 1/solution.go
+++ 2019/Day 1/solution.go
@@ -23,4 +23,17 @@ func part1(input string) {
 }
 
 func part2(input string) {
+	totalFuel := 0
+	for _, line := range strings.Split(input, "\n") {
+		mass, _ := strconv.Atoi(line)
+		for {
+			fuel := mass/3 - 2
+			if fuel <= 0 {
+				break
+			}
+			totalFuel += fuel
+			mass = fuel
+		}
+	}
+	println("The answer to part two is " + strconv.Itoa(totalFuel))
 }
