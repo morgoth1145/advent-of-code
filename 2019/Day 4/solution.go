11549e2981ab8feca19539710785910908f8b7d1
Patrick Hogg <phogg@novamoon.net>
2019-12-04 00:13:51 -0500

    2019 Day 4 Part 1

---
+++ 2019/Day 4/solution.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"advent-of-code/aochelpers"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := aochelpers.GetInput(2019, 4)
+	part1(input)
+	part2(input)
+}
+
+func parse(input string) (int, int) {
+	parts := strings.Split(input, "-")
+	v1, _ := strconv.Atoi(parts[0])
+	v2, _ := strconv.Atoi(parts[1])
+	return v1, v2
+}
+
+func getDigits(n int) []int {
+	out := []int{}
+	for n > 0 {
+		d := n % 10
+		n /= 10
+		out = append(append([]int{}, d), out...)
+	}
+	return out
+}
+
+func validate(n int) bool {
+	c := -1
+	hasDouble := false
+	for _, d := range getDigits(n) {
+		if d == c {
+			hasDouble = true
+		} else if d < c {
+			return false // Decreasing
+		}
+		c = d
+	}
+	return hasDouble
+}
+
+func part1(input string) {
+	low, high := parse(input)
+	valid := 0
+	for i := low; i <= high; i++ {
+		if validate(i) {
+			valid++
+		}
+	}
+	println("The answer to part one is " + strconv.Itoa(valid))
+}
+
+func part2(input string) {
+}
