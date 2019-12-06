06753710e75101b47b223049e0a1ec159ee0a7ed
Patrick Hogg <phogg@novamoon.net>
2019-12-06 00:48:26 -0500

    2019 Rewrite Day 6 Part 2
    My first algorithm was stupid. This is much less stupid.

--- 2019/Day 6/solution.go
+++ 2019/Day 6/solution.go
@@ -50,37 +50,35 @@ func part2(input string) {
 
 	transferCount := map[string]int{}
 
-	generation := []string{orbits["YOU"]}
-	for len(generation) > 0 {
-		nextGeneration := []string{}
-		for _, obj := range generation {
-			transfers := transferCount[obj]
-			for objA, orbited := range orbits {
-				if objA == obj {
-					{
-						_, present := transferCount[orbited]
-						if present {
-							continue
-						}
-					}
-					transferCount[orbited] = transfers + 1
-					nextGeneration = append(nextGeneration, orbited)
-				}
-				if orbited == obj {
-					{
-						_, present := transferCount[objA]
-						if present {
-							continue
-						}
-					}
-					transferCount[objA] = transfers + 1
-					nextGeneration = append(nextGeneration, objA)
-				}
-			}
+	start := orbits["YOU"]
+	transfers := 0
+	for {
+		transferCount[start] = transfers
+		transfers++
+		parent, present := orbits[start]
+		if !present {
+			break
 		}
-		generation = nextGeneration
+		start = parent
 	}
 
-	bestTransfers := transferCount[orbits["SAN"]]
-	println("The answer to part two is " + strconv.Itoa(bestTransfers))
+	target := orbits["SAN"]
+	transfers = 0
+	for {
+		{
+			otherTransfers, present := transferCount[target]
+			if present {
+				transfers += otherTransfers
+				break
+			}
+		}
+		transfers++
+		parent, present := orbits[target]
+		if !present {
+			panic("There's no route!")
+		}
+		target = parent
+	}
+
+	println("The answer to part two is " + strconv.Itoa(transfers))
 }
