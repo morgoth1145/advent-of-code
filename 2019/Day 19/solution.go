fc6c6cc60c44b993b1df9c1d0ce7c3c76a703179
Patrick Hogg <phogg@novamoon.net>
2019-12-19 00:42:00 -0500

    2019 Day 19 Part 1
    I'm on the leaderboard again at rank 43! That was pretty easy given my IntCode VM API

---
+++ 2019/Day 19/solution.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"advent-of-code/2019/channeltypes"
+	"advent-of-code/2019/intcode"
+	"advent-of-code/aochelpers"
+	"strconv"
+)
+
+func main() {
+	input := aochelpers.GetInput(2019, 19)
+	part1(input)
+	part2(input)
+}
+
+func isSquareAffected(x int, y int, program intcode.Program) bool {
+	coordInputs := channeltypes.List(int64(x), int64(y))
+	affected := program.Clone().AsyncRun(intcode.InputChannelFunction(coordInputs, intcode.EOFPanic))
+	if 1 == <-affected {
+		return true
+	}
+	return false
+}
+
+func part1(input string) {
+	program := intcode.Parse(input)
+	totalAffected := 0
+	for x := 0; x < 50; x++ {
+		for y := 0; y < 50; y++ {
+			if isSquareAffected(x, y, program) {
+				totalAffected++
+			}
+		}
+	}
+	println("The answer to part one is " + strconv.Itoa(totalAffected))
+}
+
+func part2(input string) {
+}
