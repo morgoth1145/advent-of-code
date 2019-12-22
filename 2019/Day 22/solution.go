68e070c008e888c86abeabcc1972487d32914923
Patrick Hogg <phogg@novamoon.net>
2019-12-22 16:41:07 -0500

    2019 Day 22 Part 2
    This was absolutely brutal. The code looks clean now, but getting to this point was extremely hard, I don't think that I've ever seen inverting modulus before.
    
    At first I was wondering about the cycle length of this pseudo-random number generator but never could figure it out. (After looking at the solution thread though I realize that it's pretty straightforward to do that way, just iterate forwards cardCount - 1 - iterations times.)
    
    With that out of the picture I then wanted to run the shuffle in reverse, but had no idea whatsoever how to invert the multiplication followed by modulus. Initially I tried figuring out how to do that myself but failed miserably. (I was headed in the same direction as https://www.reddit.com/r/adventofcode/comments/ee56wh/2019_day_22_part_2_so_whats_the_purpose_of_this/fbrjt19/, but didn't quite get it to work. My lack of sleep must be showing, because reading that post makes plenty of sense and I don't know how I missed it!) Ultimately I looked up how to do this on WIkipedia (and failed initially to even implement the pseudocode correctly!) Then it turns out that big.Int actually has a ModInverse method that would have come in *real* handy had I known about it (though I'd still have had to know how to use it properly). I really need to re-learn the Euclidean algorithm...
    
    The fast handling of 101741582076661 iterations was at least not too bad, it's very similar to fast exponentiation. (It took a few tries to get the substitution right though...)
    
    This is a pretty elegant solution, but I'm not sure that I like how math heavy this was. This is supposed to be Advent of Code, not Project Euler! Ah well, at least I did get it, even if it did take 5 hours.

--- 2019/Day 22/solution.go
+++ 2019/Day 22/solution.go
@@ -49,15 +49,62 @@ func parseShuffle(input string, cardCount *big.Int) shuffleRNG {
 	return shuffle
 }
 
-func (shuffle shuffleRNG) iterate(pos *big.Int) *big.Int {
-	return pos.Mul(pos, shuffle.factor).Add(pos, shuffle.increment).Mod(pos, shuffle.modulus)
+func (shuffle shuffleRNG) iterate(pos *big.Int, iterations int64) *big.Int {
+	if 0 == iterations {
+		return pos
+	}
+	if 1 == iterations%2 {
+		pos = pos.Mul(pos, shuffle.factor).Add(pos, shuffle.increment).Mod(pos, shuffle.modulus)
+	}
+	if iterations > 1 {
+		nextPowerShuffle := shuffleRNG{
+			big.NewInt(1),
+			big.NewInt(0),
+			shuffle.modulus,
+		}
+		nextPowerShuffle.factor.Mul(shuffle.factor, shuffle.factor).Mod(nextPowerShuffle.factor, shuffle.modulus)
+		nextPowerShuffle.increment.Mul(shuffle.factor, shuffle.increment).Add(nextPowerShuffle.increment, shuffle.increment).Mod(nextPowerShuffle.increment, shuffle.modulus)
+		pos = nextPowerShuffle.iterate(pos, iterations/2)
+	}
+	return pos
 }
 
 func part1(input string) {
 	shuffle := parseShuffle(input, big.NewInt(10007))
-	pos := shuffle.iterate(big.NewInt(2019))
+	pos := shuffle.iterate(big.NewInt(2019), 1)
 	println("The answer to part one is " + pos.String())
 }
 
+func (shuffle shuffleRNG) reverse(pos *big.Int, iterations int64) *big.Int {
+	if 0 == iterations {
+		return pos
+	}
+	if 1 == iterations%2 {
+		pos = big.NewInt(0).Sub(pos, shuffle.increment)
+		for pos.Cmp(big.NewInt(0)) < 0 {
+			pos.Add(pos, shuffle.modulus)
+		}
+
+		pos.Mul(big.NewInt(0).ModInverse(shuffle.factor, shuffle.modulus), pos).Mod(pos, shuffle.modulus)
+	}
+	if iterations > 1 {
+		nextPowerShuffle := shuffleRNG{
+			big.NewInt(1),
+			big.NewInt(0),
+			shuffle.modulus,
+		}
+		nextPowerShuffle.factor.Mul(shuffle.factor, shuffle.factor).Mod(nextPowerShuffle.factor, shuffle.modulus)
+		nextPowerShuffle.increment.Mul(shuffle.factor, shuffle.increment).Add(nextPowerShuffle.increment, shuffle.increment).Mod(nextPowerShuffle.increment, shuffle.modulus)
+		pos = nextPowerShuffle.reverse(pos, iterations/2)
+	}
+	return pos
+}
+
 func part2(input string) {
+	cardCount := big.NewInt(119315717514047)
+	iterations := int64(101741582076661)
+	shuffle := parseShuffle(input, cardCount)
+
+	pos2 := shuffle.reverse(big.NewInt(2020), iterations)
+	println("The answer to part two is " + pos2.String())
 }
