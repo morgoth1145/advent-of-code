c76bcd234dcbacf003f834e032f6365b672f184c
Patrick Hogg <phogg@novamoon.net>
2019-12-23 21:34:15 -0500

    Day 23 Cleanup

--- 2019/Day 23/solution.go
+++ 2019/Day 23/solution.go
@@ -21,45 +21,43 @@ type packet struct {
 
 func part1(input string) {
 	println("The correct answer to Part 1 is 27182")
-	messageBus := make(chan packet)
-	receiverWG := sync.WaitGroup{}
-	receiverWG.Add(50)
+	router := make(chan packet)
+	completedPrograms := sync.WaitGroup{}
+	completedPrograms.Add(50)
 	go func() {
-		receiverWG.Wait()
-		close(messageBus)
+		completedPrograms.Wait()
+		close(router)
 	}()
-	computerReciever := func(outChan <-chan int64) {
+	outputHandler := func(outChan <-chan int64) {
 		for dest := range outChan {
 			x := <-outChan
 			y := <-outChan
-			messageBus <- packet{dest, x, y}
+			router <- packet{dest, x, y}
 		}
-		receiverWG.Done()
+		completedPrograms.Done()
 	}
-	channels := []chan int64{}
+	messageQueues := []chan int64{}
 	for id := 0; id < 50; id++ {
-		c := make(chan int64, 10000)
-		channels = append(channels, c)
+		c := make(chan int64, 100)
+		messageQueues = append(messageQueues, c)
 		outChan := intcode.Parse(input).AsyncRun(intcode.InputChannelFunction(c, intcode.EOFTerminateProgram))
-		go computerReciever(outChan)
+		go outputHandler(outChan)
 		c <- int64(id)
 		c <- -1
 	}
-	for p := range messageBus {
+	for p := range router {
 		if p.dest == 255 {
 			println("The answer to part one is " + strconv.FormatInt(p.y, 10))
 			break
 		}
-		if p.dest > int64(len(channels)) {
-			panic("This is bad...")
-		}
-		channels[p.dest] <- p.x
-		channels[p.dest] <- p.y
+		messageQueues[p.dest] <- p.x
+		messageQueues[p.dest] <- p.y
 	}
-	for _, c := range channels {
+	for _, c := range messageQueues {
 		close(c)
 	}
-	for range messageBus {
+	// Clear the router so that all goroutines can exit
+	for range router {
 	}
 }
 
@@ -70,13 +68,13 @@ type packetTracker struct {
 
 func part2(input string) {
 	println("The correct answer to Part 2 is 19285")
-	messageBus := make(chan packet)
+	router := make(chan packet)
 	networkTracker := make(chan packetTracker)
-	receiverWG := sync.WaitGroup{}
-	receiverWG.Add(50)
+	completedPrograms := sync.WaitGroup{}
+	completedPrograms.Add(50)
 	go func() {
-		receiverWG.Wait()
-		close(messageBus)
+		completedPrograms.Wait()
+		close(router)
 		close(networkTracker)
 	}()
 
@@ -104,81 +102,82 @@ func part2(input string) {
 			if !ok {
 				runtime.Goexit()
 			}
+			messagesHandled.Done()
 			currentPacket = &nextPacket
 			return currentPacket.x
 		}
 	}
-	computerReciever := func(id int64, outChan <-chan int64) {
+	outputHandler := func(id int64, outChan <-chan int64) {
 		for dest := range outChan {
 			messagesHandled.Add(1)
 			networkTracker <- packetTracker{dest, true}
 			x := <-outChan
 			y := <-outChan
-			messageBus <- packet{dest, x, y}
+			router <- packet{dest, x, y}
 		}
-		receiverWG.Done()
+		completedPrograms.Done()
 	}
-	channels := []chan packet{}
+	messageQueues := map[int64]chan packet{}
 	for id := 0; id < 50; id++ {
 		c := make(chan packet, 100)
-		channels = append(channels, c)
+		messageQueues[int64(id)] = c
 		outChan := intcode.Parse(input).AsyncRun(computerSender(int64(id), c))
-		go computerReciever(int64(id), outChan)
+		go outputHandler(int64(id), outChan)
 	}
+	natChannel := make(chan packet)
+	messageQueues[255] = natChannel
 	var natMemory packet
 	go func() {
-		for p := range messageBus {
-			if p.dest == 255 {
-				natMemory = p
-				messagesHandled.Done()
-				continue
-			}
-			if p.dest > int64(len(channels)) {
-				panic("This is bad...")
-			}
-			channels[p.dest] <- p
+		for p := range natChannel {
+			natMemory = p
 			messagesHandled.Done()
 		}
 	}()
-	lastY := int64(0)
-	pendingPacketCounts := []int{}
-	for len(pendingPacketCounts) < 50 {
-		pendingPacketCounts = append(pendingPacketCounts, 0)
-	}
-	idleComputers := 0
-	for io := range networkTracker {
-		if io.dest == 255 {
-			continue
+	go func() {
+		lastY := int64(0)
+		pendingPacketCounts := []int{}
+		for len(pendingPacketCounts) < 50 {
+			pendingPacketCounts = append(pendingPacketCounts, 0)
 		}
-		if io.isSend {
-			pendingPacketCounts[io.dest]++
-			if pendingPacketCounts[io.dest] == 0 {
+		idleComputers := 0
+		for io := range networkTracker {
+			if io.dest == 255 {
+				continue
+			}
+			if io.isSend {
+				pendingPacketCounts[io.dest]++
+				if pendingPacketCounts[io.dest] == 0 {
+					idleComputers--
+				}
+			} else {
+				pendingPacketCounts[io.dest]--
+				if pendingPacketCounts[io.dest] == -1 {
+					idleComputers++
+				}
+			}
+			if 50 == idleComputers {
+				// Wait for all pending messages to be handled (including 255!)
+				messagesHandled.Wait()
+
+				if natMemory.y == lastY {
+					println("The answer to part two is " + strconv.FormatInt(natMemory.y, 10))
+					break
+				}
+				lastY = natMemory.y
+				messagesHandled.Add(1)
+				messageQueues[0] <- packet{0, natMemory.x, natMemory.y}
+				pendingPacketCounts[0]++
 				idleComputers--
 			}
-		} else {
-			pendingPacketCounts[io.dest]--
-			if pendingPacketCounts[io.dest] == -1 {
-				idleComputers++
-			}
 		}
-		if 50 == idleComputers {
-			// Wait for all pending messages to be handled (including 255!)
-			messagesHandled.Wait()
-
-			if natMemory.y == lastY {
-				println("The answer to part two is " + strconv.FormatInt(natMemory.y, 10))
-				break
-			}
-			lastY = natMemory.y
-			channels[0] <- packet{0, natMemory.x, natMemory.y}
-			pendingPacketCounts[0]++
-			idleComputers--
+		for _, c := range messageQueues {
+			close(c)
 		}
-	}
-	for _, c := range channels {
-		close(c)
-	}
-	// Discard the rest
-	for range networkTracker {
+		// Discard the rest
+		for range networkTracker {
+		}
+	}()
+	for p := range router {
+		messageQueues[p.dest] <- p
 	}
 }
