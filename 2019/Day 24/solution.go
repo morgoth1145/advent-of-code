de5ada5d1613a92e9139bd63688919b1d7e3d779
Patrick Hogg <phogg@novamoon.net>
2019-12-24 10:48:34 -0500

    2019 Day 24 Part 2
    Well that was a fun twist on Part 1. The algorithm is essentially the same as Part 1, I just had to switch out 2D coordinates for 3D, adjust the surrounding tiles function, and make sure that I actually processed the right tiles. Simple enough to get global rank 53!

--- 2019/Day 24/solution.go
+++ 2019/Day 24/solution.go
@@ -113,5 +113,115 @@ func part1(input string) {
 	}
 }
 
+type vector3D struct {
+	x int
+	y int
+	z int
+}
+
+func parse3D(input string) map[vector3D]bool {
+	out := map[vector3D]bool{}
+	for y, line := range strings.Split(input, "\n") {
+		for x, c := range line {
+			if x == 2 && y == 2 {
+				continue
+			}
+			if c == '#' {
+				out[vector3D{x, y, 0}] = true
+			}
+		}
+	}
+	return out
+}
+
+func surroundingTiles3D(pos vector3D) []vector3D {
+	out := []vector3D{}
+	for _, pos2D := range surroundingTiles(vector2D{pos.x, pos.y}) {
+		if pos2D.x == 2 && pos2D.y == 2 {
+			// Go *IN* one level
+			if pos.x == 2 {
+				// x varies
+				y := 0
+				if pos.y == 3 {
+					y = 4
+				}
+				for x := 0; x < 5; x++ {
+					out = append(out, vector3D{x, y, pos.z - 1})
+				}
+			} else {
+				// y varies
+				x := 0
+				if pos.x == 3 {
+					x = 4
+				}
+				for y := 0; y < 5; y++ {
+					out = append(out, vector3D{x, y, pos.z - 1})
+				}
+			}
+			continue
+		}
+		// Go *OUT* one level special cases
+		if pos2D.x < 0 {
+			out = append(out, vector3D{1, 2, pos.z + 1})
+			continue
+		} else if pos2D.x > 4 {
+			out = append(out, vector3D{3, 2, pos.z + 1})
+			continue
+		}
+		if pos2D.y < 0 {
+			out = append(out, vector3D{2, 1, pos.z + 1})
+			continue
+		} else if pos2D.y > 4 {
+			out = append(out, vector3D{2, 3, pos.z + 1})
+			continue
+		}
+		out = append(out, vector3D{pos2D.x, pos2D.y, pos.z})
+	}
+	return out
+}
+
+func getTilesToProcess3D(bugs map[vector3D]bool) []vector3D {
+	shouldHandle := map[vector3D]bool{}
+	for pos := range bugs {
+		shouldHandle[pos] = true
+		for _, n := range surroundingTiles3D(pos) {
+			shouldHandle[n] = true
+		}
+	}
+	out := []vector3D{}
+	for pos := range shouldHandle {
+		out = append(out, pos)
+	}
+	return out
+}
+
+func iterate3D(bugs map[vector3D]bool) map[vector3D]bool {
+	out := map[vector3D]bool{}
+	for _, pos := range getTilesToProcess3D(bugs) {
+		bug := bugs[pos]
+		neighborCount := 0
+		for _, n := range surroundingTiles3D(pos) {
+			if bugs[n] {
+				neighborCount++
+			}
+		}
+		if bug {
+			if 1 == neighborCount {
+				out[pos] = true
+			}
+		} else {
+			if 1 == neighborCount || 2 == neighborCount {
+				out[pos] = true
+			}
+		}
+	}
+	return out
+}
+
 func part2(input string) {
+	tiles := parse3D(input)
+	for i := 0; i < 200; i++ {
+		tiles = iterate3D(tiles)
+	}
+	println("The answer to part two is " + strconv.Itoa(len(tiles)))
 }
