a6ecfb25ced134c7879d4245d932deede0fd1a17
Patrick Hogg <phogg@novamoon.net>
2020-12-01 00:08:04 -0500

    2020 Day 1 Part 2

--- 2020/Day 1/solution.py
+++ 2020/Day 1/solution.py
@@ -10,7 +10,16 @@ def part1(s):
                 return
 
 def part2(s):
-    pass
+    nums = list(map(int, s.split()))
+    for a in nums:
+        for b in nums:
+            if a + b >= 2020:
+                continue
+            for c in nums:
+                if a + b + c == 2020:
+                    answer = a * b * c
+                    print(f'The answer to part one is {answer}')
+                    return
 
 INPUT = helpers.input.get_input(2020, 1)
 part1(INPUT)
