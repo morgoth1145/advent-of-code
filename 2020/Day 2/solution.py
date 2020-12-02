915413cbcffbac841e74222c60f3404b74368b38
Patrick Hogg <phogg@novamoon.net>
2020-12-02 09:37:01 -0500

    2020 Refactor Day 2
    - Deduplicate code between part 1 and 2
    - Use regex to split the string. It's much more succinct than string splitting, though it's harder to get right!
    - Improve the validators based on some of the megathread solutions
      - Apparently Python supports chaining comparison operators. Who knew? That's very handy!
      - Using bitwise exclusive or is cleaner for the second validator

--- 2020/Day 2/solution.py
+++ 2020/Day 2/solution.py
@@ -1,27 +1,25 @@
+import re
+
 import helpers.input
 
+def count_valid_password(s, validator):
+    valid_count = 0
+    for m in re.finditer('(\d+)\-(\d+) (\w): (\w+)', s):
+        n1, n2, c, password = m.groups()
+        if validator(int(n1), int(n2), c, password.strip()):
+            valid_count += 1
+    return valid_count
+
 def part1(s):
-    answer = 0
-    for line in s.splitlines():
-        policy, password = line.split(':')
-        nums, c = policy.split()
-        mint, maxt = list(map(int, nums.split('-')))
-        password = password.strip()
-        times = password.count(c)
-        if mint <= times and times <= maxt:
-            answer += 1
+    def validator(mint, maxt, c, password):
+        return mint <= password.count(c) <= maxt
+    answer = count_valid_password(s, validator)
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    answer = 0
-    for line in s.splitlines():
-        policy, password = line.split(':')
-        nums, c = policy.split()
-        loca, locb = list(map(int, nums.split('-')))
-        password = password.strip()
-        bits = password[loca-1] + password[locb-1]
-        if bits.count(c) == 1:
-            answer += 1
+    def validator(a, b, c, password):
+        return (password[a-1] == c) ^ (password[b-1] == c)
+    answer = count_valid_password(s, validator)
     print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 2)
