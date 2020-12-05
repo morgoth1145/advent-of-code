5015d806bc30c7d87d786176c362e0a576e15103
Patrick Hogg <phogg@novamoon.net>
2020-12-05 00:15:30 -0500

    2020 Refactor Day 5
    - Now that I have time to think about it a bit, I realized that there's no reason to care about the row or column. I just need the seat ID, which *is* the string I was given, just with B and R as 1 and F and L as 0!
    - Furthermore, I can do a cleverer approach to part 2 using sets. Instead of searching through the entire range I can create three sets, one of all the seats below one listed, one of all the seats above one listed, and all the seats. Then some set intersection and difference will filter me down to the seat which is mine!

--- 2020/Day 5/solution.py
+++ 2020/Day 5/solution.py
@@ -2,33 +2,19 @@ import helpers.input
 
 def iter_seats(s):
     for seat in s.splitlines():
-        row = seat[:7]
-        column = seat[7:]
-        row = row.replace('B', '1').replace('F', '0')
-        column = column.replace('R', '1').replace('L', '0')
-        yield int(row, 2), int(column, 2)
+        seat = seat.replace('B', '1').replace('R', '1')
+        seat = seat.replace('F', '0').replace('L', '0')
+        yield int(seat, 2)
 
 def part1(s):
-    answer = 0
-    for row, column in iter_seats(s):
-        seat_id = row * 8 + column
-        answer = max(seat_id, answer)
+    answer = max(iter_seats(s))
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    seen = set()
-    for row, column in iter_seats(s):
-        seat_id = row * 8 + column
-        seen.add(seat_id)
-    for i in range(1, 1<<10):
-        if i in seen:
-            continue
-        if i-1 not in seen:
-            continue
-        if i+1 not in seen:
-            continue
-        answer = i
-        break
+    seats = set(iter_seats(s))
+    sub_1 = {seat_id-1 for seat_id in seats}
+    plus_1 = {seat_id+1 for seat_id in seats}
+    answer = list(sub_1.intersection(plus_1) - seats)[0]
     print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 5)
