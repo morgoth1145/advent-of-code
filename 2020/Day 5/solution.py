a0c22ea902f2a3f598072d892ed9a442d8248367
Patrick Hogg <phogg@novamoon.net>
2020-12-05 00:05:14 -0500

    2020 Day 5 Part 1
    I wasted time by treating the front as 1 and back as 0. I'd have been a minute faster if I got that right the first time!

---
+++ 2020/Day 5/solution.py
@@ -0,0 +1,23 @@
+import helpers.input
+
+def iter_seats(s):
+    for seat in s.splitlines():
+        row = seat[:7]
+        column = seat[7:]
+        row = row.replace('B', '1').replace('F', '0')
+        column = column.replace('R', '1').replace('L', '0')
+        yield int(row, 2), int(column, 2)
+
+def part1(s):
+    answer = 0
+    for row, column in iter_seats(s):
+        seat_id = row * 8 + column
+        answer = max(seat_id, answer)
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = helpers.input.get_input(2020, 5)
+part1(INPUT)
+part2(INPUT)
