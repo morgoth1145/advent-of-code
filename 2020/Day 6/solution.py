d9f74489c042ece011731e373ca15c1f09c79f9e
Patrick Hogg <phogg@novamoon.net>
2020-12-06 00:11:38 -0500

    2020 Refactor Day 6
    Apparently you can intersect sets in Python with & (and &=). That's definitely less typing.

--- 2020/Day 6/solution.py
+++ 2020/Day 6/solution.py
@@ -15,7 +15,7 @@ def part2(s):
         people = g.split()
         all_yes = set(people[0])
         for p in people[1:]:
-            all_yes = all_yes.intersection(p)
+            all_yes &= set(p)
         answer += len(set(all_yes))
     print(f'The answer to part two is {answer}')
 
