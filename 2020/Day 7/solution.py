9b7df3f971483c6f1cc0fb26b4ee10b2f16d7897
Patrick Hogg <phogg@novamoon.net>
2020-12-07 00:54:40 -0500

    2020 Refactor Day 7
    I really need to get more comfortable with regex. I knew when writing the bag graph parser that regex could do it faster, but under time pressure I can't think through it that quickly!
    
    Also, I *didn't* need to topo sort my graph to go from leaf to root! That was both a waste in terms of complexity *and* because I likely would have been more ready to spot my int/str bug! (It may not have been enough to let me leaderboard, but it still cost me a bunch of time.) Honestly, I should have known that this wasn't needed since my naive container counting for part 1 worked fine. This wasn't a ridiculously huge graph after all!
    On the bright side, I did at least find and fix a bug in helpers.graph.

--- 2020/Day 7/solution.py
+++ 2020/Day 7/solution.py
@@ -1,54 +1,40 @@
-import collections
+import re
 
-import helpers.graph
 import helpers.input
 
 def parse_bags(s):
-    graph = collections.defaultdict(list)
+    graph = {}
     for line in s.splitlines():
         outer, inner = line.split('bags contain')
         outer = outer.strip()
-        inner = inner.strip()
-        if inner == 'no other bags.':
-            # This bag is empty
-            continue
-        stuff = inner.split(',')
-        for item in stuff:
-            amount, item = item.split(maxsplit=1)
-            while item[-1] in '.s':
-                item = item[:-1]
-            assert(item.endswith('bag'))
-            item = item[:-3].strip()
-            graph[outer].append((item, int(amount)))
+        contents = []
+        for amount, item in re.findall('(\d+) ([\w ]+) bag', inner):
+            contents.append((item, int(amount)))
+        graph[outer] = contents
     return graph
 
 def can_contain(graph, key, inner):
     for item, amount in graph.get(key, []):
-        if item == inner:
-            return True
-        if can_contain(graph, item, inner):
+        if item == inner or can_contain(graph, item, inner):
             return True
     return False
 
 def part1(s):
     graph = parse_bags(s)
-    answer = sum(1 for key in graph.keys() if can_contain(graph, key, 'shiny gold'))
+    answer = sum(1
+                 for key
+                 in graph.keys()
+                 if can_contain(graph, key, 'shiny gold'))
     print(f'The answer to part one is {answer}')
 
-def custom_topo_sort_thing(bag_graph):
-    graph = {}
-    for key, items in bag_graph.items():
-        graph[key] = [t for t,_ in items]
-    return helpers.graph.topological_sort(graph)
+def count_contents(graph, bag):
+    return sum(n * (1 + count_contents(graph, t))
+               for t,n
+               in graph[bag])
 
 def part2(s):
     graph = parse_bags(s)
-    leaf_to_root = list(custom_topo_sort_thing(graph))
-    bag_content_counts = {}
-    for bag in leaf_to_root:
-        total = sum(n * (1 + bag_content_counts.get(t,0)) for t,n in graph[bag])
-        bag_content_counts[bag] = total
-    answer = bag_content_counts['shiny gold']
+    answer = count_contents(graph, 'shiny gold')
     print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 7)
