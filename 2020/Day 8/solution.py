fadc904aff88f8fc8cbd9cb5f86117b3b9ea9c76
Patrick Hogg <phogg@novamoon.net>
2020-12-08 00:08:04 -0500

    2020 Day 8 Part 2
    Part 2 had an interesting curveball, modify exactly one instruction and make it boot. This required a little bit of interesting rejiggering to try a program, die on a loop, and return the accumulator on successful termination. It wasn't too bad to do, but again, people can code *ridiculously* fast! 7 minutes 42 seconds got me 93rd!

--- 2020/Day 8/solution.py
+++ 2020/Day 8/solution.py
@@ -25,8 +25,49 @@ def part1(s):
     answer = acc
     print(f'The answer to part one is {answer}')
 
+def run(instructions):
+    acc = 0
+    idx = 0
+    previously_run = set()
+    while True:
+        if idx == len(instructions):
+            return acc
+        if idx in previously_run:
+            return None
+        previously_run.add(idx)
+        op, val = instructions[idx].split()
+        val = int(val)
+        if op == 'acc':
+            acc = acc + val
+            idx += 1
+            continue
+        if op == 'jmp':
+            idx += val
+            continue
+        if op == 'nop':
+            idx += 1
+            continue
+
 def part2(s):
-    pass
+    answer = None
+    instructions = list(s.splitlines())
+    for idx in range(len(instructions)):
+        inst = instructions[idx]
+        op, val = inst.split()
+        if op == 'nop':
+            instructions[idx] = f'jmp {val}'
+            answer = run(instructions)
+            instructions[idx] = inst
+        elif op == 'jmp':
+            instructions[idx] = f'nop {val}'
+            answer = run(instructions)
+            instructions[idx] = inst
+        else:
+            continue
+        if answer is not None:
+            break
+    assert(answer is not None)
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 8)
 
