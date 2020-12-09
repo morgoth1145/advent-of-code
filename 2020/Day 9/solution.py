3ab660b8cf0973be4c2de96fe63d0cbf4e2b4ee5
Patrick Hogg <phogg@novamoon.net>
2020-12-09 00:09:23 -0500

    2020 Day 9 Part 2
    I had a couple of mistakes here with my indexing. Nothing *too* bad, but it's a very good thing that I had assert(sum(the_nums) == target) in my code to verify that I wasn't going to submit a trash answer!

--- 2020/Day 9/solution.py
+++ 2020/Day 9/solution.py
@@ -7,19 +7,40 @@ def valid_check(preamble, n):
                 return True
     return False
 
-def part1(s):
-    nums = list(map(int, s.split()))
+def get_invalid_num(nums):
     preamble = nums[:25]
     rest = nums[25:]
     for n in rest:
         if not valid_check(preamble, n):
-            answer = n
-            break
+            return n
         preamble = preamble[1:] + [n]
+
+def part1(s):
+    nums = list(map(int, s.split()))
+    answer = get_invalid_num(nums)
     print(f'The answer to part one is {answer}')
 
+def partial_sums(nums):
+    tot = 0
+    for n in nums:
+        tot += n
+        yield tot
+
 def part2(s):
-    pass
+    nums = list(map(int, s.split()))
+    target = get_invalid_num(nums)
+    sums = list(partial_sums(nums))
+    for idx, a in enumerate(sums):
+        for idx2, b in enumerate(sums[idx+1:]):
+            diff = b - a
+            if diff > target:
+                break
+            if diff == target:
+                the_nums = nums[idx+1:idx+idx2+2]
+                assert(sum(the_nums) == target)
+                answer = min(the_nums) + max(the_nums)
+                print(f'The answer to part two is {answer}')
+                return
 
 INPUT = helpers.input.get_input(2020, 9)
 
