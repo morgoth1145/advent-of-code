95c175c8f1f0dc0c4372968500e15ed2680e02cf
Patrick Hogg <phogg@novamoon.net>
2020-12-10 00:30:19 -0500

    2020 Refactor Day 10
    Part 1 - Get rid of remaining crap from my butchering of this problem. Also use collections.defaultdict. I don't know if it's at all cleaner, but whatever. I hate this part at this point.
    Part 2 - Yep, I was right. I can dynamic program this pretty easily and avoid memoization.

--- 2020/Day 10/solution.py
+++ 2020/Day 10/solution.py
@@ -1,4 +1,4 @@
-import functools
+import collections
 
 import helpers.input
 
@@ -7,36 +7,27 @@ def part1(s):
     nums += [max(nums) + 3, 0]
     nums = sorted(nums)
     last = nums[0]
+    diff_counts = collections.defaultdict(int)
     one_diffs = 0
     three_diffs = 0
     for n in nums[1:]:
-        diff = abs(n-last)
-        assert(diff in (1, 2, 3))
-        if diff == 1:
-            one_diffs += 1
-        if diff == 3:
-            three_diffs += 1
+        diff_counts[n-last] += 1
         last = n
-    answer = one_diffs * three_diffs
+    assert(max(diff_counts.keys()) <= 3)
+    answer = diff_counts[1] * diff_counts[3]
     print(f'The answer to part one is {answer}')
-
-@functools.lru_cache()
-def count_arrangements(nums):
-    if len(nums) in (0, 1):
-        return 1
-    count = 0
-    n = nums[0]
-    for idx in range(1, len(nums)):
-        if abs(nums[idx] - n) in (1, 2, 3):
-            count += count_arrangements(nums[idx:])
-    return count
 
 def part2(s):
     nums = list(map(int, s.split()))
     nums += [max(nums) + 3, 0]
     nums = sorted(nums)
-    nums = tuple(nums)
-    answer = count_arrangements(nums)
+    arrangements = [1]
+    for idx in range(1, len(nums)):
+        count = sum(arrangements[i]
+                    for i in range(idx)
+                    if nums[idx] - nums[i] in (1, 2, 3))
+        arrangements.append(count)
+    answer = arrangements[-1]
     print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 10)
