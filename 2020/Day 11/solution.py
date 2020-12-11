c29b6eb6766324f6740bd5cac20da8bd5ae2518a
Patrick Hogg <phogg@novamoon.net>
2020-12-11 00:17:43 -0500

    2020 Day 11 Part 2
    I had a series of dumb mistakes here.
    - I forgot that my candidates were coordinates rather than offsets, but I treated them like offsets anyway. Like a dumb dumb. This took a good while to spot.
    - I forgot that the occupied to empty transition now required 5 or more visible seats. (A minor change, but an important one.)
    
    I still placed in the top 200, but I would have leaderboarded if I didn't mix up coordinates and offsets :(

--- 2020/Day 11/solution.py
+++ 2020/Day 11/solution.py
@@ -38,8 +38,50 @@ def part1(s):
     answer = ''.join(''.join(l) for l in grid).count('#')
     print(f'The answer to part one is {answer}')
 
+def advance2(prev):
+    grid = [l[:] for l in prev]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == '.':
+                continue
+            occupied = 0
+            candidates = [(-1, -1),
+                          (-1, 0),
+                          (-1, +1),
+                          ( 0, -1),
+                          ( 0, +1),
+                          (+1, -1),
+                          (+1, 0),
+                          (+1, +1)]
+            for dx, dy in candidates:
+                x, y = i, j
+                while True:
+                    x += dx
+                    y += dy
+                    if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[x]):
+                        break
+                    if prev[x][y] == '.':
+                        continue
+                    if prev[x][y] == '#':
+                        occupied += 1
+                    break
+            if grid[i][j] == 'L':
+                if occupied == 0:
+                    grid[i][j] = '#'
+            else: # Occupied
+                if occupied >= 5:
+                    grid[i][j] = 'L'
+    return grid
+
 def part2(s):
-    pass
+    grid = list(map(list, s.splitlines()))
+    while True:
+        new_grid = advance2(grid)
+        if new_grid == grid:
+            break
+        grid = new_grid
+    answer = ''.join(''.join(l) for l in grid).count('#')
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 11)
 
