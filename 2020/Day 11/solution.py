7078a9dd83da5d3d5aa9dcfd7b11ba80d9c4f764
Patrick Hogg <phogg@novamoon.net>
2020-12-11 01:10:19 -0500

    2020 Optimize Day 11
    My solution worked but it feels a bit sluggish. Some optimizations can make the iteration faster. (Specifically, if we precompute the neighbor indices we can accelerate computing the next frame of the simulation.)

--- 2020/Day 11/solution.py
+++ 2020/Day 11/solution.py
@@ -1,82 +1,87 @@
 import helpers.input
 
-def advance(prev):
-    grid = [l[:] for l in prev]
+def compute_immediate_neighbors(grid):
+    grid_neighbors = [l[:] for l in grid]
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == '.':
+                grid_neighbors[i][j] = None
                 continue
-            occupied = 0
-            candidates = [(i-1,j-1),
-                          (i-1,j),
-                          (i-1,j+1),
-                          (i,j-1),
-                          (i,j+1),
-                          (i+1,j-1),
-                          (i+1,j),
-                          (i+1,j+1)]
-            for x, y in candidates:
-                if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[x]):
+            neighbors = []
+            for dx in (-1, 0, 1):
+                x = i+dx
+                if x not in range(0, len(grid)):
                     continue
-                if prev[x][y] == '#':
-                    occupied += 1
-            if grid[i][j] == 'L':
-                if occupied == 0:
+                for dy in (-1, 0, 1):
+                    if dx == dy == 0:
+                        continue
+                    y = j+dy
+                    if y not in range(0, len(grid[x])):
+                        continue
+                    if grid[x][y] == '.':
+                        continue
+                    neighbors.append((x, y))
+            grid_neighbors[i][j] = tuple(neighbors)
+    return grid_neighbors
+
+def advance(prev, neighbors, occupied_evacuation_limit):
+    grid = [l[:] for l in prev]
+    for i, row in enumerate(grid):
+        for j, c in enumerate(row):
+            if c == '.':
+                continue
+            seats = [prev[x][y] for x,y in neighbors[i][j]]
+            if c == 'L':
+                if '#' not in seats:
                     grid[i][j] = '#'
             else:
-                if occupied >= 4:
+                if seats.count('#') >= occupied_evacuation_limit:
                     grid[i][j] = 'L'
     return grid
 
 def part1(s):
     grid = list(map(list, s.splitlines()))
+    neighbors = compute_immediate_neighbors(grid)
     while True:
-        new_grid = advance(grid)
+        new_grid = advance(grid, neighbors, 4)
         if new_grid == grid:
             break
         grid = new_grid
     answer = ''.join(''.join(l) for l in grid).count('#')
     print(f'The answer to part one is {answer}')
 
-def advance2(prev):
-    grid = [l[:] for l in prev]
+def compute_seen_neighbors(grid):
+    grid_neighbors = [l[:] for l in grid]
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == '.':
+                grid_neighbors[i][j] = None
                 continue
-            occupied = 0
-            candidates = [(-1, -1),
-                          (-1, 0),
-                          (-1, +1),
-                          ( 0, -1),
-                          ( 0, +1),
-                          (+1, -1),
-                          (+1, 0),
-                          (+1, +1)]
-            for dx, dy in candidates:
-                x, y = i, j
-                while True:
-                    x += dx
-                    y += dy
-                    if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[x]):
-                        break
-                    if prev[x][y] == '.':
+            neighbors = []
+            for dx in (-1, 0, 1):
+                for dy in (-1, 0, 1):
+                    if dx == dy == 0:
                         continue
-                    if prev[x][y] == '#':
-                        occupied += 1
-                    break
-            if grid[i][j] == 'L':
-                if occupied == 0:
-                    grid[i][j] = '#'
-            else: # Occupied
-                if occupied >= 5:
-                    grid[i][j] = 'L'
-    return grid
+                    x, y = i, j
+                    while True:
+                        x += dx
+                        y += dy
+                        if x not in range(0, len(grid)):
+                            break
+                        if y not in range(0, len(grid[x])):
+                            break
+                        if grid[x][y] == '.':
+                            continue
+                        neighbors.append((x, y))
+                        break
+            grid_neighbors[i][j] = neighbors
+    return grid_neighbors
 
 def part2(s):
     grid = list(map(list, s.splitlines()))
+    neighbors = compute_seen_neighbors(grid)
     while True:
-        new_grid = advance2(grid)
+        new_grid = advance(grid, neighbors, 5)
         if new_grid == grid:
             break
         grid = new_grid
