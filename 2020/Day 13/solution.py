5866cefa1026aa6e7492780f7cf63e9668bcf10e
Patrick Hogg <phogg@novamoon.net>
2020-12-13 00:26:27 -0500

    2020 Day 13 Part 2
    Well this made me exercise my brain for sure. If it was asking for the first time where all the busses arrived at the same time it'd be an easy least common multiple problem, but the offset times threw me. For lack of any better approach I just worked through how to optimize the naive search until it was sufficiently fast. My theory was that "merging" two busses and treating them as one (by knowing when they first line up properly and the cadence between when they line up again) would sufficiently optimize the search and let me complete the search in a reasonable amount of time. Thankfully it panned out, but man is my code scuffed!

--- 2020/Day 13/solution.py
+++ 2020/Day 13/solution.py
@@ -19,8 +19,29 @@ def part1(s):
     answer = (best[0] - timestamp) * best[1]
     print(f'The answer to part one is {answer}')
 
+def find_bus_cadence(a, b, start, offset):
+    n = start
+    while (n-offset) % a != 0:
+        n += b
+    m = n+b
+    while (m-offset) % a != 0:
+        m += b
+    return n, m-n
+
 def part2(s):
-    pass
+    _, busses = s.splitlines()
+    busses = [int(b) if b != 'x' else None
+              for b in busses.split(',')][::-1]
+    offset = 0
+    cadence = busses[0]
+    answer = busses[0]
+    for b in busses[1:]:
+        offset += 1
+        if b is None:
+            continue
+        answer, cadence = find_bus_cadence(b, cadence, answer, offset)
+    answer -= offset
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 13)
 
