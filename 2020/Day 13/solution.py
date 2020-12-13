fb2f4e457a6731ec09ec3b16ce51ca4a36d55796
Patrick Hogg <phogg@novamoon.net>
2020-12-13 18:53:48 -0500

    2020 Rewrite Day 13 Part 2
    After looking into (and understanding) the Chinese remainder theorem, it absolutely applies to this problem. It's also clever, but a lot to get one's head around! On the bright side, I have it written once and forever!

--- 2020/Day 13/solution.py
+++ 2020/Day 13/solution.py
@@ -1,18 +1,17 @@
-import math
-
+import helpers.math
 import helpers.input
 
 def parse_notes(s):
-    timestamp, busses = s.splitlines()
-    busses = [(int(b), idx)
-              for idx, b in enumerate(busses.split(','))
-              if b != 'x']
-    return int(timestamp), busses
+    timestamp, rest = s.splitlines()
+    rest = rest.split(',')
+    busses = [int(b) for b in rest if b != 'x']
+    offsets = [idx for idx,b in enumerate(rest) if b != 'x']
+    return int(timestamp), busses, offsets
 
 def part1(s):
-    timestamp, busses = parse_notes(s)
+    timestamp, busses, _ = parse_notes(s)
     available_times = []
-    for b, _ in busses:
+    for b in busses:
         time_available = ((timestamp+b-1) // b) * b
         available_times.append((time_available, b))
     best_time, best_bus = min(available_times)
@@ -20,19 +19,8 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    _, busses = parse_notes(s)
-    time = 1
-    cadence = 1
-    for b, offset in busses:
-        tries = 0
-        while (time+offset) % b != 0:
-            time += cadence
-            tries += 1
-            if tries == b:
-                print('There is no answer!')
-                return
-        cadence *= b // math.gcd(cadence, b)
-    answer = time
+    _, busses, offsets = parse_notes(s)
+    answer = helpers.math.offset_chinese_remainder(zip(busses, offsets))
     print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 13)
---
+++ helpers/math.py
@@ -0,0 +1,90 @@
+import math
+
+def mod_mult_inv(n, mod):
+    '''Calculates and returns the modular multiplicative inverse of n % mod
+    such that (n * n_inv) % mod == 1
+    '''
+    if mod == 1:
+        return 1
+
+    assert(math.gcd(n, mod) == 1)
+    initial_mod = mod
+    x, y = 0, 1
+    while n > 1:
+        q = n // mod
+        n, mod = mod, n % mod
+        x, y = y - q * x, x
+    if y < 0:
+        y += initial_mod
+    return y
+
+def chinese_remainder(congruencies):
+    '''Efficiently calculates and returns the smallest number which works for
+    all given congruences. For example, given the following:
+    x % 5 == 2
+    x % 7 == 3
+    x % 11 == 8
+    The smallest x which satisfies all three is 52.
+    If no solution exists, returns None.
+
+    Arguments:
+    congruencies -- The list of congruencies to use. Expected to be a list of
+    (mod, remainder) tuples.
+    '''
+    n = 1
+    rem = 0
+    for mod, r in congruencies:
+        common_denom = math.gcd(n, mod)
+        if common_denom > 1:
+            # n and mod are not coprime, check for conflicts
+            if rem % common_denom != r % common_denom:
+                # There are no solutions as the congruences conflict
+                # That is, the first congruence (sol % n == rem) and the
+                # second congruence (sol % mod == r) imply *different* answers
+                # mod common_denom!
+                return None
+            # These can be reduced by dividing out common_denom from one of the
+            # congruences
+            mod_cand = mod // common_denom
+            if math.gcd(n, mod_cand) == 1:
+                mod = mod_cand
+                r %= mod
+            else:
+                # Dividing out common_denom from mod did *not* make n and mod coprime
+                # Thus dividing common_denom from n should make them coprime
+                n //= common_denom
+                rem %= n
+        next_n = n*mod
+        mod_inv = mod_mult_inv(mod, n)
+        n_inv = mod_mult_inv(n, mod)
+        # Given two congruences: (x % mod == a) and (x % q == b)
+        # If mod and q are coprime then multiplicative inverses exist such that
+        # mod*mod_inv % q == 1 and q*q_inv % mod == 1
+        # We can construct the following
+        # y = (a*q*q_inv + b*mod*mod_inv) % (mod*q)
+        # Then y % mod == a and y % q == b. Thus y is a solution
+        # We can then merge these into a "super congruence" and merge in the next one!
+        next_rem = (rem*mod*mod_inv + r*n*n_inv) % next_n
+        n = next_n
+        rem = next_rem
+    # The final remainder is the smallest number which works for all congruencies!
+    return rem
+
+def offset_chinese_remainder(congruencies):
+    '''Efficiently calculates and returns the smallest number which works for
+    all given *offset* congruences. For example, given the following:
+    (x + 2) % 5 == 0
+    (x + 3) % 7 == 0
+    (x + 8) % 11 == 0
+    The smallest x which satisfies all three is 52.
+    If no solution exists, returns None.
+
+    Arguments:
+    congruencies -- The list of congruencies to use. Expected to be a list of
+    (mod, offset) tuples.
+    '''
+    # Convert congruencies to chinese remainder form
+    congruencies = [(mod, (mod - (offset % mod)) % mod)
+                    for mod, offset
+                    in congruencies]
+    return chinese_remainder(congruencies)
