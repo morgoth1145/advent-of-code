2be0644a750d622a56156e58d51f44ae4b3b074e
Patrick Hogg <phogg@novamoon.net>
2020-12-14 00:22:23 -0500

    2020 Day 14 Part 2
    It took me longer than I'd like to have thought through how to deal with the floating bits, but I managed to write something working on the first try. (In fact, when my initial test of it broke it was in fact me passing the wrong parameters!) I could have done this part a bit faster if I tested it properly the first time (and if I didn't accidentally discard the floating number in the part2 code by assigning it to a "quantum" variable that wasn't used), but I don't think I'd have saved much time overall. (Certainly not enough to leaderboard.)

--- 2020/Day 14/solution.py
+++ 2020/Day 14/solution.py
@@ -5,7 +5,8 @@ import helpers.input
 def parse_mask(mask):
     to_zero = mask.translate(str.maketrans('X10', '110'))
     to_one = mask.translate(str.maketrans('X10', '010'))
-    return int(to_zero, 2), int(to_one, 2)
+    floating = mask.translate(str.maketrans('X10', '100'))
+    return int(to_zero, 2), int(to_one, 2), int(floating, 2)
 
 def part1(s):
     to_zero = 0
@@ -13,7 +14,7 @@ def part1(s):
     memory = {}
     for line in s.splitlines():
         if line.startswith('mask'):
-            to_zero, to_one = parse_mask(line.split(' = ')[1])
+            to_zero, to_one, _ = parse_mask(line.split(' = ')[1])
             continue
         if line.startswith('mem'):
             m = re.fullmatch('mem\[(\d+)\] = (\d+)', line)
@@ -26,8 +27,39 @@ def part1(s):
     answer = sum(memory.values())
     print(f'The answer to part one is {answer}')
 
+def all_addresses(addr, floating):
+    candidates = [addr]
+    mask = 1
+    while floating > 0:
+        if floating & 1:
+            new_cands = []
+            for c in candidates:
+                new_cands.append(c | mask)
+                new_cands.append(c & (~mask))
+            candidates = new_cands
+        mask *= 2
+        floating //= 2
+    return candidates
+
 def part2(s):
-    pass
+    to_one = 0
+    floating = 0
+    memory = {}
+    for line in s.splitlines():
+        if line.startswith('mask'):
+            _, to_one, floating = parse_mask(line.split(' = ')[1])
+            continue
+        if line.startswith('mem'):
+            m = re.fullmatch('mem\[(\d+)\] = (\d+)', line)
+            addr = int(m.group(1))
+            val = int(m.group(2))
+            addr = addr | to_one
+            for a in all_addresses(addr, floating):
+                memory[a] = val
+            continue
+        assert(False)
+    answer = sum(memory.values())
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 14)
 
