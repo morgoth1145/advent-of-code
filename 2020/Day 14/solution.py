85ed6ac7f12309157fc4d874ab147eefbf282a7c
Patrick Hogg <phogg@novamoon.net>
2020-12-14 01:07:18 -0500

    2020 Refactor Day 14
    - Use re a bit more fully in parsing the input
    - Looking at it, a simple str.replace would work for my to_zeros/to_ones usages since only the 'X' needs to change with how I'm doing my bit masking.
    - Keeping the mask around as a string makes it a bit easier to manage in that I'm not juggling 2-3 values for the mask. It also removes the only remaining use of str.translate(str.maketrans()) that I had left, but oh well. The resulting code *is* shorter!

--- 2020/Day 14/solution.py
+++ 2020/Day 14/solution.py
@@ -2,62 +2,56 @@ import re
 
 import helpers.input
 
-def parse_mask(mask):
-    to_zero = mask.translate(str.maketrans('X10', '110'))
-    to_one = mask.translate(str.maketrans('X10', '010'))
-    floating = mask.translate(str.maketrans('X10', '100'))
-    return int(to_zero, 2), int(to_one, 2), int(floating, 2)
-
-def part1(s):
-    to_zero = 0
-    to_one = 0
-    memory = {}
-    for line in s.splitlines():
-        if line.startswith('mask'):
-            to_zero, to_one, _ = parse_mask(line.split(' = ')[1])
+def parse_program(prog):
+    for line in prog.splitlines():
+        m = re.fullmatch('mask = ([X01]+)', line)
+        if m is not None:
+            yield 'mask', m.group(1)
             continue
-        if line.startswith('mem'):
-            m = re.fullmatch('mem\[(\d+)\] = (\d+)', line)
-            addr = int(m.group(1))
-            val = int(m.group(2))
-            val = (val & to_zero) | to_one
-            memory[addr] = val
+        m = re.fullmatch('mem\[(\d+)\] = (\d+)', line)
+        if m is not None:
+            yield 'mem', int(m.group(1)), int(m.group(2))
             continue
         assert(False)
+
+def part1(s):
+    mask = ''
+    memory = {}
+    for op in parse_program(s):
+        if op[0] == 'mask':
+            mask = op[1]
+        elif op[0] == 'mem':
+            _, addr, val = op
+            # Apply zeros in mask
+            val &= int(mask.replace('X', '1'), 2)
+            # Apply ones in mask
+            val |= int(mask.replace('X', '0'), 2)
+            memory[addr] = val
     answer = sum(memory.values())
     print(f'The answer to part one is {answer}')
 
-def all_addresses(addr, floating):
+def all_addresses(addr, addr_mask):
+    # Apply ones in mask
+    addr |= int(addr_mask.replace('X', '0'), 2)
     candidates = [addr]
     mask = 1
-    while floating > 0:
-        if floating & 1:
-            new_cands = []
-            for c in candidates:
-                new_cands.append(c | mask)
-                new_cands.append(c & (~mask))
-            candidates = new_cands
-        mask *= 2
-        floating //= 2
+    for c in addr_mask[::-1]:
+        if c == 'X':
+            candidates = [cand & (~mask) for cand in candidates]
+            candidates += [cand | mask for cand in candidates]
+        mask <<= 1
     return candidates
 
 def part2(s):
-    to_one = 0
-    floating = 0
+    mask = ''
     memory = {}
-    for line in s.splitlines():
-        if line.startswith('mask'):
-            _, to_one, floating = parse_mask(line.split(' = ')[1])
-            continue
-        if line.startswith('mem'):
-            m = re.fullmatch('mem\[(\d+)\] = (\d+)', line)
-            addr = int(m.group(1))
-            val = int(m.group(2))
-            addr = addr | to_one
-            for a in all_addresses(addr, floating):
+    for op in parse_program(s):
+        if op[0] == 'mask':
+            mask = op[1]
+        elif op[0] == 'mem':
+            _, addr, val = op
+            for a in all_addresses(addr, mask):
                 memory[a] = val
-            continue
-        assert(False)
     answer = sum(memory.values())
     print(f'The answer to part two is {answer}')
 
