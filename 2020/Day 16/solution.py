13153081857857ab518bda3bcc7b567e63e3c0d6
Patrick Hogg <phogg@novamoon.net>
2020-12-16 00:18:44 -0500

    2020 Day 16 Part 2
    This was an interesting extension to the problem. I was scared that I'd have to determine the order of the ticket fields, and lo and behold we do have to do that. But as it turns out, determining the candidates for each field and doing a naive backtracking search for an order that works runs in a small enough amount of time to be feasible. I'm sure that determine_rule_order can be optimized *way* more, but it would have absolutely cost me the leaderboard to do that! (That being said, I was worried for a moment when it took a few seconds to terminate when I first ran it!)

--- 2020/Day 16/solution.py
+++ 2020/Day 16/solution.py
@@ -29,8 +29,67 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+# TODO: This can probably be done better
+def determine_rule_order(available, rule_order_candidates):
+    if 0 == len(rule_order_candidates):
+        return []
+
+    candidates = rule_order_candidates[0]
+    rest = rule_order_candidates[1:]
+
+    for name in candidates:
+        if name in available:
+            answer = determine_rule_order(available - {name}, rest)
+            if answer is not None:
+                return [name] + answer
+
 def part2(s):
-    pass
+    rules, my_ticket, nearby = s.split('\n\n')
+
+    rules = rules.splitlines()
+
+    _, my_ticket = my_ticket.splitlines()
+    my_ticket = list(map(int, my_ticket.split(',')))
+
+    nearby = [list(map(int, l.split(','))) for l in nearby.splitlines()[1:]]
+
+    parsed_rules = {}
+    valid_numbers = set()
+    for rule in rules:
+        m = re.match('(.*): (\d+)\-(\d+) or (\d+)\-(\d+)', rule)
+        name = m.group(1)
+        range_1 = range(int(m.group(2)), int(m.group(3))+1)
+        range_2 = range(int(m.group(4)), int(m.group(5))+1)
+        valid_numbers |= set(range_1)
+        valid_numbers |= set(range_2)
+        parsed_rules[name] = set(range_1) | set(range_2)
+
+    good_nearby = []
+    for ticket in nearby:
+        is_valid = True
+        for n in ticket:
+            if n not in valid_numbers:
+                is_valid = False
+                break
+        if is_valid:
+            good_nearby.append(ticket)
+
+    rule_order_candidates = []
+    for vals in zip(*good_nearby):
+        candidates = set()
+        for name, valid_nums in parsed_rules.items():
+            if all(v in valid_nums for v in vals):
+                candidates.add(name)
+        rule_order_candidates.append(candidates)
+
+    rule_order = determine_rule_order(set(parsed_rules.keys()), rule_order_candidates)
+
+    answer = 1
+    for name, val in zip(rule_order, my_ticket):
+        if name.startswith('departure'):
+            answer *= val
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 16)
 
