3d6b157501f7c8687b7b62aec15ad3cd540b6086
Patrick Hogg <phogg@novamoon.net>
2020-12-17 00:16:51 -0500

    2020 Day 17 Part 2
    Well this is a pretty simple extension. 4d instead of 3d, but my algorithm was all tuple coord based so this didn't take long to do at all. (At least I didn't goof up like in Part 1. I would have leaderboarded were it not for that...)

--- 2020/Day 17/solution.py
+++ 2020/Day 17/solution.py
@@ -37,8 +37,41 @@ def part1(s):
     answer = len(active_coords)
     print(f'The answer to part one is {answer}')
 
+def iter_neighbors2(coord):
+    x, y, z, w = coord
+    for dx in (-1, 0, 1):
+        for dy in (-1, 0, 1):
+            for dz in (-1, 0, 1):
+                for dw in (-1, 0, 1):
+                    if dx == dy == dz == dw == 0:
+                        continue
+                    yield (x+dx, y+dy, z+dz, w+dw)
+
+def do_cycle2(prev_active):
+    active_counts = collections.defaultdict(int)
+    for coord in prev_active:
+        for neighbor in iter_neighbors2(coord):
+            active_counts[neighbor] += 1
+    new_active = []
+    for coord in prev_active:
+        count = active_counts.pop(coord, 0)
+        if count in (2, 3):
+            new_active.append(coord)
+    for coord, count in active_counts.items():
+        if count == 3:
+            new_active.append(coord)
+    return new_active
+
 def part2(s):
-    pass
+    active_coords = []
+    for y, line in enumerate(s.splitlines()):
+        for x, c in enumerate(line):
+            if c == '#':
+                active_coords.append((x, y, 0, 0))
+    for _ in range(6):
+        active_coords = do_cycle2(active_coords)
+    answer = len(active_coords)
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 17)
 
