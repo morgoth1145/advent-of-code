7670f524f53cdf5f85e0016e98db9ee3ddd0ab2d
Patrick Hogg <phogg@novamoon.net>
2020-12-17 00:14:43 -0500

    2020 Day 17 Part 1
    Well I threw away the leaderboard today. I wanted to test my code quickly before submitting it due to the complexity, but I misread the test case and only ran it foro 3 iterations instead of 6. Unsurprisingly my answer differed, and I wasted a bunch of time trying to figure out why until I realized the error!

---
+++ 2020/Day 17/solution.py
@@ -0,0 +1,46 @@
+import collections
+
+import helpers.input
+
+def iter_neighbors(coord):
+    x, y, z = coord
+    for dx in (-1, 0, 1):
+        for dy in (-1, 0, 1):
+            for dz in (-1, 0, 1):
+                if dx == dy == dz == 0:
+                    continue
+                yield (x+dx, y+dy, z+dz)
+
+def do_cycle(prev_active):
+    active_counts = collections.defaultdict(int)
+    for coord in prev_active:
+        for neighbor in iter_neighbors(coord):
+            active_counts[neighbor] += 1
+    new_active = []
+    for coord in prev_active:
+        count = active_counts.pop(coord, 0)
+        if count in (2, 3):
+            new_active.append(coord)
+    for coord, count in active_counts.items():
+        if count == 3:
+            new_active.append(coord)
+    return new_active
+
+def part1(s):
+    active_coords = []
+    for y, line in enumerate(s.splitlines()):
+        for x, c in enumerate(line):
+            if c == '#':
+                active_coords.append((x, y, 0))
+    for _ in range(6):
+        active_coords = do_cycle(active_coords)
+    answer = len(active_coords)
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = helpers.input.get_input(2020, 17)
+
+part1(INPUT)
+part2(INPUT)
