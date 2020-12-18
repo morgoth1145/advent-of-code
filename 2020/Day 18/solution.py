1aabac85adb0e1addf15d003a57863c017779714
Patrick Hogg <phogg@novamoon.net>
2020-12-18 00:26:33 -0500

    2020 Day 18 Part 2
    At least this part wasn't too bad. I was able to modify my special eval code relatively quickly to invert the order of operations here and gain 302 ranks.

--- 2020/Day 18/solution.py
+++ 2020/Day 18/solution.py
@@ -42,8 +42,30 @@ def part1(s):
     answer = sum(map(special_eval, s.splitlines()))
     print(f'The answer to part one is {answer}')
 
+def advanced_special_eval(expression):
+    parts = list(tokenize(expression))
+    for idx in range(0, len(parts), 2):
+        p = parts[idx]
+        if p[0] == '(':
+            parts[idx] = advanced_special_eval(p[1:-1])
+    while '+' in parts:
+        idx = parts.index('+')
+        a = parts[idx-1]
+        b = parts[idx+1]
+        val = int(a) + int(b)
+        parts = parts[:idx-1] + [val] + parts[idx+2:]
+    while '*' in parts:
+        idx = parts.index('*')
+        a = parts[idx-1]
+        b = parts[idx+1]
+        val = int(a) * int(b)
+        parts = parts[:idx-1] + [val] + parts[idx+2:]
+    assert(len(parts) == 1)
+    return int(parts[0])
+
 def part2(s):
-    pass
+    answer = sum(map(advanced_special_eval, s.splitlines()))
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 18)
 
