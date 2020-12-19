d7fca497201bfa9a83551fe563554681bf85cfd9
Patrick Hogg <phogg@novamoon.net>
2020-12-19 12:00:20 -0500

    2020 Refactor Day 19
    - I never used letters_to_nums so that can go away
    - expand_candidates is a better name than expand_thingy
    - gen_possibilities is a better name than iter_possibilities
    - gen_bounded_possibilities is a better name than iter_bounded_possibilities
    - gen_possibilities and gen_bounded_possibilities can just return lists instead of being generators
    - It is easier to memoize gen_possibilities and gen_bounded_possibilities if I implement them with a local recursive function which is wrapped by lru_cache. That way the unhashable (and irrelevant for memoization) rules dict doesn't interfere!

--- 2020/Day 19/solution.py
+++ 2020/Day 19/solution.py
@@ -1,7 +1,8 @@
+import functools
+
 import helpers.input
 
 def parse_rules(rule_list):
-    letters_to_nums = {}
     rules = {}
     for line in rule_list.splitlines():
         num, rule = line.split(': ')
@@ -9,45 +10,38 @@ def parse_rules(rule_list):
         if rule[0] == '"':
             rule = rule[1] # Single character
             rules[num] = [rule]
-            letters_to_nums[rule] = num
             continue
         options = []
         for opt in rule.split('|'):
             options.append(tuple(map(int, opt.split())))
         rules[num] = options
-    return letters_to_nums, rules
+    return rules
 
-def expand_thingy(candidate_options):
+def expand_candidates(candidate_options):
     if len(candidate_options) == 0:
         yield ''
         return
     for o in candidate_options[0]:
-        for sub_bit in expand_thingy(candidate_options[1:]):
+        for sub_bit in expand_candidates(candidate_options[1:]):
             yield o + sub_bit
 
-def iter_possibilities(rules, start, memo={}):
-    answers = memo.get(start)
-    if answers is not None:
-        yield from answers
-        return
-    answers = []
-    for o in rules[start]:
-        if isinstance(o, str):
-            answers.append(o)
-            yield o
-            continue
-        candidate_options = []
-        for item in o:
-            if isinstance(item, str):
-                candidate_options.append([item])
-            else:
-                candidate_options.append(list(iter_possibilities(rules,
-                                                                 item,
-                                                                 memo)))
-        expanded = list(expand_thingy(candidate_options))
-        yield from expanded
-        answers.extend(expanded)
-    memo[start] = answers
+def gen_possibilities(rules, start):
+    @functools.lru_cache()
+    def impl(start):
+        answers = []
+        for o in rules[start]:
+            if isinstance(o, str):
+                answers.append(o)
+                continue
+            candidate_options = []
+            for item in o:
+                if isinstance(item, str):
+                    candidate_options.append([item])
+                else:
+                    candidate_options.append(impl(item))
+            answers.extend(expand_candidates(candidate_options))
+        return answers
+    return impl(start)
 
 def prune_unreachable(rules):
     while True:
@@ -71,58 +65,45 @@ def prune_unreachable(rules):
 
 def part1(s):
     rule_list, messages = s.split('\n\n')
-    letters_to_nums, rules = parse_rules(rule_list)
+    rules = parse_rules(rule_list)
 
-    options_for_42 = set(iter_possibilities(rules, 42))
-    options_for_31 = set(iter_possibilities(rules, 31))
+    options_for_42 = gen_possibilities(rules, 42)
+    options_for_31 = gen_possibilities(rules, 31)
     rules[42] = options_for_42
     rules[31] = options_for_31
     rules = prune_unreachable(rules)
 
-    possibilities = set(iter_possibilities(rules, 0))
+    possibilities = set(gen_possibilities(rules, 0))
     answer = 0
     for msg in messages.splitlines():
         if msg in possibilities:
             answer += 1
     print(f'The answer to part one is {answer}')
 
-def iter_bounded_possibilities(rules, start, max_length, memo={}):
-    key = (start, max_length)
-    answer = memo.get(key)
-    if answer is not None:
-        yield from answer
-        return
-    answer = []
-    if max_length <= 0:
-        return
-    for o in rules[start]:
-        if isinstance(o, str):
-            if len(o) <= max_length:
-                answer.append(o)
-                yield o
-            continue
-        candidate_options = []
-        sub_max_length = max_length
-        for idx, item in enumerate(o):
-            if isinstance(item, str):
-                candidate_options.append([item])
-                sub_max_length -= len(item)
-            else:
-                sub_options = list(iter_bounded_possibilities(rules,
-                                                              item,
-                                                              sub_max_length,
-                                                              memo))
-                sub_options = set(sub_options)
-                if '' in sub_options:
-                    sub_options.remove('')
-                if len(sub_options) > 0:
-                    sub_max_length -= min(map(len, sub_options))
-                    candidate_options.append(sub_options)
-        expanded = list(expand_thingy(candidate_options))
-        yield from expanded
-        answer.extend(expanded)
-    answer = set(answer)
-    memo[key] = answer
+def gen_bounded_possibilities(rules, start, max_length):
+    def impl(start, max_length):
+        answer = []
+        if max_length <= 0:
+            return []
+        for o in rules[start]:
+            if isinstance(o, str):
+                if len(o) <= max_length:
+                    answer.append(o)
+                continue
+            candidate_options = []
+            sub_max_length = max_length
+            for idx, item in enumerate(o):
+                if isinstance(item, str):
+                    candidate_options.append([item])
+                    sub_max_length -= len(item)
+                else:
+                    sub_options = impl(item, sub_max_length)
+                    if len(sub_options) > 0:
+                        sub_max_length -= min(map(len, sub_options))
+                        candidate_options.append(sub_options)
+            answer.extend(expand_candidates(candidate_options))
+        return answer
+    return impl(start, max_length)
 
 def preprocess_message(msg, options_for_42, options_for_31):
     length = set(map(len, options_for_42)) | set(map(len, options_for_31))
@@ -142,11 +123,12 @@ def preprocess_message(msg, options_for_42, options_for_31):
 
 def part2(s):
     rule_list, messages = s.split('\n\n')
-    letters_to_nums, rules = parse_rules(rule_list)
+    rules = parse_rules(rule_list)
     rules[8] = [[42], [42, 8]]
     rules[11] = [[42, 31], [42, 11, 31]]
-    options_for_42 = set(iter_possibilities(rules, 42))
-    options_for_31 = set(iter_possibilities(rules, 31))
+
+    options_for_42 = gen_possibilities(rules, 42)
+    options_for_31 = gen_possibilities(rules, 31)
     rules[42] = 'c'
     rules[31] = 'd'
 
@@ -159,7 +141,7 @@ def part2(s):
 
     max_length = max(map(len, messages))
 
-    possibilities = set(iter_bounded_possibilities(rules, 0, max_length))
+    possibilities = set(gen_bounded_possibilities(rules, 0, max_length))
 
     answer = 0
     for msg in messages:
