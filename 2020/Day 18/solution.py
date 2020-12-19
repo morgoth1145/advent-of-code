7d2f9bf8a22d833d972d0fc6734df1f190bbd7e5
Patrick Hogg <phogg@novamoon.net>
2020-12-18 20:37:18 -0500

    2020 Refactor Day 18 one last time
    Using a new eval_parenthesized_expression function I can encapsulate even the recursion in the parsing helpers. That way the merger doesn't need to recurse and can assume that the list is a single level. This makes the client code really, really simple.

--- 2020/Day 18/solution.py
+++ 2020/Day 18/solution.py
@@ -2,32 +2,26 @@ import helpers.input
 import helpers.parsing
 
 def special_eval(expression):
-    def merger(tree):
-        while len(tree) > 1:
-            a, op, b = tree[:3]
-            if isinstance(a, list): a = merger(a)
-            if isinstance(b, list): b = merger(b)
-            tree[:3] = [eval(f'{a} {op} {b}')]
-        return tree[0]
-    tree = helpers.parsing.get_parenthesized_expression_parse_tree(expression)
-    return merger(tree)
+    def merger(parts):
+        while len(parts) > 1:
+            a, op, b = parts[:3]
+            parts[:3] = [eval(f'{a} {op} {b}')]
+        return parts[0]
+    return helpers.parsing.eval_parenthesized_expression(expression, merger)
 
 def part1(s):
     answer = sum(map(special_eval, s.splitlines()))
     print(f'The answer to part one is {answer}')
 
 def advanced_special_eval(expression):
-    def merger(tree):
+    def merger(parts):
         for op in '+*':
-            while op in tree:
-                idx = tree.index(op)
-                a, op, b = tree[idx-1:idx+2]
-                if isinstance(a, list): a = merger(a)
-                if isinstance(b, list): b = merger(b)
-                tree[idx-1:idx+2] = [eval(f'{a} {op} {b}')]
-        return tree[0]
-    tree = helpers.parsing.get_parenthesized_expression_parse_tree(expression)
-    return merger(tree)
+            while op in parts:
+                idx = parts.index(op)
+                a, op, b = parts[idx-1:idx+2]
+                parts[idx-1:idx+2] = [eval(f'{a} {op} {b}')]
+        return parts[0]
+    return helpers.parsing.eval_parenthesized_expression(expression, merger)
 
 def part2(s):
     answer = sum(map(advanced_special_eval, s.splitlines()))
