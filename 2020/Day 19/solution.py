e6b06751ad285bf18a2fb2527612b009f4508465
Patrick Hogg <phogg@novamoon.net>
2020-12-19 01:38:57 -0500

    2020 Day 19 Part 2
    This was definitely an interesting extension. My code is definitely not prepared for arbitrary grammars, but I did at least eventually figure out how to generate bounded possibilities (so many dumb bugs there) and how to simplify the messages/rules so that recursion ran in a reasonable amount of time. (But man, this hurt!)

--- 2020/Day 19/solution.py
+++ 2020/Day 19/solution.py
@@ -49,9 +49,36 @@ def iter_possibilities(rules, start, memo={}):
         answers.extend(expanded)
     memo[start] = answers
 
+def prune_unreachable(rules):
+    while True:
+        referenced = {0}
+        for options in rules.values():
+            for o in options:
+                if isinstance(o, str):
+                    continue
+                for item in o:
+                    referenced.add(item)
+        progress = False
+        new_rules = {}
+        for num, options in rules.items():
+            if num not in referenced:
+                progress = True
+                continue
+            new_rules[num] = options
+        if not progress:
+            return rules
+        rules = new_rules
+
 def part1(s):
     rule_list, messages = s.split('\n\n')
     letters_to_nums, rules = parse_rules(rule_list)
+
+    options_for_42 = set(iter_possibilities(rules, 42))
+    options_for_31 = set(iter_possibilities(rules, 31))
+    rules[42] = options_for_42
+    rules[31] = options_for_31
+    rules = prune_unreachable(rules)
+
     possibilities = set(iter_possibilities(rules, 0))
     answer = 0
     for msg in messages.splitlines():
@@ -59,8 +86,86 @@ def part1(s):
             answer += 1
     print(f'The answer to part one is {answer}')
 
+def iter_bounded_possibilities(rules, start, max_length, memo={}):
+    key = (start, max_length)
+    answer = memo.get(key)
+    if answer is not None:
+        yield from answer
+        return
+    answer = []
+    if max_length <= 0:
+        return
+    for o in rules[start]:
+        if isinstance(o, str):
+            if len(o) <= max_length:
+                answer.append(o)
+                yield o
+            continue
+        candidate_options = []
+        sub_max_length = max_length
+        for idx, item in enumerate(o):
+            if isinstance(item, str):
+                candidate_options.append([item])
+                sub_max_length -= len(item)
+            else:
+                sub_options = list(iter_bounded_possibilities(rules,
+                                                              item,
+                                                              sub_max_length,
+                                                              memo))
+                sub_options = set(sub_options)
+                if '' in sub_options:
+                    sub_options.remove('')
+                if len(sub_options) > 0:
+                    sub_max_length -= min(map(len, sub_options))
+                    candidate_options.append(sub_options)
+        expanded = list(expand_thingy(candidate_options))
+        yield from expanded
+        answer.extend(expanded)
+    answer = set(answer)
+    memo[key] = answer
+
+def preprocess_message(msg, options_for_42, options_for_31):
+    length = set(map(len, options_for_42)) | set(map(len, options_for_31))
+    assert(len(length) == 1)
+    length = list(length)[0]
+    if len(msg) % length != 0:
+        return None
+    answer = ''
+    for idx in range(0, len(msg), length):
+        if msg[idx:idx+length] in options_for_42:
+            answer += 'c'
+        elif msg[idx:idx+length] in options_for_31:
+            answer += 'd'
+        else:
+            return None
+    return answer
+
 def part2(s):
-    pass
+    rule_list, messages = s.split('\n\n')
+    letters_to_nums, rules = parse_rules(rule_list)
+    rules[8] = [[42], [42, 8]]
+    rules[11] = [[42, 31], [42, 11, 31]]
+    options_for_42 = set(iter_possibilities(rules, 42))
+    options_for_31 = set(iter_possibilities(rules, 31))
+    rules[42] = 'c'
+    rules[31] = 'd'
+
+    rules = prune_unreachable(rules)
+
+    messages = messages.split()
+    messages = [preprocess_message(msg, options_for_42, options_for_31)
+                for msg in messages]
+    messages = [msg for msg in messages if msg is not None]
+
+    max_length = max(map(len, messages))
+
+    possibilities = set(iter_bounded_possibilities(rules, 0, max_length))
+
+    answer = 0
+    for msg in messages:
+        if msg in possibilities:
+            answer += 1
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 19)
 
