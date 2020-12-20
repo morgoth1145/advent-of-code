e5214b21555d6420f1a65e4481bfd93e6f823704
Patrick Hogg <phogg@novamoon.net>
2020-12-20 18:04:14 -0500

    2020 Refactor Day 20
    There's a lot that can be done to make this code nicer than the rough mess I wrote last night.
    
    - get_grid_borders is a better name than determine_grid_borders. Also, have it consistently return strings.
    - Use the clever trick of counting shared borders for a tile to determine if a tile is a corner or not. (I thought of this last night but didn't correctly account for how symmetries affect the borders!)
    - To assist with the above (and later on in Part 2), pull out a get_border_to_tiles_mapping which maps the canonical border representation to the set of tiles it attaches to.
    - Modify the tiling generator in two ways:
      - Have it accept the raw tiles dictionary directly and output a dictionary of x,y pairs to the exact tile orientation chosen for that location. This encapsulates the internal logic better and removes the need for a number of auxiliary maps
      - Rework the tiling algorithm to be a single pass rather than backtracking. Assuming that tile borders are either unique or shared with exactly one other tile (true for this puzzle) we can uniquely determine what the next tile is, and its orientation, based on what conditions its top and left borders must meet. They either must be unique or must be a specific border (and orientation) which is enough to choose and orient the next tile. (Technically the initial tile has 8 choices, 2 orientations for each of the 4 corners, but we'll deal with the image symmetries later.)
    - Rename make_superimage to generate_image, and simplify it with the above generate_tiling API improvements.
    - Rename test_for_monsters to count_non_monster_tiles as it is more descriptive, and have it encapsulate trying all image symmetries. (Also pull out the monster tile counting logic into its own function.)
    
    All in all the code is still pretty long, but it's much more manageable now. (At least in my opinion!)

--- 2020/Day 20/solution.py
+++ 2020/Day 20/solution.py
@@ -14,168 +14,157 @@ def parse_tiles(s):
         tiles[num] = grid
     return tiles
 
-def determine_grid_borders(grid):
-    top = grid[0]
+def get_grid_borders(grid):
+    top = ''.join(grid[0])
     right = ''.join(l[-1] for l in grid)
-    bottom = grid[-1]
+    bottom = ''.join(grid[-1])
     left = ''.join(l[0] for l in grid)
     return (top, right, bottom, left)
 
-def grid_mirrors(grid):
-    candidates = [grid]
-    candidates.append(grid[::-1])
-    candidates.append([l[::-1] for l in grid])
-    candidates.append([l[::-1] for l in grid][::-1])
-    return candidates
+def canonical_border(border):
+    # Since borders can be reversed due to symmetries, choose the 'smaller' one
+    # as the canonical one for lookups
+    return min(border, border[::-1])
 
-def grid_rotations(grid):
-    candidates = [grid]
-    last = grid
-    for _ in range(3):
-        grid = [l[:] for l in grid]
-        for x in range(len(grid)):
-            for y in range(len(grid[x])):
-                grid[x][y] = last[len(grid[x])-y-1][x]
-        last = grid
-        candidates.append(grid)
-    return candidates
-
-def all_grid_options(grid):
-    candidates = list()
-    for opt in grid_mirrors(grid):
-        candidates.extend(grid_rotations(opt))
-    output = []
-    for opt in candidates:
-        if opt not in output:
-            output.append(opt)
-    return output
-
-def generate_tiling(tile_to_orients_to_borders):
-    dim = math.isqrt(len(tile_to_orients_to_borders))
-    def impl(tiling, x, y, seen):
-        if y == dim:
-            return tiling
-        next_x = x+1
-        next_y = y
-        if next_x == dim:
-            next_x = 0
-            next_y += 1
-        for num, options in tile_to_orients_to_borders.items():
-            if num in seen:
-                continue
-            seen.add(num)
-            for idx, borders in options.items():
-                top, _, _, left = borders
-                if x > 0:
-                    neighbor_num, neighbor_orient = tiling[x-1][y]
-                    _, neighbor_right, _, _ = tile_to_orients_to_borders[neighbor_num][neighbor_orient]
-                    if neighbor_right != left:
-                        continue
-                if y > 0:
-                    neighbor_num, neighbor_orient = tiling[x][y-1]
-                    _, _, neighbor_bottom, _ = tile_to_orients_to_borders[neighbor_num][neighbor_orient]
-                    if neighbor_bottom != top:
-                        continue
-                tiling[x][y] = (num, idx)
-                answer = impl(tiling, next_x, next_y, seen)
-                if answer is not None:
-                    return answer
-            seen.remove(num)
-        tiling[x][y] = None
-        return None
-    tiling = [[None] * dim for _ in range(dim)]
-    return impl(tiling, 0, 0, set())
+def get_border_to_tiles_mapping(tiles):
+    mapping = collections.defaultdict(set)
+    for num, grid in tiles.items():
+        for border in get_grid_borders(grid):
+            mapping[canonical_border(border)].add(num)
+    return mapping
 
 def part1(s):
     tiles = parse_tiles(s)
-    tile_options = {num:all_grid_options(grid)
-                    for num, grid in tiles.items()}
-    tile_to_orients_to_borders = collections.defaultdict(dict)
-    for num, options in tile_options.items():
-        for idx, grid in enumerate(options):
-            borders = determine_grid_borders(grid)
-            tile_to_orients_to_borders[num][idx] = borders
-    tiling = generate_tiling(tile_to_orients_to_borders)
-    parts = [tiling[0][0], tiling[0][-1], tiling[-1][0], tiling[-1][-1]]
+    border_to_tiles = get_border_to_tiles_mapping(tiles)
+
     answer = 1
-    for p, _ in parts:
-        answer *= p
+    for num, grid in tiles.items():
+        shared_borders = 0
+        for border in get_grid_borders(grid):
+            if len(border_to_tiles[canonical_border(border)]) > 1:
+                shared_borders += 1
+        if shared_borders == 2:
+            # It's a corner!
+            answer *= num
     print(f'The answer to part one is {answer}')
 
-def make_superimage(tile_options, tiling):
-    output = []
-    for row in tiling:
-        grids = []
-        for num, orient in row:
-            grid = tile_options[num][orient]
-            # Chop off borders
-            grid = [l[1:-1] for l in grid[1:-1]]
-            grids.append(grid)
-        for y in range(len(grids[0][0])):
-            out_row = []
-            for idx in range(len(grids)):
-                out_row.extend(grids[idx][x][y] for x in range(len(grids[idx])))
-            output.append(''.join(out_row))
-    return output
+def generate_grid_symmetries(grid):
+    candidates = []
 
-MONSTER_PATTERN = '''                  # 
+    # Generate all rotations
+    for _ in range(4):
+        last, grid = grid, [l[:] for l in grid]
+
+        for x in range(len(last)):
+            for y in range(len(last[x])):
+                grid[x][y] = last[len(grid[x])-y-1][x]
+
+        # Append both the grid and its vertical mirror to the candidate list
+        # Other mirrorings will be generated automatically by the 4 rotations
+        candidates.append(grid)
+        candidates.append(grid[::-1])
+    return candidates
+
+def choose_tile(tiles, border_to_tiles, tiling, x, y, unused_tiles):
+    candidates = set(unused_tiles)
+    if x > 0:
+        _, left_neighbor, _, _ = get_grid_borders(tiling[(x-1, y)])
+        candidates &= border_to_tiles[canonical_border(left_neighbor)]
+    else:
+        left_neighbor = None
+    if y > 0:
+        _, _, top_neighbor, _ = get_grid_borders(tiling[(x, y-1)])
+        candidates &= border_to_tiles[canonical_border(top_neighbor)]
+    else:
+        top_neighbor = None
+    for num in candidates:
+        for orientation in generate_grid_symmetries(tiles[num]):
+            top, _, _, left = get_grid_borders(orientation)
+            if left_neighbor is not None:
+                if left_neighbor != left:
+                    # This tile's border doesn't match its neighbor!
+                    continue
+            else:
+                if len(border_to_tiles[canonical_border(left)]) > 1:
+                    # This tile should have no possible left neighbor!
+                    continue
+            if top_neighbor is not None:
+                if top_neighbor != top:
+                    # This tile's border doesn't match its neighbor!
+                    continue
+            else:
+                if len(border_to_tiles[canonical_border(top)]) > 1:
+                    # This tile should have no possible upper neighbor!
+                    continue
+            return num, orientation
+    assert(False)
+
+def generate_tiling(tiles):
+    border_to_tiles = get_border_to_tiles_mapping(tiles)
+
+    unused_tiles = set(tiles.keys())
+
+    dim = math.isqrt(len(tiles))
+    tiling = {}
+    for x in range(dim):
+        for y in range(dim):
+            num, tile = choose_tile(tiles,
+                                    border_to_tiles,
+                                    tiling,
+                                    x, y,
+                                    unused_tiles)
+            unused_tiles.remove(num)
+            tiling[(x, y)] = tile
+    return dim, tiling
+
+def generate_image(tiles):
+    dim, tiling = generate_tiling(tiles)
+
+    image = []
+    for y in range(dim):
+        row = [tiling[(x, y)] for x in range(dim)]
+        # Chop off borders
+        row = [[l[1:-1] for l in grid[1:-1]]
+               for grid in row]
+        for tile_y in range(len(row[0])):
+            image.append([c
+                          for grid in row
+                          for c in grid[tile_y]])
+    return image
+
+def get_monster_tiles(image):
+    MONSTER_PATTERN = '''                  # 
 #    ##    ##    ###
  #  #  #  #  #  #   '''
+    monster_coords = [(dx, dy)
+                      for dy, line in enumerate(MONSTER_PATTERN.splitlines())
+                      for dx, c in enumerate(line)
+                      if c == '#']
+    max_x = max(x for x,y in monster_coords)
+    max_y = max(y for x,y in monster_coords)
 
-def test_for_monsters(image):
-    monster_coords = []
-    max_x = 0
-    max_y = 0
-    for dy, line in enumerate(MONSTER_PATTERN.splitlines()):
-        for dx, c in enumerate(line):
-            if c == '#':
-                monster_coords.append((dx, dy))
-                max_x = max(dx, max_x)
-                max_y = max(dy, max_y)
-    monster_tiles = set()
-    for y in range(len(image)):
-        if y+max_y >= len(image):
-            break
-        for x in range(len(image[y])):
-            if x+max_x >= len(image[y]):
-                break
-            has_monster = True
-            for dx, dy in monster_coords:
-                if image[y+dy][x+dx] != '#':
-                    has_monster = False
-                    break
-            if has_monster:
-                for dx, dy in monster_coords:
-                    monster_tiles.add((x+dx, y+dy))
-    if len(monster_tiles) == 0:
-        return None
-    all_pounds = set()
-    for y, row in enumerate(image):
-        for x, c in enumerate(row):
-            if c == '#':
-                all_pounds.add((x, y))
-    return len(all_pounds - monster_tiles)
+    for y in range(len(image) - max_y):
+        for x in range(len(image[y]) - max_x):
+            if all(image[y+dy][x+dx] == '#'
+                   for dx,dy in monster_coords):
+                yield from ((x+dx, y+dy)
+                            for dx,dy in monster_coords)
+
+def count_non_monster_tiles(image):
+    # Try all image symmetries
+    for image in generate_grid_symmetries(image):
+        monster_tiles = set(get_monster_tiles(image))
+        if len(monster_tiles) == 0:
+            continue
+        all_pounds = {(x, y)
+                      for y, row in enumerate(image)
+                      for x, c in enumerate(row)
+                      if c == '#'}
+        return len(all_pounds - monster_tiles)
+    assert(False)
 
 def part2(s):
-    tiles = parse_tiles(s)
-    tile_options = {num:all_grid_options(grid)
-                    for num, grid in tiles.items()}
-    tile_to_orients_to_borders = collections.defaultdict(dict)
-    for num, options in tile_options.items():
-        for idx, grid in enumerate(options):
-            borders = determine_grid_borders(grid)
-            tile_to_orients_to_borders[num][idx] = borders
-    tiling = generate_tiling(tile_to_orients_to_borders)
-
-    image = make_superimage(tile_options, tiling)
-
-    image_opts = all_grid_options([list(l) for l in image])
-
-    for opt in image_opts:
-        answer = test_for_monsters(opt)
-        if answer is not None:
-            break
-    
+    answer = count_non_monster_tiles(generate_image(parse_tiles(s)))
     print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 20)
