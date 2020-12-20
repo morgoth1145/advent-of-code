16fd0df594b44b7ddd2caafb8757e180e4c58c84
Patrick Hogg <phogg@novamoon.net>
2020-12-20 01:00:51 -0500

    2020 Day 20 Part 2
    Now *this* is surprising. I jumped up 829 places and placed 47th on Part 2! Maybe my built up infrastructure from Part 1 helped me here?
    
    Really all I had to do here was two things:
    1) Join the image together from the tiles. (I already had all the tile info, which tiles and orientations to use, so this wasn't too bad. Except for some silly bugs.)
    2) Search for monsters in every orientation of the image. I already had a function to generate image orientations so this *really* was just writing the monster test function, which also wasn't all too bad.
    
    I'm still in shock. How did I jump up that many places from Part 1?

--- 2020/Day 20/solution.py
+++ 2020/Day 20/solution.py
@@ -102,8 +102,81 @@ def part1(s):
         answer *= p
     print(f'The answer to part one is {answer}')
 
+def make_superimage(tile_options, tiling):
+    output = []
+    for row in tiling:
+        grids = []
+        for num, orient in row:
+            grid = tile_options[num][orient]
+            # Chop off borders
+            grid = [l[1:-1] for l in grid[1:-1]]
+            grids.append(grid)
+        for y in range(len(grids[0][0])):
+            out_row = []
+            for idx in range(len(grids)):
+                out_row.extend(grids[idx][x][y] for x in range(len(grids[idx])))
+            output.append(''.join(out_row))
+    return output
+
+MONSTER_PATTERN = '''                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   '''
+
+def test_for_monsters(image):
+    monster_coords = []
+    max_x = 0
+    max_y = 0
+    for dy, line in enumerate(MONSTER_PATTERN.splitlines()):
+        for dx, c in enumerate(line):
+            if c == '#':
+                monster_coords.append((dx, dy))
+                max_x = max(dx, max_x)
+                max_y = max(dy, max_y)
+    monster_tiles = set()
+    for y in range(len(image)):
+        if y+max_y >= len(image):
+            break
+        for x in range(len(image[y])):
+            if x+max_x >= len(image[y]):
+                break
+            has_monster = True
+            for dx, dy in monster_coords:
+                if image[y+dy][x+dx] != '#':
+                    has_monster = False
+                    break
+            if has_monster:
+                for dx, dy in monster_coords:
+                    monster_tiles.add((x+dx, y+dy))
+    if len(monster_tiles) == 0:
+        return None
+    all_pounds = set()
+    for y, row in enumerate(image):
+        for x, c in enumerate(row):
+            if c == '#':
+                all_pounds.add((x, y))
+    return len(all_pounds - monster_tiles)
+
 def part2(s):
-    pass
+    tiles = parse_tiles(s)
+    tile_options = {num:all_grid_options(grid)
+                    for num, grid in tiles.items()}
+    tile_to_orients_to_borders = collections.defaultdict(dict)
+    for num, options in tile_options.items():
+        for idx, grid in enumerate(options):
+            borders = determine_grid_borders(grid)
+            tile_to_orients_to_borders[num][idx] = borders
+    tiling = generate_tiling(tile_to_orients_to_borders)
+
+    image = make_superimage(tile_options, tiling)
+
+    image_opts = all_grid_options([list(l) for l in image])
+
+    for opt in image_opts:
+        answer = test_for_monsters(opt)
+        if answer is not None:
+            break
+    
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 20)
 
