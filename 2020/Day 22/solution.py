7ef0a89ade5e69ad74848046505f1d4723d5c32a
Patrick Hogg <phogg@novamoon.net>
2020-12-22 00:47:51 -0500

    2020 Day 22 Part 2
    I'm actually really pissed off about this. A critical piece of the problem was *not* highlighted (namely that only n cards of the deck get copied for the sub-game) causing me to waste an insane amount of time. I should have been able to easily leaderboard, probably even top 30. But instead because of that nonsense I ranked 1168 which is absolutely abysmal for me this problem.
    
    Whatever.

--- 2020/Day 22/solution.py
+++ 2020/Day 22/solution.py
@@ -27,8 +27,40 @@ def part1(s):
     answer = score(p1 + p2)
     print(f'The answer to part one is {answer}')
 
+def play_recursive(p1, p2):
+    seen = set()
+    while len(p1) > 0 and len(p2) > 0:
+        state = (tuple(p1), tuple(p2))
+        if state in seen:
+            # Player 1 wins!
+            return p1, []
+        seen.add(state)
+        c1, c2 = p1[0], p2[0]
+        p1, p2 = p1[1:], p2[1:]
+        if len(p1) >= c1 and len(p2) >= c2:
+            win1, win2 = play_recursive(list(p1[:c1]), list(p2[:c2]))
+            if win1:
+                p1.append(c1)
+                p1.append(c2)
+            else:
+                p2.append(c2)
+                p2.append(c1)
+            continue
+        if c1 > c2:
+            p1.append(c1)
+            p1.append(c2)
+        else:
+            p2.append(c2)
+            p2.append(c1)
+    return p1, p2
+
 def part2(s):
-    pass
+    p1, p2 = s.split('\n\n')
+    p1 = list(map(int, p1.splitlines()[1:]))
+    p2 = list(map(int, p2.splitlines()[1:]))
+    p1, p2 = play_recursive(p1, p2)
+    answer = score(p1 + p2)
+    print(f'The answer to part two is {answer}')
 
 INPUT = helpers.input.get_input(2020, 22)
 
