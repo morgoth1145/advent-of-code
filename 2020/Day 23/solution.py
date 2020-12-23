e9f5fed3afbf8952a37c2f68f1c7849157889abd
Patrick Hogg <phogg@novamoon.net>
2020-12-23 01:34:00 -0500

    2020 Day 23 Part 2
    Well today was interesting. Today I learned that min(some_range) is way, *way* slower than directly using a variable for the range's lower bound. That seems bad! That singlehandedly destroyed my runtime, even after realizing that I needed a linked list and a direct lookup for nodes. Without knowing why my Python was slow I opted to instead implement the solution in C++. That solved my performance issue, but I foolishly used ideone.com instead of a real dev environment. That made tracking down errors a ridiculously huge pain!
    
    Today was definitely an interesting problem. I just wish I'd known about the min(some_range) issue beforehand because I could have so easily leaderboarded without that insane performance hit.

--- 2020/Day 23/solution.py
+++ 2020/Day 23/solution.py
@@ -24,8 +24,53 @@ def part1(s):
     answer = ''.join(map(str, order))
     print(f'The answer to part one is {answer}')
 
+class LinkedCup:
+    def __init__(self, num):
+        self.next = None
+        self.num = num
+
+def linked_move(current, lookup, max_cup):
+    picked = []
+    in_hand = set()
+    next_picked = current.next
+    for _ in range(3):
+        in_hand.add(next_picked.num)
+        picked.append(next_picked)
+        next_picked = next_picked.next
+    current.next = next_picked
+    dest = current.num-1
+    if dest < 1:
+        dest = max_cup
+    while dest in in_hand:
+        dest -= 1
+        if dest < 1:
+            dest = max_cup
+    dest_obj = lookup[dest]
+    picked[-1].next = dest_obj.next
+    dest_obj.next = picked[0]
+
 def part2(s):
-    pass
+    MAXIMUM = 1000000
+    TURNS = 10000000
+
+    seed = list(map(int, s))
+    lookup = [None] * (MAXIMUM + 1)
+    for n in range(1, MAXIMUM+1):
+        lookup[n] = LinkedCup(n)
+    for n in range(1, MAXIMUM):
+        lookup[n].next = lookup[n+1]
+    for n, nnext in zip(seed, seed[1:] + [max(seed)+1]):
+        lookup[n].next = lookup[nnext]
+    lookup[MAXIMUM].next = lookup[seed[0]]
+    current = lookup[seed[0]]
+    for i in range(TURNS):
+        linked_move(current, lookup, MAXIMUM)
+        current = current.next
+    one = lookup[1]
+    a = one.next
+    b = a.next
+    answer = a.num*b.num
+    print(f'The answer to part one is {answer}')
 
 INPUT = helpers.input.get_input(2020, 23)
 
