a8b88c68e5bdc7323818a966f914da81f02e1a8e
Patrick Hogg <phogg@novamoon.net>
2020-12-23 13:28:50 -0500

    2020 Refactor Day 23 some more
    The reddit had a good idea about using a list of ints where the entries point to the next number in the sequence. This is essentially a linked list, but with less overhead. As such it runs twice as fast!

--- 2020/Day 23/solution.py
+++ 2020/Day 23/solution.py
@@ -1,61 +1,52 @@
 import helpers.input
 
-class LinkedCup:
-    __slots__ = ('next', 'num')
-    def __init__(self, num):
-        self.next = None
-        self.num = num
+def play_cups(seed, cup_count, turns):
+    assert(sorted(seed) == list(range(1, max(seed)+1)))
 
-def play_cups(cup_order, turns):
-    max_cup = max(cup_order)
-    assert(min(cup_order) == 1)
-    assert(len(cup_order) == max_cup)
-    lookup = [None] * (max_cup + 1)
-    for n in cup_order:
-        lookup[n] = LinkedCup(n)
-    for n, nnext in zip(cup_order, cup_order[1:]):
-        lookup[n].next = lookup[nnext]
-    lookup[cup_order[-1]].next = lookup[cup_order[0]]
-    current = lookup[cup_order[0]]
-    for i in range(turns):
-        picked = []
-        next_picked = current.next
-        for _ in range(3):
-            picked.append(next_picked)
-            next_picked = next_picked.next
-        current.next = next_picked
-        dest = current.num-1
-        if dest < 1:
-            dest = max_cup
-        while dest in [p.num for p in picked]:
+    cups = [-1] + list(range(2, cup_count+1)) + [1]
+    for cup, next_cup in zip(seed, seed[1:]):
+        cups[cup] = next_cup
+    if len(seed) < cup_count:
+        cups[seed[-1]] = max(seed)+1
+        cups[-1] = seed[0]
+    else:
+        cups[seed[-1]] = seed[0]
+
+    min_cup = min(seed)
+    current = seed[0]
+    for _ in range(turns):
+        p1 = cups[current]
+        p2 = cups[p1]
+        p3 = cups[p2]
+        cups[current] = cups[p3]
+        dest = current-1
+        while True:
+            if dest < min_cup:
+                dest = cup_count
+            if dest not in [p1, p2, p3]:
+                break
             dest -= 1
-            if dest < 1:
-                dest = max_cup
-        dest_obj = lookup[dest]
-        picked[-1].next = dest_obj.next
-        dest_obj.next = picked[0]
-        current = current.next
-    return lookup[1]
+        cups[p3] = cups[dest]
+        cups[dest] = p1
+        current = cups[current]
+    return cups
 
 def part1(s):
-    cups = list(map(int, s))
-    current = play_cups(cups, 100)
+    seed = list(map(int, s))
+    cups = play_cups(seed, len(seed), 100)
     order = []
-    while True:
-        current = current.next
-        if current.num == 1:
-            break
-        order.append(current.num)
+    current = cups[1]
+    while current != 1:
+        order.append(current)
+        current = cups[current]
     answer = ''.join(map(str, order))
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    seed = list(map(int, s))
-    cups = seed + list(range(max(seed)+1, 1000001))
-    one = play_cups(cups, 10000000)
-    a = one.next
-    b = a.next
-    answer = a.num*b.num
+    cups = play_cups(list(map(int, s)), 1000000, 10000000)
+    a = cups[1]
+    b = cups[a]
+    answer = a*b
     print(f'The answer to part one is {answer}')
 
 INPUT = helpers.input.get_input(2020, 23)
