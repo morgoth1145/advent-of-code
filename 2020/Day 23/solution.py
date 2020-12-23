da7836258f9d5702f14b89ec0d963fd392e4bea9
Patrick Hogg <phogg@novamoon.net>
2020-12-23 02:27:18 -0500

    2020 Refactor Day 23
    Well if I'm going to implement the game with linked lists, I might as well use them for Part 1 as well.
    
    Also, __slots__ has a noticeable ~33% performance improvement. This is the first time I've seen, in practice, __slots__ be useful.

--- 2020/Day 23/solution.py
+++ 2020/Day 23/solution.py
@@ -1,72 +1,58 @@
 import helpers.input
 
-def move(cups):
-    picked = cups[1:4]
-    cups[1:4] = []
-    dest = cups[0]-1
-    if dest < min(cups):
-        dest = max(cups)
-    while dest in picked:
-        dest -= 1
-        if dest < min(cups):
-            dest = max(cups)
-    idx = cups.index(dest)
-    cups[:idx+1] = cups[:idx+1] + picked
-    cups = cups[1:] + cups[:1]
-    return cups
-
-def part1(s):
-    cups = list(map(int, s))
-    for _ in range(100):
-        cups = move(cups[:])
-    idx = cups.index(1)
-    order = cups[idx+1:] + cups[:idx]
-    answer = ''.join(map(str, order))
-    print(f'The answer to part one is {answer}')
-
 class LinkedCup:
+    __slots__ = ('next', 'num')
     def __init__(self, num):
         self.next = None
         self.num = num
 
-def linked_move(current, lookup, max_cup):
-    picked = []
-    in_hand = set()
-    next_picked = current.next
-    for _ in range(3):
-        in_hand.add(next_picked.num)
-        picked.append(next_picked)
-        next_picked = next_picked.next
-    current.next = next_picked
-    dest = current.num-1
-    if dest < 1:
-        dest = max_cup
-    while dest in in_hand:
-        dest -= 1
+def play_cups(cup_order, turns):
+    max_cup = max(cup_order)
+    assert(min(cup_order) == 1)
+    assert(len(cup_order) == max_cup)
+    lookup = [None] * (max_cup + 1)
+    for n in cup_order:
+        lookup[n] = LinkedCup(n)
+    for n, nnext in zip(cup_order, cup_order[1:]):
+        lookup[n].next = lookup[nnext]
+    lookup[cup_order[-1]].next = lookup[cup_order[0]]
+    current = lookup[cup_order[0]]
+    for i in range(turns):
+        picked = []
+        next_picked = current.next
+        for _ in range(3):
+            picked.append(next_picked)
+            next_picked = next_picked.next
+        current.next = next_picked
+        dest = current.num-1
         if dest < 1:
             dest = max_cup
-    dest_obj = lookup[dest]
-    picked[-1].next = dest_obj.next
-    dest_obj.next = picked[0]
+        while dest in [p.num for p in picked]:
+            dest -= 1
+            if dest < 1:
+                dest = max_cup
+        dest_obj = lookup[dest]
+        picked[-1].next = dest_obj.next
+        dest_obj.next = picked[0]
+        current = current.next
+    return lookup[1]
+
+def part1(s):
+    cups = list(map(int, s))
+    current = play_cups(cups, 100)
+    order = []
+    while True:
+        current = current.next
+        if current.num == 1:
+            break
+        order.append(current.num)
+    answer = ''.join(map(str, order))
+    print(f'The answer to part one is {answer}')
 
 def part2(s):
-    MAXIMUM = 1000000
-    TURNS = 10000000
-
     seed = list(map(int, s))
-    lookup = [None] * (MAXIMUM + 1)
-    for n in range(1, MAXIMUM+1):
-        lookup[n] = LinkedCup(n)
-    for n in range(1, MAXIMUM):
-        lookup[n].next = lookup[n+1]
-    for n, nnext in zip(seed, seed[1:] + [max(seed)+1]):
-        lookup[n].next = lookup[nnext]
-    lookup[MAXIMUM].next = lookup[seed[0]]
-    current = lookup[seed[0]]
-    for i in range(TURNS):
-        linked_move(current, lookup, MAXIMUM)
-        current = current.next
-    one = lookup[1]
+    cups = seed + list(range(max(seed)+1, 1000001))
+    one = play_cups(cups, 10000000)
     a = one.next
     b = a.next
     answer = a.num*b.num
