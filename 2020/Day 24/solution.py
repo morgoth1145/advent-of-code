faa6036d1d2d6c303d595c6bcb76ce1665994dce
Patrick Hogg <phogg@novamoon.net>
2020-12-24 00:23:14 -0500

    2020 Refactor Day 24
    There's just a bit of duplication between the two days for me to clean up. Not too much though. (The function to iterate the state is faster now though!)

--- 2020/Day 24/solution.py
+++ 2020/Day 24/solution.py
@@ -2,17 +2,6 @@ import collections
 
 import helpers.input
 
-def split_tile_moves(line):
-    output = []
-    while line:
-        if line[0] in 'sn':
-            output.append(line[:2])
-            line = line[2:]
-        else:
-            output.append(line[0])
-            line = line[1:]
-    return output
-
 def tile_neighbor(pos, move):
     x, y = pos
     if move == 'e':
@@ -28,50 +17,52 @@ def tile_neighbor(pos, move):
     if move == 'ne':
         return x+1, y+1
 
-def get_final_position(pos, moves):
-    for m in moves:
-        pos = tile_neighbor(pos, m)
-    return pos
+def get_initially_active_tiles(move_list):
+    active_tiles = set()
+    for moves in move_list.splitlines():
+        pos = (0, 0)
+        while moves:
+            if moves[0] in 'sn':
+                pos = tile_neighbor(pos, moves[:2])
+                moves = moves[2:]
+            else:
+                pos = tile_neighbor(pos, moves[0])
+                moves = moves[1:]
+        if pos in active_tiles:
+            active_tiles.remove(pos)
+        else:
+            active_tiles.add(pos)
+    return active_tiles
 
 def part1(s):
-    lines = s.splitlines()
-    tiles = {}
-    for l in lines:
-        pos = get_final_position((0,0), split_tile_moves(l))
-        tiles[pos] = not tiles.get(pos, False)
-    answer = sum(1 for v in tiles.values() if v)
+    answer = len(get_initially_active_tiles(s))
     print(f'The answer to part one is {answer}')
 
-def get_all_neighbors(pos):
-    return [tile_neighbor(pos, m)
-            for m in ('se', 'sw', 'e', 'w', 'nw', 'ne')]
+def all_neighbors(x, y):
+    return [(x+1, y),
+            (x-1, y),
+            (x-1, y-1),
+            (x, y-1),
+            (x, y+1),
+            (x+1, y+1)]
 
 def iterate(active_tiles):
     neighbor_counts = collections.defaultdict(int)
     for pos in active_tiles:
-        for n in get_all_neighbors(pos):
+        for n in all_neighbors(*pos):
             neighbor_counts[n] += 1
-    new_active = set()
-    for pos in active_tiles:
-        count = neighbor_counts.pop(pos, 0)
-        if count in (1, 2):
-            new_active.add(pos)
-    for pos, count in neighbor_counts.items():
-        if count == 2:
-            new_active.add(pos)
+    new_active = {pos
+                  for pos in active_tiles
+                  if neighbor_counts.pop(pos, 0) in (1, 2)}
+    new_active |= {pos
+                   for pos, count in neighbor_counts.items()
+                   if count == 2}
     return new_active
 
 def part2(s):
-    lines = s.splitlines()
-    tiles = {}
-    for l in lines:
-        pos = get_final_position((0,0), split_tile_moves(l))
-        tiles[pos] = not tiles.get(pos, False)
-    active_tiles = {pos for pos,v in tiles.items() if v}
-
+    active_tiles = get_initially_active_tiles(s)
     for _ in range(100):
         active_tiles = iterate(active_tiles)
-
     answer = len(active_tiles)
     print(f'The answer to part one is {answer}')
 
