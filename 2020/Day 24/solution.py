ab25f33afcfd5f194ebae96d6b92ba023b2ed65b
Patrick Hogg <phogg@novamoon.net>
2020-12-24 00:13:49 -0500

    2020 Day 24 Part 2
    Oo, hexagonal game of life? Thankfully I can use pretty much the identical strategy as I used for the previous game of life implementations, especially with my tile_neighbor helper!

--- 2020/Day 24/solution.py
+++ 2020/Day 24/solution.py
@@ -42,8 +42,38 @@ def part1(s):
     answer = sum(1 for v in tiles.values() if v)
     print(f'The answer to part one is {answer}')
 
+def get_all_neighbors(pos):
+    return [tile_neighbor(pos, m)
+            for m in ('se', 'sw', 'e', 'w', 'nw', 'ne')]
+
+def iterate(active_tiles):
+    neighbor_counts = collections.defaultdict(int)
+    for pos in active_tiles:
+        for n in get_all_neighbors(pos):
+            neighbor_counts[n] += 1
+    new_active = set()
+    for pos in active_tiles:
+        count = neighbor_counts.pop(pos, 0)
+        if count in (1, 2):
+            new_active.add(pos)
+    for pos, count in neighbor_counts.items():
+        if count == 2:
+            new_active.add(pos)
+    return new_active
+
 def part2(s):
-    pass
+    lines = s.splitlines()
+    tiles = {}
+    for l in lines:
+        pos = get_final_position((0,0), split_tile_moves(l))
+        tiles[pos] = not tiles.get(pos, False)
+    active_tiles = {pos for pos,v in tiles.items() if v}
+
+    for _ in range(100):
+        active_tiles = iterate(active_tiles)
+
+    answer = len(active_tiles)
+    print(f'The answer to part one is {answer}')
 
 INPUT = helpers.input.get_input(2020, 24)
 
