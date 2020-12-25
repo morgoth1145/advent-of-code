5a63b1b5ba1169c210211aaa98c8d5bca2baf945
Patrick Hogg <phogg@novamoon.net>
2020-12-25 12:21:42 -0500

    2020 Optimize Day 25
    I must have been tired last night. There's no reason to compute the in-progress encryption keys in the loop when I can use pow and exploit its fast exponentiation once I have the loop count (exponent) figured out. This is 2-3x faster!

--- 2020/Day 25/solution.py
+++ 2020/Day 25/solution.py
@@ -1,22 +1,22 @@
+import itertools
+
 import helpers.input
 
 def part1(s):
     a, b = map(int, s.splitlines())
 
     cracking_val = 1
-    encrypt_a = 1
-    encrypt_b = 1
-    while True:
+    mod = 20201227
+    for exp in itertools.count():
         if cracking_val == a:
-            answer = encrypt_b
+            base = b
             break
         if cracking_val == b:
-            answer = encrypt_a
+            base = a
             break
-        cracking_val = (cracking_val * 7) % 20201227
-        encrypt_a = (encrypt_a * a) % 20201227
-        encrypt_b = (encrypt_b * b) % 20201227
+        cracking_val = (cracking_val * 7) % mod
 
+    answer = pow(base, exp, mod)
     print(f'The answer to part one is {answer}')
 
 def part2(s):
