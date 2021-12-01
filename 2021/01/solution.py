dd40c51440fbce790d75c8822830a6f38bdc414a
Patrick Hogg <phogg@novamoon.net>
2021-12-01 00:01:21 -0500

    2021 Day 1 Part 1
    Nice and simple! I wonder if I'd have leaderboarded had I not hit a 504 timeout downloading the input.

---
+++ 2021/01/solution.py
@@ -0,0 +1,17 @@
+import lib.aoc
+
+def part1(s):
+    nums = list(map(int, s.split()))
+    answer = 0
+    for i in range(len(nums)-1):
+        if nums[i] < nums[i+1]:
+            answer += 1
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2021, 1)
+part1(INPUT)
+part2(INPUT)
