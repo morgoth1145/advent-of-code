15f09a73914212c479013b6dfc71106ff67a5d2e
Patrick Hogg <phogg@novamoon.net>
2021-12-01 00:03:19 -0500

    2021 Day 1 Part 2
    Oh fun, changing to a sliding window. This is such a quick and dirty hack to make it work, but it *does* work!

--- 2021/01/solution.py
+++ 2021/01/solution.py
@@ -10,7 +10,18 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    nums = list(map(int, s.split()))
+
+    windows = []
+    for i in range(len(nums)-2):
+        windows.append(nums[i]+nums[i+1]+nums[i+2])
+
+    answer = 0
+    for i in range(len(windows)-1):
+        if windows[i] < windows[i+1]:
+            answer += 1
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 1)
 part1(INPUT)
