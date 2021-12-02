b9fac93c7324f60177a7c42a17df1ff8c20e2b40
Patrick Hogg <phogg@novamoon.net>
2021-12-02 00:03:32 -0500

    2021 Day 2 Part 2
    Took me *slightly* longer than I'd like to parse the adjustments Part 2 brought to the table, but I got it right at least.

--- 2021/02/solution.py
+++ 2021/02/solution.py
@@ -17,7 +17,22 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    x, d = 0, 0
+    aim = 0
+    for line in s.split('\n'):
+        act, n = line.split()
+        n = int(n)
+        if act == 'forward':
+            x += n
+            d += aim*n
+        elif act == 'down':
+            aim += n
+        elif act == 'up':
+            aim -= n
+
+    answer = x*d
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 2)
 part1(INPUT)
