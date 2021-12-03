69b86b41841ef0458925d30f1636214b718c68d8
Patrick Hogg <phogg@novamoon.net>
2021-12-03 00:13:50 -0500

    2021 Day 3 Part 2
    I wasted more time on Part 2 than even on Part 1! Pulling out determine_common was obvious enough, but I skimmed over the rules too quickly to see the requirement to have a fallback when the counts are even. Moreover, I was passin an int default instead of a string, and other dumb mistakes! Oh well, at least it worked in the end.

--- 2021/03/solution.py
+++ 2021/03/solution.py
@@ -1,5 +1,22 @@
 import lib.aoc
 
+def determine_common(nums, bit, default=None):
+    c1 = 0
+    c0 = 0
+    for n in nums:
+        if n[bit] == '1':
+            c1 += 1
+        else:
+            c0 += 1
+
+    if c1 == c0 and default is not None:
+        return default, default
+
+    if c1 > c0:
+        return '1', '0'
+    else:
+        return '0', '1'
+
 def part1(s):
     nums = []
     for n in s.split():
@@ -11,20 +28,10 @@ def part1(s):
     epsilon = []
 
     for b in range(bits):
-        c1 = 0
-        c0 = 0
-        for n in nums:
-            if n[b] == '1':
-                c1 += 1
-            else:
-                c0 += 1
+        most, least = determine_common(nums, b)
 
-        if c1 > c0:
-            gamma.append('1')
-            epsilon.append('0')
-        else:
-            gamma.append('0')
-            epsilon.append('1')
+        gamma.append(most)
+        epsilon.append(least)
 
     gamma = int(''.join(gamma), base=2)
     epsilon = int(''.join(epsilon), base=2)
@@ -33,8 +40,37 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def filter_matching(cands, bit, target):
+    out = []
+    for n in cands:
+        if n[bit] == target:
+            out.append(n)
+    return out
+
 def part2(s):
-    pass
+    nums = []
+    for n in s.split():
+        nums.append(n)
+
+    oxy_cands = nums[:]
+    co2_cands = nums[:]
+
+    bits = len(s.split()[0])
+
+    for b in range(bits):
+        if len(oxy_cands) > 1:
+            most, least = determine_common(oxy_cands, b, '1')
+            oxy_cands = filter_matching(oxy_cands, b, most)
+        if len(co2_cands) > 1:
+            most, least = determine_common(co2_cands, b, '0')
+            co2_cands = filter_matching(co2_cands, b, least)
+
+    oxy = int(''.join(oxy_cands[0]), base=2)
+    co2 = int(''.join(co2_cands[0]), base=2)
+
+    answer = oxy*co2
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 3)
 part1(INPUT)
