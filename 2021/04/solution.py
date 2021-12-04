ca551fc1a5f473b0e96f3bc3152f9491aa203d36
Patrick Hogg <phogg@novamoon.net>
2021-12-04 00:24:40 -0500

    2021 Day 4 Part 1
    Protip: Don't be dumb when solving problems. If the problem asks for the score of the *winning* board, make sure that you actually give *that* score, and not the score of some random board...
    
    I mean, the problem itself wasn't that bad. I actually wrote a class for this, amazingly. But *wow* was that bug dumb, and wow did it cost me a ton of time. I SOLVED PART 1 MANUALLY IN NOTEPAD BOTH TO GET AN ANSWER AND TO HELP DEBUG!

---
+++ 2021/04/solution.py
@@ -0,0 +1,71 @@
+import lib.aoc
+
+class Board:
+    def __init__(self, lines):
+        rows = [list(map(int, l.strip().split()))
+                for l in lines.split('\n')]
+        self._nums = {}
+        self._seen = set()
+
+        assert(len(rows[0]) == 5)
+        assert(len(rows) == 5)
+
+        for x in range(5):
+            for y in range(5):
+                self._nums[rows[x][y]] = (x, y)
+
+    def mark(self, n):
+        coord = self._nums.get(n)
+        if coord is not None:
+            self._seen.add(coord)
+
+    @property
+    def winning(self):
+        for x in range(5):
+            if all((x, y) in self._seen
+                   for y in range(5)):
+                return True
+        for y in range(5):
+            if all((x, y) in self._seen
+                   for x in range(5)):
+                return True
+        return False
+
+    def score(self, last_n):
+        t = 0
+        for val, coord in self._nums.items():
+            if coord in self._seen:
+                continue
+            t += val
+        return t * last_n
+
+def parse(s):
+    groups = s.split('\n\n')
+    nums = list(map(int, groups[0].split(',')))
+    groups = groups[1:]
+
+    boards = list(map(Board, groups))
+
+    return nums, boards
+
+def find_winning_score(boards, nums):
+    for n in nums:
+        for b in boards:
+            b.mark(n)
+        for b in boards:
+            if b.winning:
+                return b.score(n)
+
+def part1(s):
+    nums, boards = parse(s)
+
+    answer = find_winning_score(boards, nums)
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2021, 4)
+part1(INPUT)
+part2(INPUT)
