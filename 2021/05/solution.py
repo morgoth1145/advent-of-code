d21f7a23c470218c28c85d3fb917eca986b435d0
Patrick Hogg <phogg@novamoon.net>
2021-12-05 00:22:32 -0500

    2021 Rewrite Day 5
    Now with much less special casing! Part 1 and 2 are so similar that I pulled it out into count_coord_hits with just a flag to control whether diagonals are allowed. Beyond that it's just a fixed up version of the diagonal handling, really.

--- 2021/05/solution.py
+++ 2021/05/solution.py
@@ -2,72 +2,42 @@ import collections
 
 import lib.aoc
 
-def parse(s):
-    out = []
+def count_coord_hits(s, include_diagonals):
+    coord_hits = collections.defaultdict(int)
+
     for l in s.split('\n'):
         a, b = l.split(' -> ')
-        a = list(map(int, a.split(',')))
-        b = list(map(int, b.split(',')))
-        out.append((a, b))
-    return out
+        x0,y0 = list(map(int, a.split(',')))
+        x1,y1 = list(map(int, b.split(',')))
+
+        # Any diagonals are 45 degrees so dx/dy are simple!
+        dx = 1 if x1>x0 else -1
+        if x0 == x1:
+            dx = 0
+        dy = 1 if y1>y0 else -1
+        if y0 == y1:
+            dy = 0
+
+        if dx != 0 and dy != 0 and not include_diagonals:
+            continue
+
+        x = x0
+        y = y0
+
+        coord_hits[(x,y)] += 1
+        while x != x1 or y != y1:
+            x += dx
+            y += dy
+            coord_hits[(x,y)] += 1
+
+    return sum(hits > 1 for hits in coord_hits.values())
 
 def part1(s):
-    lines = parse(s)
-
-    touched = collections.defaultdict(int)
-
-    for start, end in lines:
-        x0,y0 = start
-        x1,y1 = end
-        if x0 == x1:
-            for y in range(min(y0, y1), max(y0, y1)+1):
-                touched[(x0, y)] += 1
-        elif y0 == y1:
-            for x in range(min(x0, x1), max(x0, x1)+1):
-                touched[(x, y0)] += 1
-
-    answer = 0
-
-    for val in touched.values():
-        if val > 1:
-            answer += 1
-
+    answer = count_coord_hits(s, include_diagonals=False)
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    lines = parse(s)
-
-    touched = collections.defaultdict(int)
-
-    for start, end in lines:
-        x0,y0 = start
-        x1,y1 = end
-        # This is stupid and can be simplified, but SPEED
-        if x0 == x1:
-            for y in range(min(y0, y1), max(y0, y1)+1):
-                touched[(x0, y)] += 1
-        elif y0 == y1:
-            for x in range(min(x0, x1), max(x0, x1)+1):
-                touched[(x, y0)] += 1
-        else:
-            dx = x1-x0
-            dx = dx // abs(dx)
-            dy = y1-y0
-            dy = dy // abs(dy)
-            x = x0
-            y = y0
-
-            touched[(x,y)] += 1
-            while x != x1:
-                x += dx
-                y += dy
-                touched[(x,y)] += 1
-
-    answer = 0
-    for val in touched.values():
-        if val > 1:
-            answer += 1
-
+    answer = count_coord_hits(s, include_diagonals=True)
     print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 5)
