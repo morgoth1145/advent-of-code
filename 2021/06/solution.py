f88fe2ba5875aed8bd5111ebff2977f898fed632
Patrick Hogg <phogg@novamoon.net>
2021-12-06 12:05:37 -0500

    2021 Optimize Day 6 even more
    I woke up thinking "Huh, maybe bulk processing would help for the circular buffer", but then kept taking it further until I realized that this really is a matrix exponentiation problem. I feel silly for not realizing this sooner, this is much cleaner and faster!
    
    (And numpy can do the matrix exponentiation way faster than I can in raw Python!)

--- 2021/06/solution.py
+++ 2021/06/solution.py
@@ -1,14 +1,28 @@
+import numpy
+
 import lib.aoc
 
 def solve(s, iterations):
     pop = [0] * 9
     for t in map(int, s.split(',')):
         pop[t] += 1
-    off = 0
-    for _ in range(iterations):
-        pop[off + 7 - 9] += pop[off]
-        off = (off + 1) % 9
-    return sum(pop)
+
+    # Matrices are faster (due to fast exponentiation
+    # and doing the work down in C!)
+    m = numpy.matrix([
+        [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    ], dtype=object)
+    m = m ** iterations
+
+    return (m * numpy.matrix(pop).reshape((9,1))).sum()
 
 def part1(s):
     answer = solve(s, 80)
