8abb929b8b4c0b7abe84b814aa77eec8b683f0df
Patrick Hogg <phogg@novamoon.net>
2021-12-07 00:21:27 -0500

    2021 Rewrite Day 7
    Man, looking at properly written code I wasted *so much effort* on this problem!

--- 2021/07/solution.py
+++ 2021/07/solution.py
@@ -1,50 +1,19 @@
-import functools
-
 import lib.aoc
 
-def calc_total_fuel(positions, target):
-    return sum(abs(p-target) for p in positions)
-
 def part1(s):
-    nums = list(map(int, s.split(',')))
-
-    best = None
-    for t in range(min(nums), max(nums)+1):
-        fuel = calc_total_fuel(nums, t)
-        if best is None:
-            best = fuel
-        elif best > fuel:
-            best = fuel
-
-    answer = best
+    positions = list(map(int, s.split(',')))
+    answer = min(sum(abs(p-t) for p in positions)
+                 for t in range(min(positions), max(positions)+1))
 
     print(f'The answer to part one is {answer}')
 
-@functools.cache
-def fuel_cost(distance):
-    if distance == 0:
-        return 0
-    return distance + fuel_cost(distance-1)
-
-def calc_total_fuel_2(positions, target):
-    return sum(fuel_cost(abs(p-target)) for p in positions)
+def tri_num(n):
+    return n*(n+1)//2
 
 def part2(s):
-    nums = list(map(int, s.split(',')))
-
-    # Warm cache
-    for d in range(0, max(nums)-min(nums), 10):
-        _ = fuel_cost(d)
-
-    best = None
-    for t in range(min(nums), max(nums)+1):
-        fuel = calc_total_fuel_2(nums, t)
-        if best is None:
-            best = fuel
-        elif best > fuel:
-            best = fuel
-
-    answer = best
+    positions = list(map(int, s.split(',')))
+    answer = min(sum(tri_num(abs(p-t)) for p in positions)
+                 for t in range(min(positions), max(positions)+1))
 
     print(f'The answer to part two is {answer}')
 
