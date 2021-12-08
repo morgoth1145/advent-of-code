220ebe492433dfbf672ae8d5352c33d75f0b9f09
Patrick Hogg <phogg@novamoon.net>
2021-12-08 00:39:41 -0500

    2021 Day 8 Part 2
    This makes my bungling of Part 1 look tame! Really this is just a pruned backtracking search, but it took me way too long to grok through how to do it! (Not to mention the right form of my valid pattern mappings!)
    
    It works well enough in the end, but holy crap did I take too long...

--- 2021/08/solution.py
+++ 2021/08/solution.py
@@ -16,8 +16,76 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+VALID_PATTERNS = [
+    'abcefg',
+    'cf',
+    'acdeg',
+    'acdfg',
+    'bcdf',
+    'abdfg',
+    'abdefg',
+    'acf',
+    'abcdefg',
+    'abcdfg'
+]
+VALID_PATTERNS = {
+    p:idx for idx,p in enumerate(VALID_PATTERNS)
+}
+
+KNOWN_LENGTHS = {
+    2: 1,
+    3: 7,
+    4: 4,
+    7: 8
+}
+
+def map_display(m, n):
+    return VALID_PATTERNS.get(''.join(sorted(m[c] for c in n)))
+
+def determine_mapping(line):
+    patterns = line[0] + line[1]
+    patterns = sorted(patterns, key=len)
+    def do_search(m, patterns, unused_chars):
+        if len(patterns) == 0:
+            yield m
+            return
+        current = patterns[0]
+        rest = patterns[1:]
+        needed_chars = [c for c in current if c not in m]
+        if needed_chars:
+            def iter_options(needed):
+                if 0 == len(needed):
+                    if map_display(m, current) is not None:
+                        yield from do_search(m, rest, unused_chars)
+                    return
+                c = needed[0]
+                rest_c = needed[1:]
+                for t in list(unused_chars):
+                    unused_chars.remove(t)
+                    m[c] = t
+                    yield from iter_options(rest_c)
+                    del m[c]
+                    unused_chars.add(t)
+            yield from iter_options(needed_chars)
+        elif map_display(m, current) is not None:
+            yield from do_search(m, rest, unused_chars)
+    return next(do_search({}, patterns, set('abcdefg')))
+
+def figure_number(mapping, ending):
+    out = 0
+    for n in ending:
+        out *= 10
+        out += VALID_PATTERNS[''.join(sorted(mapping[c] for c in n))]
+    return out
+
 def part2(s):
-    pass
+    answer = 0
+    for line in parse(s):
+        m = determine_mapping(line)
+        n = figure_number(m, line[1])
+        answer += n
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 8)
 part1(INPUT)
