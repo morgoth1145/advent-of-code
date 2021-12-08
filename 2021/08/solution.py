22dc55b7ee4c52a0728ea1f4fae1ab22f53e7041
Patrick Hogg <phogg@novamoon.net>
2021-12-08 01:03:36 -0500

    2021 Refactor Day 8
    Take out some of the cruft (some better list comprehension, use of sum and map, etc really makes this cleaner. Also avoiding premature sorting is wise...)

--- 2021/08/solution.py
+++ 2021/08/solution.py
@@ -2,56 +2,42 @@ import lib.aoc
 
 def parse(s):
     for line in s.splitlines():
-        start, end = line.split(' | ')
-        start = [''.join(sorted(set(i))) for i in start.split()]
-        end = [''.join(sorted(set(o))) for o in end.split()]
-        yield start, end
+        inputs, outputs = line.split(' | ')
+        yield inputs.split(), outputs.split()
 
 def part1(s):
-    answer = 0
-    for _, outputs in parse(s):
-        for pattern in outputs:
-            if len(pattern) in (2, 3, 4, 7):
-                answer += 1
+    answer = sum(len(pattern) in (2, 3, 4, 7)
+                 for _, outputs in parse(s)
+                 for pattern in outputs)
 
     print(f'The answer to part one is {answer}')
 
-VALID_PATTERNS = [
-    'abcefg',
-    'cf',
-    'acdeg',
-    'acdfg',
-    'bcdf',
-    'abdfg',
-    'abdefg',
-    'acf',
-    'abcdefg',
-    'abcdfg'
-]
 VALID_PATTERNS = {
-    p:idx for idx,p in enumerate(VALID_PATTERNS)
-}
-
-KNOWN_LENGTHS = {
-    2: 1,
-    3: 7,
-    4: 4,
-    7: 8
+    'abcefg':0,
+    'cf':1,
+    'acdeg':2,
+    'acdfg':3,
+    'bcdf':4,
+    'abdfg':5,
+    'abdefg':6,
+    'acf':7,
+    'abcdefg':8,
+    'abcdfg':9
 }
 
 def map_display(m, n):
-    return VALID_PATTERNS.get(''.join(sorted(m[c] for c in n)))
+    new_n = ''.join(sorted(m[c] for c in n))
+    return VALID_PATTERNS.get(new_n)
 
-def determine_mapping(line):
-    patterns = line[0] + line[1]
-    patterns = sorted(patterns, key=len)
+def find_output_number(line):
+    patterns = sorted(line[0] + line[1], key=len)
     def do_search(m, patterns, unused_chars):
         if len(patterns) == 0:
             yield m
             return
         current = patterns[0]
         rest = patterns[1:]
-        needed_chars = [c for c in current if c not in m]
+        needed_chars = sorted(c for c in current if c not in m)
         if needed_chars:
             def iter_options(needed):
                 if 0 == len(needed):
@@ -69,21 +55,16 @@ def determine_mapping(line):
             yield from iter_options(needed_chars)
         elif map_display(m, current) is not None:
             yield from do_search(m, rest, unused_chars)
-    return next(do_search({}, patterns, set('abcdefg')))
 
-def figure_number(mapping, ending):
+    mapping = next(do_search({}, patterns, set('abcdefg')))
+
     out = 0
-    for n in ending:
-        out *= 10
-        out += VALID_PATTERNS[''.join(sorted(mapping[c] for c in n))]
+    for n in line[1]:
+        out = out * 10 + map_display(mapping, n)
     return out
 
 def part2(s):
-    answer = 0
-    for line in parse(s):
-        m = determine_mapping(line)
-        n = figure_number(m, line[1])
-        answer += n
+    answer = sum(map(find_output_number, parse(s)))
 
     print(f'The answer to part two is {answer}')
 
