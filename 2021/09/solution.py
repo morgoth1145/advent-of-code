87fbb564c33569b2485b84ab7db7a3783fc56419
Patrick Hogg <phogg@novamoon.net>
2021-12-09 00:35:23 -0500

    2021 Day 9 Part 2
    Another day, another bungled solution. This is becoming far too routine this year and I don't like it.
    
    I first adopted my Part 1 solution and the minima search but that had issues. Thinking about it now I just needed to search for neighbor tiles that flowed down, nothing more. That likely would have gotten the right answer. Anyway, I had this wrong and submitted a bad answer because I included 9's. Then I removed 9's and submitted another bad answer.
    Then I rewrote the Part 2 solution using dicts thinking that I bungled the code. Nope, same answer, same wrong answer.
    
    Finally I saw the note that every cell is a 9 or in a basin. I checked the other implementations against that invariant and it didn't hold, and eventually wrote a flood fill until the 9 walls. *That* worked, with an atrocious rank of 2385...
    
    I *really* need to step up my game. This year is going worse than 2019, and I was learning go that year!

--- 2021/09/solution.py
+++ 2021/09/solution.py
@@ -30,7 +30,51 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    grid = parse(s)
+
+    handled = set()
+    for x,row in enumerate(grid):
+        for y,val in enumerate(row):
+            if val == 9:
+                handled.add((x,y))
+
+    remaining = {(x,y)
+                 for x,row in enumerate(grid)
+                 for y in range(len(row))} - handled
+
+    basin_sizes = []
+
+    while len(remaining):
+        basin = set()
+        c = list(remaining)[0]
+        to_handle = [c]
+        while to_handle:
+            c = to_handle[0]
+            to_handle = to_handle[1:]
+
+            if c in handled or c in basin:
+                continue
+
+            basin.add(c)
+            x,y = c
+
+            if x > 0:
+                to_handle.append((x-1,y))
+            if x < len(grid)-1:
+                to_handle.append((x+1,y))
+            if y > 0:
+                to_handle.append((x,y-1))
+            if y < len(grid[x])-1:
+                to_handle.append((x,y+1))
+
+        basin_sizes.append(len(basin))
+        handled |= basin
+        remaining -= basin
+
+    a, b, c = sorted(basin_sizes)[-3:]
+    answer = a*b*c
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 9)
 part1(INPUT)
