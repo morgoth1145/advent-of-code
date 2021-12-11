c30a018b41e958f542eb432f48017d219697fa4e
Patrick Hogg <phogg@novamoon.net>
2021-12-11 00:13:22 -0500

    2021 Day 11 Part 1
    Oof, dumb bugs galore here.
    1) I wasted some time forgetting to advance to_handle. Maybe I should get in the habit of using to_handle.pop(0) in problems like this?
    2) I misread the problem and thought that the octopuses flashed at 9, not when passing 9.
    3) For some idiotic reason even though I wrote the neighbors function I was iterating over to_handle as the neighbors. So I didn't actually increment the neighbors!
    4) I also was forgetting to reset the energies of flashed octopuses!
    
    Seriously, how many ways can someone mess up on a problem like this? I'm super glad I'd written the lib.grid library because that was super helpful in debugging, mostly the pretty print function.

---
+++ 2021/11/solution.py
@@ -0,0 +1,54 @@
+import lib.aoc
+
+def parse(s):
+    return {(x,y): val
+            for y, row in enumerate(s.splitlines())
+            for x, val in enumerate(map(int, row))}
+
+def neighbors(c):
+    x, y = c
+    for nx in (x-1, x, x+1):
+        for ny in (y-1, y, y+1):
+            if nx == x and ny == y:
+                continue
+            yield nx, ny
+
+def iterate(grid):
+    flashed = set()
+
+    for c, val in grid.items():
+        grid[c] = val+1
+        if val+1 > 9:
+            flashed.add(c)
+
+    to_handle = list(flashed)
+    while to_handle:
+        c = to_handle[0]
+        to_handle = to_handle[1:]
+        for n in neighbors(c):
+            if n not in grid or n in flashed:
+                continue
+            grid[n] += 1
+            if grid[n] > 9:
+                flashed.add(n)
+                to_handle.append(n)
+
+    for c in flashed:
+        grid[c] = 0
+
+    return len(flashed)
+
+def part1(s):
+    grid = parse(s)
+
+    answer = sum(iterate(grid)
+                 for _ in range(100))
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2021, 11)
+part1(INPUT)
+part2(INPUT)
