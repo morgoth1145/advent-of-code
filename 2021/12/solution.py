a79ed6b1fc6592ea2e5181fd4e7d4204e629ceaf
Patrick Hogg <phogg@novamoon.net>
2021-12-12 00:17:03 -0500

    2021 Day 12 Part 2
    This is an interesting variation. Too bad I messed *this* up royally as well. I read the problem as allowing double-visits to *all* small caves...which was taking forever to run. So I ran it on the example and got a totally different path count! This led me down a giant rabbit hole until I realized that only *one* double visit is allowed, total!
    
    I really need to stop misreading the problem, it's costing me an incredible amount of time. (Plus I need to make fewer idiotic mistakes...)

--- 2021/12/solution.py
+++ 2021/12/solution.py
@@ -33,8 +33,33 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def count_paths_2(graph):
+    def impl(path, seen_small, double_visited):
+        if path[-1] == 'end':
+            yield 1
+            return
+        for target in graph[path[-1]]:
+            if target == 'start':
+                continue
+            if target.upper() != target:
+                if target in seen_small:
+                    if double_visited:
+                        continue
+                    yield from impl(path + [target], seen_small, True)
+                else:
+                    seen_small.add(target)
+                    yield from impl(path + [target], seen_small, double_visited)
+                    seen_small.remove(target)
+            else:
+                yield from impl(path + [target], seen_small, double_visited)
+    return sum(1 for _ in impl(['start'], set(), False))
+
 def part2(s):
-    pass
+    graph = parse(s)
+
+    answer = count_paths_2(graph)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 12)
 part1(INPUT)
