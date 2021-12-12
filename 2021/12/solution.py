6e3e2f44d564d6660d0476040dd81b7e38a2b2af
Patrick Hogg <phogg@novamoon.net>
2021-12-12 00:32:36 -0500

    2021 Refactor Day 12
    Now that I have time to think I can use one solve function for both parts 1 and 2. And it's pretty clean since all that changes is allowing a second visit, which I already had parameterized internally. That just is exposed to the client code now.

--- 2021/12/solution.py
+++ 2021/12/solution.py
@@ -2,63 +2,40 @@ import collections
 
 import lib.aoc
 
-def parse(s):
+def solve(s, allow_second_visit=False):
     graph = collections.defaultdict(list)
     for line in s.splitlines():
         a, b = line.split('-')
         graph[a].append(b)
         graph[b].append(a)
-    return graph
 
-def count_paths(graph):
-    def impl(path, seen_small):
-        if path[-1] == 'end':
-            yield 1
-            return
-        for target in graph[path[-1]]:
-            if target in seen_small:
+    def gen_paths(pos, seen_small, allow_second_visit):
+        for target in graph[pos]:
+            if target == 'end':
+                yield 1
                 continue
-            if target.upper() != target:
-                seen_small.add(target)
-                yield from impl(path + [target], seen_small)
-                seen_small.remove(target)
-            else:
-                yield from impl(path + [target], seen_small)
-    return sum(1 for _ in impl(['start'], {'start'}))
-
-def part1(s):
-    graph = parse(s)
-
-    answer = count_paths(graph)
-
-    print(f'The answer to part one is {answer}')
-
-def count_paths_2(graph):
-    def impl(path, seen_small, double_visited):
-        if path[-1] == 'end':
-            yield 1
-            return
-        for target in graph[path[-1]]:
             if target == 'start':
                 continue
-            if target.upper() != target:
+            if target.upper() == target:
+                # Big cave, visit as much as we want!
+                yield from gen_paths(target, seen_small, allow_second_visit)
+            else:
+                # Small cave
                 if target in seen_small:
-                    if double_visited:
-                        continue
-                    yield from impl(path + [target], seen_small, True)
+                    if allow_second_visit:
+                        yield from gen_paths(target, seen_small, False)
                 else:
                     seen_small.add(target)
-                    yield from impl(path + [target], seen_small, double_visited)
+                    yield from gen_paths(target, seen_small, allow_second_visit)
                     seen_small.remove(target)
-            else:
-                yield from impl(path + [target], seen_small, double_visited)
-    return sum(1 for _ in impl(['start'], set(), False))
+    return sum(gen_paths('start', set(), allow_second_visit))
+
+def part1(s):
+    answer = solve(s)
+    print(f'The answer to part one is {answer}')
 
 def part2(s):
-    graph = parse(s)
-
-    answer = count_paths_2(graph)
-
+    answer = solve(s, True)
     print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 12)
