f2e0c10b23df15919068dfbd2f82dd043fad086d
Patrick Hogg <phogg@novamoon.net>
2021-12-13 10:26:17 -0500

    2021 Refactor Day 12
    I was shown a cleaner solution for day 12 which gave me some ideas on how to clean up my implementation further.
    
    1) Why was I modifying the set instead of doing a set union and sending a new set to recursive calls? That saves 2 lines and is clearer!
    2) Merging the small cave and target in seen checks into one saves a recursive call site by merging the "big cave" and "small cave but we haven't seen it yet" cases into one
    
    The other solution was a bit shorter by using two other tricks:
    1) Returning the count rather than acting like a generator. I like my formulation better since it's easy to modify to yield paths, but it is slightly longer.
    2) Iterating in the recursion step rather than in the call site. This allows for fall through/elimination of some control flow keywords, but again I don't think it's too useful in general. That formulation would be nastier if generating paths.

--- 2021/12/solution.py
+++ 2021/12/solution.py
@@ -9,25 +9,19 @@ def solve(s, allow_second_visit=False):
         graph[a].append(b)
         graph[b].append(a)
 
-    def gen_paths(pos, seen_small, allow_second_visit):
+    def gen_paths(pos, seen, allow_second_visit):
         for target in graph[pos]:
             if target == 'end':
                 yield 1
                 continue
             if target == 'start':
                 continue
-            if target.upper() == target:
-                # Big cave, visit as much as we want!
-                yield from gen_paths(target, seen_small, allow_second_visit)
-            else:
-                # Small cave
-                if target in seen_small:
-                    if allow_second_visit:
-                        yield from gen_paths(target, seen_small, False)
-                else:
-                    seen_small.add(target)
-                    yield from gen_paths(target, seen_small, allow_second_visit)
-                    seen_small.remove(target)
+            if target.islower() and target in seen:
+                if allow_second_visit:
+                    yield from gen_paths(target, seen, False)
+                continue
+            yield from gen_paths(target, seen | {target}, allow_second_visit)
+
     return sum(gen_paths('start', set(), allow_second_visit))
 
 def part1(s):
