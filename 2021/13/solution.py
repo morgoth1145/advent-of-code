2768dd73c1adba30bcaa9509b55cec3834c7dbce
Patrick Hogg <phogg@novamoon.net>
2021-12-13 00:41:32 -0500

    2021 Refactor Day 13
    The original code was so bloated. This is so much better. Also the printed text looks much cleaner now.
    
    But the worst part? I typed the *same* y, reflected(y) bug while refactoring my code. WHY AM I GETTING THAT WRONG?! That's so stupid and annoying...

--- 2021/13/solution.py
+++ 2021/13/solution.py
@@ -1,18 +1,15 @@
 import lib.aoc
 import lib.grid
+import lib.special_characters as chars
 
 def parse(s):
     groups = s.split('\n\n')
 
-    pairs = []
-    for line in groups[0].splitlines():
-        pairs.append(tuple(map(int, line.split(','))))
-
+    pairs = {tuple(map(int, line.split(',')))
+             for line in groups[0].splitlines()}
     folds = []
     for line in groups[1].splitlines():
-        assert(line.startswith('fold along '))
-        rest = line[len('fold along '):]
-        axis, n = rest.split('=')
+        axis, n = line.split()[2].split('=')
         folds.append((axis, int(n)))
 
     return pairs, folds
@@ -20,37 +17,19 @@ def parse(s):
 def do_fold(dots, fold):
     axis, n = fold
 
+    def reflect(c):
+        assert(c != n)
+        return min(c, 2*n-c)
+
     if axis == 'x':
-        new_dots = set()
-        for x, y in dots:
-            if x < n:
-                new_dots.add((x, y))
-            else:
-                assert(x > n)
-                diff = x-n
-                new_x = n-diff
-                new_dots.add((new_x, y))
-        return new_dots
+        return {(reflect(x), y)
+                for x, y in dots}
     elif axis == 'y':
-        new_dots = set()
-        for x, y in dots:
-            if y < n:
-                new_dots.add((x, y))
-            else:
-                assert(y > n)
-                diff = y-n
-                new_y = n-diff
-                new_dots.add((x, new_y))
-        return new_dots
-    else:
-        assert(False)
+        return {(x, reflect(y))
+                for x, y in dots}
 
 def part1(s):
-    pairs, folds = parse(s)
-
-    dots = set()
-    for x, y in pairs:
-        dots.add((x,y))
+    dots, folds = parse(s)
 
     dots = do_fold(dots, folds[0])
 
@@ -59,18 +38,14 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pairs, folds = parse(s)
-
-    dots = set()
-    for x, y in pairs:
-        dots.add((x,y))
+    dots, folds = parse(s)
 
     for fold in folds:
         dots = do_fold(dots, fold)
 
-    dots = {c:'#' for c in dots}
+    dots = {c:chars.FULL_BLOCK for c in dots}
 
-    grid = lib.grid.FixedGrid.from_dict(dots, missing='.')
+    grid = lib.grid.FixedGrid.from_dict(dots, missing=' ')
     grid.print('')
 
     answer = input('What are the letters? ')
