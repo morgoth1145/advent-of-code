1a23df0647ef228d05e891310fae2a77d697f5d7
Patrick Hogg <phogg@novamoon.net>
2021-12-14 00:29:44 -0500

    2021 Refactor Day 14
    Part 1 and Part 2 are literally the same, just with different iteration counts. So yeah, imma merge them.

--- 2021/14/solution.py
+++ 2021/14/solution.py
@@ -3,53 +3,14 @@ import functools
 
 import lib.aoc
 
-def parse(s):
+def solve(s, iterations):
     groups = s.split('\n\n')
     s = groups[0]
 
     rules = {}
     for line in groups[1].split('\n'):
         left, right = line.split(' -> ')
-        assert(left not in rules)
         rules[left] = right
-
-    return s, rules
-
-def apply(s, rules):
-    out = []
-
-    last = None
-    for c in s:
-        if last is None:
-            out.append(c)
-            last = c
-            continue
-        pair = last + c
-        insert = rules.get(pair)
-        if insert is not None:
-            out.append(insert)
-        out.append(c)
-        last = c
-
-    return ''.join(out)
-
-def part1(s):
-    s, rules = parse(s)
-
-    for _ in range(10):
-        s = apply(s, rules)
-
-    c = collections.Counter(s)
-
-    most = c.most_common()[0][1]
-    least = c.most_common()[-1][1]
-
-    answer = most - least
-
-    print(f'The answer to part one is {answer}')
-
-def part2(s):
-    s, rules = parse(s)
 
     @functools.cache
     def figure_counts(pair, iterations):
@@ -67,13 +28,21 @@ def part2(s):
 
     c = collections.Counter()
     for i in range(len(s)-1):
-        c += figure_counts(s[i:i+2], 40)
+        c += figure_counts(s[i:i+2], iterations)
     c -= collections.Counter(s[1:-1]) # Eliminate double-counts
 
-    most = c.most_common()[0][1]
-    least = c.most_common()[-1][1]
+    most = c.most_common()[0]
+    least = c.most_common()[-1]
 
-    answer = most - least
+    return most[1] - least[1]
+
+def part1(s):
+    answer = solve(s, 10)
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    answer = solve(s, 40)
 
     print(f'The answer to part two is {answer}')
 
