07cfe2c156839bd87b317c0d4d6374ceac5bee0c
Patrick Hogg <phogg@novamoon.net>
2021-12-14 00:24:08 -0500

    2021 Day 14 Part 2
    So this *was* an optimization problem! You know what? I'm not even mad about not leaderboarding and dropping to 287. It took me a bit to realize that I could just memoize the results of the rules after N iterations but that can get the answer *quick*. The only tricky bit is not double-counting elements in the expansion, which I *did* get wrong a couple times.
    
    I'll clean this up in a bit, there's no reason why this couldn't be done for Part 1 as well.

--- 2021/14/solution.py
+++ 2021/14/solution.py
@@ -1,4 +1,5 @@
 import collections
+import functools
 
 import lib.aoc
 
@@ -48,7 +49,33 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    s, rules = parse(s)
+
+    @functools.cache
+    def figure_counts(pair, iterations):
+        if iterations == 0:
+            return collections.Counter(pair)
+
+        a, b = pair
+        insert = rules[pair]
+
+        c = collections.Counter()
+        c += figure_counts(a+insert, iterations-1)
+        c += figure_counts(insert+b, iterations-1)
+        c[insert] -= 1 # Eliminate double-counts
+        return c
+
+    c = collections.Counter()
+    for i in range(len(s)-1):
+        c += figure_counts(s[i:i+2], 40)
+    c -= collections.Counter(s[1:-1]) # Eliminate double-counts
+
+    most = c.most_common()[0][1]
+    least = c.most_common()[-1][1]
+
+    answer = most - least
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 14)
 part1(INPUT)
