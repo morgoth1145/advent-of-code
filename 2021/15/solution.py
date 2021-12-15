9aaa53ddcf8d021039c8cdfcc4593bd7cf26b2ca
Patrick Hogg <phogg@novamoon.net>
2021-12-15 00:21:29 -0500

    2021 Day 15 Part 2
    Oh, now that's an interesting twist. It took me a little bit to think through how to neatly stitch together a bigger grid, but then I realized that my grid library can convert *from* a dictionary too so I abused that!
    
    Then it was just waiting for Dijkstra's shortest path to finish running. I really should add an optional heuristic to dijkstra_length to turn it into A* to speed things up...

--- 2021/15/solution.py
+++ 2021/15/solution.py
@@ -23,7 +23,44 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+
+    d = grid.to_dict()
+
+    for xadj in range(5):
+        for yadj in range(5):
+            adj = xadj + yadj
+            if adj == 0:
+                continue
+            xshift = grid.width * xadj
+            yshift = grid.height * yadj
+
+            for c, val in grid.items():
+                x, y = c
+                newc = x + xshift, y + yshift
+                val += adj
+                while val > 9:
+                    val -= 9
+                d[newc] = val
+
+    grid = lib.grid.FixedGrid.from_dict(d)
+
+    graph = {}
+
+    for c, val in grid.items():
+        x, y = c
+        links = []
+        for n in grid.neighbors(x, y):
+            val = grid[n]
+            links.append((n, val))
+        graph[c] = links
+
+    X = grid.width-1
+    Y = grid.height-1
+
+    answer = lib.graph.dijkstra_length(graph, (0, 0), (X, Y))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 15)
 part1(INPUT)
