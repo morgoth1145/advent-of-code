07d81a0da3ef86888c18c4ae16122bc221b35901
Patrick Hogg <phogg@novamoon.net>
2021-12-17 22:46:56 -0500

    2021 Drastically Optimize Day 17
    Since I couldn't do the problem well when it came out, I guess I got super into optimizing it. It now runs fast even on ridiculously big inputs.
    
    Part 1 is simple, the maximum possible y velocity is known and computing the max height of the parabola is simple.
    
    Part 2 is much more interesting. It's possible to find the x and y solutions independently, as well as the ranges of how many steps each component takes to reach the target. This has been *heavily* optimized to reduce redundant work by jumping ahead where possible since the low and high ends of the step ranges are monotonically increasing for each solution.
    Once this is done it's possible to loop through the y solutions in reverse and keep track of how many x solutions enter and exit the step range for the y solution. Thus we can quickly count all unique solutions of starting x and y velocities.
    
    Also I used parse.parse to parse the input. I was wanting to do that when the problem came out but I bungled that too :/

--- 2021/17/solution.py
+++ 2021/17/solution.py
@@ -1,100 +1,159 @@
+import parse
+
 import lib.aoc
 
 def parse_ranges(s):
-    _, _, x, y = s.split()
-    x0, x1 = x[2:-1].split('..')
-    y0, y1 = y[2:].split('..')
+    x0, x1, y0, y1 = parse.parse('target area: x={:d}..{:d}, y={:d}..{:d}', s)
     xrange = range(int(x0), int(x1)+1)
     yrange = range(int(y0), int(y1)+1)
     return xrange, yrange
 
-def run(xv, yv, xrange, yrange):
-    x, y = 0, 0
-    maxy = 0
-    while True:
-        x, xv = x+xv, max(xv-1, 0)
-        y, yv = y+yv, yv-1
-        maxy = max(maxy, y)
-        if x in xrange and y in yrange:
-            return maxy
-        if x > xrange[-1] or y < yrange[0]:
-            return None
-
-def solutions_brute(s):
-    xrange, yrange = parse_ranges(s)
-
-    for xv in range(xrange[-1]+1):
-        for yv in range(yrange[0], abs(yrange[0])):
-            y = run(xv, yv, xrange, yrange)
-            if y is not None:
-                yield xv, yv
-
 def tri(n):
     return n * (n+1) // 2
 
-def x_vel_cands(xrange):
-    def range_checker(min_steps, max_steps):
-        def impl(steps):
-            if steps < min_steps:
-                return False
-            if max_steps is not None and steps > max_steps:
-                return False
-            return True
-        return impl
-
-    for base_xv in range(xrange[-1]+1):
-        if tri(base_xv) < xrange[0]:
-            continue
-
-        x, xv = 0, base_xv
-        min_steps, max_steps = None, None
-
-        for step in range(xv):
-            x, xv = x+xv, xv-1
-
-            if x in xrange and min_steps is None:
-                min_steps = step+1
-            elif x > xrange[-1]:
-                max_steps = step
-                break
-
-        if min_steps is not None:
-            yield base_xv, range_checker(min_steps, max_steps)
-
-def y_vel_cands(yrange):
-    for base_yv in range(yrange[0], abs(yrange[0])):
-        good_steps = []
-
-        y, yv, step = 0, base_yv, 0
-
-        while y > yrange[0]:
-            y, yv, step = y+yv, yv-1, step+1
-
-            if y in yrange:
-                good_steps.append(step)
-
-        if good_steps:
-            yield base_yv, good_steps
-
-def solutions(s):
-    xrange, yrange = parse_ranges(s)
-
-    x_cands = list(x_vel_cands(xrange))
-    y_cands = list(y_vel_cands(yrange))
-
-    for xv, x_step_check in x_cands:
-        for yv, y_steps in y_cands:
-            if any(map(x_step_check, y_steps)):
-                yield xv, yv
-
 def part1(s):
-    answer = max(tri(yv)
-                 for xv, yv in solutions(s))
+    _, yrange = parse_ranges(s)
+
+    # Any negative initial y velocity does not reach above 0
+    # Furthermore, we know that any positive initial y velocity will, after
+    # some number of steps, get back to 0 with velocity -initial_y_velocity - 1.
+    # (It follows a parabola!)
+    # The maximum initial y velocity to stay inside the target is thus
+    # abs(y0)-1, assuming that y0 is negative (which it is for this problem).
+    max_yv = abs(yrange[0])-1
+    answer = tri(max_yv)
 
     print(f'The answer to part one is {answer}')
 
+def sum_range(first, last):
+    n = abs(first - last) + 1
+    return n * (first + last) // 2
+
+def x_vel_solutions(xrange):
+    solutions = []
+
+    min_steps = 1
+    max_steps = 0
+
+    for base_xv in range(xrange[-1], 0, -1):
+        if tri(base_xv) < xrange[0]:
+            break
+
+        # min_steps is guaranteed to not pass xrange (per the above check)
+        x = sum_range(base_xv, base_xv-min_steps+1)
+        xv = base_xv - min_steps
+
+        while x < xrange[0]:
+            x, xv = x+xv, xv-1
+            min_steps += 1
+
+        if x not in xrange:
+            continue
+
+        if max_steps is None:
+            # It's unbounded!
+            solutions.append((base_xv, min_steps, max_steps))
+            continue
+
+        step = min_steps
+
+        if max_steps > step:
+            jump_steps = max_steps - step
+            step = max_steps
+            if xv <= jump_steps:
+                x += tri(xv)
+                xv = 0
+            else:
+                x += sum_range(xv, xv - jump_steps + 1)
+                xv -= jump_steps
+        else:
+            max_steps = step
+
+        while xv != 0 and x <= xrange[-1]:
+            max_steps = step
+            x, xv, step = x+xv, xv-1, step+1
+
+        if xv == 0 and x in xrange:
+            # Signal that it's unbounded
+            max_steps = None
+
+        solutions.append((base_xv, min_steps, max_steps))
+
+    return solutions
+
+def y_vel_solutions(yrange):
+    solutions = []
+
+    min_steps = 1
+    max_steps = 0
+
+    for base_yv in range(yrange[0], abs(yrange[0])):
+        y = sum_range(base_yv, base_yv-min_steps+1)
+        yv = base_yv - min_steps
+        step = min_steps
+
+        while y > yrange[-1]:
+            y, yv, step = y+yv, yv-1, step+1
+            min_steps += 1
+
+        if y < yrange[0]:
+            continue
+
+        step = min_steps
+
+        if max_steps > step:
+            jump_steps = max_steps - step
+            step = max_steps
+            y += sum_range(yv, yv - jump_steps + 1)
+            yv -= jump_steps
+        else:
+            max_steps = step
+
+        max_steps = step
+        while y >= yrange[0]:
+            max_steps = step
+            y, yv, step = y+yv, yv-1, step+1
+
+        solutions.append((base_yv, min_steps, max_steps))
+
+    return solutions
+
 def part2(s):
-    answer = len(list(solutions(s)))
+    xrange, yrange = parse_ranges(s)
+
+    # x and y solutions have ranges of steps for which they are valid
+    # The minimum and maximum bounds of these ranges are monotonicaly increasing
+    x_solutions = x_vel_solutions(xrange)
+    y_solutions = y_vel_solutions(yrange)
+
+    # If we keep track of when x solutions enter and exit the range we care
+    # about (based on the step count) then we can easily know how many solutions
+    # exist for a given y velocity (since it's just the number of matching
+    # x solutions).
+    x_add_sol_stack = [max_steps
+                       for xv, min_steps, max_steps in x_solutions
+                       if max_steps is not None]
+    x_remove_sol_stack = [min_steps
+                          for xv, min_steps, max_steps in x_solutions]
+
+    # All unbounded x solutions start as matching!
+    x_match_count = sum(1
+                        for xv, min_steps, max_steps in x_solutions
+                        if max_steps is None)
+
+    answer = 0
+
+    for yv, min_steps, max_steps in y_solutions[::-1]:
+        # Remove expired x solutions which fell out of the range
+        while x_remove_sol_stack and x_remove_sol_stack[-1] > max_steps:
+            x_remove_sol_stack.pop(-1)
+            x_match_count -= 1
+        # Add new x solutions which entered the range
+        while x_add_sol_stack and x_add_sol_stack[-1] >= min_steps:
+            x_add_sol_stack.pop(-1)
+            x_match_count += 1
+
+        answer += x_match_count
 
     print(f'The answer to part two is {answer}')
 
