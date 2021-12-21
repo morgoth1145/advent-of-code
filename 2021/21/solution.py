60237fd8553b4e3a504d1e9483060002d5103a99
Patrick Hogg <phogg@novamoon.net>
2021-12-21 00:19:56 -0500

    2021 Day 21 Part 2
    Oh now *that* is a twist I didn't see coming! It took me too long to understand what the quantum die was supposed to do.. I was thinking the problem meant it rolled 1, 2, and 3 in separate universes then continued on but I couldn't find where the rules said that. Once I realized it really meant *just* those 3 results it made more sense!
    
    Once I realized that, it became a simple recursive memoization problem. I love functools.cache!
    
    Also RANK 29 WOOOO!!! I wonder how much time I could have saved by being quicker on the uptake, but I'll take it!

--- 2021/21/solution.py
+++ 2021/21/solution.py
@@ -1,3 +1,5 @@
+import functools
+
 import lib.aoc
 
 def parse_input(s):
@@ -46,7 +48,44 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    p1, p2 = parse_input(s)
+
+    @functools.cache
+    def impl(p1, p2, s1=0, s2=0, active_player=1):
+        p1_wins = 0
+        p2_wins = 0
+        for r1 in (1, 2, 3):
+            for r2 in (1, 2, 3):
+                for r3 in (1, 2, 3):
+                    if active_player == 1:
+                        new_p1 = p1 + r1 + r2 + r3
+                        while new_p1 > 10:
+                            new_p1 -= 10
+                        new_s1 = s1 + new_p1
+                        if new_s1 >= 21:
+                            p1_wins += 1
+                        else:
+                            sub_p1_wins, sub_p2_wins = impl(new_p1, p2, new_s1, s2, 2)
+                            p1_wins += sub_p1_wins
+                            p2_wins += sub_p2_wins
+                    else:
+                        new_p2 = p2 + r1 + r2 + r3
+                        while new_p2 > 10:
+                            new_p2 -= 10
+                        new_s2 = s2 + new_p2
+                        if new_s2 >= 21:
+                            p2_wins += 1
+                        else:
+                            sub_p1_wins, sub_p2_wins = impl(p1, new_p2, s1, new_s2, 1)
+                            p1_wins += sub_p1_wins
+                            p2_wins += sub_p2_wins
+        return p1_wins, p2_wins
+
+    p1_wins, p2_wins = impl(p1, p2)
+
+    answer = max(p1_wins, p2_wins)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2021, 21)
 part1(INPUT)
