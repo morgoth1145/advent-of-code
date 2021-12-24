08f1bc618361eb8fb8e1c8e50fb0909eb3e6dd62
Patrick Hogg <phogg@novamoon.net>
2021-12-24 07:07:31 -0500

    2021 Refactor Day 24
    I duplicated everything for part 2. Now it's deduplicated. Yay.
    
    I'm going to bed now...hopefully I can sleep...

--- 2021/24/solution.py
+++ 2021/24/solution.py
@@ -1,15 +1,13 @@
 import lib.aoc
 
-def part1(s):
+def solve_not_really_mostly_analyzed_by_hand_ouch(s, optimize_fn):
     lines = s.splitlines()
     assert(len(lines) == 18 * 14)
 
-    FUNC_TEMPLATE = '''inp w
+    FUNC_PROTO = '''inp w
 mul x 0
 add x z
 mod x 26
-div z 1
-add x 14
 eql x w
 eql x 0
 mul y 0
@@ -19,146 +17,51 @@ add y 1
 mul z y
 mul y 0
 add y w
-add y 12
 mul y x
-add z y'''.splitlines()
-    SPECIAL = (4, 5, 15)
-
-    iterations = []
-    for idx in range(0, len(lines), 18):
-        iterations.append(lines[idx:idx+18])
-
-    iter_vars = []
-
-    # Verify that the function template matches
-    for idx, func in enumerate(iterations):
-        variables = [0] * 3
-        for j, line in enumerate(func):
-            if j in SPECIAL:
-                _, _, val = line.split()
-                variables[SPECIAL.index(j)] = int(val)
-                continue
-            assert(line == FUNC_TEMPLATE[j])
-        iter_vars.append(tuple(variables))
-
-    # Function
-    # A is either 26 or 1
-    # B varies and is sometimes negative
-    # C is *never* negative!
-    '''
-    if last_z % 26 + {B} != w:
-        z //= {A} # Either 26 or 1
-        z = 26 * z + w + {C}
-    else:
-        z //= {A} # Either 26 or 1
-    '''
-
-    CAND_DIGITS = range(1, 10)
-
-    digits = [0] * 14
+add z y'''
 
     stack = []
-    for idx, (A, B, C) in enumerate(iter_vars):
+    digits = [0] * 14
+
+    # The iteations boil down to this. After stupid amounts of analysis. Ouch
+    # if z % 26 + {B} != w:
+    #     z //= {A} # Either 26 or 1
+    #     z = 26 * z + w + {C}
+    # else:
+    #     z //= {A} # Either 26 or 1
+    for group_idx, idx in enumerate(range(0, len(lines), 18)):
+        group = lines[idx:idx+18]
+        C = int(group.pop(15).split()[2])
+        B = int(group.pop(5).split()[2])
+        A = int(group.pop(4).split()[2])
+
+        assert('\n'.join(group) == FUNC_PROTO)
+
         if B >= 10:
             assert(A == 1)
-            # Forced fail, z is never negative
-            stack.append((idx, C))
+            # Forced condition failure, z is never negative
+            stack.append((group_idx, C))
         else:
-            # We need to *not* fail!
             assert(A == 26)
             prev_idx, prev_C = stack.pop(-1)
-            combined = prev_C + B
-            prev_d = max(d for d in CAND_DIGITS
-                         if d + combined in CAND_DIGITS)
-            cur_d = prev_d + combined
+            offset = prev_C + B
+            prev_d = optimize_fn(d for d in range(1, 10)
+                                 if d + offset in range(1, 10))
+            cur_d = prev_d + offset
             digits[prev_idx] = prev_d
-            digits[idx] = cur_d
-
-    answer = int(''.join(map(str, digits)))
+            digits[group_idx] = cur_d
 
     assert(len(stack) == 0)
+
+    return int(''.join(map(str, digits)))
+
+def part1(s):
+    answer = solve_not_really_mostly_analyzed_by_hand_ouch(s, max)
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    lines = s.splitlines()
-    assert(len(lines) == 18 * 14)
-
-    FUNC_TEMPLATE = '''inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 14
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 12
-mul y x
-add z y'''.splitlines()
-    SPECIAL = (4, 5, 15)
-
-    iterations = []
-    for idx in range(0, len(lines), 18):
-        iterations.append(lines[idx:idx+18])
-
-    iter_vars = []
-
-    # Verify that the function template matches
-    for idx, func in enumerate(iterations):
-        variables = [0] * 3
-        for j, line in enumerate(func):
-            if j in SPECIAL:
-                _, _, val = line.split()
-                variables[SPECIAL.index(j)] = int(val)
-                continue
-            if line != FUNC_TEMPLATE[j]:
-                print(idx, j, line, FUNC_TEMPLATE[j])
-            assert(line == FUNC_TEMPLATE[j])
-        iter_vars.append(tuple(variables))
-
-    # Function
-    # A is either 26 or 1
-    # B varies and is sometimes negative
-    # C is *never* negative!
-    '''
-    if last_z % 26 + {B} != w:
-        z //= {A} # Either 26 or 1
-        z = 26 * z + w + {C}
-    else:
-        z //= {A} # Either 26 or 1
-    '''
-
-    CAND_DIGITS = range(1, 10)
-
-    digits = [0] * 14
-
-    stack = []
-    for idx, (A, B, C) in enumerate(iter_vars):
-        if B >= 10:
-            assert(A == 1)
-            # Forced fail, z is never negative
-            stack.append((idx, C))
-        else:
-            # We need to *not* fail!
-            assert(A == 26)
-            prev_idx, prev_C = stack.pop(-1)
-            combined = prev_C + B
-            prev_d = min(d for d in CAND_DIGITS
-                         if d + combined in CAND_DIGITS)
-            cur_d = prev_d + combined
-            digits[prev_idx] = prev_d
-            digits[idx] = cur_d
-
-    answer = int(''.join(map(str, digits)))
-
-    assert(len(stack) == 0)
+    answer = solve_not_really_mostly_analyzed_by_hand_ouch(s, min)
 
     print(f'The answer to part two is {answer}')
 
