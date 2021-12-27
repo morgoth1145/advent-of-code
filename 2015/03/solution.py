086b86000d6a238b97be502bb6ae19dfe0f1792c
Patrick Hogg <phogg@novamoon.net>
2021-12-27 17:22:58 -0500

    2015 Day 3 Part 2
    Ah, we need to flip/flop between two Santas! Just a small twist.
    
    Time: 0:02:52.125809

--- 2015/03/solution.py
+++ 2015/03/solution.py
@@ -23,7 +23,30 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    seen = {(0, 0)}
+
+    santas = [(0, 0), (0, 0)]
+    current = 0
+
+    for c in s:
+        x,y = santas[current]
+        if c == '>':
+            x += 1
+        elif c == '<':
+            x -= 1
+        elif c == '^':
+            y -= 1
+        elif c == 'v':
+            y += 1
+        else:
+            assert(False)
+        seen.add((x, y))
+        santas[current] = (x,y)
+        current = (current+1)%2
+
+    answer = len(seen)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2015, 3)
 part1(INPUT)
