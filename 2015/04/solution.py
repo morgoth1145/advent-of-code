26c76827eea2634a777612e70ee7b2c2daf4f329
Patrick Hogg <phogg@novamoon.net>
2021-12-27 17:29:23 -0500

    2015 Day 4 Part 1
    To be fair, I'd just been looking at this problem due to it being referenced as computationally intensive. As such I remembered the constraints and the hashlib API.
    
    Time: 0:01:57.059666

---
+++ 2015/04/solution.py
@@ -0,0 +1,17 @@
+import hashlib
+
+import lib.aoc
+
+def part1(s):
+    answer = 0
+    while hashlib.md5((s + str(answer)).encode()).hexdigest()[:5] != '0' * 5:
+        answer += 1
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2015, 4)
+part1(INPUT)
+part2(INPUT)
