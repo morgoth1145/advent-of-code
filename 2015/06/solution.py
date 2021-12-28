d6a3ef0e8f0d8d4b7e0f430c77a8bd04e358ca16
Patrick Hogg <phogg@novamoon.net>
2021-12-27 19:32:04 -0500

    2015 Day 6 Part 2
    Yes, now let's make it slower! I'm sure that the fast way to do this is to split up the rectangles based on the intersections but who has time for that? Not I! (At least not when trying to solve as fast as possible.)
    
    Time: 0:04:32.463054

--- 2015/06/solution.py
+++ 2015/06/solution.py
@@ -31,7 +31,24 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    states = {}
+    for x in range(1000):
+        for y in range(1000):
+            states[x,y] = 0
+
+    for op, (x0, y0), (x1, y1) in parse_input(s):
+        for x in range(x0, x1+1):
+            for y in range(y0, y1+1):
+                if op == 'on':
+                    states[x,y] += 1
+                elif op == 'off':
+                    states[x,y] = max(states[x,y]-1, 0)
+                else:
+                    states[x,y] += 2
+
+    answer = sum(states.values())
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2015, 6)
 part1(INPUT)
