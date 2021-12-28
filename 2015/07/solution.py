296c2767dfd42274b3b4cfb444477dacfe98440b
Patrick Hogg <phogg@novamoon.net>
2021-12-27 19:55:57 -0500

    2015 Day 7 Part 1
    So a simple memoization problem of evaluating operations on wires. Not too bad.
    
    Time: 0:07:44.678637

---
+++ 2015/07/solution.py
@@ -0,0 +1,59 @@
+import functools
+
+import lib.aoc
+
+def parse_input(s):
+    wires = {}
+    for line in s.splitlines():
+        left, right = line.split(' -> ')
+        left = left.split()
+        wires[right] = left
+    return wires
+
+def evaluate_wires(wires, target):
+    @functools.cache
+    def impl(target):
+        try:
+            return int(target)
+        except:
+            pass
+
+        op = wires[target]
+        if 1 == len(op):
+            return impl(op[0])
+
+        if 2 == len(op):
+            NOT, other = op
+            assert(NOT == 'NOT')
+            return 0xFFFF ^ impl(other)
+
+        left, op, right = op
+        left = impl(left)
+        right = impl(right)
+
+        if op == 'AND':
+            return left & right
+        elif op == 'OR':
+            return left | right
+        elif op == 'LSHIFT':
+            return left << right
+        elif op == 'RSHIFT':
+            return left >> right
+        else:
+            assert(False)
+
+    return impl(target)
+
+def part1(s):
+    wires = parse_input(s)
+
+    answer = evaluate_wires(wires, 'a')
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2015, 7)
+part1(INPUT)
+part2(INPUT)
