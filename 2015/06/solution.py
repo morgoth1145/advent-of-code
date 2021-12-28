662f5b29a6058ddd4deb2a73708b438b5723f842
Patrick Hogg <phogg@novamoon.net>
2021-12-27 19:24:35 -0500

    2015 Day 6 Part 1
    Just run lots of commands on big ranges in a 1k by 1k grid. Nothing too much, but it isn't instant!
    
    Time: 0:03:33.765262

---
+++ 2015/06/solution.py
@@ -0,0 +1,38 @@
+import lib.aoc
+
+def parse_input(s):
+    for line in s.splitlines():
+        parts = line.split()
+        if parts[0] == 'turn':
+            parts = parts[1:]
+        op, start, _, end = parts
+        x0,y0 = start.split(',')
+        x1,y1 = end.split(',')
+        yield op, (int(x0), int(y0)), (int(x1), int(y1))
+
+def part1(s):
+    states = {}
+    for x in range(1000):
+        for y in range(1000):
+            states[x,y] = False
+
+    for op, (x0, y0), (x1, y1) in parse_input(s):
+        for x in range(x0, x1+1):
+            for y in range(y0, y1+1):
+                if op == 'on':
+                    states[x,y] = True
+                elif op == 'off':
+                    states[x,y] = False
+                else:
+                    states[x,y] = not states[x,y]
+
+    answer = sum(states.values())
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2015, 6)
+part1(INPUT)
+part2(INPUT)
