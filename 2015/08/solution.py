003ed235a02b3ef29687a481bc89e623f93cba94
Patrick Hogg <phogg@novamoon.net>
2021-12-27 20:45:03 -0500

    2015 Rewrite Day 8
    I just care about character count differences, I don't need the string!

--- 2015/08/solution.py
+++ 2015/08/solution.py
@@ -1,53 +1,35 @@
 import lib.aoc
 
-def decode(line):
+def decoded_savings(line):
     assert(line[0] == '"' == line[-1])
-    line = line[1:-1]
 
-    out = []
+    saved = 2
+    line = line[1:-1]
 
     idx = 0
     while idx < len(line):
-        c = line[idx]
+        if line[idx] == '\\':
+            if line[idx+1] == 'x':
+                idx += 4
+                saved += 3
+                continue
+            else:
+                idx += 2
+                saved += 1
+                continue
+
         idx += 1
 
-        if c != '\\':
-            out.append(c)
-            continue
-
-        nc = line[idx]
-        idx += 1
-        if nc in '"\\':
-            out.append(nc)
-            continue
-
-        assert(nc == 'x')
-        code = line[idx:idx+2]
-        idx += 2
-        c = ascii(int(code, 16))
-        out.append('#') # PLACEHOLDER FOR PART 1
-
-    return ''.join(out)
+    return saved
 
 def part1(s):
-    lines = s.splitlines()
-    answer = sum(map(len, lines)) - sum(map(len, map(decode, lines)))
+    answer = sum(map(decoded_savings, s.splitlines()))
 
     print(f'The answer to part one is {answer}')
 
-def encode(line):
-    out = []
-
-    for c in line:
-        if c in '"\\':
-            out.append('\\')
-        out.append(c)
-
-    return '"' + ''.join(out) + '"'
-
 def part2(s):
-    lines = s.splitlines()
-    answer = sum(map(len, map(encode, lines))) - sum(map(len, lines))
+    answer = sum(2 + line.count('"') + line.count('\\')
+                 for line in s.splitlines())
 
     print(f'The answer to part two is {answer}')
 
