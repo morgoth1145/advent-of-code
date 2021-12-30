0b05855f2f65ff9bedbbeea29a9f771b97073f7a
Patrick Hogg <phogg@novamoon.net>
2021-12-29 20:40:37 -0500

    2015 Day 9 Part 2
    And now we want the worst! Isn't that something?
    
    Time: 0:06:24.690989

--- 2015/09/solution.py
+++ 2015/09/solution.py
@@ -43,8 +43,37 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def worst_hamiltonian_path(start, graph):
+    def impl(path, dist):
+        if len(path) == len(graph):
+            return path, dist
+
+        best = None
+
+        for neighbor, ndist in graph[path[-1]]:
+            if neighbor in path:
+                continue
+            attempt = impl(path + [neighbor], dist + ndist)
+            if best is None or best[1] < attempt[1]:
+                best = attempt
+
+        return best
+
+    return impl([start], 0)
+
 def part2(s):
-    pass
+    graph = parse_input(s)
+
+    best = None
+
+    for start in graph.keys():
+        path, dist = worst_hamiltonian_path(start, graph)
+        if best is None or best < dist:
+            best = dist
+
+    answer = best
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2015, 9)
 part1(INPUT)
