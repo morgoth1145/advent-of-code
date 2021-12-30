81e196262de0c2abde8cb9b2e4e97eee01bb529e
Patrick Hogg <phogg@novamoon.net>
2021-12-30 14:25:17 -0500

    2015 Day 17 Part 1
    Well this is pretty easy with itertools.combinations!
    
    Time: 0:01:30.771861

---
+++ 2015/17/solution.py
@@ -0,0 +1,22 @@
+import itertools
+
+import lib.aoc
+
+def part1(s):
+    nums = list(map(int, s.splitlines()))
+
+    answer = 0
+
+    for l in range(len(nums)):
+        for combo in itertools.combinations(nums, l):
+            if sum(combo) == 150:
+                answer += 1
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2015, 17)
+part1(INPUT)
+part2(INPUT)
