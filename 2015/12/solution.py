3c8ca8731292387de5594536f746501bce927b13
Patrick Hogg <phogg@novamoon.net>
2021-12-29 22:06:31 -0500

    2015 Day 12 Part 2
    Yeah, now I have to traverse the data. Not too bad though!
    
    Time: 0:03:26.718046

--- 2015/12/solution.py
+++ 2015/12/solution.py
@@ -1,3 +1,4 @@
+import json
 import parse
 
 import lib.aoc
@@ -7,8 +8,23 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def sum_not_red(data):
+    if isinstance(data, list):
+        return sum(map(sum_not_red, data))
+    if isinstance(data, int):
+        return data
+    if isinstance(data, str):
+        return 0
+    assert(isinstance(data, dict))
+    if 'red' in data or 'red' in data.values():
+        return 0
+
+    return sum(map(sum_not_red, data.values()))
+
 def part2(s):
-    pass
+    answer = sum_not_red(json.loads(s))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2015, 12)
 part1(INPUT)
