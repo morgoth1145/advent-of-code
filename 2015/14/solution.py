d4c6425d8ca85682737da3e14377981d8697e40c
Patrick Hogg <phogg@novamoon.net>
2021-12-30 10:39:48 -0500

    2015 Refactor Day 14
    Well, if I have to run the race for part 2 I might as well deduplicate parts 1 and 2!

--- 2015/14/solution.py
+++ 2015/14/solution.py
@@ -1,36 +1,22 @@
 import lib.aoc
 
-def parse_input(s):
+def run_race(s, total_time):
+    deer = []
     for line in s.splitlines():
-        deer, _, _, speed, _, _, duration, _, _, _, _, _, _, rest, _ = line.split()
+        _, _, _, speed, _, _, duration, _, _, _, _, _, _, rest, _ = line.split()
         speed = int(speed)
         duration = int(duration)
         rest = int(rest)
-        yield deer, speed, duration, rest
+        deer.append((speed, duration, rest))
 
-def calc_distance(speed, duration, rest, total_time):
-    full_cycle = duration + rest
-    cycle_count, leftover = divmod(total_time, full_cycle)
-    leftover = min(leftover, duration)
-
-    move_time = cycle_count * duration + leftover
-    return speed * move_time
-
-def part1(s):
-    answer = max(calc_distance(speed, duration, rest, 2503)
-                 for deer, speed, duration, rest in parse_input(s))
-
-    print(f'The answer to part one is {answer}')
-
-def run_scored_race(all_deer, total_time):
-    dists = [0] * len(all_deer)
-    scores = [0] * len(all_deer)
-    cycle_time = [0] * len(all_deer)
+    dists = [0] * len(deer)
+    scores = [0] * len(deer)
+    cycle_time = [0] * len(deer)
 
     for _ in range(total_time):
         best = None
         best_i = []
-        for i, (name, speed, duration, rest) in enumerate(all_deer):
+        for i, (speed, duration, rest) in enumerate(deer):
             if cycle_time[i] < duration:
                 dists[i] += speed
             cycle_time[i] = (cycle_time[i] + 1) % (duration + rest)
@@ -44,10 +30,15 @@ def run_scored_race(all_deer, total_time):
         for i in best_i:
             scores[i] += 1
 
-    return scores
+    return dists, scores
+
+def part1(s):
+    answer = max(run_race(s, 2503)[0])
+
+    print(f'The answer to part one is {answer}')
 
 def part2(s):
-    answer = max(run_scored_race(list(parse_input(s)), 2503))
+    answer = max(run_race(s, 2503)[1])
 
     print(f'The answer to part two is {answer}')
 
