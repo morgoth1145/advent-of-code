7d7d69991d5dc117304d03f3b641b7c20539b359
Patrick Hogg <phogg@novamoon.net>
2021-12-30 12:15:28 -0500

    2015 Refactor Day 15
    There was so much wasted code. I'm sure I could simplify this further with numpy, but meh.

--- 2015/15/solution.py
+++ 2015/15/solution.py
@@ -1,16 +1,5 @@
 import lib.aoc
 
-def parse_input(s):
-    for line in s.splitlines():
-        name, _, cap, _, dur, _, flav, _, texture, _, cal = line.split()
-        name = name[:-1]
-        cap = int(cap[:-1])
-        dur = int(dur[:-1])
-        flav = int(flav[:-1])
-        texture = int(texture[:-1])
-        cal = int(cal)
-        yield name, cap, dur, flav, texture, cal
-
 def get_possible_sums(target, nums):
     if nums == 1:
         return [(target,)]
@@ -22,32 +11,36 @@ def get_possible_sums(target, nums):
     return options
 
 def optimize(s, teaspoons, calorie_requirement=None):
-    capacities = []
-    durabilities = []
-    flavors = []
-    textures = []
-    calories = []
+    properties = []
+    all_calories = []
 
-    for name, cap, dur, flav, texture, cal in parse_input(s):
-        capacities.append([i * cap for i in range(teaspoons+1)])
-        durabilities.append([i * dur for i in range(teaspoons+1)])
-        flavors.append([i * flav for i in range(teaspoons+1)])
-        textures.append([i * texture for i in range(teaspoons+1)])
-        calories.append([i * cal for i in range(teaspoons+1)])
+    for line in s.splitlines():
+        _, _, cap, _, dur, _, flav, _, texture, _, cal = line.split()
+
+        properties.append((int(cap[:-1]),
+                           int(dur[:-1]),
+                           int(flav[:-1]),
+                           int(texture[:-1])))
+        all_calories.append(int(cal))
 
     best = 0
 
-    for weights in get_possible_sums(teaspoons, len(capacities)):
-        cap, dur, flavor, texture, cal = 0, 0, 0, 0, 0
-        for i, w in enumerate(weights):
-            cap += capacities[i][w]
-            dur += durabilities[i][w]
-            flavor += flavors[i][w]
-            texture += textures[i][w]
-            cal += calories[i][w]
-        if calorie_requirement is not None and cal != calorie_requirement:
+    for weights in get_possible_sums(teaspoons, len(properties)):
+        prop_sums = [0] * len(properties)
+        calories = 0
+
+        for weight, props, cals in zip(weights, properties, all_calories):
+            calories += weight * cals
+            for i in range(len(prop_sums)):
+                prop_sums[i] += weight * props[i]
+
+        if calorie_requirement is not None and calories != calorie_requirement:
             continue
-        score = max(cap, 0) * max(dur, 0) * max(flavor, 0) * max(texture, 0)
+
+        score = 1
+        for prop in prop_sums:
+            score *= max(prop, 0)
+
         best = max(score, best)
 
     return best
