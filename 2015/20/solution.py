cd3f184f2fcedb8bce32d76e816559f2a295bee4
Patrick Hogg <phogg@novamoon.net>
2021-12-30 16:54:50 -0500

    2015 Day 20 Part 2
    I'm actually not sure why gen_houses_2 took me so long to write correctly. I suspect that I was having issues due to not seeding the queue correctly? Iunno...
    
    Time: 0:27:18.641258

--- 2015/20/solution.py
+++ 2015/20/solution.py
@@ -29,8 +29,35 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def gen_houses_2():
+    queue = [(mult, 1)
+             for mult in range(1, 51)]
+
+    current = 1
+    total = 0
+
+    while True:
+        if queue[0][0] > current:
+            yield current, total
+            current += 1
+            total = 0
+
+            # Spawn a new elf
+            for mult in range(1, 51):
+                heapq.heappush(queue, (current * mult, current))
+
+        house_n, elf_n = heapq.heappop(queue)
+        total += 11 * elf_n
+
 def part2(s):
-    pass
+    target = int(s)
+
+    for house_n, presents in gen_houses_2():
+        if presents >= target:
+            answer = house_n
+            break
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2015, 20)
 part1(INPUT)
