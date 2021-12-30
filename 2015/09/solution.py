01e668acefff7d2066652c9b52848adfe351918b
Patrick Hogg <phogg@novamoon.net>
2021-12-29 20:34:41 -0500

    2015 Day 9 Part 1
    Getting the best Hamiltonian path isn't a very common problem, given that it's NP-complete! (Okay, technically this is the traveling salesman problem, but my speed-typing mind didn't come up with that!)
    
    Time: 0:05:36.625361

---
+++ 2015/09/solution.py
@@ -0,0 +1,51 @@
+import collections
+
+import lib.aoc
+
+def parse_input(s):
+    graph = collections.defaultdict(list)
+    for line in s.splitlines():
+        a, _, b, _, dist = line.split()
+        dist = int(dist)
+        graph[a].append((b, dist))
+        graph[b].append((a, dist))
+    return graph
+
+def best_hamiltonian_path(start, graph):
+    def impl(path, dist):
+        if len(path) == len(graph):
+            return path, dist
+
+        best = None
+
+        for neighbor, ndist in graph[path[-1]]:
+            if neighbor in path:
+                continue
+            attempt = impl(path + [neighbor], dist + ndist)
+            if best is None or best[1] > attempt[1]:
+                best = attempt
+
+        return best
+
+    return impl([start], 0)
+
+def part1(s):
+    graph = parse_input(s)
+
+    best = None
+
+    for start in graph.keys():
+        path, dist = best_hamiltonian_path(start, graph)
+        if best is None or best > dist:
+            best = dist
+
+    answer = best
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2015, 9)
+part1(INPUT)
+part2(INPUT)
