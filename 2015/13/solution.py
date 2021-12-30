62a4413b1007dc9903ff372b6be43e8a02d27686
Patrick Hogg <phogg@novamoon.net>
2021-12-29 23:23:14 -0500

    2015 Day 13 Part 1
    Another traveling salesman problem?
    
    Time: 0:07:11.001179

---
+++ 2015/13/solution.py
@@ -0,0 +1,55 @@
+import collections
+
+import lib.aoc
+
+def parse_input(s):
+    pairs = collections.Counter()
+
+    for line in s.splitlines():
+        a, _, change, n, _, _, _, _, _, _, b = line.split()
+        assert(b[-1] == '.')
+        b = b[:-1]
+        n = int(n)
+        if change == 'lose':
+            n = -n
+        else:
+            assert(change == 'gain')
+        pairs[min(a,b), max(a,b)] += n
+
+    graph = collections.defaultdict(list)
+
+    for (a,b), n in pairs.items():
+        graph[a].append((b, n))
+        graph[b].append((a, n))
+
+    return graph
+
+def best_path(graph):
+    def impl(start, path, cost):
+        if len(path) == len(graph) and path[-1] == start:
+            yield cost
+            return
+
+        current = start if len(path) == 0 else path[-1]
+
+        for neighbor, diff in graph[current]:
+            if neighbor in path:
+                continue
+
+            yield from impl(start, path + [neighbor], cost + diff)
+
+    return max(max(impl(start, [], 0))
+               for start in graph.keys())
+
+def part1(s):
+    graph = parse_input(s)
+    answer = best_path(graph)
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2015, 13)
+part1(INPUT)
+part2(INPUT)
