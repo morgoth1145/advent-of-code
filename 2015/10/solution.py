920ccbfeb3b85069404cdb5ab8e4ca2ebf84f705
Patrick Hogg <phogg@novamoon.net>
2021-12-29 20:57:59 -0500

    2015 Refactor Day 10
    code.deduplicate()

--- 2015/10/solution.py
+++ 2015/10/solution.py
@@ -1,44 +1,36 @@
 import lib.aoc
 
-def expand(n):
-    out = []
-
-    last = None
-    count = 0
-
-    for c in n:
-        if last != c:
-            if last is not None:
-                out.append(count)
-                out.append(last)
-            last = c
-            count = 1
-            continue
-
-        count += 1
-
-    out.append(count)
-    out.append(last)
-
-    return out
-
-def part1(s):
+def look_and_say(s, iterations):
     n = list(map(int, s))
 
-    for _ in range(40):
-        n = expand(n)
+    for _ in range(iterations):
+        out = []
 
-    answer = len(n)
+        last = None
+        count = 0
+
+        for c in n:
+            if last != c:
+                if last is not None:
+                    out += [count, last]
+                last, count = c, 1
+                continue
+
+            count += 1
+
+        out += [count, last]
+
+        n = out
+
+    return len(n)
+
+def part1(s):
+    answer = look_and_say(s, 40)
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    n = list(map(int, s))
-
-    for _ in range(50):
-        n = expand(n)
-
-    answer = len(n)
+    answer = look_and_say(s, 50)
 
     print(f'The answer to part two is {answer}')
 
