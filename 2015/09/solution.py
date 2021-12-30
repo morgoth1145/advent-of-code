c3fe121a99570ff456627b2ed38a542d944098f2
Patrick Hogg <phogg@novamoon.net>
2021-12-29 20:48:15 -0500

    2015 Refactor Day 9
    Really the two parts are the same code but with different optimization functions. Deduplication!

--- 2015/09/solution.py
+++ 2015/09/solution.py
@@ -2,76 +2,32 @@ import collections
 
 import lib.aoc
 
-def parse_input(s):
+def solve(s, optimize_fn):
     graph = collections.defaultdict(list)
     for line in s.splitlines():
         a, _, b, _, dist = line.split()
         dist = int(dist)
         graph[a].append((b, dist))
         graph[b].append((a, dist))
-    return graph
 
-def best_hamiltonian_path(start, graph):
     def impl(path, dist):
         if len(path) == len(graph):
-            return path, dist
+            return dist
 
-        best = None
+        return optimize_fn(impl(path + [neighbor], dist + ndist)
+                           for neighbor, ndist in graph[path[-1]]
+                           if neighbor not in path)
 
-        for neighbor, ndist in graph[path[-1]]:
-            if neighbor in path:
-                continue
-            attempt = impl(path + [neighbor], dist + ndist)
-            if best is None or best[1] > attempt[1]:
-                best = attempt
-
-        return best
-
-    return impl([start], 0)
+    return optimize_fn(impl([start], 0)
+                       for start in graph.keys())
 
 def part1(s):
-    graph = parse_input(s)
-
-    best = None
-
-    for start in graph.keys():
-        path, dist = best_hamiltonian_path(start, graph)
-        if best is None or best > dist:
-            best = dist
-
-    answer = best
+    answer = solve(s, min)
 
     print(f'The answer to part one is {answer}')
 
-def worst_hamiltonian_path(start, graph):
-    def impl(path, dist):
-        if len(path) == len(graph):
-            return path, dist
-
-        best = None
-
-        for neighbor, ndist in graph[path[-1]]:
-            if neighbor in path:
-                continue
-            attempt = impl(path + [neighbor], dist + ndist)
-            if best is None or best[1] < attempt[1]:
-                best = attempt
-
-        return best
-
-    return impl([start], 0)
-
 def part2(s):
-    graph = parse_input(s)
-
-    best = None
-
-    for start in graph.keys():
-        path, dist = worst_hamiltonian_path(start, graph)
-        if best is None or best < dist:
-            best = dist
-
-    answer = best
+    answer = solve(s, max)
 
     print(f'The answer to part two is {answer}')
 
