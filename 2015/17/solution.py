03cd93b295409edeb631ddbcf9a7877b925301ed
Patrick Hogg <phogg@novamoon.net>
2021-12-30 14:29:04 -0500

    2015 Day 17 Part 2
    The hardest part was actually understanding the question here. The loop is basically the same, I just break for the smallest number of containers which can satisfy the request!
    
    Time: 0:02:56.559622

--- 2015/17/solution.py
+++ 2015/17/solution.py
@@ -15,7 +15,18 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    nums = list(map(int, s.splitlines()))
+
+    answer = 0
+
+    for l in range(len(nums)):
+        for combo in itertools.combinations(nums, l):
+            if sum(combo) == 150:
+                answer += 1
+        if answer > 0:
+            break
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2015, 17)
 part1(INPUT)
