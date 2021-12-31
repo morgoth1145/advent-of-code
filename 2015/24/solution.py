16e671424099d677a70ad396d6025344a24dc8b6
Patrick Hogg <phogg@novamoon.net>
2021-12-30 20:41:00 -0500

    2015 Day 24 Part 1
    This includes a nasty assumption that any possibly valid split leaves the rest of the numbers splittable, but it works fast!
    
    Time: 0:05:11.068583

---
+++ 2015/24/solution.py
@@ -0,0 +1,34 @@
+import itertools
+
+import lib.aoc
+
+def good_groupings(packages, target_sum, take):
+    for group in itertools.combinations(packages, take):
+        if sum(group) == target_sum:
+            yield group # Just assume the rest can be split evenly
+
+def entanglement(group):
+    val = 1
+    for p in group:
+        val *= p
+    return val
+
+def part1(s):
+    packages = list(map(int, s.splitlines()))
+
+    take = 0
+    while True:
+        take += 1
+        options = list(good_groupings(packages, sum(packages)//3, take))
+        if options:
+            answer = min(map(entanglement, options))
+            break
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2015, 24)
+part1(INPUT)
+part2(INPUT)
