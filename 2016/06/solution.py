f5e07b01c84cd19114b39a7b4085afd4ddc4b8f0
Patrick Hogg <phogg@novamoon.net>
2022-01-10 15:59:43 -0500

    2016 Day 6 Part 1
    That uh...that was easy...
    
    Time: 0:01:51.520154

---
+++ 2016/06/solution.py
@@ -0,0 +1,24 @@
+import collections
+
+import lib.aoc
+
+def part1(s):
+    columns = collections.defaultdict(collections.Counter)
+
+    for line in s.splitlines():
+        for idx, c in enumerate(line):
+            columns[idx][c] += 1
+
+    answer = ''
+
+    for idx, c in sorted(columns.items()):
+        answer += c.most_common(1)[0][0]
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2016, 6)
+part1(INPUT)
+part2(INPUT)
