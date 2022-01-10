424ef6ba0fbbacd3f547855b35513c385f0ed34b
Patrick Hogg <phogg@novamoon.net>
2022-01-10 14:17:30 -0500

    2016 Refactor Day 1
    Eliminating icky code duplication. (I'm surprised I care to do this on a Day 1 problem!)

--- 2016/01/solution.py
+++ 2016/01/solution.py
@@ -1,46 +1,33 @@
 import lib.aoc
 
-def part1(s):
+def walk_directions(s):
     d = 1j
     p = 0
 
     for move in s.split(', '):
-        t = move[0]
-        dist = int(move[1:])
-        if t == 'R':
+        if move[0] == 'R':
             d *= -1j
         else:
             d *= 1j
-        p += dist * d
+
+        for _ in range(int(move[1:])):
+            p += d
+            yield p
+
+def part1(s):
+    p = list(walk_directions(s))[-1]
 
     answer = int(abs(p.real) + abs(p.imag))
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    d = 1j
-    p = 0
-
     seen = {0}
 
-    done = False
-    for move in s.split(', '):
-        t = move[0]
-        dist = int(move[1:])
-        if t == 'R':
-            d *= -1j
-        else:
-            d *= 1j
-
-        for _ in range(dist):
-            p += d
-            if p in seen:
-                done = True
-                break
-            seen.add(p)
-
-        if done:
+    for p in walk_directions(s):
+        if p in seen:
             break
+        seen.add(p)
 
     answer = int(abs(p.real) + abs(p.imag))
 
