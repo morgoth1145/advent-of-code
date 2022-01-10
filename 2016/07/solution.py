568ee938c35e1043071860bded9deec255e39bcd
Patrick Hogg <phogg@novamoon.net>
2022-01-10 16:19:24 -0500

    2016 Day 7 Part 2
    Huh, only the supernet/hypernet split was used in part 2? I was expecting to have to search within that set further!
    
    (Fun fact: I checked later and for my input only 14 "ips" fit both TLS and SSL.)
    
    Time: 0:11:09.874829

--- 2016/07/solution.py
+++ 2016/07/solution.py
@@ -1,5 +1,27 @@
 import lib.aoc
 
+def split_ip(ip):
+    supernets = []
+    hypernets = []
+
+    while True:
+        idx = ip.find('[')
+        if idx == -1:
+            assert(-1 == ip.find(']'))
+            supernets.append(ip)
+            break
+
+        end_idx = ip.find(']')
+        assert(end_idx > idx)
+
+        supernets.append(ip[:idx])
+        hyper = ip[idx+1:end_idx]
+        ip = ip[end_idx+1:]
+        assert(hyper.find('[') == -1)
+        hypernets.append(hyper)
+
+    return supernets, hypernets
+
 def has_abba(sequence):
     for a, b, c, d in zip(sequence,
                           sequence[1:],
@@ -10,35 +32,38 @@ def has_abba(sequence):
 
     return False
 
-def is_valid(ip):
-    norm_sequences = []
-    hypernets = []
+def supports_tls(ip):
+    supernets, hypernets = split_ip(ip)
 
-    while True:
-        idx = ip.find('[')
-        if idx == -1:
-            assert(-1 == ip.find(']'))
-            norm_sequences.append(ip)
-            break
+    if any(filter(has_abba, hypernets)):
+        return False
 
-        end_idx = ip.find(']')
-        assert(end_idx > idx)
-
-        norm_sequences.append(ip[:idx])
-        hyper = ip[idx+1:end_idx]
-        ip = ip[end_idx+1:]
-        assert(hyper.find('[') == -1)
-        hypernets.append(hyper)
-
-    return not any(filter(has_abba, hypernets)) and any(filter(has_abba, norm_sequences))
+    return any(filter(has_abba, supernets))
 
 def part1(s):
-    answer = len(list(filter(is_valid, s.splitlines())))
+    answer = len(list(filter(supports_tls, s.splitlines())))
 
     print(f'The answer to part one is {answer}')
 
+def supports_ssl(ip):
+    supernets, hypernets = split_ip(ip)
+
+    for sequence in supernets:
+        for a, b, c in zip(sequence,
+                           sequence[1:],
+                           sequence[2:]):
+            if a == c and a != b:
+                bab = b + a + b
+                if any(bab in hyper_s
+                       for hyper_s in hypernets):
+                    return True
+
+    return False
+
 def part2(s):
-    pass
+    answer = len(list(filter(supports_ssl, s.splitlines())))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 7)
 part1(INPUT)
