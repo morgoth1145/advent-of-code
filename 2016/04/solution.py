7e9e6abff01173d834b5abdef069572df63fa799
Patrick Hogg <phogg@novamoon.net>
2022-01-10 15:21:45 -0500

    2016 Day 4 Part 2
    Honestly not too bad, though having to search through the decoded names for something relevant wasn't exactly nice...
    
    Time: 0:12:19.176573

--- 2016/04/solution.py
+++ 2016/04/solution.py
@@ -1,4 +1,5 @@
 import collections
+import string
 
 import lib.aoc
 
@@ -27,7 +28,29 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    for room, sector, checksum in parse_input(s):
+        counts = collections.Counter(room.replace('-', ''))
+        counts = sorted(counts.most_common(),
+                        key=lambda p: (-p[1],p[0]))[:5]
+        real_checksum = ''.join(c[0] for c in counts)
+        if checksum != real_checksum:
+            continue # Not a real room
+
+        decoded = ''
+        for c in room:
+            if c == '-':
+                decoded += ' '
+                continue
+            idx = string.ascii_lowercase.index(c)
+            idx = (idx + sector) % 26
+            c = string.ascii_lowercase[idx]
+            decoded += c
+
+        if 'northpole object storage' == decoded:
+            answer = sector
+            break
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 4)
 part1(INPUT)
