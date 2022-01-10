d2d63427f230976eeda8e4813182768744bb70d1
Patrick Hogg <phogg@novamoon.net>
2022-01-10 15:36:49 -0500

    2016 Day 5 Part 2
    Speaking of unnecessarily confusing and unnecessarily long to compute...
    
    Time: 0:08:58.836431

--- 2016/05/solution.py
+++ 2016/05/solution.py
@@ -22,7 +22,24 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    answer = [None] * 8
+    known = 0
+
+    for h in interesting_hashes(s):
+        pos = h[5]
+        if pos not in '01234567':
+            continue
+        pos = int(pos)
+        if answer[pos] is not None:
+            continue
+        answer[pos] = h[6]
+        known += 1
+        if known == 8:
+            break
+
+    answer = ''.join(answer)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 5)
 part1(INPUT)
