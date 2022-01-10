a08594047977e5323e5b046bb2a42fb9aad43fdf
Patrick Hogg <phogg@novamoon.net>
2022-01-10 14:33:14 -0500

    2016 Day 2 Part 2
    And now a diagonal keypad? Definitely a harder Day 2!
    
    Bounding wasn't too bad, just use complex coordinates again and manhattan distances. (Granted, did I really need complex coords here?) But mapping to the keys was much more annoying. I'm sure there's a smarter way to do this. I'll figure it out when cleaning this up...
    
    Time: 0:08:45.561630

--- 2016/02/solution.py
+++ 2016/02/solution.py
@@ -32,8 +32,45 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+INDICES = {
+    (0, -2): 1,
+    (-1, -1): 2,
+    (0, -1): 3,
+    (1, -1): 4,
+    (-2, 0): 5,
+    (-1, 0): 6,
+    (0, 0): 7,
+    (1, 0): 8,
+    (2, 0): 9,
+    (-1, 1): 10,
+    (0, 1): 11,
+    (1, 1): 12,
+    (0, 2): 13,
+}
+
+def interpret2(directions):
+    p = -2
+
+    for d in directions:
+        if d == 'U':
+            np = p - 1j
+        elif d == 'D':
+            np = p + 1j
+        elif d == 'L':
+            np = p - 1
+        elif d == 'R':
+            np = p + 1
+
+        man_dist = int(abs(np.real) + abs(np.imag))
+        if man_dist <= 2:
+            p = np
+
+    return '123456789ABCD'[INDICES[int(p.real), int(p.imag)]-1]
+
 def part2(s):
-    pass
+    answer = ''.join(map(interpret2, s.splitlines()))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 2)
 part1(INPUT)
