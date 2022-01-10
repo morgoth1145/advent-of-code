0e23ac40d6bc864fb57522ed2524d772046e9524
Patrick Hogg <phogg@novamoon.net>
2022-01-10 16:02:24 -0500

    2016 Day 6 Part 2
    This really should have been earlier in the month, that was trivial!
    
    Time: 0:02:26.696257

--- 2016/06/solution.py
+++ 2016/06/solution.py
@@ -17,7 +17,18 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    columns = collections.defaultdict(collections.Counter)
+
+    for line in s.splitlines():
+        for idx, c in enumerate(line):
+            columns[idx][c] += 1
+
+    answer = ''
+
+    for idx, c in sorted(columns.items()):
+        answer += c.most_common()[-1][0]
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 6)
 part1(INPUT)
