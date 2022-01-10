c91c7958c6a4d70c53c8956df9f54211ff2934d1
Patrick Hogg <phogg@novamoon.net>
2022-01-10 15:25:25 -0500

    2016 Refactor Day 4
    Again, just some deduplication. (Well, and having part 2 do slightly more fuzzy matching on the decoded room names...)

--- 2016/04/solution.py
+++ 2016/04/solution.py
@@ -3,38 +3,23 @@ import string
 
 import lib.aoc
 
-def parse_input(s):
+def real_rooms(s):
     for line in s.splitlines():
         assert(line[-7] == '[')
         assert(line[-1] == ']')
         checksum = line[-6:-1]
+
         room = line[:-7]
         idx = room.rfind('-')
-        sector = room[idx+1:]
+        sector = int(room[idx+1:])
         room = room[:idx]
-        yield room, int(sector), checksum
 
-def part1(s):
-    answer = 0
-
-    for room, sector, checksum in parse_input(s):
-        counts = collections.Counter(room.replace('-', ''))
-        counts = sorted(counts.most_common(),
-                        key=lambda p: (-p[1],p[0]))[:5]
-        real_checksum = ''.join(c[0] for c in counts)
-        if checksum == real_checksum:
-            answer += sector
-
-    print(f'The answer to part one is {answer}')
-
-def part2(s):
-    for room, sector, checksum in parse_input(s):
         counts = collections.Counter(room.replace('-', ''))
         counts = sorted(counts.most_common(),
                         key=lambda p: (-p[1],p[0]))[:5]
         real_checksum = ''.join(c[0] for c in counts)
         if checksum != real_checksum:
-            continue # Not a real room
+            continue
 
         decoded = ''
         for c in room:
@@ -46,9 +31,19 @@ def part2(s):
             c = string.ascii_lowercase[idx]
             decoded += c
 
-        if 'northpole object storage' == decoded:
-            answer = sector
-            break
+        yield decoded, int(sector)
+
+def part1(s):
+    answer = sum(sector for room, sector in real_rooms(s))
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    candidates = list(sector
+                      for room, sector in real_rooms(s)
+                      if 'north' in room)
+    assert(len(candidates) == 1)
+    answer = candidates[0]
 
     print(f'The answer to part two is {answer}')
 
