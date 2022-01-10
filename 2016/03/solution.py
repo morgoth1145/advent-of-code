c7a6f12c91bc362b89c7276a2baf09070b78b2cd
Patrick Hogg <phogg@novamoon.net>
2022-01-10 15:02:17 -0500

    2016 Day 3 Part 2
    Also not too bad. Except for the part where my dumb ape brain generated only 1/3rd of the triangles and got the wrong answer initially...
    
    Time: 0:04:23.394552

--- 2016/03/solution.py
+++ 2016/03/solution.py
@@ -14,8 +14,29 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def gen_real_tris(s):
+    lines = s.splitlines()
+    while lines:
+        a = lines.pop(0).split()
+        b = lines.pop(0).split()
+        c = lines.pop(0).split()
+
+        yield int(a.pop(0)), int(b.pop(0)), int(c.pop(0))
+        yield int(a.pop(0)), int(b.pop(0)), int(c.pop(0))
+        yield int(a.pop(0)), int(b.pop(0)), int(c.pop(0))
+
 def part2(s):
-    pass
+    answer = 0
+
+    for a, b, c in gen_real_tris(s):
+
+        tot_len = a+b+c
+        maxlen = max(a, b, c)
+        rest_len = tot_len - maxlen
+        if rest_len > maxlen:
+            answer += 1
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 3)
 part1(INPUT)
