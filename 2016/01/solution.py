848aa191b9c7b1655fa51e49762ba151172b6350
Patrick Hogg <phogg@novamoon.net>
2022-01-10 14:15:24 -0500

    2016 Day 1 Part 2
    Just finding the first double-touched square, pretty simple. Again though, competition must have been weak if this would have been 1st place.
    
    Time: 0:04:18.209729

--- 2016/01/solution.py
+++ 2016/01/solution.py
@@ -18,7 +18,33 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    d = 1j
+    p = 0
+
+    seen = {0}
+
+    done = False
+    for move in s.split(', '):
+        t = move[0]
+        dist = int(move[1:])
+        if t == 'R':
+            d *= -1j
+        else:
+            d *= 1j
+
+        for _ in range(dist):
+            p += d
+            if p in seen:
+                done = True
+                break
+            seen.add(p)
+
+        if done:
+            break
+
+    answer = int(abs(p.real) + abs(p.imag))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 1)
 part1(INPUT)
