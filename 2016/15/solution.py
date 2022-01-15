4c1ecc92519098a9934c3f60fae7c84d3cda97ef
Patrick Hogg <phogg@novamoon.net>
2022-01-14 20:54:43 -0500

    2016 Day 15 Part 1
    This is *extremely* similar to 2020 Day 13. In fact, I'd argue that at it's core it's the same problem! Since I already had the Chinese Remainder Theorem coded up it was pretty simple to use here.
    
    Time: 0:06:04.494424

---
+++ 2016/15/solution.py
@@ -0,0 +1,25 @@
+import parse
+
+import lib.aoc
+import lib.math
+
+def parse_discs(s):
+    for line in s.splitlines():
+        _, positions, _, start = list(map(lambda r:r[0], parse.findall('{:d}', line)))
+        yield positions, start
+
+def part1(s):
+    congruencies = []
+    for idx, (positions, start) in enumerate(parse_discs(s)):
+        congruencies.append((positions, start+idx+1))
+
+    answer = lib.math.offset_chinese_remainder(congruencies)
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2016, 15)
+part1(INPUT)
+part2(INPUT)
