0b4e860bd8b6dcdfcc974b82df48651e796eea13
Patrick Hogg <phogg@novamoon.net>
2022-01-15 14:18:11 -0500

    2016 Rewrite Day 19
    It turns out that part 1 is a well known problem! But more interesting is part 2. I don't have a closed form solution (the Reddit has one using logs but ew), but this is a logarithmic time calculation so I'll call that close enough :)

--- 2016/19/solution.py
+++ 2016/19/solution.py
@@ -6,7 +6,7 @@ class Link:
         self.left = None
         self.right = None
 
-def solve(s, first_target, advance_fn):
+def solve_linked_list(s, first_target, advance_fn):
     elves = list(map(Link, range(1, int(s)+1)))
 
     for left, right in zip(elves + [elves[0]], [elves[-1]] + elves):
@@ -24,15 +24,15 @@ def solve(s, first_target, advance_fn):
 
     return current.n
 
-def part1(s):
+def part1_linked_list(s):
     def advance_fn(current, remaining):
         return current.left.left
 
-    answer = solve(s, 1, advance_fn)
+    answer = solve_linked_list(s, 1, advance_fn)
 
     print(f'The answer to part one is {answer}')
 
-def part2(s):
+def part2_linked_list(s):
     def advance_fn(current, remaining):
         current = current.left
         if remaining % 2 == 1:
@@ -40,9 +40,49 @@ def part2(s):
             current = current.left
         return current
 
-    answer = solve(s, int(s) // 2, advance_fn)
+    answer = solve_linked_list(s, int(s) // 2, advance_fn)
 
     print(f'The answer to part two is {answer}')
+
+def part1_closed(s):
+    # Part 1 is actually the Josephus problem and follows a very simple pattern
+    # https://en.wikipedia.org/wiki/Josephus_problem#Bitwise
+    n = int(s)
+    max_power_of_2 = 1 << (n.bit_length() - 1)
+    answer = ((n - max_power_of_2) << 1) + 1
+
+    print(f'The answer to part one is {answer}')
+
+def part2_semi_closed(s):
+    # Part 2 also follows a simple sequence. I'm not sure if it has a name, but
+    # the solutions are as follows for 1 elf, 2 elves, 3 elves, etc.
+    # 1, 1, 3, 1, 2, 3, 5, 7, 9, 1, 2, 3, ...
+    # Essentially, count up sequentially to n then skip every other number until
+    # m, then start over. n is the last m, and m is n*3, where n and m start
+    # at 1 and 1
+    # There may be a closed form solution, but this allows for a super-fast
+    # calculation
+    remaining = int(s)
+
+    # Special case the first iteration
+    if remaining > 1:
+        remaining -= 1
+
+    n = 1
+    m = 3
+    while remaining > 2*n:
+        remaining -= 2*n
+        n, m = m, m*3
+
+    if remaining <= n:
+        answer = remaining
+    else:
+        answer = ((remaining - n) << 1) + n
+
+    print(f'The answer to part two is {answer}')
+
+part1 = part1_closed
+part2 = part2_semi_closed
 
 INPUT = lib.aoc.get_input(2016, 19)
 part1(INPUT)
