4a73048ce6421c1b2eb00693a420d4854730efbd
Patrick Hogg <phogg@novamoon.net>
2022-01-15 13:20:38 -0500

    2016 Day 19 Part 2
    Oh now *that* is an interesting twist! Took me a little bit to think through how to approach it, but we don't actually care who is stealing, we care who is removed! Some careful tracking of what the current target is makes this not too bad to solve.
    
    Well...if you actually move *left*. I wasted about 1 minute 45 seconds because I was moving to the right! That bumped me from what would have been a second place time to a third place time :(
    
    Time: 0:15:57.492390

--- 2016/19/solution.py
+++ 2016/19/solution.py
@@ -25,7 +25,36 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    elves = list(map(Link, range(1, int(s)+1)))
+
+    for left, right in zip(elves + [elves[0]], [elves[-1]] + elves):
+        left.right = right
+        right.left = left
+
+    # We don't care *who* steals, we care who is removed!
+    remaining = len(elves)
+    to_remove = remaining // 2
+
+    current_target = elves[to_remove]
+
+    while remaining > 1:
+        left = current_target.left
+        right = current_target.right
+        left.right = right
+        right.left = left
+
+        # Update current target
+        if remaining % 2 == 1:
+            # There was a tie, skip one
+            current_target = left.left
+        else:
+            current_target = left
+
+        remaining -= 1
+
+    answer = current_target.n
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 19)
 part1(INPUT)
