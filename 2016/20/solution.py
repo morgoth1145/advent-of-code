31b751b03635b0b6d09bb3ecb0e9e1b19ed04c1d
Patrick Hogg <phogg@novamoon.net>
2022-01-15 14:36:56 -0500

    2016 Day 20 Part 2
    Okay, that was abysmal. I had a dumb typo resulting in my first answer being -1, and I had an off by 1 error at the end because I subtracted from 4294967295 instead of 4294967296 (2**32)! I ended up wasting 8 minutes and 24 seconds due to that which is horrible!
    
    As a result what *would* have been equivalent to an amazing 1st place fell to 34th. Even *if* I had the dumb typo costing me a minute due to the bad -1 answer, simply eliminating the off by 1 error would *still* would have been faster than 1st place!
    
    Time: 0:12:05.336185

--- 2016/20/solution.py
+++ 2016/20/solution.py
@@ -15,7 +15,17 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    answer = 0
+    maybe_allowed = 0
+
+    for low, high in sorted(parse_input(s)):
+        if maybe_allowed < low:
+            answer += low - maybe_allowed
+        maybe_allowed = max(maybe_allowed, high+1)
+
+    answer += 4294967296 - maybe_allowed
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2016, 20)
 part1(INPUT)
