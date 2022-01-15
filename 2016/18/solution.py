ec125fb7d3498c5544bf1729f98cfc0ecfd25335
Patrick Hogg <phogg@novamoon.net>
2022-01-15 12:05:46 -0500

    2016 Day 18 Part 1
    This looks fun. Is this going to turn into a maze search?
    
    Also, this would have been 3rd on the leaderboard. I wonder when the competition really heated up in Advent of Code?
    
    Time: 0:05:02.002762

---
+++ 2016/18/solution.py
@@ -0,0 +1,39 @@
+import lib.aoc
+
+def next_row(row):
+    row = '.' + row + '.'
+
+    next_row = []
+    for left, center, right in zip(row, row[1:], row[2:]):
+        if left + center + right in ('^^.',
+                                     '.^^',
+                                     '^..',
+                                     '..^'):
+            # Trap
+            next_row.append('^')
+        else:
+            next_row.append('.')
+
+    return ''.join(next_row)
+
+def make_map(first_row, total_rows):
+    rows = [first_row]
+    last_row = first_row
+
+    for _ in range(total_rows-1):
+        last_row = next_row(last_row)
+        rows.append(last_row)
+
+    return '\n'.join(rows)
+
+def part1(s):
+    answer = make_map(s, 40).count('.')
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2016, 18)
+part1(INPUT)
+part2(INPUT)
