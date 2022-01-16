aa9935de8e43508d76917875676cf12efc18c11a
Patrick Hogg <phogg@novamoon.net>
2022-01-16 13:08:01 -0500

    2017 Day 3 Part 1
    There's probably a more efficient way than actually generating the coordinates in order, but this seemed like a good way to prepare for part 2 and wasn't bad to think through.
    
    I *should* have been a minute faster, but I was using variables in my loops in gen_coords which messed up the coordinates and of course my answer :(
    
    Would have been 27th.
    
    Time: 0:04:55.187432

---
+++ 2017/03/solution.py
@@ -0,0 +1,45 @@
+import lib.aoc
+
+def gen_coords():
+    x, y = 0, 0
+    dist = 1
+    val = 1
+
+    yield (x, y), val
+
+    while True:
+        for _ in range(dist):
+            x += 1
+            val += 1
+            yield (x, y), val
+        for _ in range(dist):
+            y += 1
+            val += 1
+            yield (x, y), val
+        dist += 1
+        for _ in range(dist):
+            x -= 1
+            val += 1
+            yield (x, y), val
+        for _ in range(dist):
+            y -= 1
+            val += 1
+            yield (x, y), val
+        dist += 1
+
+def part1(s):
+    n = int(s)
+
+    for (x, y), val in gen_coords():
+        if val == n:
+            answer = abs(x) + abs(y)
+            break
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2017, 3)
+part1(INPUT)
+part2(INPUT)
