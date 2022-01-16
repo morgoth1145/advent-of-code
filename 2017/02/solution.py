f50f795c7d1ae505a6b980564000e513b393379b
Patrick Hogg <phogg@novamoon.net>
2022-01-16 09:35:05 -0500

    2017 Day 2 Part 2
    Another easy twist (go figure, it's day 2). Again some waffling on how to best check for the numbers that are evently divisible, but not too bad.
    
    Would have been 10th
    
    Time: 0:02:38.427411

--- 2017/02/solution.py
+++ 2017/02/solution.py
@@ -11,7 +11,17 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    answer = 0
+
+    for line in s.splitlines():
+        nums = list(map(int, line.split()))
+
+        for a in nums:
+            for b in nums:
+                if a % b == 0 and a != b:
+                    answer += a // b
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 2)
 part1(INPUT)
