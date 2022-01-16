d213be023bd848ebcee2d8f756ecb7405a16ca4e
Patrick Hogg <phogg@novamoon.net>
2022-01-16 15:08:00 -0500

    2017 Day 9 Part 2
    Just counting garbage? This seems easier than part 1 to me! Oh well.
    
    Time: 0:12:41.799190

--- 2017/09/solution.py
+++ 2017/09/solution.py
@@ -14,6 +14,8 @@ def parse_input(s):
             garbage_s += s[:idx]
             s = s[idx+2:]
 
+    discarded = 0
+
     clean_s = ''
     # Remove garbage
     while garbage_s:
@@ -24,6 +26,7 @@ def parse_input(s):
         else:
             clean_s += garbage_s[:idx]
             end_idx = garbage_s.find('>', idx)
+            discarded += end_idx - idx - 1
             garbage_s = garbage_s[end_idx+1:]
 
     assert(len(set(clean_s) - set('{},')) == 0)
@@ -33,7 +36,7 @@ def parse_input(s):
         s = s.replace(',,', ',')
     s = s.replace('[,', '[')
     s = s.replace(',]', ']')
-    return json.loads(s)
+    return json.loads(s), discarded
 
 def score(tree, parent_score=0):
     self = parent_score + 1
@@ -41,12 +44,14 @@ def score(tree, parent_score=0):
                       for child in tree)
 
 def part1(s):
-    answer = score(parse_input(s))
+    answer = score(parse_input(s)[0])
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    _, answer = parse_input(s)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 9)
 part1(INPUT)
