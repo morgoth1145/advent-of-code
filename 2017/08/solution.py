27b197a8255baee1580f46263c96b59e9daa871f
Patrick Hogg <phogg@novamoon.net>
2022-01-16 14:30:12 -0500

    2017 Day 8 Part 1
    The longest part here was mapping the operator strings to the operations! At least a couple of dictionaries helped do that mapping cleanly.
    
    Would have been 25th
    
    Time: 0:05:00.497349

---
+++ 2017/08/solution.py
@@ -0,0 +1,41 @@
+import lib.aoc
+
+def parse_input(s):
+    for line in s.splitlines():
+        var1, op, val, _, var2, cond, target = line.split()
+        val = int(val)
+        target = int(target)
+        yield var1, op, val, var2, cond, target
+
+CONDITIONS = {
+    '>': lambda a, b: a > b,
+    '<': lambda a, b: a < b,
+    '>=': lambda a, b: a >= b,
+    '<=': lambda a, b: a <= b,
+    '==': lambda a, b: a == b,
+    '!=': lambda a, b: a != b,
+}
+
+OPERATIONS = {
+    'inc': lambda a, b: a + b,
+    'dec': lambda a, b: a - b,
+}
+
+def part1(s):
+    registers = {}
+
+    for var1, op, val, var2, cond, target in parse_input(s):
+        if CONDITIONS[cond](registers.get(var2, 0), target):
+            val = OPERATIONS[op](registers.get(var1, 0), val)
+            registers[var1] = val
+
+    answer = max(registers.values())
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2017, 8)
+part1(INPUT)
+part2(INPUT)
