0c88a202485d504c8e865c594634b61582ace740
Patrick Hogg <phogg@novamoon.net>
2022-01-16 14:15:33 -0500

    2017 Day 7 Part 2
    Oh interesting, tree balancing. There may be smarter ways to do this, but in my opinion this works well enough. I did initially submit the weight of the fixed subtree rather than the fixed node though. Oops!
    
    Would have been 28th.
    
    Time: 0:15:34.166579

--- 2017/07/solution.py
+++ 2017/07/solution.py
@@ -1,3 +1,5 @@
+import collections
+
 import lib.aoc
 
 def parse_input(s):
@@ -28,7 +30,53 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    seen = set()
+    referenced = set()
+
+    tree = {}
+
+    for name, weight, held in parse_input(s):
+        seen.add(name)
+        referenced.update(held)
+        tree[name] = (weight, held)
+
+    base = list(seen - referenced)
+    assert(len(base) == 1)
+
+    base = base[0]
+
+    fixes = {}
+
+    def fix_recursive(name):
+        weight, held = tree[name]
+        if len(held) == 0:
+            return weight
+
+        sub_weights = list(map(fix_recursive, held))
+
+        weight_counts = collections.Counter(sub_weights)
+        counts = weight_counts.most_common()
+        if len(counts) != 1:
+            assert(len(counts) == 2)
+            good_weight = counts[0][0]
+            bad_weight = counts[1][0]
+            assert(counts[1][1] == 1)
+            idx = sub_weights.index(bad_weight)
+            diff = good_weight - bad_weight
+            changed_node = held[idx]
+            fixes[changed_node] = tree[changed_node][0] + diff
+            sub_weights[idx] = good_weight
+
+        this_weight = weight + sum(sub_weights)
+        return this_weight
+
+    fix_recursive(base)
+
+    assert(len(fixes) == 1)
+
+    answer = list(fixes.values())[0]
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 7)
 part1(INPUT)
