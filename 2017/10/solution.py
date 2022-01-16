85ab8179f4596431b9156bd2c433cc33148d083d
Patrick Hogg <phogg@novamoon.net>
2022-01-16 15:35:04 -0500

    2017 Day 10 Part 2
    Wow am I glad that I know about ord, that made the char->int conversion so much easier.
    
    Aside from that it was pretty simple to repeatedly knot_hash, densify the hash, and convert to hex. I did waste time because I forgot to add the 5 key values at the end of the lengths array though, bah!
    
    Would have been 7th
    
    Time: 0:12:07.377328

--- 2017/10/solution.py
+++ 2017/10/solution.py
@@ -1,10 +1,7 @@
 import lib.aoc
 
-def part1(s):
-    nums = list(range(256))
-    pos = 0
-    skip = 0
-    lengths = list(map(int, s.split(',')))
+def knot_hash(nums, pos, skip, lengths):
+    nums = list(nums)
 
     for l in lengths:
         # Reverse
@@ -17,12 +14,39 @@ def part1(s):
         pos = (pos + l + skip) % len(nums)
         skip += 1
 
+    return nums, pos, skip
+
+def part1(s):
+    nums = list(range(256))
+    lengths = list(map(int, s.split(',')))
+
+    nums, _, _ = knot_hash(nums, 0, 0, lengths)
+
     answer = nums[0] * nums[1]
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    nums = list(range(256))
+    lengths = list(map(ord, s)) + [17, 31, 73, 47, 23]
+
+    pos, skip = 0, 0
+
+    for _ in range(64):
+        nums, pos, skip = knot_hash(nums, pos, skip, lengths)
+
+    dense = []
+    for i in range(0, 256, 16):
+        val = 0
+        for v in nums[i:i+16]:
+            val = val ^ v
+        dense.append(val)
+
+    answer = ''
+    for sect in dense:
+        answer += hex(sect)[2:].zfill(2)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 10)
 part1(INPUT)
