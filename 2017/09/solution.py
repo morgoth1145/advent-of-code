a79ecdb0dac08004b1ba5c9d64103cd6522d3820
Patrick Hogg <phogg@novamoon.net>
2022-01-16 15:06:35 -0500

    2017 Day 9 Part 1
    Oh wow, did competition finally heat up? This time is off the leaderboard! In fact, it's nearly off the part 2 leaderboard!
    
    Interesting problem. I may have paid a little bit for how I approached this (progressively removing trash instead of proceeding character by character) but this felt right to me!
    
    Time: 0:11:16.070229

---
+++ 2017/09/solution.py
@@ -0,0 +1,53 @@
+import json
+
+import lib.aoc
+
+def parse_input(s):
+    # Remove ignores
+    garbage_s = ''
+    while s:
+        idx = s.find('!')
+        if idx == -1:
+            garbage_s += s
+            s = ''
+        else:
+            garbage_s += s[:idx]
+            s = s[idx+2:]
+
+    clean_s = ''
+    # Remove garbage
+    while garbage_s:
+        idx = garbage_s.find('<')
+        if idx == -1:
+            clean_s += garbage_s
+            garbage_s = ''
+        else:
+            clean_s += garbage_s[:idx]
+            end_idx = garbage_s.find('>', idx)
+            garbage_s = garbage_s[end_idx+1:]
+
+    assert(len(set(clean_s) - set('{},')) == 0)
+
+    s = clean_s.translate(str.maketrans('{}', '[]'))
+    while ',,' in s:
+        s = s.replace(',,', ',')
+    s = s.replace('[,', '[')
+    s = s.replace(',]', ']')
+    return json.loads(s)
+
+def score(tree, parent_score=0):
+    self = parent_score + 1
+    return self + sum(score(child, self)
+                      for child in tree)
+
+def part1(s):
+    answer = score(parse_input(s))
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2017, 9)
+part1(INPUT)
+part2(INPUT)
