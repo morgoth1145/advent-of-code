07ffee958eae785b78746023397e66f9f7e3693a
Patrick Hogg <phogg@novamoon.net>
2022-01-15 19:23:20 -0500

    2016 Day 24 Part 1
    This reminds me a *lot* of 2019 Day 18. Preprocessing the grid to get a point of interest graph wasn't too bad and *really* accelerates the real search. lib.graph.all_reachable worked beautifully here!
    
    The problem was actually the search itself. My instinct was to use lib.graph.dijkstra_length until I realized that I actually have multiple possible end states depending on where the robot ends. Thinking about it I could have just searched for all of them and chosen the least value, but instead I wrote a custom dijkstra_length locally which only looked at part of the state to determine if we were done.
    
    Maybe I should think about how to have a weaker check for ending the dijkstra search to make this sort of thing easier?
    
    Would have been 23rd.
    
    Time: 0:19:55.161307

---
+++ 2016/24/solution.py
@@ -0,0 +1,66 @@
+import heapq
+
+import lib.aoc
+import lib.graph
+import lib.grid
+
+def get_point_of_interest_graph(s):
+    grid = lib.grid.FixedGrid.parse(s)
+
+    poi_graph = {}
+
+    for start, v in grid.items():
+        if v not in '#.':
+            destinations = []
+
+            def neighbor_fn(pos):
+                if pos != start and grid[pos] != '.':
+                    return
+                for n in grid.neighbors(*pos):
+                    nv = grid[n]
+                    if nv == '#':
+                        continue
+                    yield n, 1
+
+            for pos, dist in lib.graph.all_reachable(lib.graph.make_lazy_graph(neighbor_fn),
+                                                     start):
+                if grid[pos] == '.':
+                    continue
+                destinations.append((int(grid[pos]), dist))
+
+            poi_graph[int(v)] = destinations
+
+    return poi_graph
+
+def part1(s):
+    graph = get_point_of_interest_graph(s)
+
+    all_points_of_interest = tuple(sorted(graph.keys()))
+
+    queue = [(0, 0, (0,))]
+    handled = set()
+
+    while True:
+        current_dist, pos, seen = heapq.heappop(queue)
+
+        if seen == all_points_of_interest:
+            answer = current_dist
+            break
+
+        if (pos, seen) in handled:
+            continue
+
+        handled.add((pos, seen))
+
+        for neighbor, dist in graph[pos]:
+            new_seen = tuple(sorted(set(seen + (neighbor,))))
+            heapq.heappush(queue, (current_dist + dist, neighbor, new_seen))
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2016, 24)
+part1(INPUT)
+part2(INPUT)
