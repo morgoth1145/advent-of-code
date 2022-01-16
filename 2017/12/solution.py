c36b27b1e183ce9f61bafaf95f3a5bf05eb34218
Patrick Hogg <phogg@novamoon.net>
2022-01-16 17:26:52 -0500

    2017 Day 12 Part 2
    And now we're finding all groups. Pretty simple with some sets, but again I had some interface woes since my graph library gives me distances when I just want nodes.
    
    Would have been 22nd
    
    Time: 0:04:55.070411

--- 2017/12/solution.py
+++ 2017/12/solution.py
@@ -19,7 +19,20 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    graph = parse_graph(s)
+
+    answer = 0
+    unreached = set(graph.keys())
+
+    while unreached:
+        start = list(unreached)[0]
+        group = {start} | {n
+                           for n, dist in lib.graph.all_reachable(graph,
+                                                                  start)}
+        answer += 1
+        unreached -= group
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 12)
 part1(INPUT)
