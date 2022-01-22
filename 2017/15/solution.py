605c2912bbeea299faad2b5b7cca8df90a4071c9
Patrick Hogg <phogg@novamoon.net>
2022-01-22 15:16:23 -0500

    2017 Day 15 Part 2
    More dumb wasted time because I filtered the wrong sequence when trying to get sequence b...I *really* bungled this problem.
    
    Time: 0:20:57.078879

--- 2017/15/solution.py
+++ 2017/15/solution.py
@@ -8,24 +8,38 @@ def gen_sequence(prev, fact, mod):
         yield val
         prev = val
 
+def judge(a_seq, b_seq, to_check):
+    matched = 0
+    BIT_MASK = 0xFFFF # 16 bits
+
+    for _ in range(to_check):
+        if next(a_seq) & BIT_MASK == next(b_seq) & BIT_MASK:
+            matched += 1
+
+    return matched
+
 def part1(s):
     a, b = list(map(lambda r:r[0], parse.findall('{:d}', s)))
 
-    a_vals = gen_sequence(a, 16807, 2147483647)
-    b_vals = gen_sequence(b, 48271, 2147483647)
+    a_seq = gen_sequence(a, 16807, 2147483647)
+    b_seq = gen_sequence(b, 48271, 2147483647)
 
-    BIT_MASK = 0xFFFF # 16 bits
-
-    answer = 0
-
-    for _ in range(40000000):
-        if next(a_vals) & BIT_MASK == next(b_vals) & BIT_MASK:
-            answer += 1
+    answer = judge(a_seq, b_seq, 40000000)
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    a, b = list(map(lambda r:r[0], parse.findall('{:d}', s)))
+
+    a_seq = gen_sequence(a, 16807, 2147483647)
+    b_seq = gen_sequence(b, 48271, 2147483647)
+
+    a_seq = filter(lambda val: val & 3 == 0, a_seq)
+    b_seq = filter(lambda val: val & 7 == 0, b_seq)
+
+    answer = judge(a_seq, b_seq, 5000000)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 15)
 part1(INPUT)
