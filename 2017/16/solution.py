631ca648c418a46e01020df8ae878d9f05a84464
Patrick Hogg <phogg@novamoon.net>
2022-01-22 15:43:18 -0500

    2017 Day 16 Part 2
    And now a billion iterations. I figured just doing it 1 billion times would never finish, and after seeing how long 1k iterations took it was clear I was right. But thankfully we detect a cycle pretty quickly.
    
    It turns out that the cycle actually goes back to the starting order, but I don't like encoding that sort of knowledge in so I just stuck with my normal cycle detection logic. Then some quick arithmetic to figure out how many cycles we need to run to finalize, and we're done!
    
    Would have been 10th! I guess people really got tripped up here.
    
    Time: 0:11:10.013880

--- 2017/16/solution.py
+++ 2017/16/solution.py
@@ -1,27 +1,50 @@
 import lib.aoc
 
-def part1(s):
-    answer = 'abcdefghijklmnop'
-
+def dance(order, s):
     for inst in s.split(','):
         if inst[0] == 's':
             x = int(inst[1:])
-            answer = answer[-x:] + answer[:-x]
+            order = order[-x:] + order[:-x]
         elif inst[0] == 'x':
             a, b = sorted(map(int, inst[1:].split('/')))
-            answer = answer[:a] + answer[b] + answer[a+1:b] + answer[a] + answer[b+1:]
+            order = order[:a] + order[b] + order[a+1:b] + order[a] + order[b+1:]
         elif inst[0] == 'p':
             a = inst[1]
             assert(inst[2] == '/')
             b = inst[3]
-            answer = answer.translate(str.maketrans(a+b, b+a))
+            order = order.translate(str.maketrans(a+b, b+a))
         else:
             assert(False)
+
+    return order
+
+def part1(s):
+    answer = dance('abcdefghijklmnop', s)
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    answer = 'abcdefghijklmnop'
+
+    order_to_iter = {
+        answer: 0
+    }
+
+    while True:
+        answer = dance(answer, s)
+        if answer in order_to_iter:
+            break
+
+        order_to_iter[answer] = len(order_to_iter)
+
+    iteration = len(order_to_iter)
+    cycle = iteration - order_to_iter[answer]
+    remaining = (1000000000 - iteration) % cycle
+
+    for _ in range(remaining):
+        answer = dance(answer, s)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 16)
 part1(INPUT)
