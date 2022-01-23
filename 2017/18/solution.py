4f72ee088dec7662a6eed99fa399aa694d424ea4
Patrick Hogg <phogg@novamoon.net>
2022-01-22 20:27:34 -0500

    2017 Day 18 Part 2
    Holy crap everything fell apart here! I thought this would be a fun little coroutine problem, but nooooo.
    
    I started out writing a simple Python coroutine that I'd instantiate twice and run, but it was running forever. Dismayed I then thought that maybe this was an icky reverse engineering problem. It isn't, but I still spent a ton of time reverse-engineering the code to find that it's a bubble sort of 127 values.
    
    So I tried counting the steps for bubble sort for the generated sequence. It sort of worked, at least in confirming on the site that that was the right ballpark. However, the numbers I got were wrong. (Don't really know or care why.)
    
    Then I reimplemented the *entire* thing with a class for easier debugging. Surprisingly the number of values passed between them did *not* stay at 127, so something was clearly going very wrong!
    
    After *way* too long debugging I finally found two really, really stupid bugs:
    1) When parsing the instructions I forgot to convert parameter 0 of jgz to int if it wasn't a letter, breaking the program.
    2) I also implemented jgz with a simple equality check against zero, not checking for greater than zero!
    
    Honestly, if I'd have done those right initially I'd have saved so much time. This was an absolute disaster.
    
    An hour past the end of the part 2 leaderboard :(
    
    Time: 1:40:10.073833

--- 2017/18/solution.py
+++ 2017/18/solution.py
@@ -5,8 +5,10 @@ def parse_instructions(s):
 
     for line in s.splitlines():
         parts = line.split()
-        if parts[-1] not in 'abcdefghijklmnopqrstuvwxyz':
-            parts[-1] = int(parts[-1])
+        if parts[1] not in 'abcdefghijklmnopqrstuvwxyz':
+            parts[1] = int(parts[1])
+        if len(parts) > 2 and parts[2] not in 'abcdefghijklmnopqrstuvwxyz':
+            parts[2] = int(parts[2])
         instructions.append(parts)
 
     return instructions
@@ -42,7 +44,7 @@ def part1(s):
                 answer = last_frequency
                 break
         elif inst[0] == 'jgz':
-            if get_val(inst[1]):
+            if get_val(inst[1]) > 0:
                 idx += get_val(inst[2])
                 continue
         else:
@@ -52,8 +54,74 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+class Program:
+    def __init__(self, s, pid):
+        self.instructions = parse_instructions(s)
+        self.idx = 0
+        self.registers = {'p': pid}
+        self.send_count = 0
+        self.queue = []
+
+    def push_val(self, val):
+        self.queue.append(val)
+
+    @property
+    def blocked(self):
+        return len(self.queue) == 0
+
+    def _get_val(self, val):
+        if isinstance(val, int):
+            return val
+        return self.registers.get(val, 0)
+
+    def execute(self):
+        outputs = []
+
+        while True:
+            inst = self.instructions[self.idx]
+
+            if inst[0] == 'snd':
+                outputs.append(self._get_val(inst[1]))
+                self.send_count += 1
+            elif inst[0] == 'set':
+                self.registers[inst[1]] = self._get_val(inst[2])
+            elif inst[0] == 'add':
+                self.registers[inst[1]] = self._get_val(inst[1]) + self._get_val(inst[2])
+            elif inst[0] == 'mul':
+                self.registers[inst[1]] = self._get_val(inst[1]) * self._get_val(inst[2])
+            elif inst[0] == 'mod':
+                self.registers[inst[1]] = self._get_val(inst[1]) % self._get_val(inst[2])
+            elif inst[0] == 'rcv':
+                if len(self.queue) == 0:
+                    return outputs
+
+                self.registers[inst[1]] = self.queue.pop(0)
+            elif inst[0] == 'jgz':
+                if self._get_val(inst[1]) > 0:
+                    self.idx += self._get_val(inst[2])
+                    continue
+            else:
+                assert(False)
+
+            self.idx += 1
+
 def part2(s):
-    pass
+    a = Program(s, 0)
+    b = Program(s, 1)
+
+    while True:
+        a_outputs = a.execute()
+        for val in a_outputs:
+            b.push_val(val)
+        b_outputs = b.execute()
+        for val in b_outputs:
+            a.push_val(val)
+        if a.blocked and b.blocked:
+            break
+
+    answer = b.send_count
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 18)
 part1(INPUT)
