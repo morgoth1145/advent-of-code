3da33ebda396ddaee096745ee96d25c044ceae12
Patrick Hogg <phogg@novamoon.net>
2022-03-12 20:06:03 -0500

    2017 Day 25 Part 1 (There is no Part 2)
    Okay. I 100% got sidetracked with programmatically parsing the state description. I easily could have manually typed in the state transitions faster, but I just wanted to properly parse the input.
    
    That aside, this really wasn't bad at all. Just run a few million turing state transitions and count the number of ones on the tape at the end. Easy peasy.
    
    Would have been 83rd on the leaderboard for part 1 and 70th for part 2.
    
    Time: 0:10:01.811876

---
+++ 2017/25/solution.py
@@ -0,0 +1,66 @@
+import lib.aoc
+
+def parse_action(lines):
+    if lines[0] == '    - Write the value 1.':
+        write = 1
+    else:
+        assert(lines[0] == '    - Write the value 0.')
+        write = 0
+
+    if lines[1] == '    - Move one slot to the left.':
+        move = -1
+    else:
+        assert(lines[1] == '    - Move one slot to the right.')
+        move = 1
+
+    next_state = lines[2].split()[-1][:-1]
+
+    return write, move, next_state
+
+def parse_input(s):
+    groups = s.split('\n\n')
+    setup = groups[0].splitlines()
+
+    start_state = setup[0].split()[-1][:-1]
+    steps = int(setup[1].split()[5])
+
+    states = {}
+
+    for desc in groups[1:]:
+        lines = desc.splitlines()
+
+        name = lines[0].split()[-1][:-1]
+        assert('  If the current value is 0:' == lines[1])
+        zero_action = parse_action(lines[2:5])
+        assert('  If the current value is 1:' == lines[5])
+        one_action = parse_action(lines[6:9])
+
+        states[name] = (zero_action, one_action)
+
+    return start_state, steps, states
+
+def part1(s):
+    start_state, steps, states = parse_input(s)
+
+    tape = {}
+    cursor = 0
+
+    s = start_state
+
+    for _ in range(steps):
+        val = tape.get(cursor, 0)
+        write, move, next_state = states[s][val]
+        tape[cursor] = write
+        cursor += move
+        s = next_state
+
+    answer = sum(tape.values())
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    print('There is no part two for Christmas!')
+
+INPUT = lib.aoc.get_input(2017, 25)
+part1(INPUT)
+part2(INPUT)
