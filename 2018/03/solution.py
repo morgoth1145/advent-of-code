fb95912d011e3cfb52d02dec928a52756ceffe2d
Patrick Hogg <phogg@novamoon.net>
2022-03-13 14:55:52 -0400

    2018 Day 3 Part 2
    And now to find the claim that doesn't overlap anything else. In other words, the claim whose area has only hits of 1 from part 1. Not too bad.
    
    Would have been 2nd on the leaderboard, missing first by only 6 seconds!
    
    Time: 0:03:35.437879

--- 2018/03/solution.py
+++ 2018/03/solution.py
@@ -24,7 +24,24 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    hits = collections.Counter()
+
+    for num, x_start, y_start, w, h in parse_input(s):
+        for x in range(x_start, x_start+w):
+            for y in range(y_start, y_start+h):
+                hits[x,y] += 1
+
+    answer = sum(1 for claims in hits.values()
+                 if claims > 1)
+
+    for num, x_start, y_start, w, h in parse_input(s):
+        if all(hits[x,y] == 1
+               for x in range(x_start, x_start+w)
+               for y in range(y_start, y_start+h)):
+            answer = num
+            break
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 3)
 part1(INPUT)
