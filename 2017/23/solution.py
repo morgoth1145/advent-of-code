c1226d0ecc9dad626f6252cee19547b3874af2e6
Patrick Hogg <phogg@novamoon.net>
2022-03-12 19:20:36 -0500

    2017 Day 23 Part 2
    Ew. This is an honest to goodness reverse engineering problem, unlike the previous ones which could easily be solved with some VM optimization of certain patterns.
    
    This ended up taking me two passes to reverse engineer because I made some dumb errors in the first pass. I actually didn't 100% reverse engineer it, but I got it far enough that it gave me the answer which is the point of the exercise.
    
    Would have been 68th on the leaderboard, at least.
    
    Time: 0:44:42.572933

--- 2017/23/solution.py
+++ 2017/23/solution.py
@@ -37,7 +37,41 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    jump_points = set()
+
+    instructions = [l.split()
+                    for l in s.splitlines()]
+
+    translated = []
+
+    for idx, (cmd, arg1, arg2) in enumerate(instructions):
+        if cmd == 'set':
+            translated.append(f'{arg1} = {arg2}')
+        elif cmd == 'sub':
+            translated.append(f'{arg1} -= {arg2}')
+        elif cmd == 'mul':
+            translated.append(f'{arg1} *= {arg2}')
+        elif cmd == 'jnz':
+            offset = int(arg2)
+            dest = idx + offset
+            jump_points.add(dest)
+            translated.append(f'if {arg1} != 0: GOTO LABEL{dest}')
+        else:
+            assert(False)
+
+    for idx, cmd in enumerate(translated):
+        if idx in jump_points:
+            jump_points.remove(idx)
+            print(f'LABEL{idx}:')
+        print(f'    {cmd}')
+
+    for idx in sorted(jump_points):
+        print(f'LABEL{idx}:')
+
+    print('Please reverse-engineer the code and find the answer')
+    answer = input('Answer? ')
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2017, 23)
 part1(INPUT)
