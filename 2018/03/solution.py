56146e28c01de9fa92fabb36055f70407563a4d7
Patrick Hogg <phogg@novamoon.net>
2022-03-13 14:54:48 -0400

    2018 Day 3 Part 1
    So just turn each line into a set of coordinates and count the overlaps. Pretty easy :)
    
    Would have been 4th on the leaderboard!
    
    Time: 0:02:31.519025

---
+++ 2018/03/solution.py
@@ -0,0 +1,31 @@
+import collections
+
+import lib.aoc
+
+def parse_input(s):
+    for line in s.splitlines():
+        num, _, start, dims = line.split()
+        num = int(num[1:])
+        x, y = list(map(int, start[:-1].split(',')))
+        w, h = list(map(int, dims.split('x')))
+        yield num, x, y, w, h
+
+def part1(s):
+    hits = collections.Counter()
+
+    for num, x_start, y_start, w, h in parse_input(s):
+        for x in range(x_start, x_start+w):
+            for y in range(y_start, y_start+h):
+                hits[x,y] += 1
+
+    answer = sum(1 for claims in hits.values()
+                 if claims > 1)
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2018, 3)
+part1(INPUT)
+part2(INPUT)
