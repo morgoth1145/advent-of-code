aca3889150c237d19ec764c9ae06b76f262079fc
Patrick Hogg <phogg@novamoon.net>
2022-03-12 20:17:14 -0500

    2018 Day 1 Part 2
    That's a bit more like it. Still basic (it is day 1) but at least this required some logic.
    
    Would have been 2nd on the leaderboard! Only 16 seconds off of 1st!
    
    Time: 0:02:04.471091

--- 2018/01/solution.py
+++ 2018/01/solution.py
@@ -6,7 +6,27 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    changes = list(map(int, s.splitlines()))
+
+    f = 0
+    seen = set()
+
+    while True:
+        done = False
+
+        for d in changes:
+            f += d
+            if f in seen:
+                done = True
+                break
+            seen.add(f)
+
+        if done:
+            break
+
+    answer = f
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 1)
 part1(INPUT)
