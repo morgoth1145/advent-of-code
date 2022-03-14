36188623e5cd962ffe3d6c27c5c9da0deb2ef045
Patrick Hogg <phogg@novamoon.net>
2022-03-14 15:02:38 -0400

    2018 Day 11 Part 2
    Aaaaand everything fell apart. Aside from the absolutely abysmal complexity that I was using initially (something like O(n^4)) I kept making stupid errors (somwhere, still not sure where) which meant that I was getting the wrong answers.
    
    Ultimately presumming the rows and determining the square totals based on the presummed values is way faster, and definitely is what I should have done in the first place. But I didn't and as a result I wasted a *ton* of time floundering.
    
    (There's an even faster approach I think, but I'll leave that to a follow-up.)
    
    Would have missed the leaderboard by a mile. 26 minutes and 26 seconds after the leaderboard would have closed. Ugh.
    
    Time: 0:42:38.195034

--- 2018/11/solution.py
+++ 2018/11/solution.py
@@ -14,16 +14,37 @@ def make_grid(serial_number):
 
     return grid
 
-def part1(s):
-    grid = make_grid(int(s))
+def optimize(grid, square_size):
+    presums = {}
+
+    for y in range(1, 301):
+        power = 0
+        for x in range(1, square_size):
+            power += grid[x,y]
+
+        for x in range(1, 302 - square_size):
+            power += grid[x+square_size-1,y]
+            presums[x,y] = power
+            power -= grid[x,y]
 
     best = (0, (0, 0))
 
-    for x, y in grid:
-        tot_power = sum(grid.get((sx, sy), -100)
-                        for sx in [x, x+1, x+2]
-                        for sy in [y, y+1, y+2])
-        best = max(best, (tot_power, (x, y)))
+    for x in range(1, 302 - square_size):
+        power = 0
+        for y in range(1, square_size):
+            power += presums[x,y]
+
+        for y in range(1, 302 - square_size):
+            power += presums[x,y+square_size-1]
+            best = max(best, (power, (x, y)))
+            power -= presums[x,y]
+
+    return best
+
+def part1(s):
+    grid = make_grid(int(s))
+
+    best = optimize(grid, 3)
 
     best_x, best_y = best[1]
     answer = f'{best_x},{best_y}'
@@ -31,7 +52,19 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    grid = make_grid(int(s))
+
+    best = (-100, 0, None)
+
+    for square_size in range(1, 301):
+        power, coord = optimize(grid, square_size)
+        best = max(best, (power, square_size, coord))
+
+    _, square_size, (x, y) = best
+
+    answer = f'{x},{y},{square_size}'
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 11)
 part1(INPUT)
