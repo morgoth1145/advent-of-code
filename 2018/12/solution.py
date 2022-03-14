bdbc1dd2c608a9f3aa47335a3f334d402acc9d1d
Patrick Hogg <phogg@novamoon.net>
2022-03-14 17:23:18 -0400

    2018 Day 12 Part 2
    Hoo boy. 50 billion iterations pretty clearly screams "I end up in a cycle, find it". The trick was in finding the pattern and figuring out how to code it!
    
    In the end the pattern ended up falling into a cycle that shifts after a while. (I found that it is 100% stable even, just moving to the right each generation. That's probably true for all inputs, but I'm not going to hardcode that.)
    
    The tricky thing here is that with the pattern shifting, the representation needs to change to be the plants and their starting offset, otherwise we can't detect a matching pattern. This took quite a bit to get working, and honestly I don't even remember how much of the slowdown was bugs vs the complexity of managing data. I do know that i hit some bugs though, and I'm super glad that I got the answer for 20k generations the slow way because that let me test my skip-ahead code until I got it right without pinging the server!
    
    Would have missed the leaderboard by 6 minutes and 8 seconds, but that's fine.
    
    Time: 0:33:50.250199

--- 2018/12/solution.py
+++ 2018/12/solution.py
@@ -1,54 +1,89 @@
 import lib.aoc
 
-def parse_input(s):
+def step(plant_offsets, plant_patterns):
+    new_offsets = []
+
+    for off in range(plant_offsets[0]-2, plant_offsets[-1]+3):
+        key = []
+        for i in range(off-2, off+3):
+            key.append('#' if i in plant_offsets else '.')
+        key = ''.join(key)
+
+        if key in plant_patterns:
+            new_offsets.append(off)
+
+    adjust = new_offsets[0]
+    plant_offsets = tuple(o-adjust
+                          for o in new_offsets)
+
+    return adjust, plant_offsets
+
+def run(s, generations):
     groups = s.split('\n\n')
 
     start = groups[0].split()[2]
 
-    plants = set()
+    plant_offsets = []
     for idx, c in enumerate(start):
         if c == '#':
-            plants.add(idx)
+            plant_offsets.append(idx)
 
-    generate_plants = set()
+    start = plant_offsets[0]
+    plant_offsets = tuple(o-start
+                          for o in plant_offsets)
+
+    plant_patterns = set()
 
     for line in groups[1].splitlines():
         a, b = line.split(' => ')
         if b == '#':
-            generate_plants.add(a)
+            plant_patterns.add(a)
 
-    assert('.....' not in generate_plants)
+    assert('.....' not in plant_patterns)
 
-    return plants, generate_plants
+    memory = {}
+    sequence = []
 
-def step(plants, generate_plants):
-    out_plants = set()
+    gen_num = 0
+    while gen_num < generations:
+        sequence.append((start, plant_offsets))
+        if plant_offsets in memory:
+            gen_seen, prev_start = memory[plant_offsets]
+            # Jump ahead!
 
-    for idx in range(min(plants)-2, max(plants)+3):
-        key = []
-        for i in range(idx-2, idx+3):
-            if i in plants:
-                key.append('#')
-            else:
-                key.append('.')
-        key = ''.join(key)
-        if key in generate_plants:
-            out_plants.add(idx)
+            repeat_every = gen_num - gen_seen
+            shift_per = start - prev_start
 
-    return out_plants
+            remaining = generations - gen_num
+            jump_by = remaining // repeat_every
+            gen_num += jump_by * repeat_every
+            start += jump_by * shift_per
+            break
+
+        memory[plant_offsets] = (gen_num, start)
+        gen_num += 1
+
+        adjust, plant_offsets = step(plant_offsets, plant_patterns)
+        start += adjust
+
+    # Wrap up if needed
+    while gen_num < generations:
+        gen_num += 1
+        adjust, plant_offsets = step(plant_offsets, plant_patterns)
+        start += adjust
+
+    return [o+start
+            for o in plant_offsets]
 
 def part1(s):
-    plants, generate_plants = parse_input(s)
-
-    for _ in range(20):
-        plants = step(plants, generate_plants)
-
-    answer = sum(plants)
+    answer = sum(run(s, 20))
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    answer = sum(run(s, 50000000000))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 12)
 part1(INPUT)
