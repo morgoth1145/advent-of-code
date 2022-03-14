b3d44e5fa1b0df474f7eccaae93aae3a39e570ae
Patrick Hogg <phogg@novamoon.net>
2022-03-14 13:45:25 -0400

    2018 Day 9 Part 2
    And part 2 is just pushing it to the extreme. I do notice a slight bit of lag now, but nothing too major. It still finishes in about a second which isn't bad at all.
    
    Would have been 7th on the leaderboard! Wow, that's *really* not bad for me not really having used deque before!
    
    Time: 0:11:42.652605

--- 2018/09/solution.py
+++ 2018/09/solution.py
@@ -2,11 +2,7 @@ import collections
 
 import lib.aoc
 
-def part1(s):
-    players, _, _, _, _, _, last_marble, _ = s.split()
-    players = int(players)
-    last_marble = int(last_marble)
-
+def play(players, last_marble):
     scores = [0] * players
 
     marbles = collections.deque([0])
@@ -24,12 +20,21 @@ def part1(s):
 
         player = (player + 1) % players
 
-    answer = max(scores)
+    return scores
+
+def part1(s):
+    players, _, _, _, _, _, last_marble, _ = s.split()
+
+    answer = max(play(int(players), int(last_marble)))
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    players, _, _, _, _, _, last_marble, _ = s.split()
+
+    answer = max(play(int(players), 100 * int(last_marble)))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 9)
 part1(INPUT)
