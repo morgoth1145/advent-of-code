fc89e112166eccb6e4c0230779368e7c872f7eb6
Patrick Hogg <phogg@novamoon.net>
2022-03-13 21:52:16 -0400

    2018 Day 7 Part 2
    Man, this makes it look like I did part 1 with ease.
    
    First I did parallel processing using the the order from part 1, thinking that that would be preserved. (Spoilers: It isn't.) While doing this I had an off by one error which cost me more time! Then I started the same task on multiple workers (after a lot of bumbling about at that) because that's not wasteful or redundant!
    
    Eventually I actually coded a simulation of the parallel task handling correctly. Why was this so hard? I've literally written parallel task systems for work! Ugh.
    
    Would have been 54th on the leaderboard. Given how far I jumped up I guess others had problems too.
    
    Time: 0:23:53.887044

--- 2018/07/solution.py
+++ 2018/07/solution.py
@@ -12,32 +12,66 @@ def parse_input(s):
         all_steps.add(b)
         all_steps.add(a)
 
-    return dep_graph, all_steps
+    return dep_graph, sorted(all_steps)
+
+def best_next_step(all_steps, dep_graph, steps_started, steps_done):
+    for step in all_steps:
+        if step in steps_started:
+            continue
+        if len(dep_graph[step] - steps_done) == 0:
+            # We can do this now!
+            return step
 
 def part1(s):
     dep_graph, all_steps = parse_input(s)
 
-    todo = sorted(all_steps)
-
     done = set()
     order = []
 
-    while len(order) < len(todo):
-        for step in todo:
-            if step in done:
-                continue
-            if len(dep_graph[step] - done) == 0:
-                # We can do this now!
-                order.append(step)
-                done.add(step)
-                break
+    while len(order) < len(all_steps):
+        step = best_next_step(all_steps, dep_graph, done, done)
+        order.append(step)
+        done.add(step)
 
     answer = ''.join(order)
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    dep_graph, all_steps = parse_input(s)
+
+    done = set()
+    started = set()
+    workers = [None] * 5
+
+    answer = -1 # Start on second 0
+
+    while len(done) < len(all_steps):
+        answer += 1
+        for idx, processing in enumerate(workers):
+            if processing is None:
+                continue
+            step, remaining = processing
+            remaining -= 1
+            if remaining == 0:
+                done.add(step)
+                workers[idx] = None
+            else:
+                workers[idx] = (step, remaining)
+
+        for idx, processing in enumerate(workers):
+            if processing is not None:
+                continue
+
+            step = best_next_step(all_steps, dep_graph, started, done)
+            if step is None:
+                # No worker can start something new right now
+                break
+
+            workers[idx] = (step, 60 + ord(step) - ord('A') + 1)
+            started.add(step)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 7)
 part1(INPUT)
