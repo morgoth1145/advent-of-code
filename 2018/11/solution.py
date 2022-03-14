2bc23ef1bbb7b9b88b2b92f5885c75a6532abbff
Patrick Hogg <phogg@novamoon.net>
2022-03-14 16:47:38 -0400

    2018 Rewrite Day 11
    It turns out that my initial idea was slower, though in a way on the right track. What I needed was 2D partial sums where the partial sums all start at the origin and include a rectangle. It's possible to compute a block's power with 4 2D partial sums to eliminate sections above and to the left.
    
    Doing this doesn't speed things up that much (maybe 30-40% down to ~7 seconds), but it's a *whole* lot simpler and feels a lot better.

--- 2018/11/solution.py
+++ 2018/11/solution.py
@@ -1,6 +1,8 @@
+import functools
+
 import lib.aoc
 
-def make_grid(serial_number):
+def solve(serial_number, max_square_size=300):
     grid = {}
     for x in range(1, 301):
         for y in range(1, 301):
@@ -10,54 +12,70 @@ def make_grid(serial_number):
             power *= rack_id
             power = (power // 100) % 10
             power -= 5
-            grid[x,y] = power
+            grid[x, y] = power
 
-    return grid
+    @functools.cache
+    def partial_2d_sum(w, h):
+        # AB
+        # CD
+        # The partial 2D sum to D is the same as the 2D sum to B plus the
+        # 2D sum to C minus the 2D sum to A (double-counted) plus cell D
+        if w == 1:
+            # First column
+            if h == 1:
+                return grid[1, 1]
+            return partial_2d_sum(1, h-1) + grid[1, h]
+        if h == 1:
+            # First row
+            return partial_2d_sum(w-1, 1) + grid[w, 1]
 
-def optimize(grid, square_size):
-    presums = {}
+        return (partial_2d_sum(w-1, h) +
+                partial_2d_sum(w, h-1) -
+                partial_2d_sum(w-1, h-1) +
+                grid[w, h])
 
-    for y in range(1, 301):
-        power = 0
-        for x in range(1, square_size):
-            power += grid[x,y]
+    def block_power(x, y, w, h):
+        # ABC
+        # DEF
+        # GHI
+        # The block sum of E to I is the same as the 2D partial sum to I,
+        # minus the double-counting of column A-G and row A-C, plus the
+        # block A (because it was double-subtracted)
+        power = partial_2d_sum(x+w-1, y+h-1)
+        if x > 1:
+            power -= partial_2d_sum(x-1, y+h-1)
+        if y > 1:
+            power -= partial_2d_sum(x+w-1, y-1)
+        if x > 1 and y > 1:
+            power += partial_2d_sum(x-1, y-1)
+        return power
 
-        for x in range(1, 302 - square_size):
-            power += grid[x+square_size-1,y]
-            presums[x,y] = power
-            power -= grid[x,y]
+    best_sums = {}
 
-    best = (0, (0, 0))
+    for square_size in range(1, max_square_size+1):
+        best = (-10 * square_size**2, None)
 
-    for x in range(1, 302 - square_size):
-        power = 0
-        for y in range(1, square_size):
-            power += presums[x,y]
+        for x in range(1, 302-square_size):
+            for y in range(1, 302-square_size):
+                power = block_power(x, y, square_size, square_size)
+                best = max(best, (power, (x, y)))
 
-        for y in range(1, 302 - square_size):
-            power += presums[x,y+square_size-1]
-            best = max(best, (power, (x, y)))
-            power -= presums[x,y]
+        best_sums[square_size] = best
 
-    return best
+    return best_sums
 
 def part1(s):
-    grid = make_grid(int(s))
-
-    best = optimize(grid, 3)
-
-    best_x, best_y = best[1]
-    answer = f'{best_x},{best_y}'
+    power, (x, y) = solve(int(s), max_square_size=3)[3]
+    answer = f'{x},{y}'
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    grid = make_grid(int(s))
+    best_sums = solve(int(s))
 
     best = (-100, 0, None)
 
-    for square_size in range(1, 301):
-        power, coord = optimize(grid, square_size)
+    for square_size, (power, coord) in best_sums.items():
         best = max(best, (power, square_size, coord))
 
     _, square_size, (x, y) = best
