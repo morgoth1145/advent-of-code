1f25222fc2ede6adbc1248d6245cc9c5e2438f51
Patrick Hogg <phogg@novamoon.net>
2022-03-14 20:48:28 -0400

    2018 Day 14 Part 2
    Oo, that's nice. I was flirting with creating a state machine similar to a regex to track when the "win" condition is reached by just passing in the new recipes one at a time, but that sounds complicated. Keeping a short list of the most recent recipe scores is much easier to code. It doesn't take too long either, which works for me. (In fact, generating the recipes takes way longer, I'd initially thought that I was being slowed down by my approach!)
    
    Would have been 20th on the leaderboard! I'm surprised I jumped up so far.
    
    Time: 0:10:51.320884

--- 2018/14/solution.py
+++ 2018/14/solution.py
@@ -20,7 +20,31 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    target = list(map(int, s))
+    most_recent = [-1] * len(target)
+    most_recent = most_recent[2:] + [3, 7]
+
+    recipes = [3, 7]
+    e0, e1 = 0, 1
+
+    while True:
+        combined = recipes[e0] + recipes[e1]
+        if combined > 9:
+            recipes.append(1)
+            most_recent = most_recent[1:] + [1]
+            if most_recent == target:
+                break
+        recipes.append(combined % 10)
+        most_recent = most_recent[1:] + [recipes[-1]]
+        if most_recent == target:
+            break
+
+        e0 = (e0 + 1 + recipes[e0]) % len(recipes)
+        e1 = (e1 + 1 + recipes[e1]) % len(recipes)
+
+    answer = len(recipes) - len(target)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 14)
 part1(INPUT)
