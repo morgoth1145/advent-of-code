7f2851d3efb7f0037438bbfcb9768ebdd3d1669d
Patrick Hogg <phogg@novamoon.net>
2022-03-19 16:48:10 -0400

    2018 Day 22 Part 2
    And now for a graph search. I expected as much, honestly. My lib.graph library is quite well prepared for this so it was just a matter of getting the neighbor function correct.
    
    I did initially mistake what the end state was (thinking we just needed to reach the target with any tool) so I got the wrong answer initially. (And wasted time with dijkstra_length_fuzzy_end's end function!)
    
    I *was* a little worried when my graph search took more than a couple seconds to find the answer (taking about 12 seconds) but it just was a large state space to search through.
    
    Would have jumped up to 33rd on the leaderboard! Wow. This would have been a really solid placement had I not botched part 1.
    
    Time: 0:37:08.617164

--- 2018/22/solution.py
+++ 2018/22/solution.py
@@ -1,17 +1,14 @@
 import functools
 
 import lib.aoc
+import lib.graph
 import lib.lazy_dict
 
-def parse_input(s):
+def parse_cave(s):
     depth, target = s.splitlines()
     depth = int(depth.split()[1])
-    x, y = target.split()[1].split(',')
-
-    return depth, int(x), int(y)
-
-def part1(s):
-    depth, tx, ty = parse_input(s)
+    tx, ty = target.split()[1].split(',')
+    tx, ty = int(tx), int(ty)
 
     MOD = 20183
 
@@ -25,17 +22,59 @@ def part1(s):
             return (y * 48271 + depth) % MOD
         return (erosion_level(x-1, y) * erosion_level(x, y-1) + depth) % MOD
 
-    def risk_level(x, y):
+    def risk_level(coord):
+        x, y = coord
         return erosion_level(x, y) % 3
 
-    answer = sum(risk_level(x, y)
+    return lib.lazy_dict.make_lazy_dict(risk_level), tx, ty
+
+def part1(s):
+    cave, tx, ty = parse_cave(s)
+
+    answer = sum(cave[x, y]
                  for x in range(tx+1)
                  for y in range(ty+1))
 
     print(f'The answer to part one is {answer}')
 
+TORCH = 0
+CLIMBING = 1
+NEITHER = 2
+
+VALID_TOOLS = {
+    0: (TORCH, CLIMBING), # Rocky
+    1: (CLIMBING, NEITHER), # Wet
+    2: (TORCH, NEITHER), # Narrow
+}
+
 def part2(s):
-    pass
+    cave, tx, ty = parse_cave(s)
+
+    def neighbor_fn(state):
+        x, y, tool = state
+        this_risk = cave[x,y]
+
+        for other_tool in VALID_TOOLS[this_risk]:
+            if tool != other_tool:
+                yield (x, y, other_tool), 7
+
+        for nx, ny in [(x-1, y),
+                       (x+1, y),
+                       (x, y+1),
+                       (x, y-1)]:
+            if nx < 0 or ny < 0:
+                continue
+            if tool not in VALID_TOOLS[cave[nx, ny]]:
+                continue
+            yield (nx, ny, tool), 1
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+
+    answer = lib.graph.dijkstra_length(graph,
+                                       (0, 0, TORCH),
+                                       (tx, ty, TORCH))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2018, 22)
 part1(INPUT)
