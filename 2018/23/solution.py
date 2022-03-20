818c7a3f11429f54ffcf4cd4a93eef7df858d0e9
Patrick Hogg <phogg@novamoon.net>
2022-03-19 20:21:30 -0400

    2018 Rewrite Day 23 Part 2
    Going further on the assumption that all inputs have fairly smooth gradients, it makes sense to actually check all directions from a given point instead of just picking the first direction that seems to yield improvements. This way we make the best possible choice at any poin in the search.
    
    Furthermore, the factors can be figured out on the fly. Just keep doubling the distance searched in each direction until the number of bots in range drops again. That way we'll make the largest possible jump in any direction to either maximize the bot count or maximize distance.
    
    Ultimately I'm still not sure that this is guaranteed to work in general (it is assuming that it won't jump over the best point) but it works starting at the origin and at all bot positions for both my input and the test input. As such it seems fairly robust, at least for how this problem has its inputs generated. I have seen some adversarial inputs which break this, but I'd need an entirely different approach (probably based on the recursive partitioning idea) to handle everything properly.
    
    Alternately, I may come back to this and explore this approach which seems fast and similarly accurate: https://www.reddit.com/r/adventofcode/comments/a9co1u/comment/ecmpxad/?utm_source=reddit&utm_medium=web2x&context=3

--- 2018/23/solution.py
+++ 2018/23/solution.py
@@ -37,45 +37,61 @@ def part2(s):
 
     # Guess the center as the weighted average of the bots
     # (The weight is the inverse of the radius)
-    gx = round(sum(x/r for r, (x, y, z) in bots) / sum(1/r for r,_ in bots))
-    gy = round(sum(y/r for r, (x, y, z) in bots) / sum(1/r for r,_ in bots))
-    gz = round(sum(z/r for r, (x, y, z) in bots) / sum(1/r for r,_ in bots))
+    tot_weight = sum(1/(r**3) for r,_ in bots)
+    gx = round(sum(x/(r**3) for r, (x, y, z) in bots) / tot_weight)
+    gy = round(sum(y/(r**3) for r, (x, y, z) in bots) / tot_weight)
+    gz = round(sum(z/(r**3) for r, (x, y, z) in bots) / tot_weight)
 
-    best_count = 0
-    best_dist = 0
+    IN_RANGE = bots_in_range(bots, gx, gy, gz)
+    DIST = abs(gx) + abs(gy) + abs(gz)
 
     while True:
-        improved = False
+        best_count = (IN_RANGE, (gx, gy, gz))
+        best_dist = (DIST, (gx, gy, gz))
 
-        dx = -1 if gx > 0 else 1
-        dy = -1 if gy > 0 else 1
-        dz = -1 if gz > 0 else 1
+        for dx in (-1, 0, 1):
+            for dy in (-1, 0, 1):
+                for dz in (-1, 0, 1):
+                    if dx == dy == dz == 0:
+                        continue
 
-        for factor in (100000, 10000, 1000, 100, 10, 1):
-            for fx in (-4, -2, -1, 0, 1, 2, 4):
-                for fy in (-4, -2, -1, 0, 1, 2, 4):
-                    for fz in (-4, -2, -1, 0, 1, 2, 4):
-                        if fx == fy == fz:
-                            continue
-                        while True:
-                            nx = gx + dx * fx * factor
-                            ny = gy + dy * fy * factor
-                            nz = gz + dz * fz * factor
-                            count = bots_in_range(bots, nx, ny, nz)
-                            dist = abs(nx) + abs(ny) + abs(nz)
-                            if count < best_count:
-                                break
-                            if count == best_count and dist > best_dist:
-                                break
-                            improved = True
-                            best_count = count
-                            best_dist = dist
-                            gx, gy, gz = nx, ny, nz
+                    factor = 1
+                    last_count = IN_RANGE
+                    last_dist = DIST
+                    while True:
+                        nx = gx + dx * factor
+                        ny = gy + dy * factor
+                        nz = gz + dz * factor
 
-        if not improved:
-            break
+                        count = bots_in_range(bots, nx, ny, nz)
+                        if count < last_count:
+                            break
+                        if count > last_count:
+                            last_count = count
+                            if best_count[0] < count:
+                                best_count = (count, (nx, ny, nz))
 
-    answer = abs(gx) + abs(gy) + abs(gz)
+                        dist = abs(nx) + abs(ny) + abs(nz)
+                        if dist < last_dist:
+                            last_dist = dist
+                            if best_dist[0] > dist:
+                                best_dist = (dist, (nx, ny, nz))
+
+                        factor *= 2
+
+        if best_count[0] > IN_RANGE:
+            IN_RANGE, (gx, gy, gz) = best_count
+            DIST = abs(gx) + abs(gy) + abs(gz)
+            continue
+
+        if best_dist[0] < DIST:
+            DIST, (gx, gy, gz) = best_dist
+            continue
+
+        # We made no improvement, this should be the answer
+        break
+
+    answer = DIST
 
     print(f'The answer to part two is {answer}')
 
