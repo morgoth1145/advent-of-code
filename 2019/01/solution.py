f6426cbb7d9ce1f87129cd1c7d8c3563fc96b226
Patrick Hogg <phogg@novamoon.net>
2022-03-19 22:47:56 -0400

    2019 Day 1 Part 2
    And now fuel needs fuel recursively.
    
    Would have been 3rd, but I did the problem in 2019 so this doesn't exactly count.
    
    Time: 0:01:45.787237

--- 2019/01/solution.py
+++ 2019/01/solution.py
@@ -7,7 +7,15 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    def recursive_fuel_cost(m):
+        fuel = max(0, m // 3 - 2)
+        if fuel:
+            fuel += recursive_fuel_cost(fuel)
+        return fuel
+
+    answer = sum(map(recursive_fuel_cost, map(int, s.splitlines())))
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 1)
 part1(INPUT)
