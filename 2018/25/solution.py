1105064c00870ae5f52f9a1a593a2c762bd46092
Patrick Hogg <phogg@novamoon.net>
2022-03-19 21:44:47 -0400

    2018 Day 25 Part 1 (There is no Part 2)
    As expected, a relatively straightforward problem to round out the year. However, I bungled it like a dumbo. I thought that it'd be smart to limit the O(n^2) loop where I compute the links by iterating only over points which aren't in the outer loop. But I neglected to adjust j correctly and got bogus answers! In the end the special looping didn't even save any real time.
    
    Aside from that the problem went fine, but that was a dumb way to finish out the year...
    
    Would have been 67th on the leaderboard. (Would have been 22nd had I note wasted 4 minutes on a stupid off by one error!)
    
    Time: 0:09:18.547884

---
+++ 2018/25/solution.py
@@ -0,0 +1,45 @@
+import collections
+
+import lib.aoc
+
+def parse_input(s):
+    for line in s.splitlines():
+        yield tuple(map(int, line.split(',')))
+
+def part1(s):
+    data = list(parse_input(s))
+
+    links = collections.defaultdict(set)
+
+    for i, (x, y, z, w) in enumerate(data):
+        for j, (x2, y2, z2, w2) in enumerate(data):
+            dist = abs(x-x2) + abs(y-y2) + abs(z-z2) + abs(w-w2)
+            if dist <= 3:
+                links[i].add(j)
+
+    answer = 0
+    handled = set()
+
+    for i in range(len(data)):
+        if i in handled:
+            continue
+
+        answer += 1
+
+        to_handle = [i]
+        while to_handle:
+            i = to_handle.pop(-1)
+            handled.add(i)
+
+            for j in links[i]:
+                if j not in handled:
+                    to_handle.append(j)
+
+    print(f'The answer to part one is {answer}')
+
+def part2(s):
+    print('There is no part two for Christmas!')
+
+INPUT = lib.aoc.get_input(2018, 25)
+part1(INPUT)
+part2(INPUT)
