3f3841712ae75519a37c57b6673548646131d813
Patrick Hogg <phogg@novamoon.net>
2022-03-20 17:16:13 -0400

    2019 Day 7 Part 2
    Ah, right. And now the last output can feed back on the first input. As a result the program needs to run in the background. Good thing I already have that happening, though in 2019 this was a complete surprise.
    
    One thing I don't like is the manual linking of input and output channels. In fact, it may make sense to have the intcode VM allow clients to pass in the input channel. That would simplify the chaining here a good bit.
    
    Would have been first on the leaderboard, but I did the problem in 2019 so this doesn't exactly count. (Plus I specifically designed the Intcode VM fo chaining like this which was a surprise back when this problem landed!)
    
    Time: 0:10:09.461879

--- 2019/07/solution.py
+++ 2019/07/solution.py
@@ -1,4 +1,5 @@
 import itertools
+import threading
 
 import lib.aoc
 
@@ -19,7 +20,36 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    answer = 0
+
+    for phases in itertools.permutations(range(5, 10)):
+        first_in = None
+        last_out = None
+
+        def link_channels(prev_out, next_in):
+            for val in prev_out:
+                next_in.send(val)
+
+        for p in phases:
+            in_chan, out_chan = intcode.Program(s).run()
+            in_chan.send(p)
+            if last_out is None:
+                first_in = in_chan
+            else:
+                threading.Thread(target=link_channels,
+                                 args=(last_out, in_chan)).start()
+            last_out = out_chan
+
+        # Seed the chain
+        first_in.send(0)
+
+        for signal in last_out:
+            first_in.send(signal)
+
+        # This is the final signal
+        answer = max(answer, signal)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 7)
 part1(INPUT)
