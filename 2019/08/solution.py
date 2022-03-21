667afc2babaf7237c2bd1f7303cd0be3dfceab58
Patrick Hogg <phogg@novamoon.net>
2022-03-20 20:43:14 -0400

    2019 Day 8 Part 2
    And now to assemble the layers into a combined image. Pretty easy, then I can run it through lib.ocr for parsing the text! I did keep the wrong cells in the set initially though, and I composed the image backwards initially. At least those were easy to fix.
    
    Would have been 84th, 85th, or 86th on the leaderboard. Actually lower than I expected, but even so I did the problem in 2019 so this doesn't exactly count. Plus the lib.ocr library gave me a helping hand.
    
    Time: 0:09:56.372400

--- 2019/08/solution.py
+++ 2019/08/solution.py
@@ -1,6 +1,7 @@
 import collections
 
 import lib.aoc
+import lib.ocr
 
 def parse_layers(s, width, height):
     nums = list(map(int, s))
@@ -8,7 +9,12 @@ def parse_layers(s, width, height):
     assert(len(nums) % (width * height) == 0)
 
     for i in range(0, len(nums), width * height):
-        layer = nums[i:i+width*height]
+        layer_nums = nums[i:i+width*height]
+
+        layer = []
+
+        for j in range(0, width*height, width):
+            layer.append(layer_nums[j:j+width])
 
         yield layer
 
@@ -16,7 +22,9 @@ def part1(s):
     best = (len(s), 0)
 
     for layer in parse_layers(s, 25, 6):
-        c = collections.Counter(layer)
+        c = collections.Counter()
+        for row in layer:
+            c += collections.Counter(row)
         key = (c[0], c[1] * c[2])
         best = min(best, key)
 
@@ -25,7 +33,26 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    image = [[2] * 25
+             for _ in range(6)]
+
+    for layer in parse_layers(s, 25, 6):
+        for image_row, layer_row in zip(image, layer):
+            for x, (image_cell, layer_cell) in enumerate(zip(image_row, layer_row)):
+                if image_cell == 2:
+                    image_row[x] = layer_cell
+
+    white_cells = set()
+
+    for y, row in enumerate(image):
+        for x, cell in enumerate(row):
+            assert(cell != 2)
+            if cell == 1:
+                white_cells.add((x, y))
+
+    answer = lib.ocr.parse_coord_set(white_cells)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 8)
 part1(INPUT)
