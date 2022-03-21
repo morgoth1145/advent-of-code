94c701c44ab16b54cf593c5bd38c2ef8fe0769d8
Patrick Hogg <phogg@novamoon.net>
2022-03-20 21:57:00 -0400

    2019 Day 10 Part 2
    Ah, and now we need to know more about the angles. Namely, which asteroids are visible at any given angle and what order the angles come in.
    
    I'm sure there's a much smarter way to sort the angles, but breaking the angles into right half and left half lets me sort based on dy/dx to get clockwise order. Then it's just a matter of stitching those together with the two vertical angles and I've got the full sweep. When cycling through the angles, we can just pick the asteroid with the smallest manhattan distance to the lookout asteroid. Actually pretty easy!
    
    Would have dropped down to 8th, but I did the problem in 2019 so this doesn't exactly count. (Still though, that's high!)
    
    Time: 0:23:36.818431

--- 2019/10/solution.py
+++ 2019/10/solution.py
@@ -1,49 +1,111 @@
+import collections
 import math
 
 import lib.aoc
 import lib.grid
 
-def part1(s):
-    grid = lib.grid.FixedGrid.parse(s)
+def compute_minimum_angle(dx, dy):
+    if dx == 0:
+        if dy > 0:
+            return 0, 1
+        else:
+            return 0, -1
+    if dy == 0:
+        if dx > 0:
+            return 1, 0
+        else:
+            return -1, 0
+    else:
+        # Simplify the angle
+        denom = math.gcd(abs(dx), abs(dy))
+        return dx // denom, dy // denom
 
-    answer = 0
+# Constructs a map of angle to which asteroids are visible at that angle
+def determine_asteroid_visibility(grid, x, y):
+    asteroids = collections.defaultdict(set)
+
+    for (ox, oy), c in grid.items():
+        if c == '.':
+            continue
+        if x == ox and y == oy:
+            continue
+
+        asteroids[compute_minimum_angle(ox-x, oy-y)].add((ox, oy))
+
+    return asteroids
+
+def get_best_asteroid(grid):
+    best = (0, None)
 
     for (x, y), c in grid.items():
         if c == '.':
             continue
-        angles = set()
-        for (ox, oy), c in grid.items():
-            if c == '.':
-                continue
-            if x == ox and y == oy:
-                continue
 
-            dx, dy = x-ox, y-oy
-            if dx == 0:
-                if dy > 0:
-                    angles.add((0, 1))
-                else:
-                    angles.add((0, -1))
-            elif dy == 0:
-                if dx > 0:
-                    angles.add((1, 0))
-                else:
-                    angles.add((-1, 0))
-            else:
-                # Simplify the angle
-                denom = math.gcd(abs(dx), abs(dy))
-                dx /= denom
-                dy /= denom
-                angles.add((dx, dy))
+        asteroids = determine_asteroid_visibility(grid, x, y)
 
         # len(angles) tells us how many angles survived, so how many asteroids
         # we can see
-        answer = max(answer, len(angles))
+        best = max(best, (len(asteroids), (x, y)))
+
+    return best[1]
+
+def part1(s):
+    grid = lib.grid.FixedGrid.parse(s)
+
+    x, y = get_best_asteroid(grid)
+
+    answer = len(determine_asteroid_visibility(grid, x, y))
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s)
+
+    x, y = get_best_asteroid(grid)
+
+    asteroids = determine_asteroid_visibility(grid, x, y)
+
+    right_side_angles = []
+    left_side_angles = []
+
+    for dx, dy in asteroids:
+        if dx > 0:
+            right_side_angles.append((dx, dy))
+        elif dx < 0:
+            left_side_angles.append((dx, dy))
+
+    # Order angles in the right quadrant from top to bottom
+    right_side_angles.sort(key=lambda a: a[1]/a[0],
+                           reverse=True)
+
+    # Order angles in the right quadrant from bottom to top
+    left_side_angles.sort(key=lambda a: a[1]/a[0])
+
+    angle_order = []
+    if (0, 1) in asteroids:
+        angle_order.append((0, 1))
+    angle_order += right_side_angles
+    if (0, -1) in asteroids:
+        angle_order.append((0, -1))
+    angle_order += left_side_angles
+
+    destruction_order = []
+
+    while len(destruction_order) < 200:
+        for a in angle_order:
+            if len(asteroids[a]) == 0:
+                continue
+
+            target = min(asteroids[a], key=lambda o: abs(o[0]-x) + abs(o[1]-y))
+
+            asteroids[a].remove(target)
+            destruction_order.append(target)
+
+    x, y = destruction_order[199]
+
+    answer = x * 100 + y
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 10)
 part1(INPUT)
