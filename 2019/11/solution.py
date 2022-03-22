42c26060c962fcfa6ef19c903f8a193d7edeeebe
Patrick Hogg <phogg@novamoon.net>
2022-03-21 20:10:29 -0400

    2019 Day 11 Part 2
    Oh, we need a different starting panel color. That's not too bad though, and I just have to pass the resultant grid to lib.ocr and I get my answer! Too bad it only takes sets, I should extend it to take dictionaries as well...
    
    Would have been 5th again on the leaderboard, but I did the problem in 2019 so this doesn't exactly count. Plus by lib.ocr library gives me a good leg up here!
    
    Time: 0:07:13.777853

--- 2019/11/solution.py
+++ 2019/11/solution.py
@@ -1,9 +1,10 @@
 import lib.aoc
 import lib.math
+import lib.ocr
 
 intcode = __import__('2019.intcode').intcode
 
-def get_painted_grid(s):
+def get_painted_grid(s, first_panel):
     in_chan, out_chan = intcode.Program(s).run()
 
     grid = {}
@@ -13,7 +14,7 @@ def get_painted_grid(s):
     dx, dy = 0, -1
 
     # Initial panel
-    in_chan.send(0)
+    in_chan.send(first_panel)
 
     for color in out_chan:
         turn = out_chan.recv()
@@ -36,14 +37,22 @@ def get_painted_grid(s):
     return grid
 
 def part1(s):
-    grid = get_painted_grid(s)
+    grid = get_painted_grid(s, 0)
 
     answer = len(grid)
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    grid = get_painted_grid(s, 1)
+
+    white_panels = [coord
+                    for coord, color in grid.items()
+                    if color == 1]
+
+    answer = lib.ocr.parse_coord_set(white_panels)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 11)
 part1(INPUT)
