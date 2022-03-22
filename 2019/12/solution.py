dd75230cbdee5e205a3a405f12d9ef4b6b365a8a
Patrick Hogg <phogg@novamoon.net>
2022-03-21 20:45:00 -0400

    2019 Day 12 Part 2
    And part 2. I can't forget the trick to this even if I tried. Each component of the moon's properties acts independently so we can simulate each set of components individually to get the x, y, and z cycle congruencies. It still takes a moment, but it finishes.
    
    Then once we have those congruencies we just need to find out how long until those congruencies line up. I was going to use lib.math.chinese_remainder for this (to not have to hardcode any assumptions about the offsets in the congruencies) but that returns 0 because the offsets are all 0 in practice! As such I had to whip up the computation manually with math.gcd. I'll expand lib.math.chinese_congruencies in a follow-up.
    
    Would have been 21st, but I did the problem in 2019 so this doesn't exactly count. Especially since I already know the trick to this problem!
    
    Time: 0:22:55.327180

--- 2019/12/solution.py
+++ 2019/12/solution.py
@@ -1,3 +1,5 @@
+import math
+
 import lib.aoc
 def parse_moons(s):
     for c in '<xyz=>':
@@ -37,8 +39,43 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def get_cycle_info(moons, comp_idx):
+    # Isolate the component in question
+    moons = [([p[comp_idx]], [v[comp_idx]])
+             for p, v in moons]
+
+    def get_state_key():
+        return tuple((p[0], v[0])
+                     for p, v in moons)
+
+    step_num = 0
+    states = {get_state_key(): step_num}
+
+    while True:
+        step_num += 1
+        step(moons)
+
+        key = get_state_key()
+        last_seen = states.get(key)
+        if last_seen is not None:
+            return last_seen, step_num - last_seen
+
+        states[key] = step_num
+
 def part2(s):
-    pass
+    moons = list(parse_moons(s))
+
+    x_off, x_cycle = get_cycle_info(moons, 0)
+    y_off, y_cycle = get_cycle_info(moons, 1)
+    z_off, z_cycle = get_cycle_info(moons, 2)
+
+    assert(x_off == y_off == z_off == 0)
+
+    answer = 1
+    for val in (x_cycle, y_cycle, z_cycle):
+        answer = (answer * val) // math.gcd(answer, val)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 12)
 part1(INPUT)
