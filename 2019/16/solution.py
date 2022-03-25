4fa79b5bd062c5cd986f3461d719da33276f543a
Patrick Hogg <phogg@novamoon.net>
2022-03-24 22:01:42 -0400

    2019 Day 16 Part 2
    And part 2! Yeah, this is a doozy if you don't know what the trick is. Because the mask pattern always starts with zero we can discard any element before the answer offset. Furthermore, the offset for this problem is *deep* in the list of numbers. So deep in fact that the phase transform ends up being a reverse partial sum for the positions we care about! Partial sums are quick (unlike this FFT mask nonsense) so it's pretty easy to do 100 partial sum passes (all mod 10) and get the answer.
    
    It took me nearly an hour to figure out the trick in 2019. In fact, I'd managed to place 113th in 2019, so honestly I did *well* back then!
    
    Would have been 2nd on the leaderboard, but having done this problem in 2019 I already knew the trick. This *definitely* doesn't count!
    
    Time: 0:20:05.074362

--- 2019/16/solution.py
+++ 2019/16/solution.py
@@ -29,8 +29,36 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def partial_sums_mod_10(nums):
+    out_nums = []
+
+    s = 0
+    for n in nums:
+        s += n
+        out_nums.append(s % 10)
+
+    return out_nums
+
 def part2(s):
-    pass
+    nums = list(map(int, s))
+    offset = int(s[:7])
+
+    tot_length = len(nums) * 10000
+    # Assuming the offset is far enough in then we can discard the *vast*
+    # majority of the repeat list and compute each phase via partial sums,
+    # summing from the right
+    assert(offset * 2 - 1 >= tot_length)
+
+    remain_length = tot_length - offset
+    repeat_times = (remain_length + len(nums) - 1) // len(nums)
+    reverse_nums = (nums[::-1] * repeat_times)[:remain_length]
+
+    for _ in range(100):
+        reverse_nums = partial_sums_mod_10(reverse_nums)
+
+    answer = ''.join(map(str, reverse_nums[-8:]))[::-1]
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 16)
 part1(INPUT)
