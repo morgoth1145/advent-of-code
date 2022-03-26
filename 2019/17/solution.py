c1a2bf4b00d57eb42aa96d5930a61e00761c00bf
Patrick Hogg <phogg@novamoon.net>
2022-03-26 00:06:40 -0400

    2019 Day 17 Part 2
    Now for the really fun part of this problem. The first part here is extracting the sequence of steps the robot needs to take to traverse the entire scaffold. Technically there is some ambiguity in the problem (the robot could turn at any given intersection) but we just need to follow the path and turn whenever a turn is forced.
    
    The second part is the really crazy bit though. That path needs to be compressed into a <= 20 character main program and 3 <= 20 character functions. Back in 2019 I did this step by hand initially. That's 100% the right way to do it to try to leaderboard, but this time I decided to challenge myself and solve it programatically with no hand solving.
    
    The basic idea is a backtracking search. At any given point see if an already deduced subprogram matches the next sequence of steps that remain. If so, add that to the main program and search further. If we can't find one that matches (or if the main program gets too long and we backtrack), try and find a new subprogram (assuming we have room, we max at 3!) and see if that works. And if that fails, trim the subprogram until it's empty or we find a solution.
    
    I did of course hit some bugs in this process as it's somewhat involved.
    1) Most annoyingly, I had the wrong overall sequence to start out due to having the bot face down initially. (I always invert my y!)
    2) I accidentally computed the subprogram length to be len(remaining_steps) in find_solution at one point. Unluckily it didn't hit an exception but returned a garbage solution!
    3) I also got tripped up by forgetting to convert my characters back to ints to pass to the subprogram, and with forgetting to put a newline after disabling the continuous camera feed!
    
    Still, I did programmatically solve this in a respectable amount of time. This part really screams to be solved by hand if you want to be fast.
    
    Would have missed the leaderboard by ~5.5 minutes. In fact, I was ~6.75 minutes slower than in 2019, but I did this part by hand in 2019. I'm not too mad about my showing here :)
    
    Time: 0:50:47.789638

--- 2019/17/solution.py
+++ 2019/17/solution.py
@@ -3,10 +3,13 @@ import lib.grid
 
 intcode = __import__('2019.intcode').intcode
 
-def part1(s):
+def get_scaffold_grid(s):
     _, out_chan = intcode.Program(s).run()
 
-    grid = lib.grid.FixedGrid.parse(''.join(map(chr, out_chan)).strip())
+    return lib.grid.FixedGrid.parse(''.join(map(chr, out_chan)).strip())
+
+def part1(s):
+    grid = get_scaffold_grid(s)
 
     answer = 0
 
@@ -24,7 +27,140 @@ def part1(s):
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    grid = get_scaffold_grid(s).to_dict()
+
+    x, y = next(coord
+                for coord, c in grid.items()
+                if c == '^')
+    dx, dy = 0, -1
+
+    raw_steps = []
+
+    while True:
+        turn_options = [('L', dy, -dx),
+                        ('R', -dy, dx)]
+
+        good_turn = False
+        for turn, dx, dy in turn_options:
+            n = (x+dx, y+dy)
+            if grid.get(n) == '#':
+                good_turn = True
+                break
+
+        if not good_turn:
+            break # Must be done
+
+        raw_steps.append(turn)
+
+        move_units = 0
+        while grid.get((x+dx, y+dy)) == '#':
+            move_units += 1
+            x += dx
+            y += dy
+
+        raw_steps.append(move_units)
+
+    MAX_SEQUENCE_LENGTH = 20
+    MAX_SUBPROGRAMS = 3
+
+    def find_solution(remaining_steps, main_sequence, subprograms):
+        if len(','.join(map(str, main_sequence))) > MAX_SEQUENCE_LENGTH:
+            return None
+
+        if len(remaining_steps) == 0:
+            return main_sequence, subprograms
+
+        for subprogram_idx, subprogram in enumerate(subprograms):
+            subprogram_len = len(subprogram)
+            if len(remaining_steps) < subprogram_len:
+                continue
+
+            if not all(remaining_steps[idx] == step
+                       for idx, step in enumerate(subprogram[:-1])):
+                # This subprogram can't match the next step
+                continue
+
+            last_step = subprogram[-1]
+            if last_step == remaining_steps[subprogram_len-1]:
+                solution = find_solution(remaining_steps[subprogram_len:],
+                                         main_sequence + (subprogram_idx,),
+                                         subprograms)
+                if solution is not None:
+                    return solution
+            elif (isinstance(last_step, int) and
+                  isinstance(remaining_steps[subprogram_len-1], int) and
+                  last_step < remaining_steps[subprogram_len-1]):
+                new_remaining = list(remaining_steps[subprogram_len-1:])
+                new_remaining[0] -= last_step
+                solution = find_solution(new_remaining,
+                                         main_sequence + (subprogram_idx,),
+                                         subprograms)
+                if solution is not None:
+                    return solution
+
+        if len(subprograms) == MAX_SUBPROGRAMS:
+            return None
+
+        # Try to find a new subprogram
+        cand_subprogram = []
+
+        for step in remaining_steps:
+            cand_subprogram.append(step)
+
+            if len(','.join(map(str, cand_subprogram))) >= MAX_SEQUENCE_LENGTH:
+                break
+
+        while len(','.join(map(str, cand_subprogram))) > MAX_SEQUENCE_LENGTH:
+            if isinstance(cand_subprogram[-1], int):
+                cand_subprogram[-1] -= 1
+                if cand_subprogram[-1] == 0:
+                    cand_subprogram.pop(-1)
+            else:
+                cand_subprogram.pop(-1)
+
+        while cand_subprogram:
+            solution = find_solution(remaining_steps,
+                                     main_sequence,
+                                     subprograms + (tuple(cand_subprogram),))
+            if solution is not None:
+                return solution
+
+            # The subprogram didn't work, trim it
+            if isinstance(cand_subprogram[-1], int):
+                cand_subprogram[-1] -= 1
+                if cand_subprogram[-1] == 0:
+                    cand_subprogram.pop(-1)
+            else:
+                cand_subprogram.pop(-1)
+
+    main, subprograms = find_solution(raw_steps, tuple(), tuple())
+
+    main = ','.join('ABC'[subprogram] for subprogram in main)
+
+    assert(len(main) <= MAX_SEQUENCE_LENGTH)
+
+    subprograms = [','.join(map(str, subprogram))
+                   for subprogram in subprograms]
+
+    assert(all(len(subprogram) <= MAX_SEQUENCE_LENGTH
+               for subprogram in subprograms))
+
+    p = intcode.Program(s)
+    p.memory[0] = 2 # Wake up robot
+    in_chan, out_chan = p.run()
+
+    for routine in [main] + subprograms:
+        for c in routine:
+            in_chan.send(ord(c))
+        in_chan.send(ord('\n'))
+
+    # Turn off continuous video feed
+    in_chan.send(ord('n'))
+    in_chan.send(ord('\n'))
+
+    answer = list(out_chan)[-1]
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 17)
 part1(INPUT)
