06c1475dafc0ebd9e5634f3d6249376bd6df3533
Patrick Hogg <phogg@novamoon.net>
2022-03-26 16:40:27 -0400

    2019 Day 22 Part 2
    And here's part 2! I got to skip a bunch of the discoveries this time around and had a much nicer time solving this.
    
    First off, the shuffle operations can all be represented by modular formulae. These in turn can be composed to get a single RNG formula to perform a single shuffle. Then once that's done the RNG can be "raised" to a power (applied n times) to make a super RNG to apply the shuffle N times. This can be done with fast exponentiation because I certainly don't have time to wait for ~102 trillion applications!
    
    Finally, the trickiest part. We need to go back in time! We want to know what card ends up in position 2020. There is a clever approach that only uses forward applications of the RNG (I think that it cycles after DECK_SIZE-1 applications) but I don't really like that approach. Instead, the RNG formula needs to be inverted. Back in 2019 this was a pain since I didn't know how to get a modular multiplicative inverse (still don't really, I'm not super comfortable with the extended euclidian algorithm). But not to worry, Python knows how to do that for me! Once I get that, inverting the RNG is easy. Then the reverse RNG directly gives the right answer! Yay!
    
    Would have been 40th on the leaderboard (or something like 22nd if I hadn't wasted 11 minutes in part 1) but I did the problem in 2019 and *really* learned from it so this definitely doesn't count. Still though, I'll take that showing!
    
    Time: 0:57:39.341757

--- 2019/22/solution.py
+++ 2019/22/solution.py
@@ -1,49 +1,90 @@
 import lib.aoc
 
-def parse_steps(s):
+class RNG:
+    def __init__(self, factor, offset, mod):
+        self.factor = factor
+        self.offset = offset
+        self.mod = mod
+
+    def invert(self):
+        # Modular inverse of factor
+        new_factor = pow(self.factor, -1, self.mod)
+        return RNG(new_factor,
+                   new_factor * (self.mod - self.offset) % self.mod,
+                   self.mod)
+
+    def __add__(self, shift):
+        assert(isinstance(shift, int))
+        return RNG(self.factor,
+                   (self.offset + shift) % self.mod,
+                   self.mod)
+
+    def __mul__(self, other):
+        assert(isinstance(other, RNG))
+        assert(self.mod == other.mod)
+
+        return RNG((other.factor * self.factor) % self.mod,
+                   (other.factor * self.offset + other.offset) % self.mod,
+                   self.mod)
+
+    def __pow__(self, power):
+        if power == 0:
+            return RNG(1, 0, self.mod)
+        if power == 1:
+            return self
+
+        pow2 = self * self
+
+        if power & 1:
+            return (pow2 ** (power >> 1)) * self
+        else:
+            return pow2 ** (power >> 1)
+
+    def __call__(self, value):
+        return (self.factor * value + self.offset) % self.mod
+
+def parse_rng(s, mod):
+    rng = RNG(1, 0, mod)
+
     for line in s.splitlines():
+        if line == 'deal into new stack':
+            # Reverse
+            # This is the same as dealing with increment of (mod - 1)
+            # followed by a cut of 1
+            rng *= RNG(mod-1, mod-1, mod)
+            continue
+
         parts = line.split()
         if parts[0] == 'cut':
-            yield 'cut', int(parts[1])
+            # Adjust the offset
+            cut_by = int(parts[1])
+
+            # Cuts move to the end so the shift is negated
+            shift_by = -cut_by
+            if shift_by < 0:
+                shift_by += mod
+
+            rng += shift_by
             continue
 
-        assert(parts[0] == 'deal')
-        if parts[1] == 'with':
-            assert(parts[2] == 'increment')
-            yield 'increment', int(parts[3])
-            continue
+        assert(line.startswith('deal with increment '))
+        # Same as multiplying by a new factor
+        rng *= RNG(int(parts[3]), 0, mod)
+        continue
 
-        assert(parts[1] == 'into')
-        assert(parts[2] == 'new')
-        assert(parts[3] == 'stack')
-        yield 'reverse', 0
-
-def shuffle(cards, steps):
-    for inst, count in steps:
-        if inst == 'reverse':
-            cards = cards[::-1]
-        elif inst == 'cut':
-            cards = cards[count:] + cards[:count]
-        else:
-            assert(inst == 'increment')
-            new_cards = [None] * len(cards)
-            pos = 0
-            for idx, c in enumerate(cards):
-                new_cards[pos] = c
-                pos = (pos + count) % len(cards)
-            cards = new_cards
-            assert(None not in cards)
-    return cards
+    return rng
 
 def part1(s):
-    cards = shuffle(list(range(10007)), parse_steps(s))
-
-    answer = cards.index(2019)
+    answer = parse_rng(s, 10007)(2019)
 
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    super_rng = parse_rng(s, 119315717514047) ** 101741582076661
+
+    answer = super_rng.invert()(2020)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 22)
 part1(INPUT)
