479c816757ac19fffddfab726c7af6c5052a0531
Patrick Hogg <phogg@novamoon.net>
2022-03-26 19:38:43 -0400

    2019 Day 23 Part 2
    Oof, this was the reason I had deadlock detection in my channel library yet I still had trouble! Long story short, I included too few channels in the deadlock detection and was informed of deadlock sporadically even when it didn't happen.
    
    That aside, this also went kind of smoothly. There's a ton of duplication with part 1 and the code is cluttered, but it works well and is stable! (How many people actually dealt with real threading issues in this problem, I wonder?)
    
    Would have missed the leaderboard by ~19 minutes! But still, this was way smoother than 2019 where it took 85 minutes and was still ridden with race conditions!
    
    Time: 0:39:37.915586

--- 2019/23/solution.py
+++ 2019/23/solution.py
@@ -6,10 +6,11 @@ import lib.channels
 intcode = __import__('2019.intcode').intcode
 
 class Packet:
-    def __init__(self, dest, x, y):
+    def __init__(self, dest, x, y, nat_generated=False):
         self.dest = dest
         self.x = x
         self.y = y
+        self.nat_generated = nat_generated
 
 def read_packets_and_forward(comp_out_chan, controller_chan):
     for dest in comp_out_chan:
@@ -62,13 +63,80 @@ def part1(s):
     for in_chan in in_channels:
         in_chan.close()
 
-    p = intcode.Program(s)
-    in_chan, out_chan = p.run(stop_on_no_input=True)
-
     print(f'The answer to part one is {answer}')
 
 def part2(s):
-    pass
+    in_channels = []
+    out_channels = []
+
+    controller_chan = lib.channels.BufferedChannel()
+
+    # Set up channels
+    for _ in range(50):
+        in_channels.append(lib.channels.BufferedChannel())
+        out_channels.append(lib.channels.BufferedChannel())
+
+    deadlock_chan = lib.channels.detect_deadlock_events(101,
+                                                        controller_chan,
+                                                        *(in_channels + out_channels))
+
+    # Initialize the input channels
+    for addr, in_chan in enumerate(in_channels):
+        in_chan.send(addr)
+        # After sending useful information, pretend that there's no packet
+        # waiting and send -1
+        in_chan.send(-1)
+
+    # Forward packets from each program to the controller
+    for out_chan in out_channels:
+        threading.Thread(target=read_packets_and_forward,
+                         args=(out_chan, controller_chan)).start()
+
+    # Start programs
+    for in_chan, out_chan in zip(in_channels, out_channels):
+        intcode.Program(s).run(in_chan=in_chan, out_chan=out_chan,
+                               stop_on_no_input=True)
+
+    nat_packet = None
+
+    def nat_listener():
+        for deadlock in deadlock_chan:
+            assert(nat_packet is not None)
+
+            # Send a copy of the last nat packet to 0
+            controller_chan.send(Packet(0, nat_packet.x, nat_packet.y, True))
+    threading.Thread(target=nat_listener).start()
+
+    last_nat_sent_y = None
+
+    # Controller loop
+    for msg in controller_chan:
+        if msg.nat_generated:
+            if msg.y == last_nat_sent_y:
+                answer = msg.y
+                break
+            last_nat_sent_y = msg.y
+
+        if msg.dest == 255:
+            nat_packet = msg
+            continue
+
+        in_chan = in_channels[msg.dest]
+        in_chan.send(msg.x)
+        in_chan.send(msg.y)
+        # After sending useful information, pretend that there's no packet
+        # waiting and send -1
+        in_chan.send(-1)
+        continue
+
+    # Close all input channels to shut down the network
+    for in_chan in in_channels:
+        in_chan.close()
+
+    # And close the background deadlock listener thread
+    deadlock_chan.close()
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 23)
 part1(INPUT)
