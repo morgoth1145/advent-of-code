a9335436a12492741d27609f8f03c2aadcad8ae7
Patrick Hogg <phogg@novamoon.net>
2022-03-26 21:09:35 -0400

    2019 Day 24 Part 2
    And now the bugs went recursive on us! This is still essentially a game of life variant, but now it's unbounded with a far more interesting ruleset.
    
    Speaking of sets, using sets to solve a problem like this is very smart. That way it's easy to iterate over bugs and count which neighbor cells they touch without needing to worry about what the boundaries are, they'll work themselves out! In fact, this was super easy to code up...except for me completely flubbing my inner cell check! For some reason I was checking if (nx, ny) == 2 instead of (nx, ny) == (2, 2). This meant that no bugs thought that they recursed down! Once that was fixed everything just worked, but ugh.
    
    Would have been 21st on the leaderboard. Would have been 7th had I not had that dumb bug wasting 7 and a half minutes! (That being said, I did the problem in 2019 so this doesn't exactly count.)
    
    Time: 0:27:01.381947

--- 2019/24/solution.py
+++ 2019/24/solution.py
@@ -1,3 +1,5 @@
+import collections
+
 import lib.aoc
 import lib.grid
 
@@ -40,8 +42,86 @@ def part1(s):
 
     print(f'The answer to part one is {answer}')
 
+def recursive_step(bugs):
+    neighbor_counts = collections.Counter()
+
+    for x, y, level in bugs:
+        for nx, ny in [(x-1, y),
+                       (x+1, y),
+                       (x, y-1),
+                       (x, y+1)]:
+            if (nx, ny) == (2, 2):
+                # Recurses down to level-1
+                if x == 1:
+                    # Coming from the left
+                    for ny in range(5):
+                        neighbor_counts[0,ny,level-1] += 1
+                    continue
+
+                if x == 3:
+                    # Coming from the right
+                    for ny in range(5):
+                        neighbor_counts[4,ny,level-1] += 1
+                    continue
+
+                if y == 1:
+                    # Coming from the top
+                    for nx in range(5):
+                        neighbor_counts[nx,0,level-1] += 1
+                    continue
+
+                if y == 3:
+                    # Coming from the bottom
+                    for nx in range(5):
+                        neighbor_counts[nx,4,level-1] += 1
+                    continue
+
+                # This shouldn't be possible
+                assert(False)
+
+            if nx == -1:
+                # Recurses up to level+1
+                neighbor_counts[1,2,level+1] += 1
+                continue
+
+            if nx == 5:
+                # Recurses up to level+1
+                neighbor_counts[3,2,level+1] += 1
+                continue
+
+            if ny == -1:
+                # Recurses up to level+1
+                neighbor_counts[2,1,level+1] += 1
+                continue
+
+            if ny == 5:
+                # Recurses up to level+1
+                neighbor_counts[2,3,level+1] += 1
+                continue
+
+            assert(nx >= 0 and nx < 5 and ny >= 0 and ny < 5)
+
+            # Just a normal neighbor. Yay!
+            neighbor_counts[nx,ny,level] += 1
+            continue
+
+    return set(coord
+               for coord, count in neighbor_counts.items()
+               if count == 1 or (count == 2 and coord not in bugs))
+
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s)
+
+    bugs = set((x, y, 0)
+               for (x, y), c in grid.items()
+               if c == '#' and (x, y) != (2, 2))
+
+    for _ in range(200):
+        bugs = recursive_step(bugs)
+
+    answer = len(bugs)
+
+    print(f'The answer to part two is {answer}')
 
 INPUT = lib.aoc.get_input(2019, 24)
 part1(INPUT)
