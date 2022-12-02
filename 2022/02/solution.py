12bbacb94867811a84c57633c724f1159e19ec5b
Patrick Hogg <phogg@novamoon.net>
2022-12-02 00:05:30 -0500

    2022 Day 2 Part 2
    Ah, so X, Y, and Z determine whether we should win, lose, or draw! It's probably not the best way, but mapping all 9 pairs to what should be played and using the same scoring from part 1 is easiest to think through.
    
    Managed rank 41, at least!

--- 2022/02/solution.py
+++ 2022/02/solution.py
@@ -28,8 +28,26 @@ def part1(s):
 
     lib.aoc.give_answer(2022, 2, 1, answer)
 
+CHOICES = {
+    'AX': 'Z',
+    'BX': 'X',
+    'CX': 'Y',
+    'AY': 'X',
+    'BY': 'Y',
+    'CY': 'Z',
+    'AZ': 'Y',
+    'BZ': 'Z',
+    'CZ': 'X',
+}
+
 def part2(s):
-    pass
+    answer = 0
+
+    for a, b in parse_input(s):
+        b = CHOICES[a+b]
+        answer += SCORES[b] + RESULTS.get(a+b, 0)
+
+    lib.aoc.give_answer(2022, 2, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 2)
 part1(INPUT)
