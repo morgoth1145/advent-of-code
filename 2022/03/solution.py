a5c3a203e64c4f0d0b229ac98173bc132bd7a4f6
Patrick Hogg <phogg@novamoon.net>
2022-12-03 00:04:37 -0500

    2022 Day 3 Part 2
    So now it's the same as Part 1, except we're merging 3 sets instead of 2. I did waste a bit of time thinking we were subdividing the list in three insetad of taking 3 lines at a time, but thankfully I got a runtime error rather than having to wait for a bad answer submission.
    
    I'm *really* doing well! Rank 48 on Part 2!

--- 2022/03/solution.py
+++ 2022/03/solution.py
@@ -18,7 +18,23 @@ def part1(s):
     lib.aoc.give_answer(2022, 3, 1, answer)
 
 def part2(s):
-    pass
+    lines = s.splitlines()
+
+    answer = 0
+
+    while lines:
+        a = lines.pop(0)
+        b = lines.pop(0)
+        c = lines.pop(0)
+
+        shared = set(a) & set(b) & set(c)
+        assert(len(shared) == 1)
+
+        shared = list(shared)[0]
+
+        answer += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(shared) + 1
+
+    lib.aoc.give_answer(2022, 3, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 3)
 part1(INPUT)
