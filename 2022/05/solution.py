a963a4009ff235b18fb9e6ead0bc1c747f0a022e
Patrick Hogg <phogg@novamoon.net>
2022-12-05 00:08:11 -0500

    2022 Day 5 Part 1
    I messed this up, but only once and not too badly. I was moving crates *from* the target stack *to* the target stack, being a no-op! If I'd pulled from the source stack initially I'd have had about a 6:16 time for 82nd on the leaderboard, but alas.
    
    Parsing the input was definitely interesting today. I'm glad for parselib!
    
    Ultimately got 243rd. That was a costly error, but at least nowhere's near as bad as yesterday.

---
+++ 2022/05/solution.py
@@ -0,0 +1,45 @@
+import parse
+
+import lib.aoc
+
+def parse_input(s):
+    a, b = s.split('\n\n')
+
+    lines = a.splitlines()
+
+    stacks = [[] for _ in range(len(lines.pop().split()))]
+
+    while lines:
+        row = lines.pop()
+        for i in range(len(stacks)):
+            entry = row[4*i+1]
+            if entry != ' ':
+                stacks[i].append(entry)
+
+    moves = list(parse.findall('move {:d} from {:d} to {:d}', b))
+
+    return stacks, moves
+
+def part1(s):
+    stacks, moves = parse_input(s)
+
+    for n, source, target in moves:
+        # zero index
+        source -= 1
+        target -= 1
+
+        for _ in range(n):
+            stacks[target].append(stacks[source].pop(-1))
+
+    answer = ''
+    for s in stacks:
+        answer += s[-1]
+
+    lib.aoc.give_answer(2022, 5, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2022, 5)
+part1(INPUT)
+part2(INPUT)
