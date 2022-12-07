f28c61d36ccad9ed8fd7faf3b7ff6cb14adf7c97
Patrick Hogg <phogg@novamoon.net>
2022-12-07 00:08:53 -0500

    2022 Day 7 Part 1
    Oo, this was interesting! Parsing a UNIX-like terminal sequence is fun, especially to reconstruct a folder structure! A little bit tedious, but not too bad.
    
    After that it's just getting folder sizes with some memoization and a loop over the folders to get the answer. (Probably should have used a comprehension but oh well.)
    
    And I'm back! Rank 46!

---
+++ 2022/07/solution.py
@@ -0,0 +1,67 @@
+import functools
+import posixpath
+
+import lib.aoc
+
+def parse_input(s):
+    folders = {}
+
+    cwd = '~'
+
+    lines = s.splitlines()
+    while lines:
+        line = lines.pop(0)
+        assert(line[0] == '$')
+        cmd = line[1:].strip()
+        if cmd == 'ls':
+            contents = []
+            while lines:
+                if lines[0][0] == '$':
+                    break
+                item = lines.pop(0)
+                size, item = item.split(maxsplit=1)
+                if size != 'dir':
+                    size = int(size)
+                contents.append((size, item))
+            folders[cwd] = contents
+        else:
+            assert(cmd.startswith('cd '))
+            rest = cmd[3:]
+            if rest == '/':
+                cwd = '/'
+            elif rest == '..':
+                cwd = posixpath.split(cwd)[0]
+            else:
+                cwd = posixpath.join(cwd, rest)
+
+    return folders
+
+def part1(s):
+    folders = parse_input(s)
+
+    @functools.cache
+    def get_folder_size(name):
+        contents = folders[name]
+        dir_size = 0
+        for size, item in contents:
+            if size == 'dir':
+                dir_size += get_folder_size(posixpath.join(name, item))
+            else:
+                dir_size += size
+        return dir_size
+
+    answer = 0
+
+    for name in folders.keys():
+        size = get_folder_size(name)
+        if size <= 100000:
+            answer += size
+
+    lib.aoc.give_answer(2022, 7, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2022, 7)
+part1(INPUT)
+part2(INPUT)
