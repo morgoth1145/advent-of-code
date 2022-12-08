c6bfbbb57e77ed9d8a17602c9dab7a96f27bc038
Patrick Hogg <phogg@novamoon.net>
2022-12-08 01:14:25 -0500

    2022 Refactor Day 8
    This is marginally cleaner than I had before since there was duplication between the parts when determining tree visibilities. This still feels wonky, but at least the tree stats are determined in one place now.

--- 2022/08/solution.py
+++ 2022/08/solution.py
@@ -1,58 +1,47 @@
 import lib.aoc
 import lib.grid
 
-def part1(s):
+def get_tree_stats(s):
     grid = lib.grid.FixedGrid.parse(s, value_fn=int)
 
-    def visible(x, y):
+    for (x, y), self_height in grid.items():
+        obscured = True
+        distances = []
+
         for dx, dy in [(-1, 0),
                        (1, 0),
                        (0, -1),
                        (0, 1)]:
-            nx, ny = x+dx, y+dy
-            obscured = False
-            while (nx, ny) in grid:
-                if grid[nx,ny] >= grid[x,y]:
-                    obscured = True
+            nx, ny = x, y
+            num_trees_visible = 0
+            while True:
+                nx, ny = nx+dx, ny+dy
+                if (nx, ny) not in grid:
+                    obscured = False
                     break
-                nx += dx
-                ny += dy
-            if not obscured:
-                return True
-        return False
+                num_trees_visible += 1
+                if grid[nx, ny] >= self_height:
+                    break # Obscured
+            distances.append(num_trees_visible)
 
+        yield distances, obscured
+
+def part1(s):
     answer = sum(1
-                 for c, _ in grid.items()
-                 if visible(*c))
+                 for _, obscured in get_tree_stats(s)
+                 if not obscured)
 
     lib.aoc.give_answer(2022, 8, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+    def score(distances):
+        t = 1
+        for dist in distances:
+            t *= dist
+        return t
 
-    def score(x, y):
-        score = 1
-        self = grid[x,y]
-        for dx, dy in [(-1, 0),
-                       (1, 0),
-                       (0, -1),
-                       (0, 1)]:
-            c = 0
-            nx, ny = x+dx, y+dy
-            while (nx, ny) in grid:
-                curr = grid[nx,ny]
-                if curr < self:
-                    c += 1
-                    nx += dx
-                    ny += dy
-                else:
-                    c += 1
-                    break
-            score *= c
-        return score
-
-    answer = max(score(*c)
-                 for c, _ in grid.items())
+    answer = max(score(distances)
+                 for distances, _ in get_tree_stats(s))
 
     lib.aoc.give_answer(2022, 8, 2, answer)
 
