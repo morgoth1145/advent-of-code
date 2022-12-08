0994e1b831b6b1ff8a375523a9279340caf550d0
Patrick Hogg <phogg@novamoon.net>
2022-12-08 00:19:19 -0500

    2022 Day 8 Part 2
    Oof, this hurt way more than part 1! Advent of reading comprehension is right! I just did *not* track on the visibility rules and kept implementing the wrong thing. Nothing too interesting to say beyond that really...
    
    I finally got rank 756 when I got the (simple) visibility rules right. The only upshot is that I had the nice 365 star count for longer. 13 minutes 25 seconds to be exact.

--- 2022/08/solution.py
+++ 2022/08/solution.py
@@ -28,7 +28,33 @@ def part1(s):
     lib.aoc.give_answer(2022, 8, 1, answer)
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+
+    def score(x, y):
+        score = 1
+        self = grid[x,y]
+        for dx, dy in [(-1, 0),
+                       (1, 0),
+                       (0, -1),
+                       (0, 1)]:
+            c = 0
+            nx, ny = x+dx, y+dy
+            while (nx, ny) in grid:
+                curr = grid[nx,ny]
+                if curr < self:
+                    c += 1
+                    nx += dx
+                    ny += dy
+                else:
+                    c += 1
+                    break
+            score *= c
+        return score
+
+    answer = max(score(*c)
+                 for c, _ in grid.items())
+
+    lib.aoc.give_answer(2022, 8, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 8)
 part1(INPUT)
