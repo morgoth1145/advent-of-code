5163f197d9ee68faff60c5c544f2092c8e3ad9cf
Patrick Hogg <phogg@novamoon.net>
2022-12-09 00:24:55 -0500

    2022 Day 9 Part 2
    And here's where my mistaken understanding came to bite me. That movement rule works for one rope segment, but not further than that. I had to go back and reread the movement rules to see that there were 3 cases: If the tail is in the same row or column it catches up, but otherwise it *always* moves diagonally. That's how further segments keep up!
    
    Unfortunately when I implemented that I found that I was getting the wrong answer on the example. Why? Because I had a rope that was one segment too long! I wasted so much time debugging until I looked closer and found that. I'm not sure how much time I lost, but I may well have leaderboarded part 2 had I gotten the rope length right.
    
    In the end I dropped to rank 825 on part 2. Ouch!

--- 2022/09/solution.py
+++ 2022/09/solution.py
@@ -43,7 +43,54 @@ def part1(s):
     lib.aoc.give_answer(2022, 9, 1, answer)
 
 def part2(s):
-    pass
+    rope = [(0, 0)] * 10
+
+    seen = set()
+    seen.add(rope[-1])
+
+    def do_move(dx, dy, c):
+        nonlocal rope
+        x, y = rope[0]
+        for _ in range(c):
+            x += dx
+            y += dy
+            new_rope = [(x, y)]
+            for i, (nx, ny) in enumerate(rope[1:]):
+                i += 1
+                ax, ay = new_rope[i-1]
+                bx, by = rope[i]
+                if max(abs(ax-bx), abs(ay-by)) == 2:
+                    if ax == bx:
+                        by = (ay + by) // 2
+                    elif ay == by:
+                        bx = (ax + bx) // 2
+                    else:
+                        # Diagonal
+                        ldx = ax - bx
+                        ldy = ay - by
+                        ldx //= abs(ldx)
+                        ldy //= abs(ldy)
+                        bx += ldx
+                        by += ldy
+                new_rope.append((bx, by))
+            rope = new_rope
+            seen.add(rope[-1])
+
+    for d, c in parse_input(s):
+        if d == 'U':
+            do_move(0, -1, c)
+        elif d == 'D':
+            do_move(0, 1, c)
+        elif d == 'L':
+            do_move(-1, 0, c)
+        elif d == 'R':
+            do_move(1, 0, c)
+        else:
+            assert(False)
+
+    answer = len(seen)
+
+    lib.aoc.give_answer(2022, 9, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 9)
 part1(INPUT)
