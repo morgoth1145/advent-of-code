7115f5b705be7827967fe98dcb5ce68cadb113e4
Patrick Hogg <phogg@novamoon.net>
2022-12-09 00:33:38 -0500

    2022 Refactor Day 9
    The part 2 solution works for part 1 as well (with the right rope length), so might as well deduplicate the code.

--- 2022/09/solution.py
+++ 2022/09/solution.py
@@ -1,49 +1,7 @@
 import lib.aoc
 
-def parse_input(s):
-    for line in s.splitlines():
-        a, b = line.split()
-        yield a, int(b)
-
-def part1(s):
-    head = (0, 0)
-    tail = (0, 0)
-
-    seen = set()
-    seen.add(tail)
-
-    def do_move(dx, dy, c):
-        nonlocal head, tail
-        x, y = head
-        tx, ty = tail
-        for _ in range(c):
-            x += dx
-            y += dy
-            if max(abs(x-tx), abs(y-ty)) == 2:
-                tx = x - dx
-                ty = y - dy
-                seen.add((tx, ty))
-        head = (x, y)
-        tail = (tx, ty)
-
-    for d, c in parse_input(s):
-        if d == 'U':
-            do_move(0, -1, c)
-        elif d == 'D':
-            do_move(0, 1, c)
-        elif d == 'L':
-            do_move(-1, 0, c)
-        elif d == 'R':
-            do_move(1, 0, c)
-        else:
-            assert(False)
-
-    answer = len(seen)
-
-    lib.aoc.give_answer(2022, 9, 1, answer)
-
-def part2(s):
-    rope = [(0, 0)] * 10
+def solve(s, rope_length):
+    rope = [(0, 0)] * rope_length
 
     seen = set()
     seen.add(rope[-1])
@@ -76,7 +34,10 @@ def part2(s):
             rope = new_rope
             seen.add(rope[-1])
 
-    for d, c in parse_input(s):
+    for line in s.splitlines():
+        d, c = line.split()
+        c = int(c)
+
         if d == 'U':
             do_move(0, -1, c)
         elif d == 'D':
@@ -88,7 +49,15 @@ def part2(s):
         else:
             assert(False)
 
-    answer = len(seen)
+    return len(seen)
+
+def part1(s):
+    answer = solve(s, 2)
+
+    lib.aoc.give_answer(2022, 9, 1, answer)
+
+def part2(s):
+    answer = solve(s, 10)
 
     lib.aoc.give_answer(2022, 9, 2, answer)
 
