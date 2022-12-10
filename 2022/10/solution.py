939e523d00be7d43f3aad38ffeba4652784cdf09
Patrick Hogg <phogg@novamoon.net>
2022-12-10 00:11:29 -0500

    2022 Day 10 Part 2
    Oh, so this is rasterizing on a CRT, interesting. Took me longer than I'd like to have grok'd the rasterization system but that was easy to code, just record the position at each cycle instead of weighted strengths.
    
    After that it *was* supposed to be a matter of converting to data for my lib.ocr library to parse, but I goofed that up somehow. Not sure how yet, but it was a waste of time so I ended up producing the string and parsing it with my eyeballs. I'll have to debug the lib.ocr path later...
    
    Anyway, I did sneak in rank 72, though I definitely could have been *significantly* higher had I just made one or two fewer mistakes...

--- 2022/10/solution.py
+++ 2022/10/solution.py
@@ -1,4 +1,5 @@
 import lib.aoc
+##import lib.ocr
 
 def parse_input(s):
     for line in s.splitlines():
@@ -27,7 +28,40 @@ def part1(s):
     lib.aoc.give_answer(2022, 10, 1, answer)
 
 def part2(s):
-    pass
+    pos_per_cycle = []
+
+    x = 1
+    for inst in parse_input(s):
+        pos_per_cycle.append(x)
+        if inst[0] == 'noop':
+            continue
+        if inst[0] == 'addx':
+            # Takes two cycles
+            pos_per_cycle.append(x)
+            x += inst[1]
+            continue
+        assert(False)
+
+##    lit_pixels = set()
+
+    out = ''
+
+    for x, pos in enumerate(pos_per_cycle):
+##        y = x // 40
+        if x % 40 in (pos-1, pos, pos+1):
+##            lit_pixels.add((x, y))
+            out += '#'
+        else:
+            out += '.'
+        if x % 40 == 39:
+            out += '\n'
+
+    print(out)
+
+##    answer = lib.ocr.parse_coord_set(lit_pixels)
+    answer = input('lib.ocr is having issues. What is the answer? ')
+
+    lib.aoc.give_answer(2022, 10, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 10)
 part1(INPUT)
