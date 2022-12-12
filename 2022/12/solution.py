7487e0b20f016e186aa0f635ec30f883dea78d42
Patrick Hogg <phogg@novamoon.net>
2022-12-12 00:08:08 -0500

    2022 Day 12 Part 1
    Oof, I goofed on this. For one thing I forgot my own grid and graph library APIs and had to review them. Moreover, I skipped over the bit that said that the start and end tiles had elevations a and z! I wasted 61 seconds thanks to that...
    
    I ended up just missing the leaderboard at rank 119. I would have been 77-80 without that mistake!

---
+++ 2022/12/solution.py
@@ -0,0 +1,41 @@
+import lib.aoc
+import lib.graph
+import lib.grid
+
+def part1(s):
+    grid = lib.grid.FixedGrid.parse(s)
+
+    start = None
+    end = None
+
+    for c, val in grid.items():
+        if val == 'S':
+            start = c
+        elif val == 'E':
+            end = c
+
+    def neighbor_fn(pos):
+        self = grid[pos]
+        for n in grid.neighbors(*pos):
+            val = grid[n]
+            if self == 'S':
+                if val in 'ab':
+                    yield n, 1
+            elif val == 'E':
+                if self in 'yz':
+                    yield n, 1
+            elif ord(val) - ord(self) <= 1:
+                yield n, 1
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+
+    answer = lib.graph.dijkstra_length(graph, start, end)
+
+    lib.aoc.give_answer(2022, 12, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2022, 12)
+part1(INPUT)
+part2(INPUT)
