938a65e4491d0a1379cb73c567acad0cde41bef8
Patrick Hogg <phogg@novamoon.net>
2022-12-12 00:11:11 -0500

    2022 Day 12 Part 2
    And more goofing here! I thought "Oh hey, simple! Just take the minimum from all candidate starting points!" forgetting that some starting points may never reach the end. As such I sent -1 in as the first wrong answer.
    
    The worst part? If I'd taken a set of those candidate lengths I could have easily removed the -1 and taken the min! I wasted 90 seconds thanks to this...
    
    I slipped further down to rank 160. I might have just missed the leaderboard without the part 2 goof, but without the part 1 goof as well would have been solidly on the leaderboard. I have to start making this up tomorrow...

--- 2022/12/solution.py
+++ 2022/12/solution.py
@@ -34,7 +34,40 @@ def part1(s):
     lib.aoc.give_answer(2022, 12, 1, answer)
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s)
+
+    starts = []
+    end = None
+
+    for c, val in grid.items():
+        if val in 'Sa':
+            starts.append(c)
+        elif val == 'E':
+            end = c
+
+    def neighbor_fn(pos):
+        self = grid[pos]
+        for n in grid.neighbors(*pos):
+            val = grid[n]
+            if self == 'S':
+                if val in 'ab':
+                    yield n, 1
+            elif val == 'E':
+                if self in 'yz':
+                    yield n, 1
+            elif ord(val) - ord(self) <= 1:
+                yield n, 1
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+
+    answer = None
+    for start in starts:
+        cand = lib.graph.dijkstra_length(graph, start, end)
+        if cand != -1:
+            if answer is None or answer > cand:
+                answer = cand
+
+    lib.aoc.give_answer(2022, 12, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 12)
 part1(INPUT)
