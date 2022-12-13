2c02f640c278181a9c331d66f81f5392e897628b
Patrick Hogg <phogg@novamoon.net>
2022-12-13 01:02:06 -0500

    2022 Rewrite Day 13
    Now that I have more time to think I can make a lot of cleanups:
    - Parse the packets into a flat list for both parts. This makes part 1 marginally more complicated (needing a zip and slices to operate on the pairs) but nothing too bad. Part 2 however benefits greatly as the full packet list (plus delimiters) can now be assembled in one line.
    - is_right_order has been thrown out for an entirely rewritten packet_cmp. It sports much simpler logic internally thanks to some reordering of the checks and it conforms to the cmp function standard (-1 for less than, 1 for greater than, 0 for equal). This lets me use the snazzy functools.cmp_to_key when sorting!
    - Minor, but also inline the divider lookups in the answer computation for part 2 to save a couple of lines.

--- 2022/13/solution.py
+++ 2022/13/solution.py
@@ -1,82 +1,52 @@
+import functools
 import json
 
 import lib.aoc
 
-def parse_input(s):
-    for group in s.split('\n\n'):
-        a, b = group.splitlines()
-        a = json.loads(a)
-        b = json.loads(b)
-        yield a, b
+def parse_packets_as_flat_list(s):
+    return map(json.loads, s.replace('\n\n', '\n').splitlines())
 
-def is_right_order(a, b):
-    alen = len(a)
-    blen = len(b)
-    
-    for i in range(min(alen, blen)):
-        aitem = a[i]
-        bitem = b[i]
-        if isinstance(aitem, int) and isinstance(bitem, int):
-            if aitem < bitem:
-                return True
-            if aitem > bitem:
-                return False
-            continue
-        if isinstance(aitem, list) and isinstance(bitem, list):
-            subanswer = is_right_order(aitem, bitem)
-            if subanswer is not None:
-                return subanswer
-            continue
-        # One of them is an int
-        if isinstance(aitem, int):
-            aitem = [aitem]
-        elif isinstance(bitem, int):
-            bitem = [bitem]
-        else:
-            assert(False)
-        subanswer = is_right_order(aitem, bitem)
-        if subanswer is not None:
-            return subanswer
+def packet_cmp(a, b):
+    for aval, bval in zip(a, b):
+        if isinstance(aval, int):
+            if isinstance(bval, int):
+                if aval < bval:
+                    return -1
+                if aval > bval:
+                    return 1
+                continue
+            # bval is a list, make aval a list to match for subsequent checks
+            aval = [aval]
 
-    if alen < blen:
-        return True
-    if blen < alen:
-        return False
+        # aval is a list
+        if isinstance(bval, int):
+            bval = [bval]
+
+        subcmp = packet_cmp(aval, bval)
+        if subcmp != 0:
+            return subcmp
+
+    if len(a) < len(b):
+        return -1
+    if len(b) < len(a):
+        return 1
+    return 0
 
 def part1(s):
-    data = list(parse_input(s))
+    packets = list(parse_packets_as_flat_list(s))
 
     answer = sum(i+1
                  for i, (a, b)
-                 in enumerate(data)
-                 if is_right_order(a, b) is True)
+                 in enumerate(zip(packets[::2], packets[1::2]))
+                 if packet_cmp(a, b) == -1)
 
     lib.aoc.give_answer(2022, 13, 1, answer)
 
-class Wrapper:
-    def __init__(self, item):
-        self.item = item
-
-    def __lt__(self, other):
-        return is_right_order(self.item, other.item)
-
 def part2(s):
-    data = list(parse_input(s))
+    packets = list(parse_packets_as_flat_list(s)) + [[[2]], [[6]]]
+    packets.sort(key=functools.cmp_to_key(packet_cmp))
 
-    all_items = []
-    for a, b in data:
-        all_items += [a, b]
-
-    all_items.append([[2]])
-    all_items.append([[6]])
-
-    all_items = sorted(all_items, key=Wrapper)
-    assert(is_right_order(all_items[0], all_items[1]))
-
-    a = all_items.index([[2]]) + 1
-    b = all_items.index([[6]]) + 1
-
-    answer = a * b
+    answer = (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
 
     lib.aoc.give_answer(2022, 13, 2, answer)
 
