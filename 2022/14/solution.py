c7827348226d6c441fe191416eca8a2757192288
Patrick Hogg <phogg@novamoon.net>
2022-12-14 12:09:02 -0500

    2022 Rewrite Day 14
    There never ended up being a need to differentiate between rock and sand tiles so the cave should just be represented by a set.
    
    Furthermore, someone on reddit had the observation that DPF is perfect for this problem. They're absolutely right! Why bother retracing the path of the next grain of sand when you can just pick up right before the last piece settled? Solving this with recursion dramatically speeds up both parts (not that part 1 was slow) and also simplifies the code to boot!

--- 2022/14/solution.py
+++ 2022/14/solution.py
@@ -1,77 +1,55 @@
 import lib.aoc
 
 def parse_cave(s):
-    cave = {}
+    cave = set()
 
     for line in s.splitlines():
-        parts = line.split(' -> ')
         parts = [tuple(map(int, p.split(',')))
-                 for p in parts]
+                 for p in line.split(' -> ')]
         x, y = parts.pop(0)
-        cave[x,y] = '#'
         for x2, y2 in parts:
-            dx, dy = x2-x, y2-y
-            if dx != 0:
-                dx = dx // abs(dx)
-            if dy != 0:
-                dy = dy // abs(dy)
-
-            while x != x2 or y != y2:
-                x += dx
-                y += dy
-                cave[x,y] = '#'
+            if x == x2:
+                dy = -1 if y2 < y else 1
+                cave.update((x, y) for y in range(y, y2+dy, dy))
+                y = y2
+            else:
+                dx = -1 if x2 < x else 1
+                cave.update((x, y) for x in range(x, x2+dx, dx))
+                x = x2
 
     return cave
 
-def generate_sand(cave, in_x, in_y, max_y):
-    x = in_x
-    y = in_y
-    while (x,y) not in cave:
-        if y > max_y:
+def solve(cave, in_x, in_y):
+    max_y = max(y for x,y in cave)
+    initial_count = len(cave)
+
+    def settle(x, y):
+        if y > max_y: # Part 1 exit condition
             return False
-        if (x,y+1) not in cave:
-            y += 1
-            continue
-        if (x-1,y+1) not in cave:
-            x -= 1
-            y += 1
-            continue
-        if (x+1,y+1) not in cave:
-            x += 1
-            y += 1
-            continue
-        # This must have settled
-        cave[x,y] = 'o'
-        return True
+        if (x, y) in cave:
+            return True
+        # Will short circuit if any cannot settle
+        if settle(x, y+1) and settle(x-1, y+1) and settle(x+1, y+1):
+            cave.add((x, y))
+            return True
+        return False
+
+    settle(in_x, in_y)
+
+    return len(cave) - initial_count
 
 def part1(s):
-    cave = parse_cave(s)
-
-    max_y = max(y for x,y in cave.keys())
-
-    answer = 0
-
-    while generate_sand(cave, 500, 0, max_y):
-        answer += 1
-
+    answer = solve(parse_cave(s), 500, 0)
     lib.aoc.give_answer(2022, 14, 1, answer)
 
 def part2(s):
     cave = parse_cave(s)
 
-    max_y = max(y for x,y in cave.keys())
+    # The sand can only go so far so we don't need an infinite floor
+    floor = max(y for x,y in cave) + 2
+    cave.update((x, floor) for x in range(500-floor, 500+floor+1))
 
-    floor = max_y + 2
-
-    for x in range(500-floor-2, 500+floor+3):
-        cave[x,floor] = '#'
-
-    answer = 0
-
-    while cave.get((500,0)) is None:
-        assert(generate_sand(cave, 500, 0, floor))
-        answer += 1
-
+    answer = solve(cave, 500, 0)
     lib.aoc.give_answer(2022, 14, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 14)
