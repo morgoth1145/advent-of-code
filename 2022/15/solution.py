95c8a12db97439ba9fe5c6c3597dd346ebd7e6e3
Patrick Hogg <phogg@novamoon.net>
2022-12-15 00:17:47 -0500

    2022 Day 15 Part 2
    And part 2 went great too! I obviously had to rethink my strategy from part 1 since doing set-based computation on 4 million cells (over 4 million rows!) would take too long. Instead I worked through figuring out ranges and doing range merging which actually worked fine on the test data. After that I gave it the real data and real ranges, worried that it'd take too long. To my surprise it didn't! (I injected a print every 1k y values to see the progress, that's since been taken out.) It only took about 21 seconds!
    
    I dropped a few spots down to rank 15, but that's still crazy! My previous PB for any part was 18! Today was awesome!

--- 2022/15/solution.py
+++ 2022/15/solution.py
@@ -35,8 +35,48 @@ def part1(s):
 
     lib.aoc.give_answer(2022, 15, 1, answer)
 
+# Probably super inefficient but I don't care! It worked!
+def part2_search(s):
+    data = list(parse_input(s))
+
+    MIN_COORD = 0
+    MAX_COORD = 4000000
+
+    for y in range(MIN_COORD, MAX_COORD+1):
+        ranges = []
+        for (sens_x, sens_y), (beac_x, beac_y) in data:
+            min_manhattan = abs(sens_x-beac_x) + abs(sens_y-beac_y)
+
+            dist = abs(sens_y - y)
+            mult = min_manhattan - dist
+            if mult < 0:
+                continue
+
+            ranges.append((sens_x - mult, sens_x + mult))
+
+        ranges.sort()
+
+        compact = []
+        low_x, high_x = ranges[0]
+        for n_low_x, n_high_x in ranges[1:]:
+            if n_low_x-1 <= high_x:
+                high_x = max(high_x, n_high_x)
+            else:
+                compact.append((low_x, high_x))
+                low_x, high_x = n_low_x, n_high_x
+        compact.append((low_x, high_x))
+
+        if len(compact) != 1:
+            assert(len(compact) == 2)
+            (a, b), (c, d) = compact
+            assert(b+2 == c)
+            x = b+1
+            return x * 4000000 + y
+
 def part2(s):
-    pass
+    answer = part2_search(s)
+
+    lib.aoc.give_answer(2022, 15, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 15)
 part1(INPUT)
