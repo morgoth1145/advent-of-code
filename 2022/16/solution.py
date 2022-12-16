2bf7c157e37b3e0a65deedc6c88e42297d813d1d
Patrick Hogg <phogg@novamoon.net>
2022-12-16 01:05:04 -0500

    2022 Day 16 Part 2
    And here's part 2 to keep me humble! First off I expanded my breadth first iteration to having two players. I goofed up and had the first person continually branching off though which isn't the same problem. Submitted a wrong answer based on that in just under 19 minutes, oops! (This really is just letting you both open a valve and move in the same minute.)
    
    With the right setup the state space then exploded to a crazy amount. I went through *so* many ideas trying to solve this. I tried improving the speed of operations in the breadth first iteration, I tried converting the valve keys to bit flags to optimize the tracking of opened valves but that didn't work.
    
    Finally I tried converting this to a graph search problem where I minimized the amount of *wasted* potential. (Pressure that *would* have been released had all valves been open.) This still ran for a long time, but did give me the answer without running out of memory.
    
    While that was running I did realize that most of the valves having 0 flow rate means that they can be treated as open for the sake of this problem. This likely prunes the state space quite a bit, but I didn't have time to try it on the breadth first expansion and while I started it on a parallel instance of the dijkstra search the first instance got the right answer first.
    
    I dropped *all* the way to rank 105. Narrowly missed the leaderboard, but you know what? I'm still happy. A massive PB for part 1 and I solved this monster of a problem on my own!

--- 2022/16/solution.py
+++ 2022/16/solution.py
@@ -1,4 +1,7 @@
+import itertools
+
 import lib.aoc
+import lib.graph
 
 def parse_valves(s):
     valves = {}
@@ -10,14 +13,23 @@ def parse_valves(s):
         lead_to = ''.join(parts[9:]).split(',')
         valves[valve] = (flow_rate, lead_to)
 
-    return valves
+    valve_to_num = {}
+    for key in sorted(valves.keys()):
+        valve_to_num[key] = 1 << len(valve_to_num)
+
+    valves = {
+        valve_to_num[valve]: (flow_rate, tuple(map(valve_to_num.get, lead_to)))
+        for valve, (flow_rate, lead_to) in valves.items()
+    }
+
+    return valves, valve_to_num
 
 def part1(s):
-    valves = parse_valves(s)
+    valves, valve_to_num = parse_valves(s)
 
     TOTAL_TIME = 30
 
-    states = [('AA', set(), 0)]
+    states = [(valve_to_num['AA'], 0, 0)]
 
     best = {}
 
@@ -26,15 +38,15 @@ def part1(s):
 
         new_states = []
         for loc, opened, pressure in states:
-            key = (loc, ','.join(sorted(opened)))
+            key = (loc, opened)
             if key in best and pressure <= best[key]:
                 continue
 
             best[key] = pressure
 
             flow_rate, lead_to = valves[loc]
-            if loc not in opened and flow_rate > 0:
-                new_states.append((loc, opened | {loc}, pressure + flow_rate * (TOTAL_TIME - t)))
+            if loc & opened == 0 and flow_rate > 0:
+                new_states.append((loc, opened | loc, pressure + flow_rate * (TOTAL_TIME - t)))
             for dest in lead_to:
                 new_states.append((dest, opened, pressure))
 
@@ -44,8 +56,77 @@ def part1(s):
 
     lib.aoc.give_answer(2022, 16, 1, answer)
 
+# This is abysmally slow. But it works.
+# BFS was bogging down my memory, so I inverted this to a graph search for
+# the least amount of *wasted* pressure. (AKA, pressure that could have been
+# released had all the valves been open from the start.)
 def part2(s):
-    pass
+    valves, valve_to_num = parse_valves(s)
+
+    ALL_VALVES = 0
+    for valve in valves.keys():
+        assert(ALL_VALVES & valve == 0)
+        ALL_VALVES = ALL_VALVES | valve
+
+    TOTAL_TIME = 26
+
+    MAXIMUM_FLOW_PER_TICK = 0
+    for flow_rate, _ in valves.values():
+        MAXIMUM_FLOW_PER_TICK += flow_rate
+
+    TOTAL_MAXIMUM_FLOW = MAXIMUM_FLOW_PER_TICK * TOTAL_TIME
+
+    def calc_sub_states(loc, opened):
+        flow_rate, lead_to = valves[loc]
+        if loc & opened == 0 and flow_rate > 0:
+            yield loc, loc, flow_rate
+        for dest in lead_to:
+            yield dest, None, 0
+
+    best_seen_time = TOTAL_TIME+1
+
+    def neighbor_fn(state):
+        self, elephant, opened, current_flow, time_remaining = state
+        assert(time_remaining > 0)
+
+        nonlocal best_seen_time
+        if best_seen_time > time_remaining:
+            print(time_remaining)
+            best_seen_time = time_remaining
+
+        if opened == ALL_VALVES:
+            yield (None, None, opened, current_flow, time_remaining-1), 0
+            return
+
+        self_moves = list(calc_sub_states(self, opened))
+        elephant_moves = list(calc_sub_states(elephant, opened))
+
+        for ((dest1, open1, new_flow_1),
+             (dest2, open2, new_flow_2)) in itertools.product(self_moves,
+                                                              elephant_moves):
+            if open1 is not None and open2 is not None and open1 == open2:
+                continue
+            new_opened = opened
+            if open1 is not None:
+                new_opened |= open1
+            if open2 is not None:
+                new_opened |= open2
+
+            new_flow = current_flow + new_flow_1 + new_flow_2
+            yield (dest1, dest2, new_opened, new_flow, time_remaining-1), MAXIMUM_FLOW_PER_TICK - current_flow
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+
+    start = (valve_to_num['AA'], valve_to_num['AA'], 0, 0, TOTAL_TIME)
+
+    def end_fn(state):
+        return state[-1] == 0
+
+    least_missed_flow = lib.graph.dijkstra_length_fuzzy_end(graph, start, end_fn)
+
+    answer = TOTAL_MAXIMUM_FLOW - least_missed_flow
+
+    lib.aoc.give_answer(2022, 16, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 16)
 part1(INPUT)
