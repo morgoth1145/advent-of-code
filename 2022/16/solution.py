8a99cf7d1b1215d69795a09ae3841be14bf69ffb
Patrick Hogg <phogg@novamoon.net>
2022-12-16 21:31:00 -0500

    2022 Rewrite Day 16
    The fundamental approach for part 1 isn't any different, we still march through time and compute the sea of possible states for each time step. However, this is now done much more efficiently using numpy (and many associated tricks) to iterate as fast as possible and get the maximal pressure for each combination of opened valves. We don't actually care about which valve combination was chosen for part 1, but it's quick to do the transformation and it'll be needed in part 2. Aside from that, part 1 now also takes advantage of the fact that only valves with nonzero flow are useful.
    
    Part 2 is entirely different. Some folks on the reddit pointed out a very smart approach here. If you make one pass to see what either entity could achieve by opening sets of valves you can then search every combination of valve sets. So long as the valve set pair does not overlap then the player and elephant can be treated as if they operated independently! Since we then know the best possible pressure release for each one we can sum them to see how much total pressure was released for that valve set combination.
    
    This ends up being fastest by choosing combinations from the precomputed list, so all we need to do is check the valve sets and maximize the pressure sums over any combinations that survive.
    
    It still takes about 0.25 seconds for part 1 and about 1 second for part 2, but that's drastically better than what I had so I'll take it.

--- 2022/16/solution.py
+++ 2022/16/solution.py
@@ -1,130 +1,97 @@
 import itertools
+import numpy
 
 import lib.aoc
-import lib.graph
 
-def parse_valves(s):
+def find_best_pressures_per_valve_combo(s, total_time):
     valves = {}
+
+    valves_with_flow = []
+    valves_without_flow = []
 
     for line in s.splitlines():
         parts = line.split()
         valve = parts[1]
         flow_rate = int(parts[4][5:-1])
-        lead_to = ''.join(parts[9:]).split(',')
-        valves[valve] = (flow_rate, lead_to)
+        leads_to = ''.join(parts[9:]).split(',')
+        valves[valve] = (flow_rate, leads_to)
 
-    valve_to_num = {}
-    for key in sorted(valves.keys()):
-        valve_to_num[key] = 1 << len(valve_to_num)
+        if flow_rate == 0:
+            valves_without_flow.append(valve)
+        else:
+            valves_with_flow.append(valve)
+
+    valve_to_num = (valves_with_flow + valves_without_flow).index
 
     valves = {
-        valve_to_num[valve]: (flow_rate, tuple(map(valve_to_num.get, lead_to)))
-        for valve, (flow_rate, lead_to) in valves.items()
+        valve_to_num(valve): (flow_rate, tuple(map(valve_to_num, leads_to)))
+        for valve, (flow_rate, leads_to) in valves.items()
     }
 
-    return valves, valve_to_num
+    good_valve_combos = 2 ** len(valves_with_flow)
 
-def part1(s):
-    valves, valve_to_num = parse_valves(s)
+    # Offset "active" states by 1 to differentiate them
+    # This will be undone later on
+    states = [numpy.zeros(good_valve_combos, dtype=int)
+              for _ in range(len(valves))]
+    states[valve_to_num('AA')][0] = 1
 
-    TOTAL_TIME = 30
+    for t in range(1, total_time+1):
+        new_states = [numpy.zeros(good_valve_combos, dtype=int)
+                      for _ in range(len(valves))]
+        for loc, (locstates, new_locstates) in enumerate(zip(states,
+                                                             new_states)):
+            flow_rate, leads_to = valves[loc]
 
-    states = [(valve_to_num['AA'], 0, 0)]
+            if flow_rate > 0:
+                # Open the valve
+                loc_mask = 1 << loc
 
-    best = {}
+                unopened = (numpy.arange(good_valve_combos) & loc_mask) == 0
+                condition = unopened & (locstates > 0)
 
-    for t in range(1, TOTAL_TIME+1):
-        print(t, len(states))
+                additional_flow = flow_rate * (total_time - t)
+                new_pressure = locstates[condition] + additional_flow
 
-        new_states = []
-        for loc, opened, pressure in states:
-            key = (loc, opened)
-            if key in best and pressure <= best[key]:
-                continue
+                locs_to_adjust = condition.nonzero()[0] | loc_mask
+                states_to_adjust = new_locstates[locs_to_adjust]
 
-            best[key] = pressure
+                # Due to slicing the adjusted array needs to be assigned rather
+                # than adjusted in place
+                new_locstates[locs_to_adjust] = numpy.maximum(states_to_adjust,
+                                                              new_pressure)
 
-            flow_rate, lead_to = valves[loc]
-            if loc & opened == 0 and flow_rate > 0:
-                new_states.append((loc, opened | loc, pressure + flow_rate * (TOTAL_TIME - t)))
-            for dest in lead_to:
-                new_states.append((dest, opened, pressure))
+            for dest in leads_to:
+                # Movement
+                numpy.maximum(new_states[dest], locstates,
+                              out=new_states[dest])
 
         states = new_states
 
-    answer = max(pressure for _, _, pressure in states)
+    best = numpy.zeros(good_valve_combos, dtype=int)
+    for locstates in states:
+        numpy.maximum(best, locstates,
+                      out=best)
+
+    # Remove the offset used above
+    best -= 1
+    return list(zip(map(int, numpy.nonzero(best > 0)[0]),
+                    map(int, best[best > 0])))
+
+def part1(s):
+    answer = max(pressure
+                 for _, pressure
+                 in find_best_pressures_per_valve_combo(s, 30))
 
     lib.aoc.give_answer(2022, 16, 1, answer)
 
-# This is abysmally slow. But it works.
-# BFS was bogging down my memory, so I inverted this to a graph search for
-# the least amount of *wasted* pressure. (AKA, pressure that could have been
-# released had all the valves been open from the start.)
 def part2(s):
-    valves, valve_to_num = parse_valves(s)
+    best_pressure_per_valve_combo = find_best_pressures_per_valve_combo(s, 26)
 
-    ALL_VALVES = 0
-    for valve in valves.keys():
-        assert(ALL_VALVES & valve == 0)
-        ALL_VALVES = ALL_VALVES | valve
-
-    TOTAL_TIME = 26
-
-    MAXIMUM_FLOW_PER_TICK = 0
-    for flow_rate, _ in valves.values():
-        MAXIMUM_FLOW_PER_TICK += flow_rate
-
-    TOTAL_MAXIMUM_FLOW = MAXIMUM_FLOW_PER_TICK * TOTAL_TIME
-
-    def calc_sub_states(loc, opened):
-        flow_rate, lead_to = valves[loc]
-        if loc & opened == 0 and flow_rate > 0:
-            yield loc, loc, flow_rate
-        for dest in lead_to:
-            yield dest, None, 0
-
-    best_seen_time = TOTAL_TIME+1
-
-    def neighbor_fn(state):
-        self, elephant, opened, current_flow, time_remaining = state
-        assert(time_remaining > 0)
-
-        nonlocal best_seen_time
-        if best_seen_time > time_remaining:
-            print(time_remaining)
-            best_seen_time = time_remaining
-
-        if opened == ALL_VALVES:
-            yield (None, None, opened, current_flow, time_remaining-1), 0
-            return
-
-        self_moves = list(calc_sub_states(self, opened))
-        elephant_moves = list(calc_sub_states(elephant, opened))
-
-        for ((dest1, open1, new_flow_1),
-             (dest2, open2, new_flow_2)) in itertools.product(self_moves,
-                                                              elephant_moves):
-            if open1 is not None and open2 is not None and open1 == open2:
-                continue
-            new_opened = opened
-            if open1 is not None:
-                new_opened |= open1
-            if open2 is not None:
-                new_opened |= open2
-
-            new_flow = current_flow + new_flow_1 + new_flow_2
-            yield (dest1, dest2, new_opened, new_flow, time_remaining-1), MAXIMUM_FLOW_PER_TICK - current_flow
-
-    graph = lib.graph.make_lazy_graph(neighbor_fn)
-
-    start = (valve_to_num['AA'], valve_to_num['AA'], 0, 0, TOTAL_TIME)
-
-    def end_fn(state):
-        return state[-1] == 0
-
-    least_missed_flow = lib.graph.dijkstra_length_fuzzy_end(graph, start, end_fn)
-
-    answer = TOTAL_MAXIMUM_FLOW - least_missed_flow
+    answer = max(self_pressure + elephant_pressure
+                 for (self, self_pressure), (elephant, elephant_pressure)
+                 in itertools.combinations(best_pressure_per_valve_combo, 2)
+                 if self & elephant == 0)
 
     lib.aoc.give_answer(2022, 16, 2, answer)
 
