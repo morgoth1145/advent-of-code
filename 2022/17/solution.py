e15277108f39ee8ec4559c872b0ec6d00b80c4c9
Patrick Hogg <phogg@novamoon.net>
2022-12-17 00:55:07 -0500

    2022 Day 17 Part 2
    Ah, and part 2 is giving us a stupid number of rocks to fall. This was clearly a jumpahead problem, and my initial trepidation was that the way to track repeat states would require somehow keeping a record of the shape of the top of the tower which sounds super nasty!
    
    Anyway, I first started by just tracking the current rock and position in the jet stream. So long as those match there's a chance that we're in a cycle. That didn't match the example though, so I think I next kept a list of seen heights to keep track of the height diffs. This way we can let the tower settle into a rhythm before doing the jumpahead. Unfortunately I still didn't get the right answer for the example, it was off by 1! So I then added one and submitted thinking "Why not?" Unsurprisingly, it was off.
    
    I then tried a bunch of things, including trying to get a handle on the tower shape (just by keeping track of how far down the top item is in each column from the top of the tower) but nothing worked! The reason? For any jumpahead you need to make sure to run any "spare" cycles after the jumpahead and I was continuing from the same rock instead of the next one!
    
    After many bad answers (and even a 5 minute timeout) I finally got the right answer 15 minutes and 47 seconds after my first bad answer.
    
    I fell all they way to 295! Oof! Well, at least I got one leaderboard today. Though if I didn't have that off by one error I would have been on the leaderboard for part 2 as well...

--- 2022/17/solution.py
+++ 2022/17/solution.py
@@ -1,4 +1,8 @@
+import collections
+
 import lib.aoc
+
+TOWER_WIDTH = 7
 
 ROCKS = [
     [(2, 0), (3, 0), (4, 0), (5, 0)], # Horizontal Line
@@ -8,14 +12,8 @@ ROCKS = [
     [(2, 0), (2, 1), (3, 0), (3, 1)], # Square
 ]
 
-def part1(s):
-    WIDTH = 7
-
-    tower = {(x, 0) for x in range(WIDTH)}
-
-    jet_idx = 0
-
-    for rock_idx in range(2022):
+def run_sim(s, tower, start_rock_idx, jet_idx, total_rocks):
+    for rock_idx in range(start_rock_idx, total_rocks):
         rock = ROCKS[rock_idx % len(ROCKS)]
         y_off = max(y for x,y in tower) + 4
         rock = [(x, y+y_off) for x, y in rock]
@@ -28,7 +26,7 @@ def part1(s):
                 dx = 1
             jet_idx += 1
             new_rock = [(x+dx, y) for x, y in rock]
-            if any(x < 0 or x >= WIDTH for x, y in new_rock):
+            if any(x < 0 or x >= TOWER_WIDTH for x, y in new_rock):
                 new_rock = rock
             elif any(c in tower for c in new_rock):
                 new_rock = rock
@@ -41,12 +39,104 @@ def part1(s):
 
             rock = new_rock
 
+    return tower
+
+def part1(s):
+    tower = {(x, 0) for x in range(TOWER_WIDTH)}
+
+    tower = run_sim(s, tower, 0, 0, 2022)
+
     answer = max(y for x,y in tower)
 
     lib.aoc.give_answer(2022, 17, 1, answer)
 
+def part2_impl(s):
+    tower = {(x, 0) for x in range(TOWER_WIDTH)}
+
+    jet_idx = 0
+
+    TIMES = 1000000000000
+
+    tower_heights = collections.defaultdict(list)
+
+    for rock_idx in range(TIMES):
+        start_key = (rock_idx % len(ROCKS), jet_idx % len(s))
+
+        rock = ROCKS[rock_idx % len(ROCKS)]
+        y_off = max(y for x,y in tower) + 4
+        rock = [(x, y+y_off) for x, y in rock]
+
+        move_x, move_y = 0, 0
+
+        while True:
+            jet = s[jet_idx % len(s)]
+            if jet == '<':
+                dx = -1
+            else:
+                dx = 1
+            jet_idx += 1
+            new_rock = [(x+dx, y) for x, y in rock]
+            if any(x < 0 or x >= TOWER_WIDTH for x, y in new_rock):
+                new_rock = rock
+            elif any(c in tower for c in new_rock):
+                new_rock = rock
+            else:
+                move_x += dx
+            rock = new_rock
+            new_rock = [(x, y-1) for x,y in rock]
+            if any(c in tower for c in new_rock):
+                # It settled
+                tower.update(rock)
+
+                key = (start_key, move_x, move_y)
+                current_height = max(y for x,y in tower)
+
+                info_for_move = tower_heights[key]
+
+                layout = tuple(max(y-current_height
+                                   for x,y in tower
+                                   if x == tx)
+                               for tx in range(TOWER_WIDTH))
+
+                stats = (current_height, rock_idx, layout)
+
+                if len(info_for_move) > 1:
+                    last_diff = info_for_move[-1][0] - info_for_move[-2][0]
+                    curr_diff = current_height - info_for_move[-1][0]
+
+                    last_move_diff = info_for_move[-1][1] - info_for_move[-2][1]
+                    curr_move_diff = rock_idx - info_for_move[-1][1]
+
+                    last_layout = info_for_move[-1][2]
+
+                    if (curr_diff == last_diff and
+                        last_move_diff == curr_move_diff):
+                        assert(stats[2] == info_for_move[-1][2])
+                        remaining_times = TIMES - rock_idx - 1
+
+                        cycle_length = rock_idx - info_for_move[-1][1]
+                        cycles = remaining_times // cycle_length
+                        total_jumpahead = cycles * cycle_length
+
+                        jumped_tower = run_sim(s, tower,
+                                               rock_idx + 1 + total_jumpahead,
+                                               jet_idx, TIMES)
+
+                        return max(y for x,y in jumped_tower) + curr_diff * cycles
+
+                info_for_move.append(stats)
+                break
+
+            move_y -= 1
+
+            rock = new_rock
+
+    assert(False)
+
 def part2(s):
-    pass
+    answer = part2_impl(s)
+
+    lib.aoc.give_answer(2022, 17, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 17)
 part1(INPUT)
