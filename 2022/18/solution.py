fac8a79dc0312b03d2a44c2e9be678d0db00856b
Patrick Hogg <phogg@novamoon.net>
2022-12-18 00:49:30 -0500

    2022 Refactor Day 18
    - Since both parts use a set of lava it makes more sense to just parse the lava into a set to begin with. Also, name the lava set lava (rather than data) for clarity!
    - Part 1 can use a sum comprehension for a very compact solution.
    - Part 2 can be solved by doing a flood fill of the outside and keeping track of outside air voxels. That way the check for whether neighbor voxels are exterior is much simpler, allowing the answer to ultimately be computed in a sum comprehension like part 1. (Plus this is way faster, my original code ran in about 1.5 seconds but this runs in under 0.02 seconds!)

--- 2022/18/solution.py
+++ 2022/18/solution.py
@@ -1,73 +1,50 @@
 import lib.aoc
 
-def parse_input(s):
-    for line in s.splitlines():
-        yield tuple(map(int, line.split(',')))
+def parse_lava(s):
+    return set(tuple(map(int, line.split(',')))
+               for line in s.splitlines())
 
 def neighbors(x, y, z):
-    return [(x-1, y, z), (x+1, y, z),
+    return {(x-1, y, z), (x+1, y, z),
             (x, y-1, z), (x, y+1, z),
-            (x, y, z-1), (x, y, z+1)]
+            (x, y, z-1), (x, y, z+1)}
 
 def part1(s):
-    data = set(parse_input(s))
+    lava = parse_lava(s)
 
-    answer = 0
-
-    for x, y, z in data:
-        for n in neighbors(x, y, z):
-            if n not in data:
-                answer += 1
+    answer = sum(len(neighbors(x, y, z) - lava)
+                 for x, y, z in lava)
 
     lib.aoc.give_answer(2022, 18, 1, answer)
 
 def part2(s):
-    data = set(parse_input(s))
+    lava = parse_lava(s)
 
-    min_x = min(x for x,y,z in data)
-    max_x = max(x for x,y,z in data)
-    min_y = min(y for x,y,z in data)
-    max_y = max(y for x,y,z in data)
-    min_z = min(z for x,y,z in data)
-    max_z = max(z for x,y,z in data)
+    min_x = min(x for x,y,z in lava)
+    max_x = max(x for x,y,z in lava)
+    min_y = min(y for x,y,z in lava)
+    max_y = max(y for x,y,z in lava)
+    min_z = min(z for x,y,z in lava)
+    max_z = max(z for x,y,z in lava)
 
-    x_range = range(min_x, max_x+1)
-    y_range = range(min_y, max_y+1)
-    z_range = range(min_z, max_z+1)
+    x_range = range(min_x-1, max_x+2)
+    y_range = range(min_y-1, max_y+2)
+    z_range = range(min_z-1, max_z+2)
 
-    known_exterior = set()
+    outside_air = {(min_x-1, min_y-1, min_z-1)}
+    to_handle = [(min_x-1, min_y-1, min_z-1)]
 
-    def is_exterior(x, y, z):
-        if (x, y, z) in data:
-            return False
+    # Flood fill the exterior to find which voxels are air
+    while to_handle:
+        x, y, z = to_handle.pop()
+        if x not in x_range or y not in y_range or z not in z_range:
+            continue
+        newly_found_air = neighbors(x, y, z) - outside_air - lava
+        outside_air.update(newly_found_air)
+        to_handle.extend(newly_found_air)
 
-        checked = set()
-        todo = [(x, y, z)]
-
-        while todo:
-            x, y, z = todo.pop()
-            if (x, y, z) in checked:
-                continue
-            checked.add((x, y, z))
-            if (x, y, z) in known_exterior:
-                known_exterior.update(checked - data)
-                return True
-            if x not in x_range or y not in y_range or z not in z_range:
-                # We breached the range, it's exterior!
-                known_exterior.update(checked - data)
-                return True
-            if (x, y, z) not in data:
-                todo += neighbors(x, y, z)
-
-        # We couldn't reach the outside!
-        return False
-
-    answer = 0
-
-    for x, y, z in data:
-        for n in neighbors(x, y, z):
-            if is_exterior(*n):
-                answer += 1
+    answer = sum(len((neighbors(x, y, z) & outside_air) - lava)
+                 for x, y, z in lava)
 
     lib.aoc.give_answer(2022, 18, 2, answer)
 
