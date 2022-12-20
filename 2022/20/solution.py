c0533e5bee39e327039b1c4e169f4a4b9b9b9a1a
Patrick Hogg <phogg@novamoon.net>
2022-12-20 00:19:05 -0500

    2022 Day 20 Part 2
    Okay, so we now mix multiple times and need to mix in the same order each time. Sounds good, just keep the mix order list between loops. And we multiply by a big number but that's no big deal either. I *did* copy/paste the mix instead of passing the number of times to mix in as a parameter, not sure why I did that. Oh well...
    
    Good, I jumped up to rank 75. At least I didn't slip more than 2 places on the overall (34th to 36th), could have been worse.

--- 2022/20/solution.py
+++ 2022/20/solution.py
@@ -37,7 +37,28 @@ def part1(s):
     lib.aoc.give_answer(2022, 20, 1, answer)
 
 def part2(s):
-    pass
+    nums = collections.deque(map(Wrapper, parse_all_ints(s)))
+    for n in nums:
+        n.n *= 811589153
+
+    order = list(nums)
+
+    for _ in range(10):
+        for n in order:
+            idx = nums.index(n)
+            nums.rotate(-idx)
+            nums.popleft()
+            nums.rotate(-n.n)
+            nums.insert(0, n)
+
+    nums = [n.n for n in nums]
+
+    zero_idx = nums.index(0)
+
+    answer = sum(nums[(zero_idx + off) % len(nums)]
+                 for off in (1000, 2000, 3000))
+
+    lib.aoc.give_answer(2022, 20, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 20)
 part1(INPUT)
