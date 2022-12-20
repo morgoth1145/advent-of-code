fb6dc2f8ae99d9a1219dca07e46df6ffc901665f
Patrick Hogg <phogg@novamoon.net>
2022-12-20 00:15:58 -0500

    2022 Day 20 Part 1
    The algorithm wasn't complicated, this was just a case of me not being familiar/comfortable with Python's deque class! I tried implementing this with list slicing initially (which would have been awfully slow) but I kept messing up the indexing. Probably half of the solve time was me double checking where the deque class lives and how to use it via the REPL...
    
    Once I figured that out I did goof up the rotate direction, but that was the main bug. Still got it done, at least I didn't flounder *too* long...
    
    Rank 106. Oof...

---
+++ 2022/20/solution.py
@@ -0,0 +1,44 @@
+import collections
+
+import lib.aoc
+
+class Wrapper:
+    def __init__(self, n):
+        self.n = n
+
+    def __repr__(self):
+        return str(self.n)
+
+def parse_all_ints(s):
+    return list(map(int, s.split()))
+
+def mix(nums):
+    nums = collections.deque(map(Wrapper, nums))
+
+    for n in list(nums):
+        idx = nums.index(n)
+        nums.rotate(-idx)
+        nums.popleft()
+        nums.rotate(-n.n)
+        nums.insert(0, n)
+
+    return [n.n for n in nums]
+
+def part1(s):
+    nums = parse_all_ints(s)
+
+    nums = mix(nums)
+
+    zero_idx = nums.index(0)
+
+    answer = sum(nums[(zero_idx + off) % len(nums)]
+                 for off in (1000, 2000, 3000))
+
+    lib.aoc.give_answer(2022, 20, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2022, 20)
+part1(INPUT)
+part2(INPUT)
