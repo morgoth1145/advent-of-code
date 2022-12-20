4b215080ce5dd9d4e8319eb1a3b676f57ed30ca5
Patrick Hogg <phogg@novamoon.net>
2022-12-19 21:03:56 -0500

    2022 Refactor and optimize Day 19 even more
    - Bundling the remaining time into the state makes some of the queries cleaner as well as reduces the complexity of the best_geodes function
    - Merging all the robot build methods on the state reduces code duplication
    - Limiting the maximum ore reserves to what can possibly be used helps to collapse states improving search speed
    - Extracting the best counter to a nonlocal variable also collapses some of keys for the best_geodes function, though this then requires some more care internally as best_geodes can no longer be trusted to return the best seen geode count. As such it was renamed to update_best and no longer returns anything.
    
    All this cuts the part 1 time to about 0.25 seconds and the part 2 time to about 0.75 seconds.

--- 2022/19/solution.py
+++ 2022/19/solution.py
@@ -33,76 +33,86 @@ class State(typing.NamedTuple):
     geode_robots: int
     geodes: int
 
-    def minutes_to_build_ore_robot(self, bp, remaining_time):
+    remaining_time: int
+
+    def build(self, bp, harvest_minutes,
+              ore=0, clay=0, obsidian=0, geode=0):
+        remaining_time = self.remaining_time - harvest_minutes
+
+        remaining_useful_building_cycles = remaining_time - 1
+
+        ore_robots = self.ore_robots + ore
+        ore = (self.ore
+               + self.ore_robots * harvest_minutes
+               - ore * bp.ore_robot_ore_cost
+               - clay * bp.clay_robot_ore_cost
+               - obsidian * bp.obsidian_robot_ore_cost
+               - geode * bp.geode_robot_ore_cost)
+
+        max_ore_per_robot = max(bp.ore_robot_ore_cost,
+                                bp.clay_robot_ore_cost,
+                                bp.obsidian_robot_ore_cost,
+                                bp.geode_robot_ore_cost)
+
+        max_ore_needed = remaining_useful_building_cycles * max_ore_per_robot
+        remaining_useful_ore_harvest_cycles = remaining_useful_building_cycles - 1
+        max_reserve_ore_needed = max_ore_needed - remaining_useful_ore_harvest_cycles * ore_robots
+        ore = min(ore, max_reserve_ore_needed)
+
+        return State(ore_robots,
+                     ore,
+                     self.clay_robots + clay,
+                     self.clay + self.clay_robots * harvest_minutes - obsidian * bp.obsidian_robot_clay_cost,
+                     self.obsidian_robots + obsidian,
+                     self.obsidian + self.obsidian_robots * harvest_minutes - geode * bp.geode_robot_obsidian_cost,
+                     self.geode_robots + geode,
+                     self.geodes + self.geode_robots * harvest_minutes,
+                     self.remaining_time - harvest_minutes)
+
+    def minutes_to_build_ore_robot(self, bp):
         return max((bp.ore_robot_ore_cost - self.ore + self.ore_robots - 1) // self.ore_robots,
                    0) + 1
 
-    def build_ore_robot(self, bp, harvest_minutes):
-        return self._replace(ore_robots=self.ore_robots + 1,
-                             ore=self.ore + self.ore_robots * harvest_minutes - bp.ore_robot_ore_cost,
-                             clay=self.clay + self.clay_robots * harvest_minutes,
-                             obsidian=self.obsidian + self.obsidian_robots * harvest_minutes,
-                             geodes=self.geodes + self.geode_robots * harvest_minutes)
-
-    def minutes_to_build_clay_robot(self, bp, remaining_time):
+    def minutes_to_build_clay_robot(self, bp):
         return max((bp.clay_robot_ore_cost - self.ore + self.ore_robots - 1) // self.ore_robots,
                    0) + 1
 
-    def build_clay_robot(self, bp, harvest_minutes):
-        return self._replace(clay_robots=self.clay_robots + 1,
-                             ore=self.ore + self.ore_robots * harvest_minutes - bp.clay_robot_ore_cost,
-                             clay=self.clay + self.clay_robots * harvest_minutes,
-                             obsidian=self.obsidian + self.obsidian_robots * harvest_minutes,
-                             geodes=self.geodes + self.geode_robots * harvest_minutes)
-
-    def minutes_to_build_obsidian_robot(self, bp, remaining_time):
+    def minutes_to_build_obsidian_robot(self, bp):
         return max((bp.obsidian_robot_ore_cost - self.ore + self.ore_robots - 1) // self.ore_robots,
                    (bp.obsidian_robot_clay_cost - self.clay + self.clay_robots - 1) // self.clay_robots,
                    0) + 1
 
-    def build_obsidian_robot(self, bp, harvest_minutes):
-        return self._replace(obsidian_robots=self.obsidian_robots + 1,
-                             ore=self.ore + self.ore_robots * harvest_minutes - bp.obsidian_robot_ore_cost,
-                             clay=self.clay + self.clay_robots * harvest_minutes - bp.obsidian_robot_clay_cost,
-                             obsidian=self.obsidian + self.obsidian_robots * harvest_minutes,
-                             geodes=self.geodes + self.geode_robots * harvest_minutes)
-
-    def minutes_to_build_geode_robot(self, bp, remaining_time):
+    def minutes_to_build_geode_robot(self, bp):
         return max((bp.geode_robot_ore_cost - self.ore + self.ore_robots - 1) // self.ore_robots,
                    (bp.geode_robot_obsidian_cost - self.obsidian + self.obsidian_robots - 1) // self.obsidian_robots,
                    0) + 1
 
-    def build_geode_robot(self, bp, harvest_minutes):
-        return self._replace(geode_robots=self.geode_robots + 1,
-                             ore=self.ore + self.ore_robots * harvest_minutes - bp.geode_robot_ore_cost,
-                             clay=self.clay + self.clay_robots * harvest_minutes,
-                             obsidian=self.obsidian + self.obsidian_robots * harvest_minutes - bp.geode_robot_obsidian_cost,
-                             geodes=self.geodes + self.geode_robots * harvest_minutes)
+    def minimum_guaranteed_geodes(self):
+        return self.geodes + self.geode_robots * self.remaining_time
 
-    def minimum_guaranteed_geodes(self, remaining_time):
-        return self.geodes + self.geode_robots * remaining_time
-
-    def theoretical_maximum_geodes(self, remaining_time):
-        if remaining_time <= 1:
-            return self.minimum_guaranteed_geodes(remaining_time)
+    def theoretical_maximum_geodes(self):
+        if self.remaining_time <= 1:
+            return self.minimum_guaranteed_geodes()
 
         # If we theoretically built 1 geode robot per minute until time ran
         # out this is how many more geodes we would get
-        theoretical_additions = (remaining_time-1) * remaining_time // 2
+        theoretical_additions = (self.remaining_time-1) * self.remaining_time // 2
 
-        return self.minimum_guaranteed_geodes(remaining_time) + theoretical_additions
+        return self.minimum_guaranteed_geodes() + theoretical_additions
 
 def max_geodes(bp, minutes):
     max_ore_needed = max(bp.clay_robot_ore_cost,
                          bp.obsidian_robot_ore_cost,
                          bp.geode_robot_ore_cost)
 
-    @functools.cache
-    def best_geodes(state, best, remaining_time):
-        assert(remaining_time > 0)
+    best = 0
 
-        best = max(best, state.minimum_guaranteed_geodes(remaining_time))
-        if state.theoretical_maximum_geodes(remaining_time) <= best:
+    @functools.cache
+    def update_best(state):
+        nonlocal best
+
+        best = max(best, state.minimum_guaranteed_geodes())
+        if state.theoretical_maximum_geodes() <= best:
             return best
 
         # Only try to build robots if:
@@ -113,47 +123,39 @@ def max_geodes(bp, minutes):
 
         if state.ore_robots < max_ore_needed:
             # Harvest -> geode robot -> harvest
-            build_minutes = state.minutes_to_build_ore_robot(bp, remaining_time)
-            if build_minutes <= remaining_time-3:
-                best = max(best_geodes(state.build_ore_robot(bp, build_minutes),
-                                       best, remaining_time-build_minutes),
-                           best)
+            build_minutes = state.minutes_to_build_ore_robot(bp)
+            if build_minutes <= state.remaining_time-3:
+                update_best(state.build(bp, build_minutes, ore=1))
 
         if state.clay_robots < bp.obsidian_robot_clay_cost:
             # Harvest -> obsidian robot -> harvest -> geode robot -> harvest
-            build_minutes = state.minutes_to_build_clay_robot(bp, remaining_time)
-            if build_minutes <= remaining_time-5:
-                best = max(best_geodes(state.build_clay_robot(bp, build_minutes),
-                                       best, remaining_time-build_minutes),
-                           best)
+            build_minutes = state.minutes_to_build_clay_robot(bp)
+            if build_minutes <= state.remaining_time-5:
+                update_best(state.build(bp, build_minutes, clay=1))
 
         if state.clay_robots > 0 and state.obsidian_robots < bp.geode_robot_obsidian_cost:
-            build_minutes = state.minutes_to_build_obsidian_robot(bp, remaining_time)
+            build_minutes = state.minutes_to_build_obsidian_robot(bp)
             # Harvest -> geode robot -> harvest
-            if build_minutes <= remaining_time-3:
-                best = max(best_geodes(state.build_obsidian_robot(bp, build_minutes),
-                                       best, remaining_time-build_minutes),
-                           best)
+            if build_minutes <= state.remaining_time-3:
+                update_best(state.build(bp, build_minutes, obsidian=1))
 
         if state.obsidian_robots > 0:
-            build_minutes = state.minutes_to_build_geode_robot(bp, remaining_time)
+            build_minutes = state.minutes_to_build_geode_robot(bp)
             # Harvest
-            if build_minutes <= remaining_time-1:
-                best = max(best_geodes(state.build_geode_robot(bp, build_minutes),
-                                       best, remaining_time-build_minutes),
-                           best)
+            if build_minutes <= state.remaining_time-1:
+                update_best(state.build(bp, build_minutes, geode=1))
 
-        return best
+    update_best(State(ore_robots=1,
+                      ore=0,
+                      clay_robots=0,
+                      clay=0,
+                      obsidian_robots=0,
+                      obsidian=0,
+                      geode_robots=0,
+                      geodes=0,
+                      remaining_time=minutes))
 
-    return best_geodes(State(ore_robots=1,
-                             ore=0,
-                             clay_robots=0,
-                             clay=0,
-                             obsidian_robots=0,
-                             obsidian=0,
-                             geode_robots=0,
-                             geodes=0),
-                       0, minutes)
+    return best
 
 def part1(s):
     answer = sum((i+1) * max_geodes(bp, 24)
