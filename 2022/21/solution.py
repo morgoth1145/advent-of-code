53368b98d3b106aa2bb36a7ab0b3b9d7fbb00479
Patrick Hogg <phogg@novamoon.net>
2022-12-21 00:19:13 -0500

    2022 Day 21 Part 2
    Oh, we're solving for a variable now! My first thought was to do a brute force search for the right answer. Now that I'm typing this up I wonder if I should have tried to do a binary search or something...oh well. Anyway, I expect that z3 (which I have no experience in) could solve this super quickly but that is a no go. I tried to give the equation to Wolfram Alpha as well but that also didn't work. So unfortunately I was left solving this myself.
    
    This essentially is a parse tree and one side was pre-evaluated (yay) so I could just unwind the tree and modify the target until I got the right answer which is nice. That took a lot of typing though, some library to solve an expression for a variable (again, z3 probably can do it easily) would have been so nice.
    
    Anyway, I didn't leaderboard but I at least got rank 154. Fine by me, I was not prepared for this at all!

--- 2022/21/solution.py
+++ 2022/21/solution.py
@@ -31,7 +31,76 @@ def part1(s):
     lib.aoc.give_answer(2022, 21, 1, answer)
 
 def part2(s):
-    pass
+    barrel = parse_monkeys(s)
+
+    def yells(monkey):
+        if monkey == 'humn':
+            return 'humn'
+        op = barrel[monkey]
+        if isinstance(op, int):
+            return op
+
+        a, o, b = op
+        if monkey == 'root':
+            o = '=='
+        if o == '/':
+            o = '//'
+        a = yells(a)
+        b = yells(b)
+        if isinstance(a, int) and isinstance(b, int):
+            return eval(f'{a} {o} {b}')
+        return [a, o, b]
+
+    test = yells('root')
+
+    assert('==' == test[1])
+
+    a, _, b = test
+
+    if isinstance(a, int):
+        target = a
+        test = b
+    else:
+        assert(isinstance(b, int))
+        target = b
+        test = a
+
+    while isinstance(test, list):
+        a, op, b = test
+        if isinstance(a, int):
+            if op == '*':
+                assert(target % a == 0)
+                target //= a
+            elif op == '+':
+                target -= a
+            elif op == '-':
+                target = a - target
+            elif op == '//':
+                assert(a % target == 0)
+                target = a // target
+            else:
+                print('a', op)
+                assert(False)
+            test = b
+        else:
+            assert(isinstance(b, int))
+            if op == '*':
+                assert(target % b == 0)
+                target //= b
+            elif op == '+':
+                target -= b
+            elif op == '-':
+                target += b
+            elif op == '//':
+                target *= b
+            else:
+                print('b', op)
+                assert(False)
+            test = a
+
+    answer = target
+
+    lib.aoc.give_answer(2022, 21, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 21)
 part1(INPUT)
