da432e32c212831fcbb6c8ae000407b63912ec24
Patrick Hogg <phogg@novamoon.net>
2022-12-22 00:44:19 -0500

    2022 Day 22 Part 2
    Hold on, what?! You want us to take this and fold it to find the connection points on the cube for wraparounds, which can also alter the direction we're facing? Holy crap!
    
    Instead of trying to figure *that* craziness out in code I literally cut up a postcard in the shape of my input. Then I could figure out which edges connected and code up a wraparound function for my specific input shape. (I don't know if all inputs share the same shape or not.) That was super tedious, but much less likely for me to get stuck in a hole of code that doesn't work in the slightest.
    
    I did have a couple bugs with the wraparound function initially. One was a miscalculation which sent you into no man's land, and another was a wrap that appeared valid but was not. Thankfully I was able to detect the latter by calling wrap again on the wrapped point (facing in the opposite direction) and verify that you landed in the starting point facing the opposite direction that you started facing. If that assertion didn't match then one of the two directions was wrong! I only had one such issue thankfully, and after fixing that (with my reference paper) I got the right answer!
    
    I got rank 12! I knew that figuring out the wrapping by hand with paper was the way to go!

--- 2022/22/solution.py
+++ 2022/22/solution.py
@@ -70,7 +70,101 @@ def part1(s):
     lib.aoc.give_answer(2022, 22, 1, answer)
 
 def part2(s):
-    pass
+    grid, path, (x, y) = parse_input(s)
+
+    # HARDCODED FOR NOW! I literally figured this out with a cut out piece of
+    # paper going through all the cases!
+    def wrap(x, y, dx, dy):
+        if dx == 1:
+            # Right
+            if x == 150:
+                return 100, 151-y, -1, 0
+            if x == 100:
+                if 51 <= y <= 100:
+                    return 100 + (y - 50), 50, 0, -1
+                if 101 <= y <= 150:
+                    return 150, 51 - (y - 100), -1, 0
+            if x == 50:
+                return 50 + (y - 150), 150, 0, -1
+        elif dx == -1:
+            # Left
+            if x == 51:
+                if 1 <= y <= 50:
+                    return 1, 151 - y, 1, 0
+                if 51 <= y <= 100:
+                    return y - 50, 101, 0, 1
+            if x == 1:
+                if 101 <= y <= 150:
+                    return 51, 1 + (150 - y), 1, 0
+                if 151 <= y <= 200:
+                    return y - 150 + 50, 1, 0, 1
+        elif dy == 1:
+            # Down
+            if y == 50:
+                return 100, x - 50, -1, 0
+            if y == 150:
+                return 50, x + 100, -1, 0
+            if y == 200:
+                return x + 100, 1, 0, 1
+        elif dy == -1:
+            # Up
+            if y == 1:
+                if 51 <= x <= 100:
+                    return 1, x+100, 1, 0
+                if 101 <= x <= 150:
+                    return x-100, 200, 0, -1
+            if y == 101:
+                return 51, x+50, 1, 0
+        assert(False)
+
+    dx = 1
+    dy = 0
+
+    for step in path:
+        if isinstance(step, int):
+            for _ in range(step):
+                nx, ny = x+dx, y+dy
+                cell = grid.get((nx, ny))
+                if cell is None:
+                    # WRAP
+                    nx, ny, ndx, ndy = wrap(x, y, dx, dy)
+
+                    # Debugging!
+                    if (nx, ny) not in grid:
+                        print(x, y, dx, dy)
+                        print(nx, ny)
+
+                    # Got the wrong answer initially meaning I had some wrapping
+                    # wrong. Thankfully checking the inverse of the wrap (it
+                    # should be invertible) found the error!
+                    try:
+                        revx, revy, revdx, revdy = wrap(nx, ny, -ndx, -ndy)
+                        if (revx, revy, -revdx, -revdy) != (x, y, dx, dy):
+                            print(x, y, dx, dy)
+                            print(nx, ny, -ndx, -ndy)
+                            print(revx, revy, -revdx, -revdy)
+                            assert(False)
+                    except:
+                        assert(False)
+
+                    cell = grid[nx,ny]
+                    if cell == '#':
+                        break # Hit a wall, don't mess with direction
+                    x, y = nx, ny
+                    dx, dy = ndx, ndy
+                elif cell == '#':
+                    break # Hit a wall!
+                x, y = nx, ny
+        elif step == 'R':
+            dy, dx = dx, -dy
+        elif step == 'L':
+            dx, dy = dy, -dx
+        else:
+            assert(False)
+
+    answer = 1000 * y + 4 * x + facing_val[dx,dy]
+
+    lib.aoc.give_answer(2022, 22, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 22)
 part1(INPUT)
