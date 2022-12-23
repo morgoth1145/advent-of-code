7691dfd97b6f0b6944c95b0634b5ff5bfb66d122
Patrick Hogg <phogg@novamoon.net>
2022-12-23 00:29:15 -0500

    2022 Day 23 Part 2
    Well, at least part 2 was easy. Run that same simulation until it doesn't cause any changes and count how many cycles that took. Bit of code duplication (yuck!) and quick modifications on the looping and termination gave me that count easily.
    
    Jumped up to rank 204 with that.

--- 2022/23/solution.py
+++ 2022/23/solution.py
@@ -90,8 +90,88 @@ def part1(s):
 
     lib.aoc.give_answer(2022, 23, 1, answer)
 
+def turns_to_stationary(grid):
+    def test_north(targets, x, y):
+        if all((test_x, y-1) not in grid or grid[test_x, y-1] == '.'
+               for test_x in (x-1, x, x+1)):
+            targets[x, y-1].append(c)
+            return True
+
+        return False
+
+    def test_south(targets, x, y):
+        if all((test_x, y+1) not in grid or grid[test_x, y+1] == '.'
+               for test_x in (x-1, x, x+1)):
+            targets[x, y+1].append(c)
+            return True
+
+        return False
+
+    def test_west(targets, x, y):
+        if all((x-1, test_y) not in grid or grid[x-1, test_y] == '.'
+               for test_y in (y-1, y, y+1)):
+            targets[x-1, y].append(c)
+            return True
+
+        return False
+
+    def test_east(targets, x, y):
+        if all((x+1, test_y) not in grid or grid[x+1, test_y] == '.'
+               for test_y in (y-1, y, y+1)):
+            targets[x+1, y].append(c)
+            return True
+
+        return False
+
+    search_order = [test_north, test_south, test_west, test_east]
+
+    def neighbors(x, y):
+        for tx in (x-1, x, x+1):
+            for ty in (y-1, y, y+1):
+                if tx == x and ty == y:
+                    continue
+                yield tx, ty
+
+    turn = 0
+
+    while True:
+        turn += 1
+        targets = collections.defaultdict(list)
+
+        for c, v in grid.items():
+            x, y = c
+
+            if v == '.':
+                continue
+            if all(grid.get(n, '.') == '.'
+                   for n in neighbors(x, y)):
+                continue
+
+            for test in search_order:
+                if test(targets, x, y):
+                    break
+
+        search_order = search_order[1:] + search_order[:1]
+
+        moves = 0
+
+        for target, elves in targets.items():
+            if len(elves) > 1:
+                continue
+            elf = elves[0]
+            grid[target] = '#'
+            grid[elf] = '.'
+            moves += 1
+
+        if moves == 0:
+            return turn
+
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s).to_dict()
+
+    answer = turns_to_stationary(grid)
+
+    lib.aoc.give_answer(2022, 23, 2, answer)
 
 INPUT = lib.aoc.get_input(2022, 23)
 part1(INPUT)
