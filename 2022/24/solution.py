26a23857a59857fad31beea5e42ea96fe89a378c
Patrick Hogg <phogg@novamoon.net>
2022-12-24 00:54:20 -0500

    2022 Rewrite Day 24
    Using breadth first search is so much cleaner and faster! Not only that, but using 3 searches in a row for part 2 drastically reduces the state space since we've pruned some known suboptimal paths. This new code only takes 0.5 seconds for part 1 and 1.5 seconds for part 2, compared to who knows what for my original code!

--- 2022/24/solution.py
+++ 2022/24/solution.py
@@ -1,154 +1,87 @@
-import functools
-
 import lib.aoc
-import lib.graph
 import lib.grid
 
-def part1(s):
-    grid = lib.grid.FixedGrid.parse(s)
+class Valley:
+    def __init__(self, s):
+        grid = lib.grid.FixedGrid.parse(s)
 
-    blizzards = [(c, v) for c, v in grid.items()
-                 if v not in '.#']
+        BLIZZARD_MOVEMENT = {
+            '^': (0, -1),
+            'v': (0, 1),
+            '<': (-1, 0),
+            '>': (1, 0),
+        }
 
-    start = min(x for x in range(grid.width) if grid[x,0] == '.'), 0
-    end = min(x for x in range(grid.width) if grid[x,grid.height-1] == '.'), grid.height-1
+        self.blizzards = [(c, BLIZZARD_MOVEMENT[v]) for c, v in grid.items()
+                          if v not in '.#']
+        self.walls = {c for c, v in grid.items()
+                      if v == '#'}
+        self.start = min(x for x in range(grid.width)
+                         if grid[x,0] == '.'), 0
+        self.goal = end = min(x for x in range(grid.width)
+                              if grid[x, grid.height-1] == '.'), grid.height-1
+        # Don't let the elf escape the valley!
+        self.walls.add((self.start[0], -1))
+        self.walls.add((self.goal[0], grid.height))
 
-    BLIZZARD_MOVEMENT = {
-        '^': (0, -1),
-        'v': (0, 1),
-        '<': (-1, 0),
-        '>': (1, 0),
-    }
+    def move_optimally(self, begin, target):
+        steps = 0
+        states = {begin}
 
-    @functools.cache
-    def step_blizzards(blizzards):
-        moved = []
+        while target not in states:
+            steps += 1
 
-        for (x, y), v in blizzards:
-            dx, dy = BLIZZARD_MOVEMENT[v]
-            x += dx
-            y += dy
-            if grid[x,y] == '#':
-                x -= dx
-                y -= dy
-                while grid[x,y] != '#':
-                    x -= dx
-                    y -= dy
+            new_blizzards = []
+            blizzard_squares = set()
+
+            for (x, y), (dx, dy) in self.blizzards:
                 x += dx
                 y += dy
-            moved.append(((x, y), v))
+                if (x, y) in self.walls:
+                    x -= dx
+                    y -= dy
+                    while (x, y) not in self.walls:
+                        x -= dx
+                        y -= dy
+                    x += dx
+                    y += dy
+                new_blizzards.append(((x, y), (dx, dy)))
+                blizzard_squares.add((x, y))
 
-        return tuple(moved)
+            self.blizzards = new_blizzards
 
-    def neighbor_fn(state):
-        (x, y), blizzards = state
-        new_blizzards = step_blizzards(blizzards)
-        blizzard_squares = set(c for c,v in new_blizzards)
+            new_states = set()
 
-        for n in grid.neighbors(x, y):
-            if grid[n] == '#':
-                continue
-            if n in blizzard_squares:
-                continue
-            yield (n, new_blizzards), 1
+            for x, y in states:
+                for n in [(x-1, y),
+                          (x+1, y),
+                          (x, y-1),
+                          (x, y+1)]:
+                    if n in self.walls or n in blizzard_squares:
+                        continue
+                    new_states.add(n)
+                if (x, y) not in self.walls and (x, y) not in blizzard_squares:
+                    new_states.add((x, y))
 
-        if (x, y) not in blizzard_squares:
-            yield ((x, y), new_blizzards), 1
+            states = new_states
 
-    graph = lib.graph.make_lazy_graph(neighbor_fn)
+        return steps
 
-    def end_fn(state):
-        return state[0] == end
+def part1(s):
+    valley = Valley(s)
 
-    def heuristic(state):
-        (x, y), _ = state
-        return abs(x-end[0]) + abs(y-end[1])
-
-    answer = lib.graph.dijkstra_length_fuzzy_end(graph, (start, tuple(blizzards)),
-                                                 end_fn, heuristic=heuristic)
+    answer = valley.move_optimally(valley.start, valley.goal)
 
     lib.aoc.give_answer(2022, 24, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s)
+    valley = Valley(s)
 
-    blizzards = [(c, v) for c, v in grid.items()
-                 if v not in '.#']
-
-    start = min(x for x in range(grid.width) if grid[x,0] == '.'), 0
-    end = min(x for x in range(grid.width) if grid[x,grid.height-1] == '.'), grid.height-1
-
-    BLIZZARD_MOVEMENT = {
-        '^': (0, -1),
-        'v': (0, 1),
-        '<': (-1, 0),
-        '>': (1, 0),
-    }
-
-    @functools.cache
-    def step_blizzards(blizzards):
-        moved = []
-
-        for (x, y), v in blizzards:
-            dx, dy = BLIZZARD_MOVEMENT[v]
-            x += dx
-            y += dy
-            if grid[x,y] == '#':
-                x -= dx
-                y -= dy
-                while grid[x,y] != '#':
-                    x -= dx
-                    y -= dy
-                x += dx
-                y += dy
-            moved.append(((x, y), v))
-
-        return tuple(moved)
-
-    START_STATE = (start, 0, tuple(blizzards))
-    GOAL_1_PHASE = 0
-    RETURN_PHASE = 1
-    GOAL_2_PHASE = 2
-
-    def neighbor_fn(state):
-        (x, y), phase, blizzards = state
-        new_blizzards = step_blizzards(blizzards)
-        blizzard_squares = set(c for c,v in new_blizzards)
-
-        for n in grid.neighbors(x, y):
-            if grid[n] == '#':
-                continue
-            if n in blizzard_squares:
-                continue
-            if phase == GOAL_1_PHASE and n == end:
-                yield (n, RETURN_PHASE, new_blizzards), 1
-            elif phase == RETURN_PHASE and n == start:
-                yield (n, GOAL_2_PHASE, new_blizzards), 1
-            else:
-                yield (n, phase, new_blizzards), 1
-
-        if (x, y) not in blizzard_squares:
-            yield ((x, y), phase, new_blizzards), 1
-
-    graph = lib.graph.make_lazy_graph(neighbor_fn)
-
-    def end_fn(state):
-        return state[0] == end and state[1] == GOAL_2_PHASE
-
-    START_TO_GOAL_DIST = sum(abs(s-e) for s,e in zip(start, end))
-
-    def heuristic(state):
-        (x, y), phase, _ = state
-        if phase == GOAL_2_PHASE:
-            return abs(x-end[0]) + abs(y-end[1])
-        if phase == RETURN_PHASE:
-            return abs(x-start[0]) + abs(y-start[1]) + START_TO_GOAL_DIST
-        if phase == GOAL_1_PHASE:
-            return abs(x-end[0]) + abs(y-end[1]) + 2 * START_TO_GOAL_DIST
-        assert(False)
-
-    answer = lib.graph.dijkstra_length_fuzzy_end(graph, START_STATE,
-                                                 end_fn, heuristic=heuristic)
+    # We don't need one giant search, we can just do each phase individually!
+    answer = 0
+    answer += valley.move_optimally(valley.start, valley.goal)
+    answer += valley.move_optimally(valley.goal, valley.start)
+    answer += valley.move_optimally(valley.start, valley.goal)
 
     lib.aoc.give_answer(2022, 24, 2, answer)
 
