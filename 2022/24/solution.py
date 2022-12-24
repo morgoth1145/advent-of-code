1658dcc2db4908d53f8d1ad07040676a4a89e835
Patrick Hogg <phogg@novamoon.net>
2022-12-24 02:03:22 -0500

    2022 Refactor Day 24
    Instead of calculating the new blizzard positions at each step it make more sense to precompute the blizzard squares at each step. However, this can be wasteful if done for the full grid. Since each axis operates independently it makes more sense to precompute the occupied squares for each axis and make use of the far smaller periods.
    
    While this code is now a bit longer, it also runs way faster. (About 0.19 seconds for part 1 and about 0.35-0.36 seconds for part 2.)

--- 2022/24/solution.py
+++ 2022/24/solution.py
@@ -5,50 +5,67 @@ class Valley:
     def __init__(self, s):
         grid = lib.grid.FixedGrid.parse(s)
 
-        BLIZZARD_MOVEMENT = {
-            '^': (0, -1),
-            'v': (0, 1),
-            '<': (-1, 0),
-            '>': (1, 0),
-        }
-
-        self.blizzards = [(c, BLIZZARD_MOVEMENT[v]) for c, v in grid.items()
-                          if v not in '.#']
-        self.walls = {c for c, v in grid.items()
-                      if v == '#'}
         self.start = min(x for x in range(grid.width)
                          if grid[x,0] == '.'), 0
         self.goal = end = min(x for x in range(grid.width)
                               if grid[x, grid.height-1] == '.'), grid.height-1
+
+        self.walls = {c for c, v in grid.items()
+                      if v == '#'}
         # Don't let the elf escape the valley!
         self.walls.add((self.start[0], -1))
         self.walls.add((self.goal[0], grid.height))
 
+        # Precompute all the blizzard states. However, precompute them on a
+        # per-axis basis as those periods are much lower than the full period.
+
+        blizzards = [(c, v) for c, v in grid.items()
+                     if v in '^v']
+
+        self.vertical_blizzards = []
+        for _ in range(25): # Exclude wall tiles in the period!
+            self.vertical_blizzards.append({c
+                                            for c, v
+                                            in blizzards})
+            new_blizzards = []
+            for (x, y), v in blizzards:
+                y += 1 if v == 'v' else -1
+                if y == 0:
+                    y = grid.height-2
+                elif y == grid.height-1:
+                    y = 1
+                new_blizzards.append(((x, y), v))
+            blizzards = new_blizzards
+
+        blizzards = [(c, v) for c, v in grid.items()
+                     if v in '<>']
+
+        self.horizontal_blizzards = []
+        for _ in range(grid.width-2): # Exclude wall tiles in the period!
+            self.horizontal_blizzards.append({c
+                                              for c, v
+                                              in blizzards})
+            new_blizzards = []
+            for (x, y), v in blizzards:
+                x += 1 if v == '>' else -1
+                if x == 0:
+                    x = grid.width-2
+                elif x == grid.width-1:
+                    x = 1
+                new_blizzards.append(((x, y), v))
+            blizzards = new_blizzards
+
+        self.step = 0
+
     def move_optimally(self, begin, target):
-        steps = 0
         states = {begin}
 
         while target not in states:
-            steps += 1
+            self.step += 1
 
-            new_blizzards = []
-            blizzard_squares = set()
-
-            for (x, y), (dx, dy) in self.blizzards:
-                x += dx
-                y += dy
-                if (x, y) in self.walls:
-                    x -= dx
-                    y -= dy
-                    while (x, y) not in self.walls:
-                        x -= dx
-                        y -= dy
-                    x += dx
-                    y += dy
-                new_blizzards.append(((x, y), (dx, dy)))
-                blizzard_squares.add((x, y))
-
-            self.blizzards = new_blizzards
+            blocked = set(self.walls)
+            blocked |= self.vertical_blizzards[self.step % len(self.vertical_blizzards)]
+            blocked |= self.horizontal_blizzards[self.step % len(self.horizontal_blizzards)]
 
             new_states = set()
 
@@ -57,20 +74,20 @@ class Valley:
                           (x+1, y),
                           (x, y-1),
                           (x, y+1)]:
-                    if n in self.walls or n in blizzard_squares:
+                    if n in blocked:
                         continue
                     new_states.add(n)
-                if (x, y) not in self.walls and (x, y) not in blizzard_squares:
+                if (x, y) not in blocked:
                     new_states.add((x, y))
 
             states = new_states
 
-        return steps
-
 def part1(s):
     valley = Valley(s)
 
-    answer = valley.move_optimally(valley.start, valley.goal)
+    valley.move_optimally(valley.start, valley.goal)
+
+    answer = valley.step
 
     lib.aoc.give_answer(2022, 24, 1, answer)
 
@@ -78,10 +95,11 @@ def part2(s):
     valley = Valley(s)
 
     # We don't need one giant search, we can just do each phase individually!
-    answer = 0
-    answer += valley.move_optimally(valley.start, valley.goal)
-    answer += valley.move_optimally(valley.goal, valley.start)
-    answer += valley.move_optimally(valley.start, valley.goal)
+    valley.move_optimally(valley.start, valley.goal)
+    valley.move_optimally(valley.goal, valley.start)
+    valley.move_optimally(valley.start, valley.goal)
+
+    answer = valley.step
 
     lib.aoc.give_answer(2022, 24, 2, answer)
 
