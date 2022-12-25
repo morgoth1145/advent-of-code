9e05aaf6acaa0d1f325cbc243ec087f24d2b3137
Patrick Hogg <phogg@novamoon.net>
2022-12-25 00:27:39 -0500

    2022 Rewrite Day 25
    Converting to SNAFU is much simpler if you add offsets of 2. That way it works out as a normal quinary conversion! Too bad I didn't think of this until after solving the problem...
    
    (Also changed translate_num to parse_snafu and cleaned up that logic slightly.)

--- 2022/25/solution.py
+++ 2022/25/solution.py
@@ -1,49 +1,26 @@
 import lib.aoc
 
-def translate_num(num):
+def parse_snafu(num):
     place = 1
     total = 0
     for c in num[::-1]:
-        if c == '-':
-            c = -1
-        elif c == '=':
-            c = -2
-        else:
-            c = int(c)
-        total += c * place
+        total += ('=-012'.index(c) - 2) * place
         place *= 5
 
     return total
 
 def to_snafu(num):
-    places = [1]
-    while places[0] < num:
-        places.insert(0, 5 * places[0])
+    output = ''
 
-    def impl(n, places):
-        if len(places) == 0:
-            if n == 0:
-                return ''
-            else:
-                return None
+    while num != 0:
+        # Offsetting the number at this place makes conversion simple
+        num, place = divmod(num + 2, 5)
+        output += '=-012'[place]
 
-        if places[0] * 3 < n:
-            return None
-
-        if places[0] * -3 > n:
-            return None
-
-        for mult, c in zip((2, 1, 0, -1, -2), '210-='):
-            res = impl(n - mult * places[0], places[1:])
-            if res is not None:
-                return c + res
-
-        return None
-
-    return impl(num, places).lstrip('0')
+    return output[::-1]
 
 def part1(s):
-    answer = to_snafu(sum(map(translate_num, s.splitlines())))
+    answer = to_snafu(sum(map(parse_snafu, s.splitlines())))
 
     lib.aoc.give_answer(2022, 25, 1, answer)
 
