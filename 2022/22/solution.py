4aa9949f66e9ecf0e877522547fdf02620d1ddaf
Patrick Hogg <phogg@novamoon.net>
2023-11-23 16:38:12 -0500

    2022 Refactor Day 22
    Fundamentally Day 22 parts 1 and 2 are the same except for different wraparound logic. As such a unified solve function taking a wrap function factory drastically reduces the duplication between the two parts.

--- 2022/22/solution.py
+++ 2022/22/solution.py
@@ -28,36 +28,35 @@ def parse_input(s):
 
     return grid, path, start
 
-# dx, dy
-facing_val = {
-    (1, 0): 0,
-    (0, 1): 1,
-    (-1, 0): 2,
-    (0, -1): 3,
-}
+def solve(s, wrap_fn_factory):
+    grid, path, start = parse_input(s)
 
-def part1(s):
-    grid, path, (x, y) = parse_input(s)
+    width = max(x for x,y in grid)
+    height = max(y for x,y in grid)
 
-    dx = 1
-    dy = 0
+    wrapper = wrap_fn_factory(grid, width, height)
+
+    x, y = s.index('.')+1, 1
+    dx, dy = 1, 0
 
     for step in path:
         if isinstance(step, int):
             for _ in range(step):
-                nx, ny = x+dx, y+dy
-                cell = grid.get((nx, ny))
-                if cell is None:
-                    # WRAP
-                    nnx, nny = x, y
-                    while (nnx, nny) in grid:
-                        nx, ny = nnx, nny
-                        nnx -= dx
-                        nny -= dy
-                    cell = grid[nx,ny]
-                if cell == '#':
-                    break # Hit a wall!
-                x, y = nx, ny
+                val = grid.get((x+dx, y+dy))
+                if val == '.':
+                    x, y = x+dx, y+dy
+                    continue
+
+                if val is None:
+                    # Fell off the edge, wrap around
+                    n, nd = wrapper(x, y, dx, dy)
+
+                    if grid[n] == '.':
+                        (x, y), (dx, dy) = n, nd
+                        continue
+
+                # Hit a wall
+                break
         elif step == 'R':
             dy, dx = dx, -dy
         elif step == 'L':
@@ -65,104 +64,81 @@ def part1(s):
         else:
             assert(False)
 
-    answer = 1000 * y + 4 * x + facing_val[dx,dy]
+    facing_val = {
+        (1, 0): 0,
+        (0, 1): 1,
+        (-1, 0): 2,
+        (0, -1): 3,
+    }
+
+    return 1000 * y + 4 * x + facing_val[dx,dy]
+
+def simple_wrap_factory(grid, width, height):
+    def wrap_fn(x, y, dx, dy):
+        nnx, nny = x, y
+        while (nnx, nny) in grid:
+            nx, ny = nnx, nny
+            nnx -= dx
+            nny -= dy
+        return (nx, ny), (dx, dy)
+
+    return wrap_fn
+
+def part1(s):
+    answer = solve(s, simple_wrap_factory)
 
     lib.aoc.give_answer(2022, 22, 1, answer)
 
-def part2(s):
-    grid, path, (x, y) = parse_input(s)
-
+def cubenet_wrap_factory(grid, width, height):
     # HARDCODED FOR NOW! I literally figured this out with a cut out piece of
     # paper going through all the cases!
-    def wrap(x, y, dx, dy):
+    def wrap_fn(x, y, dx, dy):
         if dx == 1:
             # Right
             if x == 150:
-                return 100, 151-y, -1, 0
+                return (100, 151-y), (-1, 0)
             if x == 100:
                 if 51 <= y <= 100:
-                    return 100 + (y - 50), 50, 0, -1
+                    return (100 + (y - 50), 50), (0, -1)
                 if 101 <= y <= 150:
-                    return 150, 51 - (y - 100), -1, 0
+                    return (150, 51 - (y - 100)), (-1, 0)
             if x == 50:
-                return 50 + (y - 150), 150, 0, -1
+                return (50 + (y - 150), 150), (0, -1)
         elif dx == -1:
             # Left
             if x == 51:
                 if 1 <= y <= 50:
-                    return 1, 151 - y, 1, 0
+                    return (1, 151 - y), (1, 0)
                 if 51 <= y <= 100:
-                    return y - 50, 101, 0, 1
+                    return (y - 50, 101), (0, 1)
             if x == 1:
                 if 101 <= y <= 150:
-                    return 51, 1 + (150 - y), 1, 0
+                    return (51, 1 + (150 - y)), (1, 0)
                 if 151 <= y <= 200:
-                    return y - 150 + 50, 1, 0, 1
+                    return (y - 150 + 50, 1), (0, 1)
         elif dy == 1:
             # Down
             if y == 50:
-                return 100, x - 50, -1, 0
+                return (100, x - 50), (-1, 0)
             if y == 150:
-                return 50, x + 100, -1, 0
+                return (50, x + 100), (-1, 0)
             if y == 200:
-                return x + 100, 1, 0, 1
+                return (x + 100, 1), (0, 1)
         elif dy == -1:
             # Up
             if y == 1:
                 if 51 <= x <= 100:
-                    return 1, x+100, 1, 0
+                    return (1, x+100), (1, 0)
                 if 101 <= x <= 150:
-                    return x-100, 200, 0, -1
+                    return (x-100, 200), (0, -1)
             if y == 101:
-                return 51, x+50, 1, 0
+                return (51, x+50), (1, 0)
         assert(False)
 
-    dx = 1
-    dy = 0
+    return wrap_fn
 
-    for step in path:
-        if isinstance(step, int):
-            for _ in range(step):
-                nx, ny = x+dx, y+dy
-                cell = grid.get((nx, ny))
-                if cell is None:
-                    # WRAP
-                    nx, ny, ndx, ndy = wrap(x, y, dx, dy)
-
-                    # Debugging!
-                    if (nx, ny) not in grid:
-                        print(x, y, dx, dy)
-                        print(nx, ny)
-
-                    # Got the wrong answer initially meaning I had some wrapping
-                    # wrong. Thankfully checking the inverse of the wrap (it
-                    # should be invertible) found the error!
-                    try:
-                        revx, revy, revdx, revdy = wrap(nx, ny, -ndx, -ndy)
-                        if (revx, revy, -revdx, -revdy) != (x, y, dx, dy):
-                            print(x, y, dx, dy)
-                            print(nx, ny, -ndx, -ndy)
-                            print(revx, revy, -revdx, -revdy)
-                            assert(False)
-                    except:
-                        assert(False)
-
-                    cell = grid[nx,ny]
-                    if cell == '#':
-                        break # Hit a wall, don't mess with direction
-                    x, y = nx, ny
-                    dx, dy = ndx, ndy
-                elif cell == '#':
-                    break # Hit a wall!
-                x, y = nx, ny
-        elif step == 'R':
-            dy, dx = dx, -dy
-        elif step == 'L':
-            dx, dy = dy, -dx
-        else:
-            assert(False)
-
-    answer = 1000 * y + 4 * x + facing_val[dx,dy]
+def part2(s):
+    answer = solve(s, cubenet_wrap_factory)
 
     lib.aoc.give_answer(2022, 22, 2, answer)
 
