f5ef11d1ecbf973c730571c94d770cfa00d99920
Patrick Hogg <phogg@novamoon.net>
2023-12-01 00:16:47 -0500

    2023 Day 1 Part 2
    Okay, so now we have word numbers too! Simple, replace the words with numbers and run part 1 again, right? Well, if the same letter is part of two numbers we take the *first* word for the beginning number and the *second* word for the ending number. That's...non-obvious. Plus I botched other things in trying to whip this up.
    
    I rose to rank 1270, but that's still really not good. I hope that AoC 2023 isn't *all* like this!

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -21,8 +21,51 @@ def part1(s):
 
     lib.aoc.give_answer(2023, 1, 1, answer)
 
+MAP = {'one': 1,
+       'two': 2,
+       'three': 3,
+       'four': 4,
+       'five': 5,
+       'six': 6,
+       'seven': 7,
+       'eight': 8,
+       'nine': 9,
+       'zero': 0}
+for d in '0123456789':
+    MAP[d] = int(d)
+
 def part2(s):
-    pass
+    answer = 0
+
+    for line in s.splitlines():
+        best = None
+        best_idx = None
+        for a, d in MAP.items():
+            if a in line:
+                idx = line.index(a)
+                if best_idx is None or best_idx > idx:
+                    best = d
+                    best_idx = idx
+
+        val = best * 10
+
+        line = line[::-1]
+
+        best = None
+        best_idx = None
+        for a, d in MAP.items():
+            a = a[::-1]
+            if a in line:
+                idx = line.index(a)
+                if best_idx is None or best_idx > idx:
+                    best = d
+                    best_idx = idx
+
+        val += best
+
+        answer += val
+
+    lib.aoc.give_answer(2023, 1, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 1)
 part1(INPUT)
