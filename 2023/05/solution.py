5311ed667714398cf02d9b2bc2a4e88f53f7b0dc
Patrick Hogg <phogg@novamoon.net>
2023-12-05 01:45:20 -0500

    2023 Rewrite Day 5
    Parts 1 and 2 can actually use the same fundamental logic (part 1 can use ranges of length 1), the only difference being how the seed line is interpreted. Furthermore, the input gives the mappings in the correct order so there is no reason to determine the application sequence either. All that's really left is the range mapping logic from part 2, but that can be (and has been) significantly cleaned up.

--- 2023/05/solution.py
+++ 2023/05/solution.py
@@ -1,133 +1,58 @@
 import lib.aoc
 
-def parse_input(s):
+def solve(s, seed_interpreter):
     groups = s.split('\n\n')
 
-    seeds = groups[0]
-    rest = groups[1:]
+    seed_ranges = seed_interpreter(list(map(int, groups[0].split()[1:])))
 
-    seeds = list(map(int, seeds.split()[1:]))
-
-    maps = {}
-
-    for mapping in rest:
-        lines = mapping.splitlines()
-        key = lines[0]
-        vals = []
-        for l in lines[1:]:
-            vals.append(tuple(map(int, l.split())))
-
-        key = key.split()[0]
-        a, b = key.split('-to-')
-        maps[a,b] = vals
-
-    return seeds, maps
-
-def find_sequence(maps, start_type, end_type):
-    seq = [start_type]
-
-    while start_type != end_type:
-        next_type = None
-        for a, b in maps:
-            if a == start_type:
-                assert(next_type is None)
-                next_type = b
-        assert(next_type is not None)
-        seq.append(next_type)
-        start_type = next_type
-
-    return seq
-
-def do_mapping(val, maps, seq):
-    seq = seq[:]
-    while len(seq) > 1:
-        a = seq.pop(0)
-        b = seq[0]
-        mapping = maps[a,b]
-
-        new_val = None
-
-        for dst, src, size in mapping:
-            if src <= val < src+size:
-                assert(new_val is None)
-                off = val - src
-                new_val = dst + off
-
-        if new_val is None:
-            new_val = val
-
-        val = new_val
-
-    return val
-
-def part1(s):
-    seeds, maps = parse_input(s)
-
-    seq = find_sequence(maps, 'seed', 'location')
-
-    answer = min(do_mapping(val, maps, seq)
-                 for val in seeds)
-
-    lib.aoc.give_answer(2023, 5, 1, answer)
-
-def do_mapping_2_help(vals, mapping):
-    new_vals = []
-
-    while len(vals) > 0:
-        handled = False
-        best_off = len(vals)
-
-        for dst, src, size in mapping:
-            if src <= vals[0] < src+size:
-                assert(not handled)
-                off = vals[0] - src
-                new_start = dst + off
-                new_len = min(size - off, len(vals))
-                new_vals.append(range(new_start, new_start+new_len))
-                vals = vals[new_len:]
-                handled = True
-                break
-            elif src < vals[0]:
-                off = vals[0] - src
-                best_off = min(best_off, off)
-
-        assert(best_off > 0)
-
-        if not handled:
-            new_vals.append(vals[:best_off])
-            vals = vals[best_off:]
-
-    return new_vals
-
-def do_mapping_2(val_ranges, maps, seq):
-    seq = seq[:]
-    while len(seq) > 1:
-        a = seq.pop(0)
-        b = seq[0]
-        mapping = maps[a,b]
+    for g in groups[1:]:
+        step_mapping = [tuple(map(int, l.split()))
+                        for l in g.splitlines()[1:]]
 
         new_ranges = []
 
-        for r in val_ranges:
-            new_ranges += do_mapping_2_help(r, mapping)
+        for start, r_len in seed_ranges:
+            while r_len != 0:
+                found_match = False
+                best_dist = r_len
 
-        val_ranges = new_ranges
+                for dst, src, length in step_mapping:
+                    if src <= start < src+length:
+                        # Found a match
+                        off = start - src
+                        rem_length = min(length - off, r_len)
+                        new_ranges.append((dst+off, rem_length))
+                        start += rem_length
+                        r_len -= rem_length
+                        found_match = True
+                        break
+                    else:
+                        if start < src:
+                            best_dist = min(src - start, best_dist)
 
-        assert(len(val_ranges) < 100)
+                if not found_match:
+                    handling_len = min(best_dist, r_len)
+                    new_ranges.append((start, handling_len))
+                    start += handling_len
+                    r_len -= handling_len
 
-    return val_ranges
+        seed_ranges = new_ranges
+
+    return min(start for start, length in seed_ranges)
+
+def part1(s):
+    def seed_interpreter(nums):
+        return [(n, 1) for n in nums]
+
+    answer = solve(s, seed_interpreter)
+
+    lib.aoc.give_answer(2023, 5, 1, answer)
 
 def part2(s):
-    seeds, maps = parse_input(s)
+    def seed_interpreter(nums):
+        return list(zip(nums[::2], nums[1::2]))
 
-    seq = find_sequence(maps, 'seed', 'location')
-
-    final_sequences = []
-
-    for a, b in zip(seeds[::2], seeds[1::2]):
-        final_sequences += do_mapping_2([range(a, a+b)], maps, seq)
-
-    answer = min(r.start for r in final_sequences)
+    answer = solve(s, seed_interpreter)
 
     lib.aoc.give_answer(2023, 5, 2, answer)
 
