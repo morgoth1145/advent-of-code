131c8037050eab4a358a2f56c662a17b3b5675a2
Patrick Hogg <phogg@novamoon.net>
2023-12-06 00:02:54 -0500

    2023 Day 6 Part 1
    Wow, I think this may be the first problem that felt like it was an appropriate difficulty for the day! It looks daunting at first, but really it's just a walk over the times and a simple multiplication to see if we win the race. Do that for all 4 races with a simple math.prod and we have our answer!
    
    Rank 66, at least I'm not totally washed!

---
+++ 2023/06/solution.py
@@ -0,0 +1,35 @@
+import math
+import parse
+
+import lib.aoc
+
+def parse_all_ints(s):
+    return list(map(lambda r:r[0], parse.findall('{:d}', s)))
+
+def parse_input(s):
+    for line in s.splitlines():
+        yield parse_all_ints(line)
+
+def ways_to_beat(time, dist):
+    ways = 0
+
+    for t in range(1, time):
+        rem = time - t
+        if rem * t > dist:
+            ways += 1
+
+    return ways
+
+def part1(s):
+    times, distances = parse_input(s)
+
+    answer = math.prod(ways_to_beat(t, d) for t, d in zip(times, distances))
+
+    lib.aoc.give_answer(2023, 6, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2023, 6)
+part1(INPUT)
+part2(INPUT)
