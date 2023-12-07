e8fcba93b60b1bc9d910eb431ae3626a894badd4
Patrick Hogg <phogg@novamoon.net>
2023-12-07 01:40:51 -0500

    2023 Rewrite Day 7
    There are only two differences between parts 1 and 2
    1) The card ranking is different (just needs a different string)
    2) Jokers need to be substituted when computing the hand type
    
    This (along with the excessively long way that the part 1 solution was written) left a ton of room for cleanup. Instead of a hand class and comparison operator, generating tuples of the hand strength and bid will have the same effect. Plus, the classification code could be drastically simplified, I kept a lot of intermediate data around for debugging (and because I wasn't sure what all I would need as I was writing the part 1 solution). The new simplified code speaks for itself, there was a lot of waste!

--- 2023/07/solution.py
+++ 2023/07/solution.py
@@ -2,127 +2,54 @@ import collections
 
 import lib.aoc
 
-HIGH_CARD = 0
-ONE_PAIR = 1
-TWO_PAIR = 2
-THREE_OF_A_KIND = 3
-FULL_HOUSE = 4
-FOUR_OF_A_KIND = 5
-FIVE_OF_A_KIND = 6
+def classify(hand):
+    stats = sorted(collections.Counter(hand).values())
+    if stats == [5]:            return 6 # Five of a kind
+    if stats == [1, 4]:         return 5 # Four of a kind
+    if stats == [2, 3]:         return 4 # Full house
+    if stats == [1, 1, 3]:      return 3 # Three of a kind
+    if stats == [1, 2, 2]:      return 2 # Two pair
+    if stats == [1, 1, 1, 2]:   return 1 # One pair
+    return 0 # High card
 
-class Hand:
-    def __init__(self, s):
-        self.cards = s
-        # Higher is better
-        self.card_power = list(map('23456789TJQKA'.index, s))
-        self.counted = collections.Counter(sorted(self.card_power))
-        def stat_key(p):
-            power, count = p
-            return count, power
-        self.stats = sorted(map(stat_key, self.counted.most_common()),
-                            reverse=True)
+def solve(s, card_rank_order, hand_fixer=None):
+    hands = []
 
-    def classify(self):
-        if self.stats[0][0] == 5:
-            return FIVE_OF_A_KIND
-        if self.stats[0][0] == 4:
-            return FOUR_OF_A_KIND
-        if self.stats[0][0] == 3:
-            if self.stats[1][0] == 2:
-                return FULL_HOUSE
-            return THREE_OF_A_KIND
-        if self.stats[0][0] == 2:
-            if self.stats[1][0] == 2:
-                return TWO_PAIR
-            return ONE_PAIR
-        return HIGH_CARD
-
-    def __lt__(self, other):
-        assert(isinstance(other, Hand))
-        sc = self.classify()
-        oc = other.classify()
-        if sc < oc:
-            return True
-        if oc < sc:
-            return False
-        # This is weird
-        return self.card_power < other.card_power
-
-def parse_input(s):
     for line in s.splitlines():
-        a, b = line.split()
-        yield Hand(a), int(b)
+        hand, bid = line.split()
+        hand_to_classify = hand if hand_fixer is None else hand_fixer(hand)
+
+        t = classify(hand_to_classify)
+        ranks = tuple(map(card_rank_order.index, hand))
+        hands.append((t, ranks, int(bid)))
+
+    return sum(rank * bid
+               for rank, (_, _, bid)
+               in enumerate(sorted(hands), start=1))
 
 def part1(s):
-    data = sorted(parse_input(s))
-
-    answer = sum(rank * bid
-                 for rank, (hand, bid)
-                 in enumerate(data, start=1))
+    answer = solve(s, '23456789TJQKA')
 
     lib.aoc.give_answer(2023, 7, 1, answer)
 
-class Hand2:
-    def __init__(self, s):
-        self.cards = s
-        # Higher is better
-        ORDER = 'J23456789TQKA'
-        self.card_power = list(map(ORDER.index, s))
-        jokers = s.count('J')
-        if jokers == 5:
-            best = 'A'
-        else:
-            # Replace jokers with the most common, most powerful card
-            first = list(map(ORDER.index, s.replace('J', '')))
-            first = collections.Counter(sorted(first))
-            first = first.most_common()[0]
-            best = first[0]
-            best = ORDER[best]
-        count_power = s.replace('J', best)
-        self.counted = collections.Counter(sorted(count_power))
-        def stat_key(p):
-            power, count = p
-            return count, power
-        self.stats = sorted(map(stat_key, self.counted.most_common()),
-                            reverse=True)
+PART2_CARD_RANK_ORDER = 'J23456789TQKA'
 
-    def classify(self):
-        if self.stats[0][0] == 5:
-            return FIVE_OF_A_KIND
-        if self.stats[0][0] == 4:
-            return FOUR_OF_A_KIND
-        if self.stats[0][0] == 3:
-            if self.stats[1][0] == 2:
-                return FULL_HOUSE
-            return THREE_OF_A_KIND
-        if self.stats[0][0] == 2:
-            if self.stats[1][0] == 2:
-                return TWO_PAIR
-            return ONE_PAIR
-        return HIGH_CARD
+def wild_substituter(hand):
+    counts = collections.Counter(hand)
+    jokers = counts.pop('J', 0)
+    if jokers == 5:
+        return 'AAAAA'
 
-    def __lt__(self, other):
-        assert(isinstance(other, Hand2))
-        sc = self.classify()
-        oc = other.classify()
-        if sc < oc:
-            return True
-        if oc < sc:
-            return False
-        # This is weird
-        return self.card_power < other.card_power
-
-def parse_input2(s):
-    for line in s.splitlines():
-        a, b = line.split()
-        yield Hand2(a), int(b)
+    # Replace jokers with the strongest of the most common cards
+    # This will be the strongest hand possible
+    target_count = max(counts.values())
+    target = max((c for c, count in counts.items()
+                  if count == target_count),
+                 key=PART2_CARD_RANK_ORDER.index)
+    return hand.replace('J', target)
 
 def part2(s):
-    data = sorted(parse_input2(s))
-
-    answer = sum(rank * bid
-                 for rank, (hand, bid)
-                 in enumerate(data, start=1))
+    answer = solve(s, PART2_CARD_RANK_ORDER, wild_substituter)
 
     lib.aoc.give_answer(2023, 7, 2, answer)
 
