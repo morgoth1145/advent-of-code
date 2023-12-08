51d2911fa04cabe76955d50e92133b4cc2a8b5b2
Patrick Hogg <phogg@novamoon.net>
2023-12-08 00:09:20 -0500

    2023 Day 8 Part 2
    Oo, and now we start at a set of nodes and travel to a set of valid target nodes simultaneously. Obviously I need a more general step counter, so part 1 got a quick refactoring. Then since the various ghosts reach the end nodes at different times (and loop!) we'll need to synchronize them all.
    
    Initially I just wanted some info, thinking synchronizing them may be complicated. (Just because it takes N steps to reach a target node initially doesn't mean it will take N steps to reach it a second time, the target node may link to other intermediate nodes before resuming the cycle or it may resume the cycle partway through.) But to my surprise, the least common multiple of the minimum steps for each ghost *was* the answer! That must be a nice property of the inputs because that's not guaranteed by the problem text as far as I know, but I'll take it!
    
    I'll probably whip up a more general solution though, just so it feels better.
    
    Rank 71, there we go!

--- 2023/08/solution.py
+++ 2023/08/solution.py
@@ -1,3 +1,5 @@
+import math
+
 import lib.aoc
 
 def parse_input(s):
@@ -15,24 +17,39 @@ def parse_input(s):
 
     return seq, out
 
+def count_steps(seq, mapping, pos, end_fn):
+    steps = 0
+
+    while True:
+        for side in seq:
+            steps += 1
+            side = 1 if side == 'R' else 0
+            pos = mapping[pos][side]
+            if end_fn(pos):
+                return steps
+
 def part1(s):
     seq, out = parse_input(s)
 
-    pos = 'AAA'
-    answer = 0
-
-    while pos != 'ZZZ':
-        for side in seq:
-            answer += 1
-            side = 1 if side == 'R' else 0
-            pos = out[pos][side]
-            if pos == 'ZZZ':
-                break
+    answer = count_steps(seq, out, 'AAA', lambda pos: pos == 'ZZZ')
 
     lib.aoc.give_answer(2023, 8, 1, answer)
 
 def part2(s):
-    pass
+    seq, mapping = parse_input(s)
+
+    positions = []
+    for pos in mapping:
+        if pos[-1] == 'A':
+            positions.append(pos)
+
+    min_steps = [count_steps(seq, mapping, pos, lambda pos: pos[-1] == 'Z')
+                 for pos in positions]
+
+    # Wait, this is right?! That's hilarous!
+    answer = math.lcm(*min_steps)
+
+    lib.aoc.give_answer(2023, 8, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 8)
 part1(INPUT)
