5f511982a55b3ba6f97cbc95720a7a0f5e7332d6
Patrick Hogg <phogg@novamoon.net>
2023-12-11 00:08:35 -0500

    2023 Day 11 Part 2
    Haha, and part 2 is just expanding by a larger factor than part 1! My part 1 solution was perfectly set up for that, but I did lose a minute due to expanding by the wrong factor. (I need to add 999999 rows/columns per expansion, not 1000000...)
    
    Still got rank 75 at least! (But with that minute lost I did lose 25 ranks...)

--- 2023/11/solution.py
+++ 2023/11/solution.py
@@ -34,7 +34,36 @@ def part1(s):
     lib.aoc.give_answer(2023, 11, 1, answer)
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s)
+
+    rows_without_galaxies = []
+    for y in range(grid.height):
+        if all(grid[x,y] == '.' for x in range(grid.width)):
+            rows_without_galaxies.append(y)
+
+    cols_without_galaxies = []
+    for x in range(grid.width):
+        if all(grid[x,y] == '.' for y in range(grid.height)):
+            cols_without_galaxies.append(x)
+
+    def adjust(x,y):
+        x += sum(c < x
+                 for c in cols_without_galaxies) * (1000000-1)
+        y += sum(r < y
+                 for r in rows_without_galaxies) * (1000000-1)
+        return x,y
+
+    galaxies = [adjust(x,y)
+                for (x,y), c in grid.items()
+                if c == '#']
+
+    answer = 0
+
+    for idx, (x, y) in enumerate(galaxies):
+        for x2, y2 in galaxies[idx+1:]:
+            answer += abs(x-x2) + abs(y-y2)
+
+    lib.aoc.give_answer(2023, 11, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 11)
 part1(INPUT)
