6c4ef0a7f27f14993bc937ab8a7fabe736f73fe6
Patrick Hogg <phogg@novamoon.net>
2023-12-12 00:36:53 -0500

    2023 Refactor Day 12
    Parts 1 and 2 can use the same counter, and even the same solve method if it parameterizes the number of copies to make. Furthermore, lifting the match counter to be global may speed things up further by retaining the cache between pattern matches. All this nearly halves the amount of code!

--- 2023/12/solution.py
+++ 2023/12/solution.py
@@ -2,79 +2,47 @@ import functools
 
 import lib.aoc
 
-def parse_input(s):
-    for line in s.splitlines():
-        a, b = line.split()
-        yield a, list(map(int, b.split(',')))
-
 @functools.cache
-def gen_options(size, splits):
-    def gen(rem_len, rem_splits):
-        if len(rem_splits) == 0:
-            yield '.' * rem_len
-            return
+def count_matches(pattern, size, splits):
+    if len(splits) == 0:
+        if all(c in '.?' for c in pattern):
+            return 1
+        return 0
 
-        a = rem_splits[0]
-        rest = rem_splits[1:]
-        after = sum(rest) + len(rest)
+    a = splits[0]
+    rest = splits[1:]
+    after = sum(rest) + len(rest)
 
-        for before in range(rem_len-after-a+1):
-            cand = '.' * before + '#' * a + '.'
-            for opt in gen(rem_len-a-before-1, rest):
-                yield cand + opt
+    count = 0
 
-    return list(gen(size, splits))
+    for before in range(size-after-a+1):
+        cand = '.' * before + '#' * a + '.'
+        if all(c0 == c1 or c0=='?'
+               for c0,c1 in zip(pattern, cand)):
+            count += count_matches(pattern[len(cand):],
+                                   size-a-before-1,
+                                   rest)
 
-def find_matches(pattern, splits):
-    options = gen_options(len(pattern), tuple(splits))
+    return count
 
-    for o in options:
-        if all((c0==c1 or c0=='?')
-               for c0,c1 in zip(pattern, o)):
-            yield o
-
-def part1(s):
-    data = list(parse_input(s))
-
+def solve(s, copies=1):
     answer = 0
 
-    for a, b in data:
-        answer += len(list(find_matches(a, b)))
+    for line in s.splitlines():
+        pattern, splits = line.split()
+        pattern = '?'.join((pattern,) * copies)
+        splits = tuple(map(int, splits.split(','))) * copies
+        answer += count_matches(pattern, len(pattern), tuple(splits))
+
+    return answer
+
+def part1(s):
+    answer = solve(s)
 
     lib.aoc.give_answer(2023, 12, 1, answer)
 
-def count_matches2(pattern, splits):
-    @functools.cache
-    def gen(rem_pattern, rem_len, rem_splits):
-        if len(rem_splits) == 0:
-            if all(c in '.?' for c in rem_pattern):
-                return 1
-            return 0
-
-        a = rem_splits[0]
-        rest = rem_splits[1:]
-        after = sum(rest) + len(rest)
-
-        count = 0
-
-        for before in range(rem_len-after-a+1):
-            cand = '.' * before + '#' * a + '.'
-            if all(c0 == c1 or c0=='?'
-                   for c0,c1 in zip(rem_pattern, cand)):
-                rest_pattern = rem_pattern[len(cand):]
-                count += gen(rest_pattern, rem_len-a-before-1, rest)
-
-        return count
-
-    return gen(pattern, len(pattern), tuple(splits))
-
 def part2(s):
-    data = list(parse_input(s))
-
-    answer = 0
-
-    for a, b in data:
-        answer += count_matches2('?'.join((a,a,a,a,a)), b*5)
+    answer = solve(s, copies=5)
 
     lib.aoc.give_answer(2023, 12, 2, answer)
 
