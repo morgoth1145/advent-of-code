563de3349032bb5a015f971f3c8ddbd114ee0698
Patrick Hogg <phogg@novamoon.net>
2023-12-12 01:09:25 -0500

    2023 Optimize Day 12
    There's no need to check the entire candidate pattern every iteration of the match counter loop. Instead, the next before character can be validated each iteration of the loop and *just* the damaged (and immediately following operational) sprint characters can be validated. This, along with avoiding the final recursive call when split is empty, saves about 2/3rds of the runtime.

--- 2023/12/solution.py
+++ 2023/12/solution.py
@@ -3,25 +3,23 @@ import functools
 import lib.aoc
 
 @functools.cache
-def count_matches(pattern, size, splits):
-    if len(splits) == 0:
-        if all(c in '.?' for c in pattern):
-            return 1
-        return 0
-
+def count_matches(pattern, splits):
     a = splits[0]
     rest = splits[1:]
     after = sum(rest) + len(rest)
 
     count = 0
 
-    for before in range(size-after-a+1):
-        cand = '.' * before + '#' * a + '.'
-        if all(c0 == c1 or c0=='?'
-               for c0,c1 in zip(pattern, cand)):
-            count += count_matches(pattern[len(cand):],
-                                   size-a-before-1,
-                                   rest)
+    for before in range(len(pattern)-after-a+1):
+        if all(c in '#?' for c in pattern[before:before+a]):
+            if len(rest) == 0:
+                if all(c in '.?' for c in pattern[before+a:]):
+                    count += 1
+            elif pattern[before+a] in '.?':
+                count += count_matches(pattern[before+a+1:], rest)
+
+        if pattern[before] not in '.?':
+            break
 
     return count
 
@@ -32,7 +30,7 @@ def solve(s, copies=1):
         pattern, splits = line.split()
         pattern = '?'.join((pattern,) * copies)
         splits = tuple(map(int, splits.split(','))) * copies
-        answer += count_matches(pattern, len(pattern), tuple(splits))
+        answer += count_matches(pattern, tuple(splits))
 
     return answer
 
