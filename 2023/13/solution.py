1c122812087534bb4f36bd2326d61c16e1426eff
Patrick Hogg <phogg@novamoon.net>
2023-12-13 00:57:44 -0500

    2023 Rewrite Day 13
    There are a few areas to improve the original solution:
    1) Instead of constructing the left/right or top/bottom lists from scratch each time, individual rows/columns can be moved from one list to the other
    2) In addition, it is possible to directly find the symmetry that will be fixed by a single smudge by counting how many differences each potential symmetry has. (This is made more efficient with sum(starmap(operator.ne)) which moves the iteration to the C layer.)
    
    In addition, some simple cleanups (such as returning the score instead of a list of candidate reflections and eliminating assertion code) make the code much nicer!

--- 2023/13/solution.py
+++ 2023/13/solution.py
@@ -1,88 +1,42 @@
+from itertools import starmap
+import operator
+
 import lib.aoc
 import lib.grid
 
-def parse_input(s):
-    for group in s.split('\n\n'):
-        yield lib.grid.FixedGrid.parse(group)
+def score_reflection(grid, smudges=0):
+    right = list(map(''.join, map(grid.col, grid.x_range)))
+    left = []
+    while len(right) > 1:
+        left.insert(0, right.pop(0))
+        num_diffs = sum(starmap(operator.ne,
+                                zip(''.join(left),
+                                    ''.join(right))))
+        if num_diffs == smudges:
+            return len(left)
 
-def find_reflection(grid):
-    verts = []
-    for i in range(1, grid.width):
-        left = []
-        right = []
-        for j in range(i-1, -1, -1):
-            left.append(grid.col(j))
-        for j in range(i, grid.width):
-            right.append(grid.col(j))
-        n = min(len(left), len(right))
-        left = left[:n]
-        right = right[:n]
-        if left == right:
-            verts.append(i)
+    bottom = list(map(''.join, map(grid.row, grid.y_range)))
+    top = []
+    while len(bottom) > 1:
+        top.insert(0, bottom.pop(0))
+        num_diffs = sum(starmap(operator.ne,
+                                zip(''.join(top),
+                                    ''.join(bottom))))
+        if num_diffs == smudges:
+            return len(top) * 100
 
-    horizontals = []
-    for i in range(1, grid.height):
-        top = []
-        bottom = []
-        for j in range(i-1, -1, -1):
-            top.append(grid.row(j))
-        for j in range(i, grid.height):
-            bottom.append(grid.row(j))
-        n = min(len(top), len(bottom))
-        top = top[:n]
-        bottom = bottom[:n]
-        if top == bottom:
-            horizontals.append(i)
-
-    return verts, horizontals
+def solve(s, smudges=0):
+    return sum(score_reflection(g, smudges)
+               for g in map(lib.grid.FixedGrid.parse,
+                            s.split('\n\n')))
 
 def part1(s):
-    data = list(parse_input(s))
-
-    answer = 0
-
-    for g in data:
-        vert, horiz = find_reflection(g)
-        if len(vert) == 1:
-            assert(len(horiz) == 0)
-            answer += vert[0]
-        else:
-            assert(len(vert) == 0)
-            assert(len(horiz) == 1)
-            answer += horiz[0] * 100
+    answer = solve(s)
 
     lib.aoc.give_answer(2023, 13, 1, answer)
 
-def fix_smudge(g):
-    orig_vert, orig_horiz = find_reflection(g)
-    for coord, c in g.items():
-        if c == '.':
-            g[coord] = '#'
-        else:
-            g[coord] = '.'
-        vert, horiz = find_reflection(g)
-        vert = list(set(vert) - set(orig_vert))
-        horiz = list(set(horiz) - set(orig_horiz))
-        g[coord] = c
-
-        if len(vert) + len(horiz) == 1:
-            return vert, horiz
-
 def part2(s):
-    data = list(parse_input(s))
-
-    answer = 0
-
-    for idx, g in enumerate(data):
-        print(f'{idx}/{len(data)}')
-        vert, horiz = fix_smudge(g)
-        if len(vert) == 1:
-            assert(len(horiz) == 0)
-            answer += vert[0]
-        else:
-            assert(len(vert) == 0)
-            assert(len(horiz) == 1)
-            answer += horiz[0] * 100
+    answer = solve(s, smudges=1)
 
     lib.aoc.give_answer(2023, 13, 2, answer)
 
