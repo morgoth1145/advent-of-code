4fc6157f763bb740b52d6534732d86674b437e61
Patrick Hogg <phogg@novamoon.net>
2023-12-15 00:11:15 -0500

    2023 Day 15 Part 2
    Okay, so we're writing our own hashmap with buckets for collision detection. Fair enough, but again I was so slow on the uptake. Not much more to say than that really.
    
    Rank 110. Definitely paid for being slow, oh well.

--- 2023/15/solution.py
+++ 2023/15/solution.py
@@ -17,7 +17,48 @@ def part1(s):
     lib.aoc.give_answer(2023, 15, 1, answer)
 
 def part2(s):
-    pass
+    bits = s.split(',')
+
+    hashmap = [[] for _ in range(256)]
+
+    for part in bits:
+        if '=' in part:
+            label = part[:part.index('=')]
+            op = '='
+            rest = part[part.index('=')+1:]
+            focal = int(rest)
+
+            box_idx = the_hash(label)
+            box = hashmap[box_idx]
+            replaced = False
+            for i in range(len(box)):
+                if box[i][0] == label:
+                    box[i] = (label, focal)
+                    replaced = True
+            if not replaced:
+                box.append((label, focal))
+        elif '-' in part:
+            label = part[:part.index('-')]
+            op = '-'
+            rest = part[part.index('-')+1:]
+            assert(len(rest) == 0)
+
+            box_idx = the_hash(label)
+            box = hashmap[box_idx]
+            for i in range(len(box)):
+                if box[i][0] == label:
+                    box.pop(i)
+                    break
+        else:
+            assert(False)
+
+    answer = 0
+
+    for box_idx, box in enumerate(hashmap, start=1):
+        for idx, (_, focal) in enumerate(box, start=1):
+            answer += box_idx * idx * focal
+
+    lib.aoc.give_answer(2023, 15, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 15)
 part1(INPUT)
