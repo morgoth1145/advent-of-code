6a690a9cea607985500103c379892e39dd9cc9ec
Patrick Hogg <phogg@novamoon.net>
2023-12-16 11:31:18 -0500

    2023 Optimize Day 16
    In many ways this problem is similar to a graph traversal. In fact, the grid can be simplified to just connections between points of interest, most notably splitters. The only way for light to enter a loop is through a splitter since light enters the contraption from the outside. As such, whenever tracing the light we can simply carry on until we hit the side of a splitter and actually split, or until we exit the contraption.
    
    Furthermore, many of the paths the light takes are the same for every entry point in part 2. As such, caching the paths speeds part 2 up immensely.

--- 2023/16/solution.py
+++ 2023/16/solution.py
@@ -1,69 +1,93 @@
 import lib.aoc
 import lib.grid
 
-def energize(grid, x, y, dx, dy):
+import functools
+
+def parse_contraption(s):
+    grid = lib.grid.FixedGrid.parse(s)
+
+    @functools.cache
+    def shoot_to_split(x, y, dx, dy):
+        traversed = []
+
+        while True:
+            traversed.append((x,y))
+
+            c = grid[x,y]
+
+            if c == '.':
+                x, y = x+dx, y+dy
+            elif c == '/':
+                dx, dy = -dy, -dx
+                x, y = x+dx, y+dy
+            elif c == '\\':
+                dx, dy = dy, dx
+                x, y = x+dx, y+dy
+            elif c == '|':
+                if dx == 0:
+                    y += dy
+                else:
+                    out = []
+                    if y > 0:
+                        out.append((x, y-1, 0, -1))
+                    if y < grid.height-1:
+                        out.append((x, y+1, 0, 1))
+                    return traversed, out
+            elif c == '-':
+                if dy == 0:
+                    x += dx
+                else:
+                    out = []
+                    if x > 0:
+                        out.append((x-1, y, -1, 0))
+                    if x < grid.width-1:
+                        out.append((x+1, y, 1, 0))
+                    return traversed, out
+            else:
+                assert(False)
+
+            if 0 > x or x >= grid.width or 0 > y or y >= grid.height:
+                return traversed, []
+
+    return grid.width, grid.height, shoot_to_split
+
+def energize(shoot_to_split, x, y, dx, dy):
     good = set()
     handled = set()
 
-    todo = [(x, y, dx, dy)]
+    todo = [(x,y,dx,dy)]
 
     while todo:
         key = todo.pop()
         if key in handled:
             continue
         handled.add(key)
-        x, y, dx, dy = key
 
-        if x < 0 or x >= grid.width:
-            continue
-        if y < 0 or y >= grid.height:
-            continue
-
-        good.add((x,y))
-
-        c = grid[x,y]
-        if c == '.':
-            todo.append((x+dx, y+dy, dx, dy))
-        elif c == '/':
-            todo.append((x-dy, y-dx, -dy, -dx))
-        elif c == '\\':
-            todo.append((x+dy, y+dx, dy, dx))
-        elif c == '|':
-            if dx == 0:
-                todo.append((x, y+dy, 0, dy))
-            else:
-                todo.append((x, y-1, 0, -1))
-                todo.append((x, y+1, 0, 1))
-        elif c == '-':
-            if dx == 0:
-                todo.append((x-1, y, -1, 0))
-                todo.append((x+1, y, 1, 0))
-            else:
-                todo.append((x+dx, y, dx, 0))
-        else:
-            assert(False)
+        traversed, out = shoot_to_split(*key)
+        good.update(traversed)
+        todo.extend(out)
 
     return len(good)
 
 def part1(s):
-    grid = lib.grid.FixedGrid.parse(s)
+    _, _, shoot_to_split = parse_contraption(s)
 
-    answer = energize(grid, 0, 0, 1, 0)
+    answer = energize(shoot_to_split, 0, 0, 1, 0)
 
     lib.aoc.give_answer(2023, 16, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s)
+    width, height, shoot_to_split = parse_contraption(s)
 
     options = []
-    for x in grid.x_range:
+    for x in range(width):
         options.append((x, 0, 0, 1))
-        options.append((x, grid.height-1, 0, -1))
-    for y in grid.y_range:
+        options.append((x, height-1, 0, -1))
+    for y in range(height):
         options.append((0, y, 1, 0))
-        options.append((grid.width-1, y, -1, 0))
+        options.append((width-1, y, -1, 0))
 
-    answer = max(energize(grid, *start)
+    answer = max(energize(shoot_to_split, *start)
                  for start in options)
 
     lib.aoc.give_answer(2023, 16, 2, answer)
