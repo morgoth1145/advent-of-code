7e0c04d123b8888f4bc40e39fdff64e5593f6c7e
Patrick Hogg <phogg@novamoon.net>
2023-12-17 00:10:31 -0500

    2023 Day 17 Part 1
    Hey, finally a grid input with numbers. Also, this is a graph search problem finally!
    
    The requirement to go at most 3 blocks in one direction before turning (and only turning in 90 degree increments) is interesting. Thankfully my graph search library is able to handle pretty generic states so I just had to make a good state with all the info recorded for me to handle this requirement.
    
    That said, I did goof and accidentally miss that only 90 degree turns were allowed, Looking at my submission times I lost ~4 minutes on that.
    
    Rank 37?! Wow, I thought I threw hard with my mistake! In fact, if I had not had the mistake I probably would have been top 10!

---
+++ 2023/17/solution.py
@@ -0,0 +1,53 @@
+import lib.aoc
+import lib.graph
+import lib.grid
+
+def part1(s):
+    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+
+    def neighbor_fn(state):
+        x, y, dx, dy, num_in_dir = state
+
+        for n in [(x-1, y),
+                  (x+1, y),
+                  (x, y-1),
+                  (x, y+1)]:
+            ndx = n[0] - x
+            ndy = n[1] - y
+            if ndx == dx and ndy == dy:
+                if num_in_dir == 3:
+                    continue
+                new_num_in_dir = num_in_dir+1
+            elif ndx == -dx or ndy == -dy:
+                continue
+            else:
+                new_num_in_dir = 1
+
+            if 0 <= n[0] < grid.width:
+                if 0 <= n[1] < grid.height:
+                    new_state = (n[0], n[1],
+                                 ndx, ndy,
+                                 new_num_in_dir)
+                    yield new_state, grid[n]
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+
+    def end_fn(state):
+        x, y, _, _, _ = state
+        win = (x == grid.width-1 and y == grid.height-1)
+        return win
+
+    answer = lib.graph.dijkstra_length_fuzzy_end(graph,
+                                                 (0, 0,
+                                                  -1, 0,
+                                                  0),
+                                                 end_fn=end_fn)
+
+    lib.aoc.give_answer(2023, 17, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2023, 17)
+part1(INPUT)
+part2(INPUT)
