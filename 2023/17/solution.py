c0232aae3d3eb5f1d6520f57ea38e02de839d6d8
Patrick Hogg <phogg@novamoon.net>
2023-12-17 01:34:56 -0500

    2023 Refactor and optimize Day 17
    Part 1 and 2 fundamentally are pretty similar, the only difference is how many minimum steps are needed before turning and the maximum steps before turning. As such a single solve function can be extracted.
    
    In addition, the search can be done more efficiently with a heuristic. This heuristic can be based on an unconstrained cost of moving from the tile to the end as the constrained path cannot do any better (and quite likely may do worse due to needing to turn).
    
    Finally, fix a bug where end_fn did not verify that the path ended in 4+ steps in the same direction before reaching the final node. Surprisingly this did not cause a problem when solving, despite being noted in the problem description.

--- 2023/17/solution.py
+++ 2023/17/solution.py
@@ -2,95 +2,77 @@ import lib.aoc
 import lib.graph
 import lib.grid
 
-def part1(s):
+def generate_heuristic_to_end_node(grid):
+    def heuristic_neighbor(state):
+        x, y = state
+        for n in [(x-1,y), (x+1,y),
+                  (x,y-1), (x,y+1)]:
+            if n in grid:
+                yield n, grid[x,y]
+
+    heuristic_graph = lib.graph.make_lazy_graph(heuristic_neighbor)
+
+    heuristic = dict(lib.graph.all_reachable(heuristic_graph,
+                                             (grid.width-1,
+                                              grid.height-1)))
+    heuristic[grid.width-1, grid.height-1] = 0
+    return heuristic.__getitem__
+
+def solve(s, min_steps, max_steps):
     grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+
+    # Avoid repeatedly calling the property getter for performance
+    g_width, g_height = grid.width, grid.height
 
     def neighbor_fn(state):
         x, y, dx, dy, num_in_dir = state
 
-        for n in [(x-1, y),
-                  (x+1, y),
-                  (x, y-1),
-                  (x, y+1)]:
-            ndx = n[0] - x
-            ndy = n[1] - y
-            if ndx == dx and ndy == dy:
-                if num_in_dir == 3:
-                    continue
-                new_num_in_dir = num_in_dir+1
-            elif ndx == -dx and ndx != 0:
-                continue
-            elif ndy == -dy and ndy != 0:
-                continue
-            else:
-                new_num_in_dir = 1
+        if num_in_dir < max_steps:
+            nx, ny = x+dx, y+dy
+            if 0 <= nx < g_width and 0 <= ny < g_height:
+                new_state = (nx, ny, dx, dy, num_in_dir+1)
+                yield new_state, grid[nx,ny]
 
-            if 0 <= n[0] < grid.width:
-                if 0 <= n[1] < grid.height:
-                    new_state = (n[0], n[1],
-                                 ndx, ndy,
-                                 new_num_in_dir)
-                    yield new_state, grid[n]
+        if num_in_dir >= min_steps:
+            # Left turn
+            nx, ny = x+dy, y-dx
+            if 0 <= nx < g_width and 0 <= ny < g_height:
+                new_state = (nx, ny, dy, -dx, 1)
+                yield new_state, grid[nx,ny]
+
+            # Right turn
+            nx, ny = x-dy, y+dx
+            if 0 <= nx < g_width and 0 <= ny < g_height:
+                new_state = (nx, ny, -dy, dx, 1)
+                yield new_state, grid[nx,ny]
 
     graph = lib.graph.make_lazy_graph(neighbor_fn)
 
     def end_fn(state):
-        x, y, _, _, _ = state
-        win = (x == grid.width-1 and y == grid.height-1)
-        return win
+        x, y, _, _, num_in_dir = state
+        return (x == g_width-1 and
+                y == g_height-1 and
+                num_in_dir >= min_steps)
 
-    answer = lib.graph.dijkstra_length_fuzzy_end(graph,
-                                                 (0, 0,
-                                                  0, 0,
-                                                  0),
-                                                 end_fn=end_fn)
+    heuristic_to_end = generate_heuristic_to_end_node(grid)
+
+    def heuristic(state):
+        x, y, _, _, _ = state
+        return heuristic_to_end((x,y))
+
+    return lib.graph.dijkstra_length_fuzzy_end(graph,
+                                               [(0, 0, 1, 0, 0),
+                                                (0, 0, 0, 1, 0)],
+                                               end_fn=end_fn,
+                                               heuristic=heuristic)
+
+def part1(s):
+    answer = solve(s, 0, 3)
 
     lib.aoc.give_answer(2023, 17, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
-
-    def neighbor_fn(state):
-        x, y, dx, dy, num_in_dir = state
-
-        for n in [(x-1, y),
-                  (x+1, y),
-                  (x, y-1),
-                  (x, y+1)]:
-            ndx = n[0] - x
-            ndy = n[1] - y
-            if ndx == dx and ndy == dy:
-                if num_in_dir == 10:
-                    continue
-                new_num_in_dir = num_in_dir+1
-            elif ndx == -dx and ndx != 0:
-                continue
-            elif ndy == -dy and ndy != 0:
-                continue
-            else:
-                if num_in_dir < 4:
-                    continue
-                new_num_in_dir = 1
-
-            if 0 <= n[0] < grid.width:
-                if 0 <= n[1] < grid.height:
-                    new_state = (n[0], n[1],
-                                 ndx, ndy,
-                                 new_num_in_dir)
-                    yield new_state, grid[n]
-
-    graph = lib.graph.make_lazy_graph(neighbor_fn)
-
-    def end_fn(state):
-        x, y, _, _, _ = state
-        win = (x == grid.width-1 and y == grid.height-1)
-        return win
-
-    answer = lib.graph.dijkstra_length_fuzzy_end(graph,
-                                                 (0, 0,
-                                                  0, 0,
-                                                  4),
-                                                 end_fn=end_fn)
+    answer = solve(s, 4, 10)
 
     lib.aoc.give_answer(2023, 17, 2, answer)
 
