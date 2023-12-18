e4b99f4074bb860157872e7e338b422e6839a7e4
Patrick Hogg <phogg@novamoon.net>
2023-12-18 02:37:11 -0500

    2023 Refactor Day 18
    Parts 1 and 2 can use the same solve logic, just with different dig sequence parsing code. I did quickly try switching to a more normal space filling approach and it ran slower!
    
    I see comments in the megathread about a Pick's Theorum and Shoelace Formula, I guess I have reading up to do...

--- 2023/18/solution.py
+++ 2023/18/solution.py
@@ -1,65 +1,11 @@
 import lib.aoc
 
-def parse_input(s):
+def parse_dig_sequence(s):
     for line in s.splitlines():
         a, b, c = line.split()
-        b = int(b)
-        c = int(c[2:-1], base=16)
-        yield a, b, c
+        yield a, int(b)
 
-def part1(s):
-    data = list(parse_input(s))
-
-    x, y = 0, 0
-
-    seen = set()
-    seen.add((x, y))
-
-    for d, num, _ in data:
-        if d == 'U':
-            dx, dy = 0, -1
-        elif d == 'D':
-            dx, dy = 0, 1
-        elif d == 'R':
-            dx, dy = 1, 0
-        else:
-            dx, dy = -1, 0
-
-        for _ in range(num):
-            x, y = x+dx, y+dy
-            seen.add((x, y))
-
-    min_x = min(x for x,y in seen)
-    max_x = max(x for x,y in seen)
-    min_y = min(y for x,y in seen)
-    max_y = max(y for x,y in seen)
-
-    x_range = range(min_x, max_x+1)
-    y_range = range(min_y, max_y+1)
-
-    fill = set(seen)
-
-    for y in y_range:
-        is_enclosed = False
-        for x in x_range:
-            if (x,y) in seen:
-                if (x, y-1) in seen and (x, y+1) in seen:
-                    is_enclosed = not is_enclosed
-                elif (x,y+1) in seen and (x+1,y) in seen:
-                    is_enclosed = not is_enclosed
-                elif (x,y+1) in seen and (x-1,y) in seen:
-                    is_enclosed = not is_enclosed
-            else:
-                if is_enclosed:
-                    fill.add((x,y))
-
-    answer = len(fill)
-
-    lib.aoc.give_answer(2023, 18, 1, answer)
-
-def part2(s):
-    data = list(parse_input(s))
-
+def solve(dig_sequence):
     x, y = 0, 0
 
     corners = [(x,y)]
@@ -67,20 +13,16 @@ def part2(s):
     x_points = set()
     y_points = set()
 
-    for _, _, num in data:
-        d = 'RDLU'[num&0xF]
-        num //= 16
+    for d, num in dig_sequence:
         if d == 'U':
-            dx, dy = 0, -1
+            y -= num
         elif d == 'D':
-            dx, dy = 0, 1
+            y += num
         elif d == 'R':
-            dx, dy = 1, 0
+            x += num
         else:
-            dx, dy = -1, 0
+            x -= num
 
-        x += dx*num
-        y += dy*num
         corners.append((x,y))
         x_points.add(x)
         y_points.add(y)
@@ -154,8 +96,20 @@ def part2(s):
                 if is_enclosed:
                     fill.add((x,y))
 
-    answer = sum(map(grid_sizes.__getitem__,
-                     fill))
+    return sum(map(grid_sizes.__getitem__, fill))
+
+def part1(s):
+    answer = solve(parse_dig_sequence(s))
+
+    lib.aoc.give_answer(2023, 18, 1, answer)
+
+def parse_dig_sequence_2(s):
+    for line in s.splitlines():
+        num = int(line.split()[2][2:-1], base=16)
+        yield 'RDLU'[num&0xF], num // 16
+
+def part2(s):
+    answer = solve(parse_dig_sequence_2(s))
 
     lib.aoc.give_answer(2023, 18, 2, answer)
 
