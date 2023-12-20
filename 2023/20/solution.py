f3f6c1160948edc476b3f7f7abf7c0d2da324466
Patrick Hogg <phogg@novamoon.net>
2023-12-20 00:32:36 -0500

    2023 Day 20 Part 2
    Hoo boy! I was expecting something tricky in part 2 given this graph setup, and I was not disappointed or surprised.
    
    First thing I tried was a brute force. I figured it wouldn't work, but it wasn't hard to cook up and I could leave it running in the background as I worked through a real solution.
    
    With that out of the way, an inspection of the input reveals that rx has a single conjunction module as an input, and that conjunction module has 4 conjunction modules as its inputs. Suspiciously, broadcaster sends to 4 modulse itself! After a bit of time (probably too much time) quickly sanity checking I guessed that there are 4 distinct sub-graphs in the network, each with their own period.
    
    With these subgraphs having their own period all we have to do is detect each subgraph's period and take the LCM of the periods to get the answer. I didn't fully vet that the network cleanly splits in that manner (that would take *way* too much time to code) but in the end the answer was accepted so it definitely seems to be the case! I'll have to verify this more properly when refactoring this solution.
    
    I got rank 18! Yes! Glad I spotted the pattern/trick relatively quickly on this one.

--- 2023/20/solution.py
+++ 2023/20/solution.py
@@ -1,4 +1,5 @@
 import collections
+import math
 
 import lib.aoc
 
@@ -95,7 +96,104 @@ def part1(s):
     lib.aoc.give_answer(2023, 20, 1, answer)
 
 def part2(s):
-    pass
+    data = parse_input(s)
+
+    num_low = 0
+    num_high = 0
+    memory = {}
+
+    input_map = collections.defaultdict(list)
+
+    for node, (_, dests) in data.items():
+        for d in dests:
+            input_map[d].append(node)
+
+    for node, (t, _) in data.items():
+        if t is None:
+            continue
+        if t == '%':
+            memory[node] = False
+        if t == '&':
+            memory[node] = {d:False
+                            for d in input_map[node]}
+
+    assert(len(input_map['rx']) == 1)
+
+    single = input_map['rx'][0]
+
+    assert(data[single][0] == '&')
+
+    sources = input_map[single]
+
+    assert(all(data[s][0] == '&'
+               for s in sources))
+
+    # Assume that there are n sub-trees and they each have their own cycle
+
+    low_counts = {}
+
+    cycle = 0
+
+    while len(low_counts) < len(sources):
+        cycle += 1
+        if cycle % 100000 == 0:
+            print(cycle)
+        todo = [(None, 'broadcaster', False)]
+
+        while todo:
+            new_todo = []
+
+            for src, node, is_high_pulse in todo:
+                if node in sources:
+                    if not is_high_pulse:
+                        if node not in low_counts:
+                            print(node, cycle)
+                            low_counts[node] = cycle
+                if node == 'rx' and not is_high_pulse:
+                    rx_count += 1
+
+                if is_high_pulse:
+                    num_high += 1
+                else:
+                    num_low += 1
+
+                info = data.get(node)
+                if info is None:
+                    continue
+
+                t, dests = info
+                if t == '%':
+                    if is_high_pulse:
+                        continue
+                    state = memory[node]
+                    memory[node] = not state
+                    for d in dests:
+                        new_todo.append((node, d, not state))
+                    continue
+                if t == '&':
+                    state = memory[node]
+                    state[src] = is_high_pulse
+
+                    if sum(state.values()) == len(state):
+                        # All are high, send a low pulse
+                        to_send = False
+                    else:
+                        to_send = True
+
+                    for d in dests:
+                        new_todo.append((node, d, to_send))
+                    continue
+                if t is None:
+                    for d in dests:
+                        new_todo.append((node, d, is_high_pulse))
+                    continue
+                assert(False)
+
+            todo = new_todo
+
+    answer = math.lcm(*low_counts.values())
+
+    lib.aoc.give_answer(2023, 20, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 20)
 part1(INPUT)
