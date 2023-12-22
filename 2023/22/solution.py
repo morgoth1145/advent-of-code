8cea3ed2a7ee17e342b0318b862cf279d4e61b69
Patrick Hogg <phogg@novamoon.net>
2023-12-22 00:29:11 -0500

    2023 Day 22 Part 2
    Okay, so part 2 is basically the same as part 1 except that we count how many bricks fall! A slight tweak to the dumb O(n^2) algorithm and it worked fine, the main thing to be concerned with is that settled_positions needs to be updated to count how many bricks fall so we need to store a copy of the original set to restore at the end. It's definitely slower than part 1, but we're still only talking about 1456 bricks so it's faster than writing something smarter!
    
    Okay good, I managed to jump to 93 and leaderboard (barely). But man, I'd have been around rank 10 without my part 1 bug...

--- 2023/22/solution.py
+++ 2023/22/solution.py
@@ -97,7 +97,72 @@ def part1(s):
     lib.aoc.give_answer(2023, 22, 1, answer)
 
 def part2(s):
-    pass
+    data = sorted(parse_input(s), key=lambda brick: brick[0][2])
+
+    bricks = []
+
+    for (x0, y0, z0), (x1, y1, z1), _ in data:
+        b = set()
+        for x in range(x0, x1+1):
+            for y in range(y0, y1+1):
+                # Only the bottom and top row are needed
+                for z in set([z0, z1]):
+                    b.add((x,y,z))
+
+        bricks.append(b)
+
+    settled_positions = set()
+    unsettled_positions = set()
+
+    test = 0
+
+    for b in bricks:
+        unsettled_positions |= b
+        test += len(b)
+
+    assert(test == len(unsettled_positions))
+
+    def fall(b):
+        assert(len(b & unsettled_positions) == 0)
+
+        while True:
+            new_b = set()
+            for x,y,z in b:
+                new_b.add((x,y,z-1))
+            assert(len(new_b & unsettled_positions) == 0)
+            if len(new_b & settled_positions) != 0:
+                return b # Settled
+            if min(z for x,y,z in new_b) <= 0:
+                return b # Settled
+            b = new_b
+
+    for idx, b in enumerate(bricks):
+        unsettled_positions -= b
+
+        b = fall(b)
+        settled_positions |= b
+        bricks[idx] = b
+
+    answer = 0
+
+    for idx, b in enumerate(bricks):
+        old_settled = set(settled_positions)
+
+        settled_positions -= b
+
+        for i2, b2 in enumerate(bricks[idx+1:], start=idx+1):
+            settled_positions -= b2
+            new_b2 = fall(b2)
+            if b2 != new_b2:
+                answer += 1
+            settled_positions |= new_b2
+
+        # Restore
+        settled_positions = old_settled
+
+        assert(len(settled_positions) == test)
+
+    lib.aoc.give_answer(2023, 22, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 22)
 part1(INPUT)
