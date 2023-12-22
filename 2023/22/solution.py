1886061a97cf0631b7a7a92c01804ff6bf67e663
Patrick Hogg <phogg@novamoon.net>
2023-12-22 00:26:01 -0500

    2023 Day 22 Part 1
    For a moment when reading the problem I thought we might need to do tetris, but thankfully no! We do need to look out for intersections just like tetris, but no row disintegrations.
    
    One important observation: If we sort the initial list of bricks based on their lowest z point then it is guaranteed that every brick must be supported by the ground or by a brick that came earlier in the list.
    
    Fundamentally this is pretty simple with sets for the points in a brick, as well as a set for all the currently settled positions. The 1456 bricks are only comprised of 4211 bricks so while a smarter solution will run faster, a dumb O(n^2) algorithm works fine for seeing if any bricks are safe to disintegrate, just test all subsequent bricks and see if they fall!
    
    I did have a dumb mistake though: When testing to see if bricks fell after disintigration I forgot to remove them from the settled positions set too. This meant that I got false positives for supported bricks took 12 minutes to debug...
    
    Only got rank 189, so that bug cost me a lot. In fact, my original solution submission time would have been around rank 25 had I not had that bug!

---
+++ 2023/22/solution.py
@@ -0,0 +1,104 @@
+import lib.aoc
+
+def parse_input(s):
+    for idx, line in enumerate(s.splitlines(), start=1):
+        a, b = line.split('~')
+        a = tuple(map(int, a.split(',')))
+        b = tuple(map(int, b.split(',')))
+
+        x0, y0, z0 = a
+        x1, y1, z1 = b
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+        if y0 > y1:
+            y0, y1 = y1, y0
+        if z0 > z1:
+            z0, z1 = z1, z0
+
+        assert(x0 <= x1)
+        assert(y0 <= y1)
+        assert(z0 <= z1)
+
+        yield (x0, y0, z0), (x1, y1, z1), idx
+
+def part1(s):
+    data = sorted(parse_input(s), key=lambda brick: brick[0][2])
+
+    bricks = []
+
+    for (x0, y0, z0), (x1, y1, z1), _ in data:
+        b = set()
+        for x in range(x0, x1+1):
+            for y in range(y0, y1+1):
+                # Only the bottom and top row are needed
+                for z in set([z0, z1]):
+                    b.add((x,y,z))
+
+        bricks.append(b)
+
+    settled_positions = set()
+    unsettled_positions = set()
+
+    test = 0
+
+    for b in bricks:
+        unsettled_positions |= b
+        test += len(b)
+
+    assert(test == len(unsettled_positions))
+
+    def fall(b):
+        assert(len(b & unsettled_positions) == 0)
+
+        while True:
+            new_b = set()
+            for x,y,z in b:
+                new_b.add((x,y,z-1))
+            assert(len(new_b & unsettled_positions) == 0)
+            if len(new_b & settled_positions) != 0:
+                return b # Settled
+            if min(z for x,y,z in new_b) <= 0:
+                return b # Settled
+            b = new_b
+
+    for idx, b in enumerate(bricks):
+        unsettled_positions -= b
+
+        b = fall(b)
+        settled_positions |= b
+        bricks[idx] = b
+
+    answer = 0
+
+    for idx, b in enumerate(bricks):
+        settled_positions -= b
+
+        assert(len(settled_positions) == test - len(b))
+
+        was_safe = True
+        tested = []
+        for i2, b2 in enumerate(bricks[idx+1:]):
+            tested.append(idx+1+i2)
+            settled_positions -= b2
+            if fall(b2) != b2:
+                was_safe = False
+                settled_positions |= b2
+                break
+            settled_positions |= b2
+
+        if was_safe:
+            answer += 1
+
+        settled_positions |= b
+
+        assert(len(settled_positions) == test)
+
+    lib.aoc.give_answer(2023, 22, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2023, 22)
+part1(INPUT)
+part2(INPUT)
