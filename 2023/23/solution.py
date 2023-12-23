8168257c30b15aa13a811ef758fde6ace41a8857
Patrick Hogg <phogg@novamoon.net>
2023-12-23 03:15:41 -0500

    2023 Rewrite Day 23
    Parts 1 and 2 are fundamentally the same so long as the input is preprocessed in part 2 to eliminate slopes. This can be exploited to extract a single solve function.
    
    Furthermore, finding the longest path via recursion is a good bit faster than the iterative method I had coded up. Part 2 still takes about 35 seconds though, I feel like there has to be some other trick I'm still missing to make this run faster but that's a problem for another time.

--- 2023/23/solution.py
+++ 2023/23/solution.py
@@ -2,7 +2,7 @@ import lib.aoc
 import lib.graph
 import lib.grid
 
-def part1(s):
+def solve(s):
     grid = lib.grid.FixedGrid.parse(s)
 
     end_y = grid.height-1
@@ -64,99 +64,31 @@ def part1(s):
 
     graph = lib.graph.make_lazy_graph(neighbor_fn)
 
-    answer = 0
+    def longest_path(path, dist):
+        best = -1
+        for n, n_dist in graph[path[-1]]:
+            if n in path:
+                continue
+            if n == end:
+                best = max(best, dist + n_dist)
+                continue
 
-    paths = [((start,),0)]
+            best = max(best, longest_path(path + (n,), dist + n_dist))
 
-    while paths:
-        new_paths = set()
+        return best
 
-        for p, d in paths:
-            for n, n_d in graph[p[-1]]:
-                if n in p:
-                    continue
-                if n == end:
-                    answer = max(answer, d+n_d)
-                    continue
-                new_paths.add((p + (n,), d+n_d))
+    return longest_path((start,), 0)
 
-        paths = new_paths
+def part1(s):
+    answer = solve(s)
 
     lib.aoc.give_answer(2023, 23, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s)
+    for c in '<>^v':
+        s = s.replace(c, '.')
 
-    end_y = grid.height-1
-    for x in grid.x_range:
-        if grid[x,0] == '.':
-            start = x,0
-        if grid[x,end_y] == '.':
-            end = x,end_y
-
-    def neighbor_fn(coord):
-        x,y = coord
-
-        handled = {coord}
-        todo = [(n, 1) for n in grid.neighbors(*coord)
-                if grid[n] != '#']
-
-        while todo:
-            c, d = todo.pop()
-            handled.add(c)
-
-            if c == end:
-                yield c, d
-                continue
-
-            neighbors = list(grid.neighbors(*c))
-
-            neighbors = [n for n in neighbors
-                         if n not in handled
-                         if grid[n] != '#']
-
-            if len(neighbors) == 0:
-                continue
-
-            if len(neighbors) > 1:
-                # Intersection
-                yield c, d
-                continue
-
-            n = neighbors[0]
-            todo.append((n, d+1))
-
-    graph = lib.graph.make_lazy_graph(neighbor_fn)
-
-    todo = [start]
-
-    seen = set()
-    while todo:
-        coord = todo.pop()
-        if coord in seen:
-            continue
-        seen.add(coord)
-
-        for n, _ in graph[coord]:
-            todo.append(n)
-
-    answer = 0
-
-    paths = [((start,),0)]
-
-    while paths:
-        new_paths = set()
-
-        for p, d in paths:
-            for n, n_d in graph[p[-1]]:
-                if n in p:
-                    continue
-                if n == end:
-                    answer = max(answer, d+n_d)
-                    continue
-                new_paths.add((p + (n,), d+n_d))
-
-        paths = new_paths
+    answer = solve(s)
 
     lib.aoc.give_answer(2023, 23, 2, answer)
 
