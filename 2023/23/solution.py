d7a2c1dd3083b17f71fe1c7e4adef471ee9cc57f
Patrick Hogg <phogg@novamoon.net>
2023-12-23 00:51:20 -0500

    2023 Day 23 Part 2
    And part 2 eliminates the slopes which explodes the search space further! Here's where I realized that I needed to compress to a graph connecting intersections. I whipped that up, though I did have a bug for a while where I accidentally allowed stepping into forest tiles on the *first* step out of an intersection. Other than that change the approach is about the same as part 1 was, and likewise slow.
    
    In fact, it was slow enough that I ended up letting it run in the background and work on rewriting part 1 with a graph connecting intersections. This turned out to be good as it helped me spot some of the issues I had in the code (such as allowing stepping into forest tiles) which was making part 2 take even longer, and it would have caused it to get the wrong answer! But I didn't finish the rewrite before the final background solve finished so that will be in its own commit.
    
    Rank 214. Better, but still bad for today. But at least I have it solved.

--- 2023/23/solution.py
+++ 2023/23/solution.py
@@ -1,4 +1,5 @@
 import lib.aoc
+import lib.graph
 import lib.grid
 
 def part1(s):
@@ -55,7 +56,80 @@ def part1(s):
     lib.aoc.give_answer(2023, 23, 1, answer)
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s)
+
+    end_y = grid.height-1
+    for x in grid.x_range:
+        if grid[x,0] == '.':
+            start = x,0
+        if grid[x,end_y] == '.':
+            end = x,end_y
+
+    def neighbor_fn(coord):
+        x,y = coord
+
+        handled = {coord}
+        todo = [(n, 1) for n in grid.neighbors(*coord)
+                if grid[n] != '#']
+
+        while todo:
+            c, d = todo.pop()
+            handled.add(c)
+
+            if c == end:
+                yield c, d
+                continue
+
+            neighbors = list(grid.neighbors(*c))
+
+            neighbors = [n for n in neighbors
+                         if n not in handled
+                         if grid[n] != '#']
+
+            if len(neighbors) == 0:
+                continue
+
+            if len(neighbors) > 1:
+                # Intersection
+                yield c, d
+                continue
+
+            n = neighbors[0]
+            todo.append((n, d+1))
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+
+    todo = [start]
+
+    seen = set()
+    while todo:
+        coord = todo.pop()
+        if coord in seen:
+            continue
+        seen.add(coord)
+
+        for n, _ in graph[coord]:
+            todo.append(n)
+
+    answer = 0
+
+    paths = [((start,),0)]
+
+    while paths:
+        new_paths = set()
+
+        for p, d in paths:
+            for n, n_d in graph[p[-1]]:
+                if n in p:
+                    continue
+                if n == end:
+                    answer = max(answer, d+n_d)
+                    continue
+                new_paths.add((p + (n,), d+n_d))
+
+        paths = new_paths
+
+    lib.aoc.give_answer(2023, 23, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 23)
 part1(INPUT)
