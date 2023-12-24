9906e84f89a24659b875028ab031d5e7b90e91a9
Patrick Hogg <phogg@novamoon.net>
2023-12-24 01:38:05 -0500

    2023 Day 24 Part 2
    This also threw me for a loop, and it really shouldn't have thrown me for as much of a loop!
    
    Fundamentally we have (for each hailstone) these 3 equations:
    x + t * xv = p.x + t * v.x
    y + t * yv = p.y + t * v.y
    z + t * zv = p.z + t * v.z
    Where x, y, z is the initial positions of the stone, t is the time of collision, and xv, yv, zv is the velocity of the stone.
    
    Initially I tried sending those equations for *all* the hailstones at z3. It could do it for the example but it choked on my real input, sadly. I left that running in the background and tried some other things.
    1) I misread the example and thought that it implied that at t=1 and t=2 there will always be a collision, so I tried iterating through pairs of hailstones to extrapolate the stone's position and velocity, but this never worked for the example. (The first two intersections are t=1 and t=3!)
    2) I tried to solve the systems of equations for 3 hailstones and the stone by hand. This gets super messy...
    3) I started to code up a 3-deep nested loop to have z3 try to solve the system of equations for 3 hailstones for me.
    
    However, while working on #3 I realized that we're guaranteed that *any* three hailstones are hit and are solvable. As such, the nested loop is dumb! Given this, I tried setting up z3 to just solve the system of equations for the first 3 hailstones, but it choked on that too. Thankfully I remembered about sympy and after verifying its API, I gave it a shot. It pretty much immediately gave me the answer, finally!
    
    I clearly don't do this sort of work enough, this was way more painful than it should have been.
    
    Rank 502. Oof, wish I'd have thought of sympy sooner...

--- 2023/24/solution.py
+++ 2023/24/solution.py
@@ -1,3 +1,5 @@
+import sympy
+
 import lib.aoc
 from lib.graphics import *
 
@@ -41,7 +43,43 @@ def part1(s):
     lib.aoc.give_answer(2023, 24, 1, answer)
 
 def part2(s):
-    pass
+    data = list(parse_input(s))
+
+    x = sympy.var('x')
+    y = sympy.var('y')
+    z = sympy.var('z')
+
+    xv = sympy.var('xv')
+    yv = sympy.var('yv')
+    zv = sympy.var('zv')
+
+    t1 = sympy.var('t1')
+    t2 = sympy.var('t2')
+    t3 = sympy.var('t3')
+
+    p1, v1 = data[0]
+    p2, v2 = data[1]
+    p3, v3 = data[3]
+
+    equations = []
+
+    equations.append(sympy.Eq(x + t1*xv, p1.x + v1.x*t1))
+    equations.append(sympy.Eq(y + t1*yv, p1.y + v1.y*t1))
+    equations.append(sympy.Eq(z + t1*zv, p1.z + v1.z*t1))
+
+    equations.append(sympy.Eq(x + t2*xv, p2.x + v2.x*t2))
+    equations.append(sympy.Eq(y + t2*yv, p2.y + v2.y*t2))
+    equations.append(sympy.Eq(z + t2*zv, p2.z + v2.z*t2))
+
+    equations.append(sympy.Eq(x + t3*xv, p3.x + v3.x*t3))
+    equations.append(sympy.Eq(y + t3*yv, p3.y + v3.y*t3))
+    equations.append(sympy.Eq(z + t3*zv, p3.z + v3.z*t3))
+
+    d = sympy.solve(equations)[0]
+
+    answer = sum(d[v] for v in (x, y, z))
+
+    lib.aoc.give_answer(2023, 24, 2, answer)
 
 INPUT = lib.aoc.get_input(2023, 24)
 part1(INPUT)
