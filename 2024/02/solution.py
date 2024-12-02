067aed1ffdecdef6fb9db0775d22ddc1b9f98731
Patrick Hogg <phogg@novamoon.net>
2024-12-02 00:09:02 -0500

    2024 Day 2 Part 2
    More server issues, it took me multiple attempts to get my initial answer submitted! (But I also goofed...)
    
    Anyway, dropping a number to check if a line is "safe" sounds easy and is...but I accidentally double-counted a number of lines that were safe to begin with and submitted an inflated answer. Lost ~3.5 minutes to that dumb mistake...
    
    Rank 627. Missed the leaderboard by 4 minutes, 20 seconds. A big chunk was my dumb bug, but the server issues compounded that further. :(

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -4,28 +4,46 @@ def parse_input(s):
     for line in s.splitlines():
         yield tuple(map(int, line.split()))
 
+def test(line):
+    if line[1] > line[0]:
+        for a, b in zip(line, line[1:]):
+            if not (1 <= b-a <= 3):
+                return False
+    else:
+        for a, b in zip(line, line[1:]):
+            if not (1 <= a-b <= 3):
+                return False
+    return True
+
 def part1(s):
     data = parse_input(s)
 
     answer = 0
 
     for line in data:
-        safe = True
-        if line[1] > line[0]:
-            for a, b in zip(line, line[1:]):
-                if not (1 <= b-a <= 3):
-                    safe = False
-        else:
-            for a, b in zip(line, line[1:]):
-                if not (1 <= a-b <= 3):
-                    safe = False
-        if safe:
+        if test(line):
             answer += 1
 
     lib.aoc.give_answer(2024, 2, 1, answer)
 
+def test2(line):
+    if test(line):
+        return True
+    if any(test(line[:i] + line[i+1:])
+           for i in range(len(line))):
+        return True
+    return False
+
 def part2(s):
-    pass
+    data = parse_input(s)
+
+    answer = 0
+
+    for line in data:
+        if test2(line):
+            answer += 1
+
+    lib.aoc.give_answer(2024, 2, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 2)
 part1(INPUT)
