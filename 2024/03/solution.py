5aab59447f23bda7f5f532a9a581bee66b4d7f6b
Patrick Hogg <phogg@novamoon.net>
2024-12-03 00:05:41 -0500

    2024 Day 3 Part 2
    Ah, so now we enable/disable based on new "commands". Honestly I probably would have been faster had I used searching instead of splitting, but still not too bad to implement.
    
    Jumped up to rank 315. But with 1st place getting this star in 1:01 I again have to suspect LLM abuse (again, unless I'm missing something obvious or was just slower than I thought).

--- 2024/03/solution.py
+++ 2024/03/solution.py
@@ -11,7 +11,23 @@ def part1(s):
     lib.aoc.give_answer(2024, 3, 1, answer)
 
 def part2(s):
-    pass
+    answer = 0
+
+    parts = s.split('don\'t()')
+    parts[0] = 'do()' + parts[0]
+
+    to_handle = ''
+
+    for p in parts:
+        if 'do()' in p:
+            i = p.index('do()')
+            to_handle += p[i:]
+
+    for group in re.findall('mul\(([0-9][0-9]?[0-9]?),([0-9][0-9]?[0-9]?)\)',
+                            to_handle):
+        answer += int(group[0]) * int(group[1])
+
+    lib.aoc.give_answer(2024, 3, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 3)
 part1(INPUT)
