2e089f7f7d1d0ea04423279846aff1b01531d5ac
Patrick Hogg <phogg@novamoon.net>
2024-12-05 00:13:54 -0500

    2024 Day 5 Part 2
    Okay, I definitely dropped the ball speed-wise here. I juggled a number of ideas on how to properly reorder the bad lists before settling on a proper graph-like one, then took an irksome amount of time to write (and debug!) it! I was comparing lists with tuples which always failed, resulting in me not reordering the list at all. It's a good thing I added a check to verify that the reorder was successful, but this took an annoying amount of time to debug.
    
    And in retrospect I realized I could have done a simpler, more brute-force approach by checking all positions for each addition and accepting the one that is allowed. Way less thought and faster to implement, even if the runtime is marginally worse...
    
    All the way down to rank 753...missed the leaderboard by 10:11. Oof.

--- 2024/05/solution.py
+++ 2024/05/solution.py
@@ -13,25 +13,58 @@ def parse_input(s):
 
     return alist, blist
 
+def verify(rules, l):
+    for r0, r1 in rules:
+        if r0 in l and r1 in l:
+            if l.index(r0) > l.index(r1):
+                return False
+    return True
+
 def part1(s):
     rules, b = parse_input(s)
 
     answer = 0
 
     for l in b:
-        good = True
-        for r0, r1 in rules:
-            if r0 in l and r1 in l:
-                if l.index(r0) > l.index(r1):
-                    good = False
-                    break
-        if good:
+        if verify(rules, l):
             answer += l[len(l)//2]
 
     lib.aoc.give_answer(2024, 5, 1, answer)
 
+def reorder(rules, l):
+    g = {}
+    for a, b in rules:
+        key = tuple(sorted([a, b]))
+        g[key] = (a, b)
+
+    new_l = []
+
+    for item in l:
+        placed = False
+        for i, other in enumerate(new_l):
+            key = tuple(sorted([item, other]))
+            if key in g:
+                if g[key] == (item, other):
+                    new_l.insert(i, item)
+                    placed = True
+                    break
+        if not placed:
+            new_l.append(item)
+
+    return new_l
+
 def part2(s):
-    pass
+    rules, b = parse_input(s)
+
+    answer = 0
+
+    for l in b:
+        if not verify(rules, l):
+            l = reorder(rules, l)
+            assert(verify(rules, l))
+            answer += l[len(l)//2]
+
+    lib.aoc.give_answer(2024, 5, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 5)
 part1(INPUT)
