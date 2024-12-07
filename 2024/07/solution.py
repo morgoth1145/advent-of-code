d106ee5d1d7e0cc786afa4fcaa39684e0d7e45e1
Patrick Hogg <phogg@novamoon.net>
2024-12-07 00:05:19 -0500

    2024 Day 7 Part 2
    Ah, so we're adding a concatenation operator! Implementing this efficiently isn't worth it, just abuse int and str!
    
    Rank 202, only missed the leaderboard by 1 minute 30 seconds. Totally would have leaderboarded without my part 1 mis-parse...

--- 2024/07/solution.py
+++ 2024/07/solution.py
@@ -34,8 +34,36 @@ def part1(s):
 
     lib.aoc.give_answer(2024, 7, 1, answer)
 
+def is_solvable2(target, parts):
+    def impl(current, rest):
+        if len(rest) == 0:
+            if current == target:
+                return True
+            return False
+
+        a = rest[0]
+        rest = rest[1:]
+
+        if impl(current+a, rest):
+            return True
+        if impl(current*a, rest):
+            return True
+        if impl(int(str(current)+str(a)), rest):
+            return True
+        return False
+
+    return impl(parts[0], parts[1:])
+
 def part2(s):
-    pass
+    data = parse_input(s)
+
+    answer = 0
+
+    for target, parts in data:
+        if is_solvable2(target, parts):
+            answer += target
+
+    lib.aoc.give_answer(2024, 7, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 7)
 part1(INPUT)
