36b0c5fc05efbcf255d1c45284bad370d6661886
Patrick Hogg <phogg@novamoon.net>
2024-12-08 00:08:09 -0500

    2024 Day 8 Part 2
    Okay, so now antinodes are every point in line with the antenna of the same type. Much easier to implement, done nice and quick.
    
    Rank 140, missed the top 100 by 57 seconds. Quite a jump, and I definitely would have leaderboarded if part 1 was easier to grok!

--- 2024/08/solution.py
+++ 2024/08/solution.py
@@ -39,7 +39,39 @@ def part1(s):
     lib.aoc.give_answer(2024, 8, 1, answer)
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s)
+
+    a_to_locs = collections.defaultdict(list)
+
+    for coord, c in grid.items():
+        if c != '.':
+            a_to_locs[c].append(coord)
+
+    antinodes = set()
+
+    for c, coords in a_to_locs.items():
+        for coord_a in coords:
+            for coord_b in coords:
+                if coord_a == coord_b:
+                    continue
+                x0, y0 = coord_a
+                x1, y1 = coord_b
+
+                dx = x1-x0
+                dy = y1-y0
+
+                x, y = x1, y1
+                while (x, y) in grid:
+                    antinodes.add((x, y))
+                    x, y = x+dx, y+dy
+                x, y = x1, y1
+                while (x, y) in grid:
+                    antinodes.add((x, y))
+                    x, y = x-dx, y-dy
+
+    answer = len(antinodes)
+
+    lib.aoc.give_answer(2024, 8, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 8)
 part1(INPUT)
