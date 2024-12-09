80cc622a8c02c104e9b6d0761ef90527877ab282
Patrick Hogg <phogg@novamoon.net>
2024-12-09 00:18:24 -0500

    2024 Day 9 Part 2
    Oh hey, now we're moving the whole files to reduce fragmentation. I was anticipating this in part 1 (and it's part of what tripped me up!) This implementation is terrible, there should *not* be a quadratic search to find free blocks but I didn't want to spend the time implementing something better.
    
    Rank 176. At least I jumped up, but still, pain. Would have been an easy leaderboard had I understood part 1 in a reasonable period of time...

--- 2024/09/solution.py
+++ 2024/09/solution.py
@@ -37,7 +37,56 @@ def part1(s):
     lib.aoc.give_answer(2024, 9, 1, answer)
 
 def part2(s):
-    pass
+    nums = list(map(int, s))
+
+    blocks = []
+    file_starts = []
+
+    is_file = True
+    file_id = 0
+
+    for n in nums:
+        if is_file:
+            file_starts.append(len(blocks))
+            for _ in range(n):
+                blocks.append(file_id)
+            file_id += 1
+        else:
+            for _ in range(n):
+                blocks.append(None)
+        is_file = not is_file
+
+    def find_first_gap(target_length):
+        for idx, v in enumerate(blocks):
+            if v is None and idx+target_length <= len(blocks):
+                if all(blocks[idx+i] is None
+                       for i in range(target_length)):
+                    return idx
+        return None
+
+    while file_starts:
+        idx = file_starts.pop()
+        file_id = blocks[idx]
+        end = idx
+        while end+1 < len(blocks) and blocks[end+1] == file_id:
+            end += 1
+        length = end - idx + 1
+
+        dest = find_first_gap(length)
+        if dest is None:
+            continue
+        if dest < idx:
+            for i in range(length):
+                blocks[dest+i] = file_id
+                blocks[idx+i] = None
+
+    answer = 0
+
+    for pos, v in enumerate(blocks):
+        if v is not None:
+            answer += pos * v
+
+    lib.aoc.give_answer(2024, 9, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 9)
 part1(INPUT)
