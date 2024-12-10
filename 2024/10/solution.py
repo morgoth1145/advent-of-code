3231e11dbc58b19bb9fe976348ee82835fa237f0
Patrick Hogg <phogg@novamoon.net>
2024-12-10 00:09:46 -0500

    2024 Day 10 Part 1
    Another awful performance. This is a pretty simple grid walking problem which I did *not* do well on due to a combination of a few things:
    1) I prematurely started bringing in my graph library thinking we'd be doing some search or something. I did that way too eagerly (honestly I've been expecting a graph search for a while) and just wasted time.
    2) More importantly, I had a really silly mistake in my recursive trail walking code. Instead of yield from I just had yield, so I ended up returning iterators and essentially counting how many neighbors each trailhead had. This took me way too long to find and fix...
    
    Rank 1015. Awful. My premature jump to graphs already put me well outside of leaderboard range (didn't have code running for part 1 until part 2 was already closed) and the second issue just ate up more time...

---
+++ 2024/10/solution.py
@@ -0,0 +1,41 @@
+import lib.aoc
+import lib.grid
+
+def part1(s):
+    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+
+    starts = grid.coords_by_value()[0]
+
+    answer = 0
+
+    def score_trailhead(coord):
+        assert(grid[coord] == 0)
+        seen = set()
+
+        def impl(coord, v):
+            if coord in seen:
+                return
+            seen.add(coord)
+
+            if v == 9:
+                yield coord
+                return
+
+            for n in grid.neighbors(*coord):
+                v2 = grid[n]
+                if v2 == v+1:
+                    yield from impl(n, v2)
+
+        ends = set(impl(coord, 0))
+        return len(ends)
+
+    answer = sum(map(score_trailhead, starts))
+
+    lib.aoc.give_answer(2024, 10, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2024, 10)
+part1(INPUT)
+part2(INPUT)
