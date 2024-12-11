9a256e966ac399656d511cd7725fe184ea78b3f4
Patrick Hogg <phogg@novamoon.net>
2024-12-11 00:38:23 -0500

    2024 Rewrite Day 11
    Parts 1 and 2 can use the same approach since the cycle count is all that differs so it's cleaner to have a single solve function. Also, instead of recursion and memoization this can be solved pretty easily with iteration and collections.Counter instead. This makes things a little cleaner and reduces the number of map keys to deal with, providing better runtime guarantees.
    
    This does still bog down with large cycle counts, but I did observe that eventually the stone numbers seem to stabilize into a given set of numbers. As such, the relation can probably be encoded into a matrix and matrix exponentiation used to more quickly compute the final answer, I may look into that next.

--- 2024/11/solution.py
+++ 2024/11/solution.py
@@ -1,56 +1,37 @@
-import functools
+import collections
 
 import lib.aoc
 
-def blink(stones):
-    n = []
-    for i, s in enumerate(stones):
-        if s == 0:
-            n.append(1)
-            continue
-        digits = str(s)
-        if len(digits) % 2 == 0:
-            left = digits[:len(digits)//2]
-            right = digits[len(digits)//2:]
-            n.append(int(left))
-            n.append(int(right))
-            continue
-        n.append(s*2024)
-    return n
+def solve(s, num_cycles):
+    counts = collections.Counter(map(int, s.split()))
+
+    for _ in range(num_cycles):
+        new_counts = collections.Counter()
+
+        for v, c in counts.items():
+            if v == 0:
+                new_counts[1] += c
+                continue
+            s = str(v)
+            if len(s) % 2 == 0:
+                left = int(s[:len(s)//2])
+                right = int(s[len(s)//2:])
+                new_counts[left] += c
+                new_counts[right] += c
+            else:
+                new_counts[v*2024] += c
+
+        counts = new_counts
+
+    return sum(counts.values())
 
 def part1(s):
-    stones = tuple(map(int, s.split()))
-
-    for _ in range(25):
-        stones = blink(stones)
-
-    answer = len(stones)
+    answer = solve(s, 25)
 
     lib.aoc.give_answer(2024, 11, 1, answer)
 
-@functools.cache
-def count_blink_stones(s, blinks_left):
-    if blinks_left == 0:
-        return 1
-
-    if s == 0:
-        return count_blink_stones(1, blinks_left-1)
-
-    digits = str(s)
-    if len(digits) % 2 == 0:
-        left = digits[:len(digits)//2]
-        right = digits[len(digits)//2:]
-        left = count_blink_stones(int(left), blinks_left-1)
-        right = count_blink_stones(int(right), blinks_left-1)
-        return left + right
-
-    return count_blink_stones(s*2024, blinks_left-1)
-
 def part2(s):
-    stones = tuple(map(int, s.split()))
-
-    answer = sum(count_blink_stones(s, 75)
-                 for s in stones)
+    answer = solve(s, 75)
 
     lib.aoc.give_answer(2024, 11, 2, answer)
 
