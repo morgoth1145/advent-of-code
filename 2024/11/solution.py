f36433c14c4bdc6efff5129cf70dc02793f33382
Patrick Hogg <phogg@novamoon.net>
2024-12-11 00:07:04 -0500

    2024 Day 11 Part 2
    This is what I feared in part 1, ramping the number of cycles up. Honestly I should have recognized that the stones are independent a little quicker and noticed that I could just DP this problem, but oh well. Once I recognized that I had a memoized counter pretty quickly!
    
    Rank 131, missing the leaderboard by a closer 40 seconds. Definitely within a margin where recognizing the trick a bit faster would have let me have it, but at least I had respectable ranks for both parts today!

--- 2024/11/solution.py
+++ 2024/11/solution.py
@@ -1,3 +1,5 @@
+import functools
+
 import lib.aoc
 
 def blink(stones):
@@ -26,8 +28,31 @@ def part1(s):
 
     lib.aoc.give_answer(2024, 11, 1, answer)
 
+@functools.cache
+def count_blink_stones(s, blinks_left):
+    if blinks_left == 0:
+        return 1
+
+    if s == 0:
+        return count_blink_stones(1, blinks_left-1)
+
+    digits = str(s)
+    if len(digits) % 2 == 0:
+        left = digits[:len(digits)//2]
+        right = digits[len(digits)//2:]
+        left = count_blink_stones(int(left), blinks_left-1)
+        right = count_blink_stones(int(right), blinks_left-1)
+        return left + right
+
+    return count_blink_stones(s*2024, blinks_left-1)
+
 def part2(s):
-    pass
+    stones = tuple(map(int, s.split()))
+
+    answer = sum(count_blink_stones(s, 75)
+                 for s in stones)
+
+    lib.aoc.give_answer(2024, 11, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 11)
 part1(INPUT)
