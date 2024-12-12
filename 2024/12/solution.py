ba83e167e8b0a57adb62db3e26f588535b2ce9c9
Patrick Hogg <phogg@novamoon.net>
2024-12-12 01:50:12 -0500

    2024 Rewrite Day 12
    The part 1 and 2 logic become much simpler with a unified Region class encapsulating some of the logic. This also makes it possible to write a single solve function taking a region scoring function making part 1 and 2 very short.
    
    Most importantly though, the side counting algorithm can be drastically simplified using an observation nthistle made: The sides can be counted very similarly to the perimeter just by excluding edges if there is an edge to the "right" of it. That way exactly one edge segment will be counted per side! Very easy to implement and understand.

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -1,129 +1,81 @@
-import collections
-
 import lib.aoc
 import lib.grid
 
-def part1(s):
-    grid = lib.grid.FixedGrid.parse(s)
+class Region:
+    def __init__(self, grid, start_cell):
+        self.cells = set()
+        target = grid[start_cell]
 
-    seen = set()
-
-    sections = []
-
-    def fill_section(coord):
-        v = grid[coord]
-        sect = set()
-
-        to_handle = [coord]
+        to_handle = [start_cell]
+        seen = set(to_handle)
 
         while to_handle:
-            p = to_handle.pop()
-            sect.add(p)
-            for n in grid.neighbors(*p):
-                if n in seen:
+            c = to_handle.pop()
+            self.cells.add(c)
+            for n in grid.neighbors(*c):
+                if n in seen or grid[n] != target:
                     continue
-                nv = grid[n]
-                if nv == v:
-                    seen.add(n)
-                    to_handle.append(n)
+                seen.add(n)
+                to_handle.append(n)
 
-        seen.update(sect)
-        sections.append(sect)
+    @property
+    def area(self):
+        return len(self.cells)
 
-    for coord, c in grid.items():
-        if coord in seen:
-            continue
-        fill_section(coord)
-
-    answer = 0
-
-    def score_section(sect):
+    @property
+    def perimeter(self):
         perimeter = 0
 
-        for x, y in sect:
-            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                cand = (x+dx, y+dy)
-                if cand not in sect:
+        for x, y in self.cells:
+            for n in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+                if n not in self.cells:
                     perimeter += 1
 
-        v = grid[sorted(sect)[0]]
+        return perimeter
 
-        return perimeter * len(sect)
+    @property
+    def num_sides(self):
+        num_sides = 0
 
-    answer = sum(map(score_section, sections))
+        # Count similarly to counting the perimeter, but only include the
+        # "rightmost" edge on any given side. This ensures that each side
+        # will only be counted once!
+        for x, y in self.cells:
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                if (x+dx, y+dy) not in self.cells:
+                    # This is part of the perimeter, now check if it's a "key"
+                    # section for counting sides
+                    rdx, rdy = -dy, dx
+                    if ((x+rdx, y+rdy) in self.cells and
+                        (x+dx+rdx, y+dy+rdy) not in self.cells):
+                        # Not a key section, don't count it
+                        continue
+                    num_sides += 1
+
+        return num_sides
+
+def solve(s, region_scorer):
+    grid = lib.grid.FixedGrid.parse(s)
+
+    answer = 0
+    handled = set()
+
+    for coord, c in grid.items():
+        if coord in handled:
+            continue
+        r = Region(grid, coord)
+        handled.update(r.cells)
+        answer += region_scorer(r)
+
+    return answer
+
+def part1(s):
+    answer = solve(s, lambda r: r.area * r.perimeter)
 
     lib.aoc.give_answer(2024, 12, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s)
-
-    seen = set()
-
-    sections = []
-
-    def fill_section(coord):
-        v = grid[coord]
-        sect = set()
-
-        to_handle = [coord]
-
-        while to_handle:
-            p = to_handle.pop()
-            sect.add(p)
-            for n in grid.neighbors(*p):
-                if n in seen:
-                    continue
-                nv = grid[n]
-                if nv == v:
-                    seen.add(n)
-                    to_handle.append(n)
-
-        seen.update(sect)
-        sections.append(sect)
-
-    for coord, c in grid.items():
-        if coord in seen:
-            continue
-        fill_section(coord)
-
-    answer = 0
-
-    def score_section(sect):
-        perimeter = collections.defaultdict(list)
-
-        for x, y in sect:
-            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                cand = (x+dx, y+dy)
-                if cand not in sect:
-                    perimeter[cand].append((dx, dy))
-
-        sides = 0
-
-        handled = set()
-
-        for (x, y), directs in perimeter.items():
-            for dx, dy in directs:
-                key = ((x, y), (dx, dy))
-                if key in handled:
-                    continue
-                sides += 1
-                for dx2, dy2 in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                    if (dx2, dy2) == (dx, dy):
-                        continue
-                    x2, y2 = x+dx2, y+dy2
-                    while (x2,y2) in perimeter:
-                        if (dx, dy) not in perimeter[(x2,y2)]:
-                            break
-                        handled.add(((x2, y2), (dx, dy)))
-                        x2 += dx2
-                        y2 += dy2
-
-        c0 = sorted(sect)[0]
-        v = grid[c0]
-
-        return sides * len(sect)
-
-    answer = sum(map(score_section, sections))
+    answer = solve(s, lambda r: r.area * r.num_sides)
 
     lib.aoc.give_answer(2024, 12, 2, answer)
 
