d800e80947c955e794e7fe13037e3d2d9526314b
Patrick Hogg <phogg@novamoon.net>
2024-12-12 00:32:34 -0500

    2024 Day 12 Part 2
    So this sort of fell apart. Figuring out which edges were part of the same side took me way too long to get working.
    
    Initially I was thinking of tracking edges as "halfway" between the in and out sides and that sort of works, but if you're not careful you can "tunnel through" the region when regions have offshoots with a width of 1.
    
    Then I started working on trying to find neighbor edge cells that were the same "direction" from the region. This does work, but I had a nasty little issue where I was "jumping" across gaps because some cells were part of the perimeter but were reached from the opposite direction! That took quite a while to nail down actually, something like 12 minutes! When I finally figured it out it was pretty easy to correct, but ouch.
    
    Rank 710. What a mess of a day, and I know my solution is incredibly inelegant...

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -1,3 +1,5 @@
+import collections
+
 import lib.aoc
 import lib.grid
 
@@ -53,7 +55,77 @@ def part1(s):
     lib.aoc.give_answer(2024, 12, 1, answer)
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s)
+
+    seen = set()
+
+    sections = []
+
+    def fill_section(coord):
+        v = grid[coord]
+        sect = set()
+
+        to_handle = [coord]
+
+        while to_handle:
+            p = to_handle.pop()
+            sect.add(p)
+            for n in grid.neighbors(*p):
+                if n in seen:
+                    continue
+                nv = grid[n]
+                if nv == v:
+                    seen.add(n)
+                    to_handle.append(n)
+
+        seen.update(sect)
+        sections.append(sect)
+
+    for coord, c in grid.items():
+        if coord in seen:
+            continue
+        fill_section(coord)
+
+    answer = 0
+
+    def score_section(sect):
+        perimeter = collections.defaultdict(list)
+
+        for x, y in sect:
+            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                cand = (x+dx, y+dy)
+                if cand not in sect:
+                    perimeter[cand].append((dx, dy))
+
+        sides = 0
+
+        handled = set()
+
+        for (x, y), directs in perimeter.items():
+            for dx, dy in directs:
+                key = ((x, y), (dx, dy))
+                if key in handled:
+                    continue
+                sides += 1
+                for dx2, dy2 in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                    if (dx2, dy2) == (dx, dy):
+                        continue
+                    x2, y2 = x+dx2, y+dy2
+                    while (x2,y2) in perimeter:
+                        if (dx, dy) not in perimeter[(x2,y2)]:
+                            break
+                        handled.add(((x2, y2), (dx, dy)))
+                        x2 += dx2
+                        y2 += dy2
+
+        c0 = sorted(sect)[0]
+        v = grid[c0]
+
+        return sides * len(sect)
+
+    answer = sum(map(score_section, sections))
+
+    lib.aoc.give_answer(2024, 12, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 12)
 part1(INPUT)
