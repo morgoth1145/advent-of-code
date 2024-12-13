281bda177f21efe9d1cc15537676be63fefa2526
Patrick Hogg <phogg@novamoon.net>
2024-12-13 00:19:23 -0500

    2024 Day 13 Part 2
    And here's where I paid for not solving this properly the first time! Searching a ridiculous search space would not work with my first implementation so I ended up having to use z3 as I anticipated in part 1 anyway. Could have gotten this done much faster had I listened to the voice in the back of my mind.
    
    I did have some goofs here in getting the value back out of the z3 model, I don't use the library often enougha nd wasn't getting the syntax quite right initially. At least I got that fixed without too much debugging.
    
    Rank 429. A good climb but could have been way better.

--- 2024/13/solution.py
+++ 2024/13/solution.py
@@ -1,4 +1,5 @@
 import functools
+import z3
 
 import lib.aoc
 
@@ -54,8 +55,43 @@ def part1(s):
 
     lib.aoc.give_answer(2024, 13, 1, answer)
 
+def optimize_cost2(a_delta, a_cost, b_delta, b_cost, prize, press_limit=None):
+    ax, ay = a_delta
+    bx, by = b_delta
+    px, py = prize
+
+    a_times = z3.Int('a_times')
+    b_times = z3.Int('b_times')
+    cost = z3.Int('cost')
+
+    o = z3.Optimize()
+    o.add(a_times*ax + b_times*bx == px)
+    o.add(a_times*ay + b_times*by == py)
+    if press_limit is not None:
+        o.add(a_times <= press_limit)
+        o.add(b_times <= press_limit)
+
+    o.add(cost == a_times*a_cost + b_times*b_cost)
+    o.minimize(cost)
+    o.check()
+
+    cost = o.model()[cost]
+
+    return cost.as_long() if cost is not None else None
+
 def part2(s):
-    pass
+    data = list(parse_input(s))
+
+    answer = 0
+
+    for a, b, prize in data:
+        px, py = prize
+        prize = (10000000000000+px, 10000000000000+py)
+        cost = optimize_cost2(a, 3, b, 1, prize)
+        if cost is not None:
+            answer += cost
+
+    lib.aoc.give_answer(2024, 13, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 13)
 part1(INPUT)
