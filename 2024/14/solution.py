92ea969d4ea5773c9ecff15429d963173e76ded5
Patrick Hogg <phogg@novamoon.net>
2024-12-14 00:38:34 -0500

    2024 Day 14 Part 2
    This is definitely not what I expected. I was thinking with the moving robots we might be asked to run this for more time and read out the text or something, but instead we need to look for "a Christmas tree". That's honestly super vague and unhelpful, a Chistmas tree could mean many things! I interpreted it as being arranged in a triangle in the grid and only got the answer out of dumb luck! So yeah, honestly I don't like this problem.
    
    (I also burned time thinking about the Chinese Remainder Theorem but that didn't end up applying, all the robots in my input have a cycle of 10403 seconds.)
    
    I guess thinking now (and having glanced at the reddit already) I see some folks talking about looking for rows of robots, maybe you were supposed to track the "connectivity" or "clustering" of the robots? That definitely wasn't clear in the problem text though, and manually looking at 10403 states is not realistic...
    
    Rank 1262. I'm just glad to be done with this mess.

--- 2024/14/solution.py
+++ 2024/14/solution.py
@@ -9,23 +9,29 @@ def parse_all_ints(s):
 def parse_input(s):
     for line in s.splitlines():
         px, py, vx, vy = parse_all_ints(line)
-        yield (px, py), (vx, vy)
+        yield [(px, py), (vx, vy)]
+
+def step_robot(px, py, vx, vy, width, height):
+    px += vx
+    py += vy
+
+    if px < 0:
+        px += width
+    if px >= width:
+        px -= width
+
+    if py < 0:
+        py += height
+    if py >= height:
+        py -= height
+
+    return px, py
 
 def project(robot, width, height, steps):
     (px, py), (vx, vy) = robot
 
-    px += steps * vx
-    py += steps * vy
-
-    while px < 0:
-        px += width
-    while px >= width:
-        px -= width
-
-    while py < 0:
-        py += height
-    while py >= height:
-        py -= height
+    for _ in range(steps):
+        px, py = step_robot(px, py, vx, vy, width, height)
 
     return px, py
 
@@ -58,7 +64,56 @@ def part1(s):
     lib.aoc.give_answer(2024, 14, 1, answer)
 
 def part2(s):
-    pass
+    robots = list(parse_input(s))
+
+    WIDTH = 101
+    HEIGHT = 103
+
+    MIDDLE_X = WIDTH//2
+    MIDDLE_Y = HEIGHT//2
+
+    def maybe_christmas(px, py):
+        x = abs(px - MIDDLE_X)
+        return (py+2)//2 > x
+
+    def tree_score():
+        return sum(maybe_christmas(px, py)
+                   for (px, py), _
+                   in robots)
+
+    def print_state():
+        has_bot = set(p for p, v in robots)
+
+        for y in range(HEIGHT):
+            l = ''
+            for x in range(WIDTH):
+                if maybe_christmas(x, y):
+                    c = 'T' if (x,y) in has_bot else '.'
+                else:
+                    c = '#' if (x,y) in has_bot else ' '
+                l += c
+            print(l)
+
+        print('-'*75)
+        print('-'*75)
+        print('-'*75)
+
+    answer = 0
+    best_score = -1
+
+    for i in range(50000):
+        s = tree_score()
+        if s > best_score:
+            print(i)
+            print_state()
+            answer = i
+            best_score = s
+        for idx, robot in enumerate(robots):
+            (px, py), (vx, vy) = robot
+            px, py = step_robot(px, py, vx, vy, WIDTH, HEIGHT)
+            robot[0] = (px, py)
+
+    lib.aoc.give_answer(2024, 14, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 14)
 part1(INPUT)
