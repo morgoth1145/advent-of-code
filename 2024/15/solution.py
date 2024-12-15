9ded3720dc5f88612f6067002458343eb1a43d74
Patrick Hogg <phogg@novamoon.net>
2024-12-15 00:34:57 -0500

    2024 Day 15 Part 2
    That's an interesting twist! Making everything twice as wide sets up some interesting chains/trees when pushing boxes up or down since one box can touch two boxes. I opted to handle this by keeping a list of cells I still need to check so that I could extend the "to check" list easily for the double-wide boxes. (This also requires keeping a list of which cells ultimately move.)
    
    Unfortunately, while this strategy was smart I did have multiple bugs.
    1) I had a similar bug to part 1 where I forgot to update the robot's position. (Or rather, I accidentally deleted that code and took time to figure that out...)
    2) I accidentally was extending the to_check list for the other "side" of a box for horizontal moves as well as vertical moves. Doing that for horizontal moves actually messed things up, and it's not needed since horizontal moves automatically get the other size without special handling.
    3) I was pulling things from my to_check list from the back instead of the front. This had dire consequenses in that it ended up obliterating boxes due to ultimately moving cells out of order! This took way too long to realize, but was an easy fix in pulling things from the front of the to_check list.
    
    Rank 156. Missed the leaderboard by under 3 minutes, I was close, those tricky bugs cost me!

--- 2024/15/solution.py
+++ 2024/15/solution.py
@@ -47,7 +47,85 @@ def part1(s):
     lib.aoc.give_answer(2024, 15, 1, answer)
 
 def part2(s):
-    pass
+    grid, moves = parse_input(s)
+    width, height = grid.width*2, grid.height
+
+    new_grid = {}
+
+    start = None
+
+    for (x, y), c in grid.items():
+        if c == '#':
+            new_grid[2*x,y] = '#'
+            new_grid[2*x+1,y] = '#'
+        if c == 'O':
+            new_grid[2*x,y] = '['
+            new_grid[2*x+1,y] = ']'
+        if c == '.':
+            new_grid[2*x,y] = '.'
+            new_grid[2*x+1,y] = '.'
+        if c == '@':
+            new_grid[2*x,y] = '@'
+            new_grid[2*x+1,y] = '.'
+            start = 2*x,y
+
+    x, y = start
+    grid = new_grid
+
+    for i, c in enumerate(moves):
+        dx, dy = {'^': (0, -1),
+                  'v': (0, 1),
+                  '<': (-1, 0),
+                  '>': (1, 0)
+                  }[c]
+
+        to_move = []
+        to_check = [(x, y)]
+        handled = set(to_check)
+        can_move = True
+
+        while to_check:
+            srcx, srcy = to_check.pop(0)
+            destx, desty = srcx+dx, srcy+dy
+            c = grid[destx,desty]
+            if c == '#':
+                can_move = False
+                break
+            to_move.append((srcx, srcy, destx, desty, grid[srcx,srcy]))
+            if c == '.':
+                continue # Safe
+            new_to_check = [(destx, desty)]
+            if c == '[':
+                if dx == 0:
+                    new_to_check.append((destx+1, desty))
+            elif c == ']':
+                if dx == 0:
+                    new_to_check.append((destx-1, desty))
+            else:
+                assert(False)
+
+            for n in new_to_check:
+                if n in handled:
+                    continue
+                handled.add(n)
+                to_check.append(n)
+
+        if can_move:
+            while to_move:
+                srcx, srcy, destx, desty, srcc = to_move.pop()
+                grid[destx,desty] = srcc
+                grid[srcx,srcy] = '.'
+
+            x += dx
+            y += dy
+
+    answer = 0
+
+    for (x, y), c in grid.items():
+        if c == '[':
+            answer += x + 100*y
+
+    lib.aoc.give_answer(2024, 15, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 15)
 part1(INPUT)
