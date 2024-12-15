7e73e3791c5091141434a05a06e5bd2e215b7e2a
Patrick Hogg <phogg@novamoon.net>
2024-12-15 01:40:29 -0500

    2024 Refactor Day 15
    The part 2 code can be used for part 1 as well with a few minor tweaks, so pull that out into a single solve function to be used by both parts. Part 2 just needs an extra step to widen the grid which isn't much at all.
    
    Also, clean up the movement code a little bit.

--- 2024/15/solution.py
+++ 2024/15/solution.py
@@ -2,57 +2,72 @@ import lib.aoc
 import lib.grid
 
 def parse_input(s):
-    a, b = s.split('\n\n')
+    grid, moves = s.split('\n\n')
 
-    a = lib.grid.FixedGrid.parse(a)
-    return a, b.replace('\n', '')
+    return lib.grid.FixedGrid.parse(grid), moves.replace('\n', '')
 
-def part1(s):
-    grid, moves = parse_input(s)
-
+def solve(grid, moves):
     x, y = grid.find('@')
 
-    for i, c in enumerate(moves):
+    for c in moves:
         dx, dy = {'^': (0, -1),
                   'v': (0, 1),
                   '<': (-1, 0),
-                  '>': (1, 0)
-                  }[c]
+                  '>': (1, 0)}[c]
 
-        destx, desty = x+dx, y+dy
+        to_move = []
+        to_check = [(x, y)]
+        handled = set()
+        can_move = True
 
-        while grid[destx,desty] not in '.#':
-            destx, desty = destx+dx, desty+dy
+        while to_check:
+            srcx, srcy = to_check.pop(0)
+            if (srcx, srcy) in handled:
+                continue
+            handled.add((srcx, srcy))
 
-        if grid[destx,desty] == '#':
-            # Wall, no move
-            continue
+            destx, desty = srcx+dx, srcy+dy
 
-        srcx, srcy = destx, desty
+            c = grid[destx,desty]
+            if c == '#':
+                can_move = False
+                break
 
-        while (srcx, srcy) != (x, y):
-            srcx, srcy = srcx-dx, srcy-dy
-            grid[destx,desty] = grid[srcx,srcy]
-            destx, desty = destx-dx, desty-dy
+            to_move.append((srcx, srcy, destx, desty))
+            if c == '.':
+                continue # Safe, moving into an empty tile
 
-        grid[srcx,srcy] = '.'
-        x, y = x+dx, y+dy
+            to_check.append((destx, desty))
+            if dx == 0:
+                if c == '[':
+                    to_check.append((destx+1, desty))
+                elif c == ']':
+                    to_check.append((destx-1, desty))
+
+        if can_move:
+            while to_move:
+                srcx, srcy, destx, desty = to_move.pop()
+                grid[destx,desty] = grid[srcx,srcy]
+                grid[srcx,srcy] = '.'
+
+            x, y = x+dx, y+dy
 
     answer = 0
 
     for (x, y), c in grid.items():
-        if c == 'O':
+        if c in 'O[':
             answer += x + 100*y
+
+    return answer
+
+def part1(s):
+    grid, moves = parse_input(s)
+    answer = solve(grid, moves)
 
     lib.aoc.give_answer(2024, 15, 1, answer)
 
-def part2(s):
-    grid, moves = parse_input(s)
-    width, height = grid.width*2, grid.height
-
+def widen_grid(grid):
     new_grid = {}
-
-    start = None
 
     for (x, y), c in grid.items():
         if c == '#':
@@ -69,61 +84,11 @@ def part2(s):
             new_grid[2*x+1,y] = '.'
             start = 2*x,y
 
-    x, y = start
-    grid = new_grid
+    return lib.grid.FixedGrid.from_dict(new_grid)
 
-    for i, c in enumerate(moves):
-        dx, dy = {'^': (0, -1),
-                  'v': (0, 1),
-                  '<': (-1, 0),
-                  '>': (1, 0)
-                  }[c]
-
-        to_move = []
-        to_check = [(x, y)]
-        handled = set(to_check)
-        can_move = True
-
-        while to_check:
-            srcx, srcy = to_check.pop(0)
-            destx, desty = srcx+dx, srcy+dy
-            c = grid[destx,desty]
-            if c == '#':
-                can_move = False
-                break
-            to_move.append((srcx, srcy, destx, desty, grid[srcx,srcy]))
-            if c == '.':
-                continue # Safe
-            new_to_check = [(destx, desty)]
-            if c == '[':
-                if dx == 0:
-                    new_to_check.append((destx+1, desty))
-            elif c == ']':
-                if dx == 0:
-                    new_to_check.append((destx-1, desty))
-            else:
-                assert(False)
-
-            for n in new_to_check:
-                if n in handled:
-                    continue
-                handled.add(n)
-                to_check.append(n)
-
-        if can_move:
-            while to_move:
-                srcx, srcy, destx, desty, srcc = to_move.pop()
-                grid[destx,desty] = srcc
-                grid[srcx,srcy] = '.'
-
-            x += dx
-            y += dy
-
-    answer = 0
-
-    for (x, y), c in grid.items():
-        if c == '[':
-            answer += x + 100*y
+def part2(s):
+    grid, moves = parse_input(s)
+    answer = solve(widen_grid(grid), moves)
 
     lib.aoc.give_answer(2024, 15, 2, answer)
 
