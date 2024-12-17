6f45683b3c077ab47778aa0c23b0532509eef248
Patrick Hogg <phogg@novamoon.net>
2024-12-17 00:47:38 -0500

    2024 Day 17 Part 2
    I was afraid of that! Decompilation. Well, at least I'm more practiced at it nowadays, and I have some strategies to employ. I took a little longer than I'd have liked to have converted the program into something human-readable, but that didn't go too badly.
    
    What did go less well was my decompilation attempt. It took me way too long to recognize that I only needed to partially decompile this! The overall program is set up to pull 3 bits at a time from register a and do some processing to then generate the next output. As such a reversal program can simply generate bits in reverse, testing all 8 possibilities for each value in the target output and keeping the one(s) which match the target. In the end, only one result will survive, and that's the answer.
    
    Oh well. I've had *way* worse decompilation attempts in the past so at least I finished relatively quickly today.
    
    Rank 119. I missed the leaderboard by 3 minutes (and 1 second) *again*! And like yesterday I had a bug in part 1 that was a bit longer than my leaderboard miss! I'm not liking this pattern...

--- 2024/17/solution.py
+++ 2024/17/solution.py
@@ -9,8 +9,8 @@ def parse_input(s):
 
     return (a, b, c), prog
 
-def part1(s):
-    (a, b, c), prog = parse_input(s)
+def run_prog(a, b, c, prog):
+    prog = prog[:]
 
     ip = 0
 
@@ -85,12 +85,94 @@ def part1(s):
             ip += 2
             continue
 
+    return out
+
+def part1(s):
+    (a, b, c), prog = parse_input(s)
+
+    out = run_prog(a, b, c, prog)
     answer = ','.join(map(str, out))
 
     lib.aoc.give_answer(2024, 17, 1, answer)
 
+def solve_manual(s):
+    _, prog = parse_input(s)
+
+    def combo(operand):
+        if operand in (0, 1, 2, 3):
+            return str(operand)
+        if operand == 4:
+            return 'a'
+        if operand == 5:
+            return 'b'
+        if operand == 6:
+            return 'c'
+        print(f'Bad combo operand {operand}!')
+        assert(False)
+
+    print('a = 0')
+    print('b = 0')
+    print('c = 0')
+    print()
+
+    ip = 0
+
+    for ip in range(0, len(prog), 2):
+        print(f'LABEL{ip}:')
+
+        opcode = prog[ip]
+        operand = prog[ip+1]
+
+        if opcode == 0:
+            # adv
+            print(f'    a = a // (2 ** {combo(operand)})')
+            continue
+
+        if opcode == 1:
+            # bxl
+            print(f'    b = b ^ {operand}')
+            continue
+
+        if opcode == 2:
+            # bst
+            print(f'    b = {combo(operand)} % 8')
+            continue
+
+        if opcode == 3:
+            # jnz
+            print('    if a != 0:')
+            print(f'        goto LABEL{operand}')
+            continue
+
+        if opcode == 4:
+            # bxc
+            print(f'    b = b ^ c')
+            continue
+
+        if opcode == 5:
+            # out
+            print(f'    out.append({combo(operand)} % 8)')
+            continue
+
+        if opcode == 6:
+            # bdv
+            print(f'    b = a // (2 ** {combo(operand)})')
+            continue
+
+        if opcode == 7:
+            # cdv
+            print(f'    c = a // (2 ** {combo(operand)})')
+            continue
+
+    answer = int(input('Please decompile, analyze, and give the answer: '))
+
+    assert(list(run_prog(answer, 0, 0, prog)) == list(prog))
+    return answer
+
 def part2(s):
-    pass
+    answer = solve_manual(s)
+
+    lib.aoc.give_answer(2024, 17, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 17)
 part1(INPUT)
