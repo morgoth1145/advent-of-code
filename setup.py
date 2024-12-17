56bb7682ceb92cd7a243bc8f7816b68104339a31
Patrick Hogg <phogg@novamoon.net>
2024-12-16 22:14:52 -0500

    Update template to include a note for increasing the recursion limit
    Just in case recursion goes wild I would like to be able to quickly increase the recursion limit without needing to google it so having it noted in the template is going to be useful.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ import math
 import parse
 import re
 import sympy # sympy.parse_expr, sympy.solve, sympy.Eq
+import sys # sys.setrecursionlimit(1000000)
 import z3 # x = z3.Int('x'); x < 0; (x-1) >= 0
 # z3.If(x >= 0, x, -x); z3.And(); z3.Or(); z3.Not()
 # s = z3.Solver(); solver.add(constraint); s.check(); s.model()[x].as_long()
