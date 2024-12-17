b1457bb9c3f5ab244b9f8ff5f99e25409ddf9684
Patrick Hogg <phogg@novamoon.net>
2024-12-17 02:21:52 -0500

    2024 Rewrite Day 17
    - Simplify the program VM. It was written significantly more verbosely than required initially.
    - Write a generic reverse solver for part 2. It turns out to be much simpler than I first thought: So long as we assume that the program loops until a is 0 and that it consumes a in chunks of 3 bits we can simply construct a 3 bits at a time and only keep the candidates around that match the tail of the output that we're targeting. This requires no extra special knowledge of the program since it is run in full at each iteration!

--- 2024/17/solution.py
+++ 2024/17/solution.py
@@ -1,24 +1,22 @@
 import lib.aoc
 
-def parse_input(s):
+def parse_prog(s):
     registers, prog = s.split('\n\n')
 
-    prog = tuple(map(int, prog.split(': ')[1].split(',')))
-    a, b, c = tuple(int(l.split(': ')[1])
-                    for l in registers.splitlines())
+    registers = list(int(l.split(': ')[1])
+                     for l in registers.splitlines())
+    prog = list(map(int, prog.split(': ')[1].split(',')))
 
-    return (a, b, c), prog
+    return registers, prog
 
-def run_prog(a, b, c, prog):
-    prog = prog[:]
-
+def exec_prog(prog, a, b, c):
     ip = 0
 
     out = []
 
     while ip < len(prog):
         def combo(operand):
-            if operand in (0, 1, 2, 3):
+            if 0 <= operand <= 3:
                 return operand
             if operand == 4:
                 return a
@@ -33,144 +31,65 @@ def run_prog(a, b, c, prog):
 
         if opcode == 0:
             # adv
-            res = a // (2**combo(operand))
-            a = res
-            ip += 2
-            continue
-
-        if opcode == 1:
+            a = a >> combo(operand)
+        elif opcode == 1:
             # bxl
-            res = b ^ operand
-            b = res
-            ip += 2
-            continue
-
-        if opcode == 2:
+            b = b ^ operand
+        elif opcode == 2:
             # bst
             b = combo(operand) % 8
-            ip += 2
-            continue
-
-        if opcode == 3:
+        elif opcode == 3:
             # jnz
-            if a == 0:
-                ip += 2
-            else:
+            if a != 0:
                 ip = operand
-            continue
-
-        if opcode == 4:
+                continue
+        elif opcode == 4:
             # bxc
             b = b ^ c
-            ip += 2
-            continue
-
-        if opcode == 5:
+        elif opcode == 5:
             # out
             out.append(combo(operand) % 8)
-            ip += 2
-            continue
-
-        if opcode == 6:
+        elif opcode == 6:
             # bdv
-            res = a // (2**combo(operand))
-            b = res
-            ip += 2
-            continue
-
-        if opcode == 7:
+            b = a >> combo(operand)
+        elif opcode == 7:
             # cdv
-            res = a // (2**combo(operand))
-            c = res
-            ip += 2
-            continue
+            c = a >> combo(operand)
+
+        ip += 2
 
     return out
 
 def part1(s):
-    (a, b, c), prog = parse_input(s)
-
-    out = run_prog(a, b, c, prog)
-    answer = ','.join(map(str, out))
+    registers, prog = parse_prog(s)
+    answer = ','.join(map(str, exec_prog(prog, *registers)))
 
     lib.aoc.give_answer(2024, 17, 1, answer)
 
-def solve_manual(s):
-    _, prog = parse_input(s)
-
-    def combo(operand):
-        if operand in (0, 1, 2, 3):
-            return str(operand)
-        if operand == 4:
-            return 'a'
-        if operand == 5:
-            return 'b'
-        if operand == 6:
-            return 'c'
-        print(f'Bad combo operand {operand}!')
-        assert(False)
-
-    print('a = 0')
-    print('b = 0')
-    print('c = 0')
-    print()
-
-    ip = 0
-
-    for ip in range(0, len(prog), 2):
-        print(f'LABEL{ip}:')
-
-        opcode = prog[ip]
-        operand = prog[ip+1]
-
-        if opcode == 0:
-            # adv
-            print(f'    a = a // (2 ** {combo(operand)})')
-            continue
-
-        if opcode == 1:
-            # bxl
-            print(f'    b = b ^ {operand}')
-            continue
-
-        if opcode == 2:
-            # bst
-            print(f'    b = {combo(operand)} % 8')
-            continue
-
-        if opcode == 3:
-            # jnz
-            print('    if a != 0:')
-            print(f'        goto LABEL{operand}')
-            continue
-
-        if opcode == 4:
-            # bxc
-            print(f'    b = b ^ c')
-            continue
-
-        if opcode == 5:
-            # out
-            print(f'    out.append({combo(operand)} % 8)')
-            continue
-
-        if opcode == 6:
-            # bdv
-            print(f'    b = a // (2 ** {combo(operand)})')
-            continue
-
-        if opcode == 7:
-            # cdv
-            print(f'    c = a // (2 ** {combo(operand)})')
-            continue
-
-    answer = int(input('Please decompile, analyze, and give the answer: '))
-
-    assert(list(run_prog(answer, 0, 0, prog)) == list(prog))
-    return answer
-
 def part2(s):
-    answer = solve_manual(s)
+    _, prog = parse_prog(s)
+
+    # Assume that the program is structured to loop until a is 0
+    # Also assume that a is consumed 3 bits at a time
+
+    candidates = [0]
+
+    expected = []
+
+    for target in prog[::-1]:
+        expected.insert(0, target)
+
+        valid = []
+
+        for a in candidates:
+            for a_cand in range(a*8, a*8+8):
+                if exec_prog(prog, a_cand, 0, 0) == expected:
+                    valid.append(a_cand)
+
+        assert(len(valid) > 0)
+        candidates = valid
+
+    answer = min(candidates)
 
     lib.aoc.give_answer(2024, 17, 2, answer)
 
