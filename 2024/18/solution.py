cddebe07c0e493675e14a711c11c42cf7f8f0d83
Patrick Hogg <phogg@novamoon.net>
2024-12-18 21:08:41 -0500

    2024 Rewrite Day 18 Part 2
    Someone on reddit had an interesting idea: Do a normal BFS, but keep track of the last good time for each tile. This allows for a pretty quick calculation of when access to the end tile finally breaks thus which falling byte breaks it.
    
    Furthermore, this can be optimized by applying the same idea as dijkstra's: Flood fill from the last accessible tiles before tiles which get blocked sooner. This way less of the space needs to be explored and each tile is only processed once.
    
    All told, this ends up computing the part 2 answer in less time than part 1, though a big part of that is part 1 using a more generic dijkstra's implementation with some extra overhead. It's also not too complex (much simpler than the union-find approach I was originally exploring)!

--- 2024/18/solution.py
+++ 2024/18/solution.py
@@ -1,3 +1,5 @@
+import heapq
+
 import lib.algorithms
 import lib.aoc
 import lib.graph
@@ -30,12 +32,52 @@ def part1(s):
 
     lib.aoc.give_answer(2024, 18, 1, answer)
 
-def part2(s):
+def part2_bisect(s):
     def predicate(idx):
         return find_num_steps(s, 70, idx+1) == -1
 
     first_bad_idx = lib.algorithms.bisect(0, len(s.splitlines()), predicate)
     answer = s.splitlines()[first_bad_idx]
+
+    lib.aoc.give_answer(2024, 18, 2, answer)
+
+def part2(s):
+    MAX_GRID_COORD = 70
+
+    bad_bytes = list(tuple(map(int, l.split(',')))
+                     for l in s.splitlines())
+    bad_byte_times = {pos: time
+                      for time, pos in enumerate(bad_bytes)}
+
+    grid = {}
+
+    start = (0, 0)
+    end = (MAX_GRID_COORD, MAX_GRID_COORD)
+
+    todo = [(-len(bad_bytes), start)]
+    grid[start] = len(bad_bytes)
+
+    while todo:
+        neg_time, (x, y) = heapq.heappop(todo)
+        if (x, y) == end:
+            break
+        time = -neg_time
+
+        for nx, ny in [(x-1, y),
+                       (x+1, y),
+                       (x, y-1),
+                       (x, y+1)]:
+            if 0 <= nx <= MAX_GRID_COORD and 0 <= ny <= MAX_GRID_COORD:
+                bad_time = bad_byte_times.get((nx, ny), time+1)
+                last_good_time = min(time, bad_time-1)
+
+                known_good_time = grid.get((nx, ny))
+                if known_good_time is None or known_good_time < last_good_time:
+                    grid[nx,ny] = last_good_time
+                    heapq.heappush(todo, (-last_good_time, (nx, ny)))
+
+    last_good_idx = grid[end]
+    answer = ','.join(map(str, bad_bytes[last_good_idx+1]))
 
     lib.aoc.give_answer(2024, 18, 2, answer)
 
