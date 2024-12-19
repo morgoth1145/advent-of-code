fb0d76e92e450b0a333d6f8e1efbfb53fce64f9b
Patrick Hogg <phogg@novamoon.net>
2024-12-19 00:10:15 -0500

    2024 Day 19 Part 2
    Ah, and now we're counting the ways that a design can be constructed. A pretty trivial modification to the part 1 solution.
    
    Rank 655. Jumped up a lot, but still pretty low thanks to the regex sidetrack in part 1. (Even without that there's no way I would have leaderboarded with the times on the leaderboard, most of which are quite unrealistic.)

--- 2024/19/solution.py
+++ 2024/19/solution.py
@@ -32,7 +32,24 @@ def part1(s):
     lib.aoc.give_answer(2024, 19, 1, answer)
 
 def part2(s):
-    pass
+    a, b = parse_input(s)
+
+    @functools.cache
+    def count_ways(remaining):
+        if len(remaining) == 0:
+            return 1
+
+        ways = 0
+
+        for prefix in a:
+            if remaining.startswith(prefix):
+                ways += count_ways(remaining[len(prefix):])
+
+        return ways
+
+    answer = sum(map(count_ways, b))
+
+    lib.aoc.give_answer(2024, 19, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 19)
 part1(INPUT)
