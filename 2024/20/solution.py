06786d5ec7497a7ef15005fe159322a0761583be
Patrick Hogg <phogg@novamoon.net>
2024-12-20 15:03:10 -0500

    2024 Optimize Day 20
    Using some clever array slicing it is possible to compute batches of cheats which have moved the same amount. As this moves a bunch of looping down into the C layer it ends up being faster, computing the part 2 solution roughly 20x faster. In fact, it's so efficient that this code can now compute the answer for cheats up to 100 picoseconds in about half a second, nearly 2x as fast as the original solve code!
    
    The key idea is to precompute the "start" and "end" times then shift the arrays based on the "cheat delta" and add them together (plus the cheat time) to get the overall path runtime. Then some vectorized comparisons against the normal solve time and minimum required savings for each cheat delta gets the answer! It takes a little bit more time to think through, but the speedup is well worth it.

--- 2024/20/solution.py
+++ 2024/20/solution.py
@@ -1,3 +1,5 @@
+import numpy
+
 import lib.aoc
 import lib.graph
 import lib.grid
@@ -14,40 +16,54 @@ def solve(s, max_cheat_time, minimum_savings):
 
     graph = lib.graph.make_lazy_graph(neighbor_fn)
 
-    def precompute_distances(start):
-        distances = dict(lib.graph.all_reachable(graph, start))
-        distances[start] = 0
-        return distances
+    def make_dist_arr(start):
+        # Default everywhere to a large value to "poison" walls
+        arr = numpy.zeros((grid.width, grid.height)) + len(s)
+
+        for pos, time in lib.graph.all_reachable(graph, start):
+            arr[pos] = time
+        arr[start] = 0 # Not included in all_reachable
+
+        return arr
 
     start = grid.find('S')
+    end = grid.find('E')
 
-    cheat_starts = precompute_distances(start)
-    remaining_time = precompute_distances(grid.find('E'))
+    a_start = make_dist_arr(start)
+    a_end = make_dist_arr(end)
 
-    normal_time = remaining_time[start]
+    normal_time = a_start[end]
+
+    def cheat_slices(delta, size):
+        if delta > 0:
+            return slice(delta, size), slice(0, size-delta)
+        else:
+            return slice(0, size+delta), slice(-delta, size)
+
+    def cheat(dx, dy):
+        extra_time = abs(dx) + abs(dy)
+
+        start_x_slice, end_x_slice = cheat_slices(dx, grid.width)
+        start_y_slice, end_y_slice = cheat_slices(dy, grid.height)
+
+        return (a_start[start_x_slice, start_y_slice] +
+                a_end[end_x_slice, end_y_slice] +
+                extra_time)
 
     answer = 0
 
-    def cheat(pos):
-        x, y = pos
-        for end_y in range(max(0, y-max_cheat_time),
-                           min(y+max_cheat_time+1, grid.height)):
-            y_dist = abs(y-end_y)
-            rem_dist = max_cheat_time - y_dist
-            for end_x in range(max(0, x-rem_dist),
-                               min(x+rem_dist+1, grid.height)):
-                end_time = remaining_time.get((end_x, end_y))
-                if end_time is None:
-                    continue
+    for dx in range(-max_cheat_time, max_cheat_time+1):
+        if abs(dx) >= grid.width:
+            # In case of absurd cheat times, make sure the slicing is sane!
+            continue
+        rem_cheat = max_cheat_time - abs(dx)
+        for dy in range(-rem_cheat, rem_cheat+1):
+            if abs(dy) >= grid.height:
+                # In case of absurd cheat times, make sure the slicing is sane!
+                continue
+            savings = normal_time - cheat(dx, dy)
 
-                yield abs(x-end_x) + y_dist + end_time
-
-    for cheat_start, time in cheat_starts.items():
-        for cheat_time in cheat(cheat_start):
-            total_time = time + cheat_time
-
-            if normal_time - total_time >= minimum_savings:
-                answer += 1
+            answer += numpy.count_nonzero(savings >= minimum_savings)
 
     return answer
 
