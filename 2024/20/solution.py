6647ff77d90e5cf47a05b5dfc3f074fac90db0e7
Patrick Hogg <phogg@novamoon.net>
2024-12-20 02:03:46 -0500

    2024 Refactor Day 20
    - Extract the part 2 solve algorithm to solve(s) an generalize it so that part 1 can reuse it as well.
    - Simplify the cheat code to simply generate a diamond of possible cheat locations rather than doing set operations. The set operations add up and aren't worth it since the cheat end locations can be directly computed!
    - Otherwise simplify the solve code, both making it simpler and cheaper/faster

--- 2024/20/solution.py
+++ 2024/20/solution.py
@@ -2,143 +2,62 @@ import lib.aoc
 import lib.graph
 import lib.grid
 
-def part1(s):
+def solve(s, max_cheat_time, minimum_savings):
     grid = lib.grid.FixedGrid.parse(s)
-
-    start = grid.find('S')
-    end = grid.find('E')
 
     def neighbor_fn(pos):
         for n in grid.neighbors(*pos):
-            nv = grid[n]
-
-            if nv == '#':
+            if grid[n] == '#':
                 continue
 
             yield n, 1
 
     graph = lib.graph.make_lazy_graph(neighbor_fn)
 
-    normal_time = lib.graph.find_shortest_path_length(graph, start, end)
+    def precompute_distances(start):
+        distances = dict(lib.graph.all_reachable(graph, start))
+        distances[start] = 0
+        return distances
+
+    start = grid.find('S')
+
+    cheat_starts = precompute_distances(start)
+    remaining_time = precompute_distances(grid.find('E'))
+
+    normal_time = remaining_time[start]
 
     answer = 0
 
-    def cheat_options(pos):
-        cands = set()
-        for n in grid.neighbors(*pos):
-            cands.update(grid.neighbors(*n))
+    def cheat(pos):
+        x, y = pos
+        for end_y in range(max(0, y-max_cheat_time),
+                           min(y+max_cheat_time+1, grid.height)):
+            y_dist = abs(y-end_y)
+            rem_dist = max_cheat_time - y_dist
+            for end_x in range(max(0, x-rem_dist),
+                               min(x+rem_dist+1, grid.height)):
+                end_time = remaining_time.get((end_x, end_y))
+                if end_time is None:
+                    continue
 
-        for n2 in cands:
-            if n2 == pos or grid[n2] == '#':
-                continue
-            yield n2
+                yield abs(x-end_x) + y_dist + end_time
 
-    remaining_time = {}
-    remaining_time[end] = 0
+    for cheat_start, time in cheat_starts.items():
+        for cheat_time in cheat(cheat_start):
+            total_time = time + cheat_time
 
-    for pos, dist in lib.graph.all_reachable(graph, end):
-        remaining_time[pos] = dist
-
-    assert(end in remaining_time)
-
-    cheat_starts = {}
-    cheat_starts[start] = 0
-
-    for pos, dist in lib.graph.all_reachable(graph, start):
-        cheat_starts[pos] = dist
-
-    assert(start in cheat_starts)
-
-    for cheat_start, dist in cheat_starts.items():
-        for cheat_next in cheat_options(cheat_start):
-            sub_dist = remaining_time.get(cheat_next)
-
-            total_dist = dist + 2 + sub_dist
-
-            savings = normal_time - total_dist
-
-            if savings <= 0:
-                continue
-
-            if total_dist + 100 <= normal_time:
-                assert(savings >= 100)
+            if normal_time - total_time >= minimum_savings:
                 answer += 1
-            else:
-                assert(savings < 100)
+
+    return answer
+
+def part1(s):
+    answer = solve(s, 2, 100)
 
     lib.aoc.give_answer(2024, 20, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s)
-
-    start = grid.find('S')
-    end = grid.find('E')
-
-    def neighbor_fn(pos):
-        for n in grid.neighbors(*pos):
-            nv = grid[n]
-
-            if nv == '#':
-                continue
-
-            yield n, 1
-
-    graph = lib.graph.make_lazy_graph(neighbor_fn)
-
-    normal_time = lib.graph.find_shortest_path_length(graph, start, end)
-
-    answer = 0
-
-    def cheat_options(pos):
-        cheat_ends = {}
-        all_cands = set()
-        cands = set()
-        cands.add(pos)
-        for time in range(20):
-            new_cands = set()
-            for p in cands:
-                cheat_ends[p] = time
-                new_cands.update(grid.neighbors(*p))
-            cands = new_cands - all_cands
-            all_cands.update(cands)
-        for p in cands:
-            cheat_ends[p] = 20
-        return cheat_ends.items()
-
-    remaining_time = {}
-    remaining_time[end] = 0
-
-    for pos, dist in lib.graph.all_reachable(graph, end):
-        remaining_time[pos] = dist
-
-    assert(end in remaining_time)
-
-    cheat_starts = {}
-    cheat_starts[start] = 0
-
-    for pos, dist in lib.graph.all_reachable(graph, start):
-        cheat_starts[pos] = dist
-
-    assert(start in cheat_starts)
-
-    for cheat_start, dist in cheat_starts.items():
-        for cheat_next, cheat_time in cheat_options(cheat_start):
-            sub_dist = remaining_time.get(cheat_next)
-            if sub_dist is None:
-                continue
-
-            total_dist = dist + cheat_time + sub_dist
-
-            savings = normal_time - total_dist
-
-            if savings <= 0:
-                continue
-
-            if total_dist + 100 <= normal_time:
-                assert(savings >= 100)
-                answer += 1
-            else:
-                assert(savings < 100)
+    answer = solve(s, 20, 100)
 
     lib.aoc.give_answer(2024, 20, 2, answer)
 
