5b40f07fb315652b3c35f63de04e5f0d3667a60a
Patrick Hogg <phogg@novamoon.net>
2024-12-20 00:41:57 -0500

    2024 Day 20 Part 1
    I...did not do well here. It's an interesting twist, but I got stuck going down a couple of completely wrong rabbit holes (trying to enumerate all paths and count the cheats, then just enumerating the number of paths *with* cheating that reach the end). It took me nearly 20 minutes to even begin to thinkin the right terms!
    
    Then when I did realize that we need to enumerate the cheat points to and invoke cheating from there, it took me an annoying amount of time to realize that Dijkstra's from that point would be way too slow and we should just enumerate all remaining times from the end as well!
    
    Finally, I had goofs with my cheat options function where I treated all paths to a cheat as unique even though only the start and end locations matter. That took annoyingly long to find...
    
    Rank 2167. Truly terrible, I really should have figured this out faster...

---
+++ 2024/20/solution.py
@@ -0,0 +1,76 @@
+import lib.aoc
+import lib.graph
+import lib.grid
+
+def part1(s):
+    grid = lib.grid.FixedGrid.parse(s)
+
+    start = grid.find('S')
+    end = grid.find('E')
+
+    def neighbor_fn(pos):
+        for n in grid.neighbors(*pos):
+            nv = grid[n]
+
+            if nv == '#':
+                continue
+
+            yield n, 1
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+
+    normal_time = lib.graph.find_shortest_path_length(graph, start, end)
+
+    answer = 0
+
+    def cheat_options(pos):
+        cands = set()
+        for n in grid.neighbors(*pos):
+            cands.update(grid.neighbors(*n))
+
+        for n2 in cands:
+            if n2 == pos or grid[n2] == '#':
+                continue
+            yield n2
+
+    remaining_time = {}
+    remaining_time[end] = 0
+
+    for pos, dist in lib.graph.all_reachable(graph, end):
+        remaining_time[pos] = dist
+
+    assert(end in remaining_time)
+
+    cheat_starts = {}
+    cheat_starts[start] = 0
+
+    for pos, dist in lib.graph.all_reachable(graph, start):
+        cheat_starts[pos] = dist
+
+    assert(start in cheat_starts)
+
+    for cheat_start, dist in cheat_starts.items():
+        for cheat_next in cheat_options(cheat_start):
+            sub_dist = remaining_time.get(cheat_next)
+
+            total_dist = dist + 2 + sub_dist
+
+            savings = normal_time - total_dist
+
+            if savings <= 0:
+                continue
+
+            if total_dist + 100 <= normal_time:
+                assert(savings >= 100)
+                answer += 1
+            else:
+                assert(savings < 100)
+
+    lib.aoc.give_answer(2024, 20, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2024, 20)
+part1(INPUT)
+part2(INPUT)
