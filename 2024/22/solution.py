049a84ee86e11db2b565df9c076b5790df0b33e8
Patrick Hogg <phogg@novamoon.net>
2024-12-22 15:57:17 -0500

    2024 Optimize Day 22 Part 2 just a bit more
    It's also possible to use a single "delta key" integer value instead of a tuple/set of indices. This isn't all that hard to compute and is quite a bit friendlier to numpy, getting the solve time down to about 0.6 seconds on my machine.

--- 2024/22/solution.py
+++ 2024/22/solution.py
@@ -27,36 +27,45 @@ def part2(s):
     NUM_TO_GENERATE = 2000
 
     seller_arr = generate_rngs(s, NUM_TO_GENERATE, output_mod=10, output_dtype=numpy.int8)
-    delta_arr = (seller_arr[:,1:] - seller_arr[:,:-1]).astype(numpy.int8)
-
     num_sellers = seller_arr.shape[0]
 
-    handled = numpy.zeros((num_sellers,) + (19,)*4, dtype=numpy.bool_)
+    # Instead of a tuple, represent each delta sequence a as a single int.
+    # Essentially, treat it as a base 19 number!
+    DELTA_KEY_BASE = 19
+    DELTA_KEY_OFFSET = 9 # Shift by 9 to keep delta values positive
+    NUM_DELTA_KEYS = DELTA_KEY_BASE**4
+
+    handled = numpy.zeros((num_sellers, NUM_DELTA_KEYS), dtype=numpy.bool_)
     # Track sell values per-seller initially. Otherwise if two sellers
     # match the same delta string at the same time then they won't both
     # be counted in the totals!
-    sell_values = numpy.zeros((num_sellers,) + (19,)*4, numpy.int8)
+    sell_values = numpy.zeros((num_sellers, NUM_DELTA_KEYS), numpy.int8)
 
     all_sellers_key = numpy.arange(num_sellers)
+    delta_key = numpy.zeros(num_sellers, numpy.int32)
 
-    for i in range(NUM_TO_GENERATE-3):
-        d0 = delta_arr[:,i]
-        d1 = delta_arr[:,i+1]
-        d2 = delta_arr[:,i+2]
-        d3 = delta_arr[:,i+3]
+    last_prices = seller_arr[:,0]
 
-        # Get the old handled flags and update them in the matrix
-        was_handled = handled[all_sellers_key, d0, d1, d2, d3]
-        to_handle = 1 - was_handled
-        handled[all_sellers_key, d0, d1, d2, d3] = True
+    for i in range(NUM_TO_GENERATE):
+        # Drop the most significant "digit", shift, and add the new delta
+        prices = seller_arr[:,i+1]
+        delta = prices - last_prices + DELTA_KEY_OFFSET
+        last_prices = prices
+        delta_key = (delta_key * DELTA_KEY_BASE) % NUM_DELTA_KEYS + delta
 
-        # Extract the prices. Zero out any for sellers that already have this
-        # delta sequence handled.
-        price = seller_arr[:,i+4] * to_handle
+        if i >= 3:
+            # Get the old handled flags and update them in the matrix
+            was_handled = handled[all_sellers_key, delta_key]
+            to_handle = 1 - was_handled
+            handled[all_sellers_key, delta_key] = True
 
-        sell_values[all_sellers_key, d0, d1, d2, d3] += price
+            # Zero out any prices for sellers that already have this
+            # delta sequence handled.
+            price = prices * to_handle
 
-    totals = numpy.zeros((19,)*4, numpy.int64)
+            sell_values[all_sellers_key, delta_key] += price
+
+    totals = numpy.zeros(NUM_DELTA_KEYS, numpy.int64)
 
     for s in range(num_sellers):
         totals += sell_values[s]
