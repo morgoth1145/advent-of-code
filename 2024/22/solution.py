29f8666b76cd029660a60563d9b573651da31d5f
Patrick Hogg <phogg@novamoon.net>
2024-12-22 03:52:39 -0500

    2024 Further optimize (and fix) Day 22
    Vectorizing part 2 is a little complicated, but feasible. The key idea is to handle all sellers at once when computing the delta sequences and tracking the sell prices per seller/sequence in a giant 5D array. (This is done per seller so that two sellers hitting the same sequence at the same time do not conflict with each other.) Then in a second finalization pass each seller's final sell totals can be added to a global total array (4D for all delta sequences) before getting the final answer as the maximum value in the global totals array.
    
    Also, fix a bug where the initial secret value state was not kept by generate_rngs. According to the problem text in part 2 that should be used in computing the first delta!

--- 2024/22/solution.py
+++ 2024/22/solution.py
@@ -1,4 +1,3 @@
-import collections
 import numpy
 
 import lib.aoc
@@ -7,45 +6,62 @@ def generate_rngs(s, num_to_gen, output_mod=16777216):
     rngs = numpy.array(list(map(int, s.splitlines())),
                        dtype=numpy.uint32)
 
-    out = numpy.zeros((len(rngs), num_to_gen), dtype=numpy.int64)
+    out = numpy.zeros((len(rngs), num_to_gen+1), dtype=numpy.int64)
+    out[:,0] = rngs % output_mod
 
     for i in range(num_to_gen):
         rngs = (rngs ^ (rngs << 6)) & 0xffffff
         rngs = (rngs ^ (rngs >> 5)) & 0xffffff
         rngs = (rngs ^ (rngs << 11)) & 0xffffff
 
-        out[:,i] = rngs % output_mod
+        out[:,i+1] = rngs % output_mod
 
     return out
 
 def part1(s):
-    answer = numpy.sum(generate_rngs(s, 2000)[:,-1])
+    answer = generate_rngs(s, 2000)[:,-1].sum()
 
     lib.aoc.give_answer(2024, 22, 1, answer)
 
-def make_delta_to_price_map(prices, deltas):
-    c = {}
-
-    delta_sequences = zip(deltas, deltas[1:], deltas[2:], deltas[3:])
-    sell_prices = prices[4:]
-
-    for key, price in zip(delta_sequences, sell_prices):
-        if key not in c:
-            c[key] = price
-
-    return c
-
 def part2(s):
-    seller_arr = generate_rngs(s, 2000, output_mod=10)
+    NUM_TO_GENERATE = 2000
+
+    seller_arr = generate_rngs(s, NUM_TO_GENERATE, output_mod=10)
     delta_arr = seller_arr[:,1:] - seller_arr[:,:-1]
 
-    c = collections.Counter()
+    num_sellers = seller_arr.shape[0]
 
-    for i in range(seller_arr.shape[0]):
-        c += make_delta_to_price_map(seller_arr[i],
-                                     delta_arr[i])
+    handled = numpy.zeros((num_sellers,) + (19,)*4, dtype=numpy.bool_)
+    # Track sell values per-seller initially. Otherwise if two sellers
+    # match the same delta string at the same time then they won't both
+    # be counted in the totals!
+    sell_values = numpy.zeros((num_sellers,) + (19,)*4, numpy.int64)
 
-    answer = max(c.values())
+    all_sellers_key = numpy.arange(num_sellers)
+
+    for i in range(NUM_TO_GENERATE-3):
+        d0 = delta_arr[:,i]
+        d1 = delta_arr[:,i+1]
+        d2 = delta_arr[:,i+2]
+        d3 = delta_arr[:,i+3]
+
+        # Get the old handled flags and update them in the matrix
+        was_handled = handled[all_sellers_key, d0, d1, d2, d3]
+        to_handle = 1 - was_handled
+        handled[all_sellers_key, d0, d1, d2, d3] = True
+
+        # Extract the prices. Zero out any for sellers that already have this
+        # delta sequence handled.
+        price = seller_arr[:,i+4] * to_handle
+
+        sell_values[all_sellers_key, d0, d1, d2, d3] += price
+
+    totals = numpy.zeros((19,)*4, numpy.int64)
+
+    for s in range(num_sellers):
+        totals += sell_values[s]
+
+    answer = totals.max()
 
     lib.aoc.give_answer(2024, 22, 2, answer)
 
