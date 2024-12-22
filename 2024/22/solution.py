e1d69af1fae6fc73e4e6d2bc0c344adef1d3927e
Patrick Hogg <phogg@novamoon.net>
2024-12-22 00:20:25 -0500

    2024 Day 22 Part 2
    Interesting, so the RNG sequences are a source for prices (how many bannanas we'll get for info), and we have monkeys which will buy bannanas when they see a certain price delta sequence.
    
    Initially I tried brute forcing, but that ended up running waaaay longer than I expected. (My crude estimate is that it would have taken at least 4.5 days to complete!) So instead, it is much smarter to precompute the number of bannanas that we'll get for all delta sequences for a given seller in one shot. This way we only process each seller once, saving a ton of time. Plus, collections.Counter() helpfully allows you to add two counters together to merge them, so getting the final answer is trivial!
    
    Rank 333. Missed the leaderboard by 8 minutes and 10 seconds, I don't think I had any shot of leaderboarding even if I did the right solution first here...

--- 2024/22/solution.py
+++ 2024/22/solution.py
@@ -1,3 +1,5 @@
+import collections
+
 import lib.aoc
 
 def generate_random(seed):
@@ -28,8 +30,41 @@ def part1(s):
 
     lib.aoc.give_answer(2024, 22, 1, answer)
 
+def get_seller_prices(seed):
+    generator = generate_random(seed)
+    prices = []
+    while len(prices) < 2000:
+        prices.append(next(generator) % 10)
+    return prices
+
+def make_sell_map(prices):
+    c = collections.Counter()
+
+    deltas = [b-a for a,b in zip(prices, prices[1:])]
+
+    for i in range(len(prices)):
+        if i+4 >= len(deltas):
+            break
+        key = tuple(deltas[i+dd] for dd in range(4))
+        if key in c:
+            continue
+        c[key] = prices[i+4]
+
+    return c
+
 def part2(s):
-    pass
+    nums = list(map(int, s.splitlines()))
+
+    sellers = list(map(get_seller_prices, nums))
+
+    c = collections.Counter()
+
+    for prices in sellers:
+        c += make_sell_map(prices)
+
+    answer = max(c.values())
+
+    lib.aoc.give_answer(2024, 22, 2, answer)
 
 INPUT = lib.aoc.get_input(2024, 22)
 part1(INPUT)
