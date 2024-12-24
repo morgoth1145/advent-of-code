0ebf0e4351464505590580855d4a85b670441645
Patrick Hogg <phogg@novamoon.net>
2024-12-24 03:55:17 -0500

    2024 Rewrite Day 24 Part 2
    I'm never satisfied until I have a programmatic solution, so here's one for part 2. In some ways it has similar logic to the auto-renaming to detect what gates are what in the binary adder and to find errors. It's definitely a bit messy and there may be smarter/better ways to do this, but it at least works for my input. Not sure if other inputs may have edge cases that I missed here.

--- 2024/24/solution.py
+++ 2024/24/solution.py
@@ -66,12 +66,12 @@ def part1(s):
 
     lib.aoc.give_answer(2024, 24, 1, answer)
 
-def dump_gates(signals, gates):
+def dump_gates_pretty(gates):
     renames = {}
 
     @functools.cache
     def check_rename(wire):
-        if wire in signals:
+        if wire not in gates:
             return wire
         if wire.startswith('z'):
             return wire
@@ -131,7 +131,7 @@ def dump_gates(signals, gates):
         return wire
     @functools.cache
     def dump(wire):
-        if wire in signals:
+        if wire not in gates:
             return wire
         left, op, right = gates[wire]
         left = dump(left)
@@ -141,7 +141,8 @@ def dump_gates(signals, gates):
         print(f'{pad(wire)} = {pad(left)} {pad(op, 5)} {pad(right)}')
         return wire
 
-    num_bits = max(int(s[1:]) for s in signals)+1
+    num_bits = max(int(s[1:]) for s in gates
+                   if s[0] == 'z')
 
     for i in range(num_bits+1):
         s = 'z' + str(i).zfill(2)
@@ -154,13 +155,22 @@ def dump_gates(signals, gates):
     print()
     print()
 
-def part2(s):
+def validate_fixed_gates(signals, gates):
+    x = extract_num('x', signals)
+    y = extract_num('y', signals)
+
+    target = x+y
+
+    assert(run_wires(signals, gates) == target)
+    print('Success! The swaps fixed the system, numbers added properly!')
+
+def part2_manual(s):
     signals, gates = parse_input(s)
 
     swaps = []
 
     while len(swaps) < 8:
-        dump_gates(signals, gates)
+        dump_gates_pretty(gates)
         print('Please analyze and find a pair of wires to swap.')
         first = input('First wire in the swap: ')
         second = input('Second wire in the swap: ')
@@ -168,13 +178,105 @@ def part2(s):
         swaps.append(first)
         swaps.append(second)
 
-    x = extract_num('x', signals)
-    y = extract_num('y', signals)
+    validate_fixed_gates(signals, gates)
 
-    target = x+y
+    answer = ','.join(sorted(swaps))
 
-    assert(run_wires(signals, gates) == target)
-    print('Success! The swaps fixed the system and it added the numbers properly!')
+    lib.aoc.give_answer(2024, 24, 2, answer)
+
+def part2(s):
+    signals, gates = parse_input(s)
+
+    num_bits = max(int(s[1:]) for s in signals)+1
+
+    def find_error():
+        # Restart from scratch each time since swapping occurs
+        op_to_wire = {}
+
+        xy_xors = {}
+        xy_ands = {}
+        for wire, (left, op, right) in gates.items():
+            op_to_wire[left, op, right] = wire
+            if ((left[0] == 'x' and right[0] == 'y') or
+                (left[0] == 'y' and right[0] == 'x')):
+                assert(left[1:] == right[1:])
+                zid = int(left[1:])
+                if op == 'XOR':
+                    assert(zid not in xy_xors)
+                    xy_xors[zid] = wire
+                elif op == 'AND':
+                    assert(zid not in xy_ands)
+                    xy_ands[zid] = wire
+                else:
+                    assert(False)
+
+        assert(len(xy_xors) == num_bits)
+        assert(len(xy_ands) == num_bits)
+
+        z_carry = {}
+
+        for i in range(num_bits+1):
+            xid = 'x' + str(i).zfill(2)
+            yid = 'y' + str(i).zfill(2)
+            zid = 'z' + str(i).zfill(2)
+            if i == 0:
+                if xy_xors[0] != zid:
+                    op_target = (xid, 'XOR', yid)
+                    if op_target not in op_to_wire:
+                        # It must be reversed!
+                        op_target = op_target[::-1]
+                    # Fix the first output wire!
+                    return zid, op_to_wire[op_target]
+                z_carry[0] = xy_ands[0]
+            else:
+                zout_target = (z_carry[i-1], 'XOR', xy_xors[i])
+                if zout_target not in op_to_wire:
+                    zout_target = zout_target[::-1]
+                if zout_target not in op_to_wire:
+                    # We need to swap z_carry[i-1] into the right position!
+                    current_left, current_op, current_right = gates[zid]
+                    assert(current_op == 'XOR')
+                    if current_left == xy_xors[i]:
+                        # Left is good, swap the right
+                        return current_right, z_carry[i-1]
+                    elif current_right == xy_xors[i]:
+                        # Right is good, swap the left
+                        return current_left, z_carry[i-1]
+                    elif current_left == z_carry[i-1]:
+                        # Left is good, swap the right
+                        return current_right, xy_xors[i]
+                    elif current_right == z_carry[i-1]:
+                        # Right is good, swap the left
+                        return current_left, xy_xors[i]
+                    print('Cannot fix this automatically (yet)')
+                    assert(False)
+                zout = op_to_wire[zout_target]
+                if zout != zid:
+                    # The output wire is misplaced! Fix it
+                    return zid, zout
+
+                carry_and_target = (z_carry[i-1], 'AND', xy_xors[i])
+                if carry_and_target not in op_to_wire:
+                    carry_and_target = carry_and_target[::-1]
+                carry_and = op_to_wire[carry_and_target]
+
+                carry_target = (carry_and, 'OR', xy_ands[i])
+                if carry_target not in op_to_wire:
+                    carry_target = carry_target[::-1]
+                carry = op_to_wire[carry_target]
+
+                z_carry[i] = carry
+
+        return None
+
+    swaps = []
+    for i in range(4):
+        first, second = find_error()
+        print(f'Swapping {first} with {second}...')
+        gates[first], gates[second] = gates[second], gates[first]
+        swaps += [first, second]
+
+    validate_fixed_gates(signals, gates)
 
     answer = ','.join(sorted(swaps))
 
