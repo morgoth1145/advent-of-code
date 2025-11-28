1fbbbc762f43b1c821707f208f5cb4dd25af2165
Patrick Hogg <phogg@novamoon.net>
2025-11-28 17:06:59 -0500

    WIP - Cherry pick 2024 lib updates. (I can't be bothered to finish up 2024 WIP stuff right now...)

--- lib/graph.py
+++ lib/graph.py
@@ -16,6 +16,11 @@ def topological_sort(graph):
     for key in graph.keys():
         yield from impl(key)
 
+def topological_sort_root_first(graph):
+    '''Outputs graph nodes in topological order, going from the root to the leaf
+    NOTE: THIS DOES NOT WORK FOR CYCLIC GRAPHS!'''
+    return list(topological_sort(graph))[::-1]
+
 def longest_minimal_path_length(graph, start):
     '''Returns the destination and length of the longest minimal path from
     start to somewhere in the graph.
@@ -109,6 +114,8 @@ def dijkstra_length_fuzzy_end(graph, start, end_fn, heuristic=None):
                                    neighbor_node))
 
     return -1
+# Alias for ease
+find_shortest_path_length_fuzzy_end = dijkstra_length_fuzzy_end
 
 def dijkstra_length(graph, start, end, heuristic=None):
     '''Returns the length of the path from start to end in the graph.
@@ -136,6 +143,202 @@ def dijkstra_length(graph, start, end, heuristic=None):
         return state in end_candidates
 
     return dijkstra_length_fuzzy_end(graph, start, end_fn, heuristic)
+# Alias for ease
+find_shortest_path_length = dijkstra_length
+
+# TODO: Deduplicate with dijkstra_length/dijkstra_length_fuzzy_end
+def make_shortest_path_graph_fuzzy_end(graph, start, end_fn, heuristic=None):
+    '''Returns a graph of the states and edges which comprise the shortest
+    paths.
+    Returns path_graph, start(s), shortest_path_distance, total_num_paths.
+    Any path from a start to any leaf (terminal) state in path_graph is a
+    shortest path in the original graph.
+    graph[node] must return a list of (neighbor, distance) pairs
+
+    Arguments:
+    start - Either the starting state or a list of starting states
+    end_fn - Function accepting a state. Returns True if this is an end state
+    and False otherwise
+    heuristic - If supplied, provides an estimate of the remaining distance
+    from a given node to the end
+    '''
+    if heuristic is None:
+        heuristic = lambda n: 0
+
+    if not isinstance(start, list):
+        start = [start]
+
+    queue = [(heuristic(s), 0, s, None) for s in start]
+    heapq.heapify(queue)
+    handled = set()
+
+    class Sources:
+        def __init__(self, dist):
+            self.dist = dist
+            self.src_nodes = []
+            self.num_src_paths = 0
+
+        def add_source(self, src_node, src_node_info):
+            if src_node is None:
+                # Start state
+                self.num_src_paths = 1
+            else:
+                self.src_nodes.append(src_node)
+                self.num_src_paths += src_node_info.num_src_paths
+
+    state_to_sources = {}
+    best_dist = None
+    total_num_paths = 0
+    end_states = set()
+
+    while len(queue) > 0:
+        _, current_dist, current_node, src_node = heapq.heappop(queue)
+
+        if best_dist is not None and current_dist > best_dist:
+            break # No more paths exist
+
+        src_info = state_to_sources.get(current_node)
+        if src_info is None or src_info.dist > current_dist:
+            src_info = state_to_sources[current_node] = Sources(current_dist)
+        elif src_info.dist < current_dist:
+            continue # Too expensive!
+        src_info.add_source(src_node, state_to_sources.get(src_node))
+
+        if end_fn(current_node):
+            key = (current_node, current_dist)
+            if key not in end_states:
+                best_dist = current_dist
+                end_states.add((current_node, current_dist))
+                total_num_paths += src_info.num_src_paths
+            continue
+
+        if current_node in handled:
+            continue
+        handled.add(current_node)
+
+        for neighbor_node, neighbor_dist in graph[current_node]:
+            if neighbor_node in handled:
+                continue # Already handled, can't be cheaper!
+
+            new_dist = current_dist + neighbor_dist
+            heapq.heappush(queue, (heuristic(neighbor_node) + new_dist,
+                                   new_dist,
+                                   neighbor_node,
+                                   current_node))
+
+    shortest_path_graph = {}
+    start_states = []
+
+    to_handle = list(end_states)
+
+    while to_handle:
+        state, dist = to_handle.pop()
+
+        src_info = state_to_sources[state]
+
+        if 0 == len(src_info.src_nodes):
+            assert(src_info.dist == 0)
+            start_states.append(state)
+            continue
+
+        for src in src_info.src_nodes:
+            src_dist = state_to_sources[src].dist
+            src_links = shortest_path_graph.get(src)
+            if src_links is None:
+                src_links = shortest_path_graph[src] = []
+                to_handle.append((src, src_dist))
+            src_links.append((state, dist-src_dist))
+
+    # Ensure that terminal states have an empty list of neighbors.
+    for state, _ in end_states:
+        shortest_path_graph[state] = []
+
+    return shortest_path_graph, start_states, best_dist, total_num_paths
+
+def make_shortest_path_graph(graph, start, end, heuristic=None):
+    '''Returns a graph of the states and edges which comprise the shortest
+    paths.
+    Returns path_graph, start(s), shortest_path_distance, total_num_paths.
+    Any path from a start to any leaf (terminal) state in path_graph is a
+    shortest path in the original graph.
+    graph[node] must return a list of (neighbor, distance) pairs
+
+    Arguments:
+    start - Either the starting state or a list of starting states
+    end - Either the ending state or a list of possible ending states
+    heuristic - If supplied, provides an estimate of the remaining distance
+    from a given node to the end
+    '''
+    if not isinstance(end, list):
+        end = [end]
+
+    # Verify that all end states are valid nodes. I ran into dumb bugs once
+    # when refactoring when passing in a list instead of a tuple!
+    for e in end:
+        hash(e)
+        if heuristic is not None:
+            assert(heuristic(e) == 0)
+
+    end_candidates = set(end)
+    def end_fn(state):
+        return state in end_candidates
+
+    return make_shortest_path_graph_fuzzy_end(graph, start, end_fn, heuristic)
+
+def find_shortest_paths_fuzzy_end(graph, start, end_fn, heuristic=None):
+    '''Yields the (path, distance) pairs for the shortest paths from
+    start to any end state in the graph.
+    graph[node] must return a list of (neighbor, distance) pairs
+
+    Arguments:
+    start - Either the starting state or a list of starting states
+    end_fn - Function accepting a state. Returns True if this is an end state
+    and False otherwise
+    heuristic - If supplied, provides an estimate of the remaining distance
+    from a given node to the end
+    '''
+    shortest_path_graph, start_states, _, _ = make_shortest_path_graph_fuzzy_end(graph, start, end_fn, heuristic)
+
+    expansion_queue = [(state, 0, []) for state in start_states]
+    while expansion_queue:
+        state, dist, path = expansion_queue.pop()
+        path = path + [state]
+
+        links = shortest_path_graph[state]
+        if len(links) == 0:
+            # Terminal state
+            yield path, dist
+            continue
+
+        for neighbor, cost in links:
+            expansion_queue.append((neighbor, dist+cost, path))
+
+def find_shortest_paths(graph, start, end, heuristic=None):
+    '''Yields the (path, distance) pairs for the shortest paths from
+    start to end in the graph.
+    graph[node] must return a list of (neighbor, distance) pairs
+
+    Arguments:
+    start - Either the starting state or a list of starting states
+    end - Either the ending state or a list of possible ending states
+    heuristic - If supplied, provides an estimate of the remaining distance
+    from a given node to the end
+    '''
+    if not isinstance(end, list):
+        end = [end]
+
+    # Verify that all end states are valid nodes. I ran into dumb bugs once
+    # when refactoring when passing in a list instead of a tuple!
+    for e in end:
+        hash(e)
+        if heuristic is not None:
+            assert(heuristic(e) == 0)
+
+    end_candidates = set(end)
+    def end_fn(state):
+        return state in end_candidates
+
+    return find_shortest_paths_fuzzy_end(graph, start, end_fn, heuristic)
 
 def make_lazy_graph(neighbor_fn):
     def fn(key):
@@ -179,6 +382,15 @@ def to_distance_graph(graph):
             yield n, 1
     return make_lazy_graph(neighbor_fn)
 
+def to_simple_graph(graph):
+    '''Some usages more naturally require graphs without distance information.
+    Most utilities in this library expect a distance graph, so a converter
+    makes some usages much easier.'''
+    def neighbor_fn(node):
+        for n, _ in graph[node]:
+            yield n
+    return make_lazy_graph(neighbor_fn)
+
 def node_dist_list_to_nodes(node_dist_list):
     '''Some usages only care about the list of nodes, not the distances.
     A converter from node-distance lists to a list of nodes can simplify such
@@ -186,3 +398,52 @@ def node_dist_list_to_nodes(node_dist_list):
     '''
     for node, dist in node_dist_list:
         yield node
+
+def plot_graph(graph, directed=True, distance=True, start=None,
+               coloring=None,
+               fix_nodes_after_dragging=False):
+    import networkx
+    import gravis
+
+    # Extract the starting states before any graph conversions
+    if start is None:
+        nodes_to_expand = list(graph)
+    elif not isinstance(start, list):
+        nodes_to_expand = [start]
+    else:
+        # Make a copy to avoid corrupting the passed in list
+        nodes_to_expand = list(start)
+
+    # Convert all graphs to simple graphs for simplicity below
+    if distance:
+        graph = to_simple_graph(graph)
+
+    nodes_to_plot = set()
+
+    while nodes_to_expand:
+        node = nodes_to_expand.pop()
+        if node in nodes_to_plot:
+            continue
+        nodes_to_plot.add(node)
+
+        for neighbor in graph[node]:
+            nodes_to_expand.append(neighbor)
+
+    if directed:
+        g = networkx.DiGraph()
+    else:
+        g = networkx.Graph()
+
+    for node in nodes_to_plot:
+        label = str(node)
+
+        color = 'black'
+        if coloring is not None:
+            color = coloring.get(node, color)
+        g.add_node(label, color=color)
+
+        for neighbor in graph[node]:
+            neighbor_label = str(neighbor)
+            g.add_edge(label, neighbor_label)
+
+    gravis.d3(g, graph_height=800, node_drag_fix=fix_nodes_after_dragging).display()
--- setup.py
+++ setup.py
@@ -37,13 +37,15 @@ import itertools
 import json
 import math
 import parse
-import re
+import re2 as re
 import sympy # sympy.parse_expr, sympy.solve, sympy.Eq
+import sys # sys.setrecursionlimit(1000000)
 import z3 # x = z3.Int('x'); x < 0; (x-1) >= 0
 # z3.If(x >= 0, x, -x); z3.And(); z3.Or(); z3.Not()
 # s = z3.Solver(); solver.add(constraint); s.check(); s.model()[x].as_long()
 # o = z3.Optimize(); o.minimize(x); o.check(); o.model()[x].as_long()
 
+import lib.algorithms
 import lib.aoc
 import lib.cyk
 import lib.graph
