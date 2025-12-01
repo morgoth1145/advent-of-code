9ec10068ad0796fbc49cfd9a05073aeb4f969d1e
Patrick Hogg <phogg@novamoon.net>
2025-12-01 00:03:00 -0500

    2025 Day 1 Part 1
    Okay, working on a numbered dial like a padlock, interesting! Need to handle wraparound here of course but this isn't too bad. Would have been better had I noticed that the dial starts at 50 and not 0, oof...
    
    Finished at 3:00

---
+++ 2025/01/solution.py
@@ -0,0 +1,36 @@
+import lib.aoc
+
+def parse_input(s):
+    for line in s.splitlines():
+        c = line[0]
+        n = int(line[1:])
+        yield c, n
+
+def part1(s):
+    data = parse_input(s)
+
+    pos = 50
+
+    answer = 0
+
+    for d, n in data:
+        if d == 'R':
+            pos += n
+        elif d == 'L':
+            pos -= n
+        else:
+            assert(False)
+
+        pos = pos % 100
+
+        if pos == 0:
+            answer += 1
+
+    lib.aoc.give_answer(2025, 1, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2025, 1)
+part1(INPUT)
+part2(INPUT)
