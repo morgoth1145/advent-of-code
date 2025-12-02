1d830ce310bbe296a7d67e6ca57415ec3e425db5
Patrick Hogg <phogg@novamoon.net>
2025-12-02 00:04:53 -0500

    2025 Day 2 Part 2
    Okay, so we're getting pieces that repeat 2 or more times to get the number now. Not too bad, honestly took me longer to get typed out than I'd have liked but pretty straightforward. (I did accidentally waste time with two semicolons instead of one for subscripting in part 2, glad I caught the bogus tiny number before submitting a bad answer!)
    
    Finished at 4:53

--- 2025/02/solution.py
+++ 2025/02/solution.py
@@ -24,7 +24,30 @@ def part1(s):
     lib.aoc.give_answer(2025, 2, 1, answer)
 
 def part2(s):
-    pass
+    ranges = s.split(',')
+
+    answer = 0
+
+    for r in ranges:
+        a, b = r.split('-')
+        a = int(a)
+        b = int(b)
+        r = range(a, b+1)
+
+        for i in r:
+            istr = str(i)
+
+            n = len(istr)
+            for j in range(2, n+1):
+                if n % j != 0:
+                    continue
+
+                fir = istr[:n//j]
+                if fir*j == istr:
+                    answer += i
+                    break
+
+    lib.aoc.give_answer(2025, 2, 2, answer)
 
 INPUT = lib.aoc.get_input(2025, 2)
 part1(INPUT)
