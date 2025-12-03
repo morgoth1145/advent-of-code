42c8a4ae59478fd763132c1302a7df7aff75a38f
Patrick Hogg <phogg@novamoon.net>
2025-12-03 00:11:05 -0500

    2025 Day 3 Part 2
    And now it's 12 digits! That definitely ups the ante here. For some dumb reason I started with brute force, I don't know why, and that just ate up time.
    
    After recognizing that that won't work (duh) I finally got to implementing a smart number filter. The key insight here is that Axxxxxxx will always be larger than Bxxxxxxx if A > B, so we just need to pick the largest digit we can that leaves room for picking the other batteries in our bank. It took me an annoying amount of time to notice that (and then coding up the code to do just that) but at least it worked well. (After fixing a dumb typo where I wasn't returning the best value...)
    
    Finally finished at 11:05

--- 2025/03/solution.py
+++ 2025/03/solution.py
@@ -1,6 +1,30 @@
 import lib.aoc
 import lib.grid
 
+def find_max_as_arr(r, rem_items):
+    if rem_items == 0:
+        return []
+
+    assert(rem_items <= len(r))
+
+    best_i = None
+    best = -1
+
+    for i, v in enumerate(r):
+        if len(r) - i - 1 < rem_items - 1:
+            break
+
+        if v > best:
+            best = v
+            best_i = i
+
+    return [best] + find_max_as_arr(r[best_i+1:], rem_items-1)
+
+def find_max(r, num_items):
+    a = find_max_as_arr(r, num_items)
+    assert(len(a) == num_items)
+    return int(''.join(map(str, a)))
+
 def part1(s):
     grid = lib.grid.FixedGrid.parse(s, value_fn=int)
 
@@ -8,16 +32,20 @@ def part1(s):
 
     for y in grid.y_range:
         r = grid.row(y)
-        cands = []
-        for i, v in enumerate(r):
-            for v2 in r[i+1:]:
-                cands.append(v*10+v2)
-        answer += max(cands)
+        answer += find_max(r, 2)
 
     lib.aoc.give_answer(2025, 3, 1, answer)
 
 def part2(s):
-    pass
+    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+
+    answer = 0
+
+    for i, y in enumerate(grid.y_range):
+        r = grid.row(y)
+        answer += find_max(r, 12)
+
+    lib.aoc.give_answer(2025, 3, 2, answer)
 
 INPUT = lib.aoc.get_input(2025, 3)
 part1(INPUT)
