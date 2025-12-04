a953583e68b4b966513773ed11ec9988a1a34c75
Patrick Hogg <phogg@novamoon.net>
2025-12-04 00:59:03 -0500

    2025 Rewrite Day 4
    A simple deduplication refactor would have sufficed here but part 2 was annoyingly slow so I decided to fully rewrite and optimize the solution instead. It runs much faster when:
    1) Pre-calculating neighbor counts and updating the neighbor counts when TP is removed
    2) Tracking which cells are affected by the last iteration and only checking those instead of all cells
    
    Now part 2 takes ~0.06 seconds instead ~0.6 seconds, a nice 10x speedup!

--- 2025/04/solution.py
+++ 2025/04/solution.py
@@ -1,45 +1,58 @@
 import lib.aoc
 import lib.grid
 
-def part1(s):
+def solve(s, max_layers=None):
     grid = lib.grid.FixedGrid.parse(s)
+
+    # Convert grid to a dict of tp to neighbor counts
+    # This helps limit work in subsequent iterations making everything faster
+    tp_neighbor_counts = {
+        pos: sum(grid[n] == '@'
+                 for n
+                 in grid.neighbors(*pos, diagonals=True))
+        for pos, c in grid.items()
+        if c == '@'
+    }
 
     answer = 0
 
-    for pos, c in grid.items():
-        if c != '@':
-            continue
-        adj = 0
-        for n in grid.neighbors(*pos, diagonals=True):
-            if grid[n] == '@':
-                adj += 1
+    to_check = list(tp_neighbor_counts.keys())
 
-        if adj < 4:
-            answer += 1
+    while to_check:
+        to_remove = [pos for pos in to_check
+                     if tp_neighbor_counts.get(pos, 4) < 4]
+
+        answer += len(to_remove)
+
+        # Record which cells have been affected to minimize redundant checks
+        new_to_check = set()
+
+        for pos in to_remove:
+            del tp_neighbor_counts[pos]
+
+            for n in grid.neighbors(*pos, diagonals=True):
+                new_c = tp_neighbor_counts.get(n)
+                if new_c is None:
+                    continue
+                tp_neighbor_counts[n] = new_c - 1
+                new_to_check.add(n)
+
+        if max_layers is not None:
+            max_layers -= 1
+            if max_layers == 0:
+                break
+
+        to_check = new_to_check
+
+    return answer
+
+def part1(s):
+    answer = solve(s, max_layers=1)
 
     lib.aoc.give_answer(2025, 4, 1, answer)
 
 def part2(s):
-    grid = lib.grid.FixedGrid.parse(s)
-
-    answer = 0
-
-    while True:
-        progress = False
-        for pos, c in grid.items():
-            if c != '@':
-                continue
-            adj = 0
-            for n in grid.neighbors(*pos, diagonals=True):
-                if grid[n] == '@':
-                    adj += 1
-
-            if adj < 4:
-                answer += 1
-                grid[pos] = '.'
-                progress = True
-        if not progress:
-            break
+    answer = solve(s)
 
     lib.aoc.give_answer(2025, 4, 2, answer)
 
