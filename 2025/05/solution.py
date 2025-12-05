3aa0742c85e3dea651fe2ccd20e6bfe4fb2c8fa0
Patrick Hogg <phogg@novamoon.net>
2025-12-05 00:47:40 -0500

    2025 Rewrite Day 5
    - Deduplicate parsing
    - Make use of Python ranges in both parts (I forgot that the in keyword is efficient for ranges making them work for part 1 as well!)

--- 2025/05/solution.py
+++ 2025/05/solution.py
@@ -1,61 +1,42 @@
 import lib.aoc
 
+def parse_input(s):
+    ranges, nums = s.split('\n\n')
+
+    ranges = [tuple(map(int, l.split('-')))
+              for l in ranges.splitlines()]
+    ranges = [range(int(a), int(b)+1)
+              for a, b in ranges]
+    nums = list(map(int, nums.splitlines()))
+
+    return ranges, nums
+
 def part1(s):
-    first, second = s.split('\n\n')
+    ranges, nums = parse_input(s)
 
-    ranges = []
-    for l in first.splitlines():
-        a, b = l.split('-')
-        r = (int(a), int(b))
-        ranges.append(r)
-
-    answer = 0
-
-    for n in map(int, second.splitlines()):
-        good = False
-        for a, b in ranges:
-            if a <= n <= b:
-                good = True
-                break
-
-        if good:
-            answer += 1
+    answer = sum(any(n in r for r in ranges)
+                 for n in nums)
 
     lib.aoc.give_answer(2025, 5, 1, answer)
 
 def part2(s):
-    first, second = s.split('\n\n')
-
-    ranges = []
-    for l in first.splitlines():
-        a, b = l.split('-')
-        r = (int(a), int(b))
-        ranges.append(r)
-
-    ranges.sort()
+    ranges, _ = parse_input(s)
 
     answer = 0
 
-    def add_part(a, b):
-        if a <= b:
-            ranges.append((a, b))
-
-    def process(a, b):
-        nonlocal answer
-
-        for a2, b2 in ranges:
-            if a2 <= b and b2 >= a:
-                # Overlap
-                first = (a, a2-1)
-                second = (b2+1, b)
-                add_part(*first)
-                add_part(*second)
-                return
-
-        answer += b-a+1
-
     while ranges:
-        process(*ranges.pop())
+        r = ranges.pop()
+
+        good = True
+        for r2 in ranges:
+            if r2.start < r.stop and r2.stop > r.start:
+                ranges.append(range(r.start, r2.start))
+                ranges.append(range(r2.stop, r.stop))
+                good = False
+                break
+
+        if good:
+            answer += len(r)
 
     lib.aoc.give_answer(2025, 5, 2, answer)
 
