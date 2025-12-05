4f1ff053dd050d23ced2525dcf521d677162920b
Patrick Hogg <phogg@novamoon.net>
2025-12-05 00:10:59 -0500

    2025 Day 5 Part 2
    Ah, fun! Counting the total span of the ranges excluding overlaps is a good problem! The only problem is I immediately was thinking of 2021 day 22 which was similar but in 3D space. I had a clever solution there for handling overlaps without splitting the ranges but I couldn't think through how it worked on the fly so it was just a waste of time. Splitting the ranges is much simpler to implement and is what I should have gone with initially.
    
    Finished in 10:59. I definitely should have been faster...

--- 2025/05/solution.py
+++ 2025/05/solution.py
@@ -24,7 +24,40 @@ def part1(s):
     lib.aoc.give_answer(2025, 5, 1, answer)
 
 def part2(s):
-    pass
+    first, second = s.split('\n\n')
+
+    ranges = []
+    for l in first.splitlines():
+        a, b = l.split('-')
+        r = (int(a), int(b))
+        ranges.append(r)
+
+    ranges.sort()
+
+    answer = 0
+
+    def add_part(a, b):
+        if a <= b:
+            ranges.append((a, b))
+
+    def process(a, b):
+        nonlocal answer
+
+        for a2, b2 in ranges:
+            if a2 <= b and b2 >= a:
+                # Overlap
+                first = (a, a2-1)
+                second = (b2+1, b)
+                add_part(*first)
+                add_part(*second)
+                return
+
+        answer += b-a+1
+
+    while ranges:
+        process(*ranges.pop())
+
+    lib.aoc.give_answer(2025, 5, 2, answer)
 
 INPUT = lib.aoc.get_input(2025, 5)
 part1(INPUT)
