e9028c291490225aec1c048a3fc2b0c402c4c8fe
Patrick Hogg <phogg@novamoon.net>
2025-12-06 00:10:09 -0500

    2025 Day 6 Part 2
    Now that's a twist, didn't see that coming! I've never had to parse a text table where the whitespace matters before and definitely don't have any experience doing that before. I was already deep into using my lib.grid library so much of my time was spent trying to figure out how to get the parsing working with that library, but already as I'm writing this I'm realizing I could have done this more simply had I stepped away from that library. Oh well!
    
    I did mess up the parsing initially (not realizing that the whitespace mattered, I just pulled the characters from the numbers as-is which breaks when there's leading whitespace). It took me just shy of 5 minutes to go from that to the right parsing logic. Oof!
    
    Finished at 10:09.

--- 2025/06/solution.py
+++ 2025/06/solution.py
@@ -27,7 +27,63 @@ def part1(s):
     lib.aoc.give_answer(2025, 6, 1, answer)
 
 def part2(s):
-    pass
+    lines = s.splitlines()
+
+    split_indices = []
+
+    for i in range(len(lines[0])):
+        if all(l[i] == ' ' for l in lines):
+            split_indices.append(i)
+
+    def split_line(line):
+        out = []
+        start = 0
+        for i in split_indices:
+            out.append(line[start:i])
+            start = i+1
+        out.append(line[start:])
+        return out
+
+    grid = lib.grid.FixedGrid.parse(s,
+                                    linesplit_fn=lambda line: split_line(line),
+                                    value_fn=str)
+
+    answer = 0
+
+    for x in grid.x_range:
+        col = grid.col(x)
+
+        op = col[-1].strip()
+
+        pre_nums = col[:-1]
+
+        nums2 = []
+
+        i = 0
+        while True:
+            out = ''
+
+            for n in pre_nums:
+                if i >= len(n) or n[i] == ' ':
+                    continue
+                out += n[i]
+
+            if out == '':
+                break
+
+            nums2.append(out)
+            i += 1
+
+        nums = list(map(int, nums2))
+
+        if op == '+':
+            answer += sum(nums)
+        elif op == '*':
+            answer += math.prod(nums)
+        else:
+            assert(False)
+
+    lib.aoc.give_answer(2025, 6, 2, answer)
 
 INPUT = lib.aoc.get_input(2025, 6)
 part1(INPUT)
