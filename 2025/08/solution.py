3edf5304cd1e8faf96a3c02476b325e1bcfb187b
Patrick Hogg <phogg@novamoon.net>
2025-12-08 00:13:04 -0500

    2025 Day 8 Part 1
    Oof, I got Advent of Reading Comprehension'd hard here! I missed that we're making the shortest remaining connections overall, not the shortest connection from each junction *once*. I'm not sure how much time I wasted until I realized what the problem actually was here, but it was not a small amount of time.
    
    Thankfully the remaining implementation wasn't too bad once I realized what I actually needed to do. One nice trick to avoid floats is to skip sqrt in the distance formula, we just need to compare distances rather than truly get the straight line distance. Beyond that, just precomputing the distances and picking the 1000 shortest ones does what we need, then some basic graph traversal can extract the circuits.
    
    Finished at 13:04

---
+++ 2025/08/solution.py
@@ -0,0 +1,56 @@
+import collections
+
+import lib.aoc
+
+def part1(s):
+    coords = list(tuple(map(int, l.split(',')))
+                  for l in s.splitlines())
+
+    graph = collections.defaultdict(set)
+
+    distances = []
+
+    for i, (x, y, z) in enumerate(coords):
+        for x2, y2, z2 in coords[i+1:]:
+            d = ((x-x2)**2 + (y-y2)**2 + (z-z2)**2)
+            distances.append((d, (x, y, z), (x2, y2, z2)))
+
+    distances.sort()
+
+    for d, (x, y, z), (x2, y2, z2) in distances[:1000]:
+        graph[x,y,z].add((x2, y2, z2))
+        graph[x2,y2,z2].add((x,y,z))
+
+    circuits = []
+
+    handled = set()
+
+    def make_circuit(x, y, z, circuit):
+        circuit.add((x, y, z))
+        if (x, y, z) in handled:
+            return
+        handled.add((x, y, z))
+
+        for x2, y2, z2 in graph[x,y,z]:
+            make_circuit(x2, y2, z2, circuit)
+
+    for x, y, z in coords:
+        if (x, y, z) in handled:
+            continue
+        circuit = set()
+        make_circuit(x, y, z, circuit)
+
+        circuits.append(circuit)
+
+    circuits.sort(key=lambda c: len(c))
+
+    answer = len(circuits[-1]) * len(circuits[-2]) * len(circuits[-3])
+
+    lib.aoc.give_answer(2025, 8, 1, answer)
+
+def part2(s):
+    pass
+
+INPUT = lib.aoc.get_input(2025, 8)
+part1(INPUT)
+part2(INPUT)
