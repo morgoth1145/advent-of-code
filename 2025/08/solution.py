a866e7ff3595684ad3ed4961c47a055b06b6c4f2
Patrick Hogg <phogg@novamoon.net>
2025-12-08 00:16:05 -0500

    2025 Day 8 Part 2
    Ah, we're trying to create one big circuit! That's not too bad so long as you don't care about being super efficient, just connect one at a time and continually check if you have a completed circuit yet. I'm definitely going to revisit this to optimize the check (maintaining circuit sets will make this much more efficient) but this was quick to code and quick enough to run so I'll take it.
    
    Finished at 16:05

--- 2025/08/solution.py
+++ 2025/08/solution.py
@@ -49,7 +49,43 @@ def part1(s):
     lib.aoc.give_answer(2025, 8, 1, answer)
 
 def part2(s):
-    pass
+    coords = list(tuple(map(int, l.split(',')))
+                  for l in s.splitlines())
+
+    graph = collections.defaultdict(set)
+
+    distances = []
+
+    for i, (x, y, z) in enumerate(coords):
+        for x2, y2, z2 in coords[i+1:]:
+            d = ((x-x2)**2 + (y-y2)**2 + (z-z2)**2)
+            distances.append((d, (x, y, z), (x2, y2, z2)))
+
+    distances.sort()
+
+    def check_fully_connected():
+        handled = set()
+
+        def make_circuit(x, y, z):
+            if (x, y, z) in handled:
+                return
+            handled.add((x, y, z))
+
+            for x2, y2, z2 in graph[x,y,z]:
+                make_circuit(x2, y2, z2)
+
+        make_circuit(*coords[0])
+        return len(handled) == len(coords)
+
+    for i, (d, (x, y, z), (x2, y2, z2)) in enumerate(distances):
+        graph[x,y,z].add((x2, y2, z2))
+        graph[x2,y2,z2].add((x,y,z))
+
+        if check_fully_connected():
+            answer = x*x2
+            break
+
+    lib.aoc.give_answer(2025, 8, 2, answer)
 
 INPUT = lib.aoc.get_input(2025, 8)
 part1(INPUT)
