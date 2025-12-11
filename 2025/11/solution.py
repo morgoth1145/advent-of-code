61f9f8ac275b4e56d3b47e98fb546d70cb9eb805
Patrick Hogg <phogg@novamoon.net>
2025-12-11 00:36:27 -0500

    2025 Day 11 Part 2
    Interesting, we have to find paths that include two specific nodes on the way! I did flail around a bit before realizing that I could compute the steps between the "waypoints" and multiply them together, as well as reducing/segmenting the graph such that computing the steps finishes in a reasonable amount of time. But I got it! (And I was able to use my lib.graph.plot_graph utility in practice to help analyze the input! I'm very glad I finally got that together...)
    
    Also, it looks like there's no possibility of cycles in the input graphs which drastically simplifies the path counting code, I may not have even needed to reduce the graphs thinking about it!
    
    Finished at 36:27

--- 2025/11/solution.py
+++ 2025/11/solution.py
@@ -1,3 +1,4 @@
+import collections
 import functools
 
 import lib.aoc
@@ -36,7 +37,67 @@ def part1(s):
     lib.aoc.give_answer(2025, 11, 1, answer)
 
 def part2(s):
-    pass
+    g = parse_input(s)
+
+    inv_g = collections.defaultdict(set)
+
+    for pos, neighbors in g.items():
+        for n in neighbors:
+            inv_g[n].add(pos)
+
+    def count_path_wrap(start, dest, forbidden):
+        todo = [dest]
+        can_reach_dest = set(todo)
+        while todo:
+            pos = todo.pop()
+            for n in inv_g[pos]:
+                if n in can_reach_dest or n in forbidden:
+                    continue
+                can_reach_dest.add(n)
+                todo.append(n)
+
+        use_g = collections.defaultdict(set)
+
+        for pos, neighbors in g.items():
+            if pos not in can_reach_dest:
+                continue
+            use_g[pos] = [n for n in neighbors if n in can_reach_dest]
+
+        @functools.cache
+        def count_paths(pos, seen):
+            if pos == dest:
+                return 1
+
+            seen = set(seen)
+
+            total = 0
+            for n in use_g[pos]:
+                if n in seen or n in forbidden:
+                    continue
+                seen.add(n)
+                total += count_paths(n, tuple(sorted(seen)))
+                seen.remove(n)
+
+            return total
+
+        return count_paths(start, tuple())
+
+    answer = 0
+
+    dac_to_out = count_path_wrap('dac', 'out', {'fft', 'srv'})
+    srv_to_fft = count_path_wrap('svr', 'fft', {'out', 'dac'})
+    fft_to_dac = count_path_wrap('fft', 'dac', {'out', 'srv'})
+
+    answer += srv_to_fft * fft_to_dac * dac_to_out
+
+    # Not possible with my input
+##    srv_to_dac = count_path_wrap('svr', 'dac', {'out', 'fft'})
+##    fft_to_out = count_path_wrap('fft', 'out', {'dac', 'srv'})
+##    dac_to_fft = count_path_wrap('dac', 'fft', {'out', 'srv'})
+##
+##    answer += srv_to_dac * dac_to_fft * fft_to_out
+
+    lib.aoc.give_answer(2025, 11, 2, answer)
 
 INPUT = lib.aoc.get_input(2025, 11)
 part1(INPUT)
