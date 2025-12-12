a5174f1bba1ee52b2673fbacac44c8420f6e62c0
Patrick Hogg <phogg@novamoon.net>
2025-12-12 01:23:06 -0500

    2025 Rewrite Day 12
    Since count-based logic is enough to solve this in practice I can rip out all the extra stuff that was for a general solution. Instead, just check if the presents *must* fit (there's guaranteed to be enough room in the grid) and if not, verify that they *must not* fit (no packing could fit them by pidgeonhole principle). If any regions are left then they need more extensive checks so they should be reported, but inputs appear to be set up such that a true packing does not need to be determined to solve this so this is good enough for a "final" solution (unless I see something on reddit where someone makes an efficient true packing solution).

--- 2025/12/solution.py
+++ 2025/12/solution.py
@@ -1,98 +1,58 @@
-import collections
-
 import lib.aoc
-import lib.grid
+
+def solve(s):
+    groups = s.split('\n\n')
+
+    presents = []
+
+    for g in groups[:-1]:
+        lines = g.splitlines()
+        idx = int(lines[0].split(':')[0])
+        assert(idx == len(presents))
+
+        pres_pattern = '\n'.join(lines[1:])
+
+        width = len(lines[1])
+        height = len(lines[1:])
+        occupied = pres_pattern.count('#')
+
+        presents.append((width, height, occupied, pres_pattern))
+
+    pres_w = set(w for w,_,_,_ in presents)
+    pres_h = set(h for _,h,_,_ in presents)
+
+    assert(len(pres_w | pres_h) == 1)
+    pres_dim = list(pres_w)[0]
+
+    low_bound = 0
+    high_bound = 0
+
+    for line in groups[-1].splitlines():
+        dim, counts = line.split(': ')
+        width, height = map(int, dim.split('x'))
+        counts = list(map(int, counts.split()))
+
+        if sum(counts) <= (width // pres_dim) * (height // pres_dim):
+            # Must fit if there are enough DIMxDIM areas in the region
+            low_bound += 1
+            high_bound += 1
+            continue
+
+        total_occupied = sum(c*o for c, (_,_,o,_) in zip(counts, presents))
+        if total_occupied > width*height:
+            # Cannot fit by pidgeonhole principle, there would be more occupied tiles than available in the region
+            continue
+
+        print(f'Region {width}x{height}: {counts} may be solvable, this code is not smart enough to check right now.')
+        high_bound += 1
+
+    print(f'{low_bound}-{high_bound} regions are satisfied')
+    assert(low_bound == high_bound)
+
+    return low_bound
 
 def part1(s):
-    groups = s.split('\n\n')
-    grids = groups[:-1]
-    stuff = groups[-1]
-
-    grids2 = []
-    for g in grids:
-        lines = g.splitlines()
-        g = lines[1:]
-        grids2.append(lib.grid.FixedGrid.parse('\n'.join(lines[1:])))
-
-    grid_widths = [g.width for g in grids2]
-    grid_heights = [g.height for g in grids2]
-
-    assert(set(grid_widths + grid_heights) == {3})
-
-    PRES_DIM = 3
-
-    presents = collections.defaultdict(list)
-
-    for idx, g in enumerate(grids2):
-        seen = set()
-        for _ in range(4):
-            gs = g.as_str()
-            if gs not in seen:
-                presents[idx].append(g)
-                seen.add(gs)
-
-            gp = g.transpose()
-            gps = gp.as_str()
-            if gps not in seen:
-                presents[idx].append(gp)
-                seen.add(gps)
-
-            # Rotate TODO
-            rot = ''
-            for x in g.x_range:
-                col = g.col(x)
-                rot += ''.join(col[::-1])
-                rot += '\n'
-
-            g = lib.grid.FixedGrid.parse(rot.strip())
-
-    stuff2 = []
-    for l in stuff.splitlines():
-        a, rest = l.split(': ')
-        rest = list(map(int, rest.split()))
-        a, b = map(int, a.split('x'))
-        stuff2.append((a, b, rest))
-
-    answer = 0
-
-    def test_pack_quick(g, present_picks):
-        needed = 0
-        for pidx, p in enumerate(present_picks):
-            per_pres = 0
-            pshape = presents[pidx][0]
-            for _, c in pshape.items():
-                if c == '#':
-                    per_pres += 1
-
-            needed += per_pres * p
-
-        return needed <= g.width*g.height
-
-##    def test_pack(g, present_picks):
-##        if sum(present_picks) == 0:
-##            return True
-##
-##        for pidx, p in enumerate(present_picks):
-##            if p > 0:
-##                present_picks[pidx] -= 1
-##
-##                # Put in g
-##
-##                ans = test_pack(g, present_picks)
-##                present_picks[pidx] += 1
-##                if ans:
-##                    return True
-##
-##        return False
-
-    test_pack = test_pack_quick
-
-    for w, h, picks in stuff2:
-        grid = '\n'.join(['.' * w]*h)
-        g = lib.grid.FixedGrid.parse(grid)
-
-        if test_pack(g, picks):
-            answer += 1
+    answer = solve(s)
 
     lib.aoc.give_answer(2025, 12, 1, answer)
 
